7caf300216d6f5ff6787edc1d189729d625222af
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-12-16 18:11:29 +1100

    added onezonemodel

--- tardis/model.py
+++ tardis/model.py
@@ -1,3 +1,14 @@
 #File contains model class
 class Model(object):
     pass
+
+class OneZoneBaseModel(Model):
+    def __init__(self, r1, r2, v1):
+        """Initialize the model
+        Parameters
+        r1 : float
+            inner radius
+        r2 : float
+            outer radius
+        v1 : float
+            velocity at inner radius
