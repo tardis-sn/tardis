dced0309a8fcf24f8d92ce0806557d4a086e5966
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-01-11 11:20:43 +1100

    added random blackbody function. draws randomly from blackbody function

--- tardis/photon.py
+++ tardis/photon.py
@@ -40,3 +40,20 @@ class SimplePhotonSource(PhotonSource):
     def __call__(self):
         # returns random frequency and random mu
         return numpy.random.uniform(self.nu_start, self.nu_end), numpy.sqrt(numpy.random.random())
+        
+        
+        
+def blackbody_nu(nu, T):
+    return ((2*constants.h*nu**3)/(constants.c**2))/(numpy.exp((constants.h*nu)/(constants.kb*T)) - 1)
+
+def blackbody_lambda(wavelength, T):
+    wavelength = wavelength.copy() * 1e-8
+#    return blackbody_nu(constants.c/(wavelength * 1e-8), T)
+    return ((2*constants.h*constants.c**2)/wavelength**5)/(numpy.exp((constants.h*constants.c)/(wavelength*constants.kb*T)))
+
+def random_blackbody_lambda(T, wl_range=(2000, 12000), size=100):
+    wl = numpy.arange(*wl_range)
+    intens = blackbody_lambda(wl, T)
+    cum_blackbody = numpy.cumsum(intens)
+    norm_cum_blackbody = cum_blackbody / cum_blackbody.max()
+    return wl[norm_cum_blackbody.searchsorted(numpy.random.random(size))]
