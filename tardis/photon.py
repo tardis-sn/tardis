66bfdb76cc47d96c083385bce8cf160205550aac
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-22 19:19:09 -0400

    fixed discrete random sampling of input nus

--- tardis/photon.py
+++ tardis/photon.py
@@ -1,6 +1,6 @@
 #creating photons
 
-import numpy
+import numpy as np
 import constants
 class PhotonSource(object):
     pass
@@ -35,25 +35,32 @@ class SimplePhotonSource(PhotonSource):
         """
         self.nu_start = nu_start
         self.nu_end = nu_end
-        numpy.random.seed(seed)
+        np.random.seed(seed)
         
     def __call__(self):
         # returns random frequency and random mu
-        return numpy.random.uniform(self.nu_start, self.nu_end), numpy.sqrt(numpy.random.random())
+        return np.random.uniform(self.nu_start, self.nu_end), np.sqrt(np.random.random())
         
         
         
 def blackbody_nu(nu, T):
-    return ((2*constants.h*nu**3)/(constants.c**2))/(numpy.exp((constants.h*nu)/(constants.kb*T)) - 1)
+    return ((2*constants.h*nu**3)/(constants.c**2))/(np.exp((constants.h*nu)/(constants.kb*T)) - 1)
 
 def blackbody_lambda(wavelength, T):
     wavelength = wavelength.copy() * 1e-8
 #    return blackbody_nu(constants.c/(wavelength * 1e-8), T)
-    return ((2*constants.h*constants.c**2)/wavelength**5)/(numpy.exp((constants.h*constants.c)/(wavelength*constants.kb*T)))
+    return ((2*constants.h*constants.c**2)/wavelength**5)/(np.exp((constants.h*constants.c)/(wavelength*constants.kb*T)))
 
 def random_blackbody_lambda(T, wl_range=(2000, 12000), size=100):
-    wl = numpy.arange(*wl_range)
+    wl = np.arange(*wl_range)
     intens = blackbody_lambda(wl, T)
-    cum_blackbody = numpy.cumsum(intens)
+    cum_blackbody = np.cumsum(intens)
     norm_cum_blackbody = cum_blackbody / cum_blackbody.max()
-    return wl[norm_cum_blackbody.searchsorted(numpy.random.random(size))]
+    return wl[norm_cum_blackbody.searchsorted(np.random.random(size))]
+    
+def random_blackbody_nu(T, nu_range=(1e8*constants.c / 5000, 1e8*constants.c/ 9000), sample=1000, size=int(1e6)):
+    nu = np.linspace(*nu_range, num=sample)
+    intens = blackbody_nu(nu, T)
+    cum_blackbody = np.cumsum(intens)
+    norm_cum_blackbody = cum_blackbody / cum_blackbody.max()
+    return nu[norm_cum_blackbody.searchsorted(np.random.random(size))] + np.random.random(size=size)*(nu[1] - nu[0])
