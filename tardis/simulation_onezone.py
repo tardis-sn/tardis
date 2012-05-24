8ab3fc8400f91a1d8ba523837dab4bf047306ab9
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-24 16:59:15 -0400

    first simple framework of temperature iteration

--- tardis/simulation_onezone.py
+++ tardis/simulation_onezone.py
@@ -4,3 +4,29 @@ import initialize
 def run_oned(conn, fname):
     initial_config = initialize.read_simple_config(fname)
     return initial_config
+    
+    t_rad = initial_config['t_inner']
+    w = 1.
+    energy_of_packet = 1 / initial_config['packets']
+    
+    
+    
+    while True:
+        #myplasma = plasma.LTEPlasma.from_db('simple_run.tconf', conn)
+        nus = photon.random_blackbody_nu(T, nu_range=(1e8*constants.c / 2000, 1e8*constants.c/ 9000), size=no_of_packets)
+        mus = np.sqrt(np.random.random(no_of_packets))
+        
+        #WRITE!!
+        tau_sobolev = myplasma.calculate_tau_sobolev()
+        
+        
+        #add energy to montecarlo function
+        nu, energy, j_estimator, nubar_estimator = c_montecarlo.run_simple_oned(nus, mus, line_list_nu, tau_sobolevs,
+                    initial_config['r_inner'], initial_config['r_outer'], initial_config['v_inner'], myplasma.electron_density, energy_of_packet)
+        
+        new_trad = constants.trad_estimator_constant * nubar_estimator / j_estimator
+        new_w = constants.w_estimator_constant * j_estimator / new_trad**4 / (inital_config['time_simulation'] * (4/3) * np.pi * (r_outer**3 - r_inner**3))
+        print "trad / new_trad = %.2f/%.2f w / new_w =  %.2f/%.2f" % (trad, new_trad, w, new_w)
+        w = (new_w + w)*.5
+        trad = (new_trad + trad)*.5
+        
