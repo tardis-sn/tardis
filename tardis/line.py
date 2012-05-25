d7c4185bc7dd764e534e8f801c344dde58489ab4
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-25 08:54:09 -0400

    getting ready to exclude elements from line list

--- tardis/line.py
+++ tardis/line.py
@@ -1,7 +1,7 @@
 #calculation for line interaction
 import constants
 import numpy as np
-
+import initialize
 def get_tau_line(nu, f_line, dens_line, t_exp):
     tau_line = constants.sobolev_coeff * f_line * (constants.c / nu) * dens_line * t_exp
     return tau_line
@@ -10,8 +10,10 @@ def get_r_sobolev(r, mu, d_line):
     return np.sqrt(r**2 + d_line**2 + 2 * r * d_line * mu)
     
     
-def read_line_list(conn):
-    
+def read_line_list(conn, atoms=None, symbol2z=None):
+    #if symbol2z:
+    #    initialize.read_symbol2z()
+        
     raw_data = []
     curs = conn.execute('select wl, loggf, g_lower, g_upper, e_lower, e_upper, level_id_lower, level_id_upper, atom, ion from lines')
     for wl, loggf, g_lower, g_upper, e_lower, e_upper, level_id_lower, level_id_upper, atom, ion in curs:
