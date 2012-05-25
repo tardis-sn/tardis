54d2e19e66121edd524ce218bc2de975fdf852bb
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-25 08:50:06 -0400

    fixed the flux conversion problem

--- tardis/synspec.py
+++ tardis/synspec.py
@@ -7,9 +7,9 @@ import pyspec
 def get_lambda_spec(nu, energy, wave_min, wave_max, samples=50):
     wave = 1e8 * constants.c / np.array(nu)
     nu_bins = np.linspace(constants.c / wave_min, constants.c / wave_max, samples)
-    wavelength_bins = 1e8 * constants.c / np.array(nu_bins)
+    wavelength_bins = 1e8 * constants.c / (np.array(nu_bins))
     wavelength = wavelength_bins[:-1] + np.diff(wavelength_bins)*0.5
     intens = np.histogram(wave, weights=energy, bins=wavelength_bins)[0]
     
-    return wavelength, intens
+    return wavelength, intens*constants.c/wavelength**2
     
