8e5e05b19ccb4ae38822d8d7a46e955a77b6ea4d
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-31 17:42:32 -0400

    added simple w7 model

--- tardis/model.py
+++ tardis/model.py
@@ -2,69 +2,39 @@
 
 import constants
 import numpy as np
-import c_distance
+import os
 miss_distance = 1e99
+
+w7model_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'w7model.dat'))
 
 class Model(object):
     pass
 
 
-
-class OneZoneBaseModel(Model):
-    def __init__(self, r_inner, r_outer, v_outer):
-        """Initialize the model
-        Parameters
-        r_inner : float
-            inner radius
-        r_outer : float
-            outer radius
-        v_inner : float
-            velocity at inner radius
-        """
-        self.r_inner = r_inner
-        self.r_outer = r_outer
-        self.v_outer = v_outer
-        self.t_exp = self.r_outer / self.v_outer
-        self.inverse_t_exp = 1 / self.t_exp
-        self.v_inner = self.r_inner / self.t_exp
-        self.ne = 1 / (constants.sigma_thomson * (self.r_outer - self.r_inner))
-        self.ne *= 1.
-        self. inverse_ne = 1 / self.ne
+class MultiZoneRadial(Model):
+    @classmethod
+    def from_w7(cls, current_time):
+        velocities, densities = loadtxt(w7model_file, usecols=(1, 2), unpack=1)
+        velocities *= 1e5
+        densities = 10**densities
+        time_0 = 0.000231481 * 86400
+        cls(velocities[100::5], densities[100::5], current_time, time_0)
         
-    def compute_distance2outer(self, r, mu):
-        #compute distance to the outer layer
-        return c_distance.distance2outer(self.r_outer, r, mu)
-        d = np.sqrt(self.r_outer**2 + ((mu**2 - 1.) * r**2)) - (r * mu)
-        return d
     
-    def compute_distance2inner(self, r, mu):
-        #compute distance to the inner layer
-        #check if intersection is possible?
-        check = self.r_inner**2 + (r**2 * (mu**2 - 1.))
-        if check < 0:
-            return miss_distance
-        else:
-            if mu < 0:
-               return -r * mu - np.sqrt(check)
-            else:
-                return miss_distance
-    
-    def compute_distance2line(self, r, mu, nu, nu_line):
-        #computing distance to line
-        cur_v = r * self.inverse_t_exp
-        #beta = np.abs(mu) * cur_v / constants.c
-        nu_cmf = nu * (1. - (mu * r * self.inverse_t_exp * constants.inverse_c))
-        #nu_cmf = nu * np.sqrt((1-beta)/(1+beta))
+    def __init__(self, velocities, densities, current_time, time_0):
+        #constructing zones
+        self.v_inner = velocities[:-1]
+        self.v_outer = velocities[1:]
         
-        #this_nucmf = this_freq * (1. - (this_mu * v1 * this_r / r1 / CLIGHT));
-        if nu_cmf < nu_line:
-            return miss_distance
-        else:
-            return ((nu_cmf - nu_line) / nu) * constants.c * self.t_exp
-    
-    def compute_distance2electron(self, r, mu, tau_event):
-        return tau_event * self.inverse_ne * constants.inverse_sigma_thomson
-
-class OneDMultiZoneModel(Model):
-    pass
-    
+        
+        self.r_inner = self.v_inner * time_exp
+        self.r_outer = self.v_outer * time_exp
+        self.r_middle = 0.5 * (self.r_inner + self.r_outer)
+        self.initial_w = 0.5 * (1 - np.sqrt(1 - self.r_inner[0]**2/ self.r_middle**2))
+        self.densities_middle = (current_time / time_0)**-3 * self.densities[1:]
+        
+    def read_abundances_uniform(self, abundances):
+        self.abundances = np.empty((self.r_inner.size, self.abundances.size))
+        for line in abundances:
+            self.abundances = line
+            
