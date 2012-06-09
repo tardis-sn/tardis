9c309b6ad44b8f1014b0040bd90a434401d4456c
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-06-09 18:56:15 -0400

    cleaning up and restructure of atomic model and plasma

--- tardis/constants.py
+++ tardis/constants.py
@@ -6,7 +6,7 @@ erg2ev = 6.24150974e11
 
 #speed of light
 c = 2.99792458e10 #cm/s
-inverse_c = 1/c
+inverse_c = 1 / c
 #masses
 mh = 1.67352e-24 #  of hydrogen atom
 me = 9.10938188e-28 # electron mass in grams
@@ -34,9 +34,12 @@ inverse_sigma_thomson = 1 / sigma_thomson
 
 
 #sobolev coeff
-sobolev_coeff = (np.pi * e**2) / (me * c) #(pi*e^2)/(m_e*c)
+sobolev_coeff = (np.pi * e ** 2) / (me * c) #(pi*e^2)/(m_e*c)
 
 #
 trad_estimator_constant = 0.260944706 * h / kb # (pi**4 / 15)/ (24*zeta(5))
 
-w_estimator_constant = (c**2 / (2*h)) * (15/np.pi**4) * (h/kb)**4 / (4*np.pi) 
+w_estimator_constant = (c ** 2 / (2 * h)) * (15 / np.pi ** 4) * (h / kb) ** 4 / (4 * np.pi)
+
+#atomic mass unit
+u = 1.66053886e-24 # atomic mass unit in g
