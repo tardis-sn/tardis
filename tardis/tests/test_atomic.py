2bd22f3de630e3b1560044b74d54328e88ed77c9
maryam patel <maryam.patel22@gmail.com>
2012-11-02 14:50:08 -0400

    added test for ionization asserting that the first ionization energy level is close to 13.

--- tardis/tests/test_atomic.py
+++ tardis/tests/test_atomic.py
@@ -4,9 +4,14 @@ __author__ = 'maryam'
 
 from tardis import atomic
 
+from numpy import testing
+
 def test_atomic_h5_readin():
     data = atomic.read_atomic_data()
     assert data['symbol'][25] == "Fe"
 
 
 
+def test_ionization_h5_readin():
+    data = atomic.read_ionization_data()
+    testing.assert_almost_equal(data['ionization_energy'][0],13.)
