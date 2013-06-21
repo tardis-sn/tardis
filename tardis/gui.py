9b5152a2fe062bf1f10478e23321aa031aed03e0
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2013-06-21 17:51:37 -0400

    added switchable shell viewer (t_rads or Ws)

--- tardis/gui.py
+++ tardis/gui.py
@@ -21,7 +21,7 @@ class ModelViewer(QtGui.QWidget):
             start_event_loop_qt4(app)
         except ImportError:
             app.exec_()
-            
+
         super(ModelViewer, self).__init__(parent)
         self.model = None
         self.setGeometry(20, 35, 1200, 500)
@@ -29,11 +29,17 @@ class ModelViewer(QtGui.QWidget):
         self.tablemodel = MyTableModel(["t_rad", "Ws"])
         self.tableview = QtGui.QTableView()
         self.graph = MatplotlibWidget(self, 'model')
+        self.graph_label = QtGui.QLabel('Select Property to Plot:')
+        self.graph_button = QtGui.QToolButton()
         self.spectrum = MatplotlibWidget(self)
-        self.spectrum_button = QtGui.QToolButton(self)
+        self.spectrum_label = QtGui.QLabel('Select Spectrum to Plot:')
+        self.spectrum_button = QtGui.QToolButton()
         self.layout = QtGui.QHBoxLayout()
-        self.sublayout = QtGui.QVBoxLayout()
-        
+        self.graph_sublayout = QtGui.QVBoxLayout()
+        self.graph_subsublayout = QtGui.QHBoxLayout()
+        self.spectrum_sublayout = QtGui.QVBoxLayout()
+        self.spectrum_subsublayout = QtGui.QHBoxLayout()
+
     def show_model(self, model=None):
         if model:
             self.change_model(model)
@@ -41,18 +47,28 @@ class ModelViewer(QtGui.QWidget):
         self.tableview.setMinimumWidth(200)
         self.tableview.connect(self.tableview.verticalHeader(), QtCore.SIGNAL('sectionDoubleClicked(int)'),
                                self.on_header_double_clicked)
+        self.graph_button.setText('t_rads')
         self.spectrum_button.setText('spec_flux_angstrom')
-        #self.spectrum_button.setGeometry(QtCore.Qt.AlignCenter)
+        self.graph_button.setPopupMode(QtGui.QToolButton.MenuButtonPopup)
         self.spectrum_button.setPopupMode(QtGui.QToolButton.MenuButtonPopup)
+        self.graph_button.setMenu(QtGui.QMenu(self.graph_button))
         self.spectrum_button.setMenu(QtGui.QMenu(self.spectrum_button))
-        self.spectrum_button.menu().addAction('spec_flux_angstrom').triggered.connect(self.change_to_spec_flux_angstrom)
-        self.spectrum_button.menu().addAction('spec_virtual_flux_angstrom').triggered.connect(self.change_to_spec_virtual_flux_angstrom)
+        self.graph_button.menu().addAction('t_rads').triggered.connect(self.change_graph_to_t_rads)
+        self.graph_button.menu().addAction('Ws').triggered.connect(self.change_graph_to_ws)
+        self.spectrum_button.menu().addAction('spec_flux_angstrom').triggered.connect(self.change_spectrum_to_spec_flux_angstrom)
+        self.spectrum_button.menu().addAction('spec_virtual_flux_angstrom').triggered.connect(self.change_spectrum_to_spec_virtual_flux_angstrom)
         self.layout.addWidget(self.tableview)
-        self.layout.addWidget(self.graph)
-        self.sublayout.addWidget(self.spectrum_button)
-        self.sublayout.addWidget(self.spectrum)
-        self.sublayout.addWidget(self.spectrum.toolbar)
-        self.layout.addLayout(self.sublayout)
+        self.graph_subsublayout.addWidget(self.graph_label)
+        self.graph_subsublayout.addWidget(self.graph_button)
+        self.graph_sublayout.addLayout(self.graph_subsublayout)
+        self.graph_sublayout.addWidget(self.graph)
+        self.layout.addLayout(self.graph_sublayout)
+        self.spectrum_subsublayout.addWidget(self.spectrum_label)
+        self.spectrum_subsublayout.addWidget(self.spectrum_button)
+        self.spectrum_sublayout.addLayout(self.spectrum_subsublayout)
+        self.spectrum_sublayout.addWidget(self.spectrum)
+        self.spectrum_sublayout.addWidget(self.spectrum.toolbar)
+        self.layout.addLayout(self.spectrum_sublayout)
         self.setLayout(self.layout)
         self.plot_model()
         self.plot_spectrum()
@@ -66,11 +82,13 @@ class ModelViewer(QtGui.QWidget):
                 index = self.tablemodel.createIndex(r, c)
                 self.tablemodel.setData(index, QtCore.QVariant(self.tablemodel.arraydata[c][r]))
         self.plot_model()
+        if self.graph_button.text == 'Ws':
+            self.change_graph_to_ws()
         self.plot_spectrum()
         if self.spectrum_button.text == 'spec_virtual_flux_angstrom':
-            self.change_to_spec_virtual_flux_angstrom()
+            self.change_spectrum_to_spec_virtual_flux_angstrom()
         self.show()
-        
+
     def change_model(self, model):
         self.model = model
         self.tablemodel.arraydata = []
@@ -80,31 +98,59 @@ class ModelViewer(QtGui.QWidget):
     def add_data(self, datain):
         self.tablemodel.add_data(datain)
 
-    def change_to_spec_virtual_flux_angstrom(self):
-        self.spectrum_button.setText('spect_virtual_flux_angstrom')
-        self.spectrum.data_plot[0].set_ydata(self.model.spec_virtual_flux_angstrom)
+    def change_spectrum_to_spec_virtual_flux_angstrom(self):
+        self.change_spectrum(self.model.spec_virtual_flux_angstrom, 'spec_virtual_flux_angstrom')
+
+    def change_spectrum_to_spec_flux_angstrom(self):
+        self.change_spectrum(self.model.spec_flux_angstrom, 'spec_flux_angstrom')
+
+    def change_spectrum(self, data, name):
+        self.spectrum_button.setText(name)
+        self.spectrum.dataplot[0].set_ydata(data)
+        self.spectrum.ax.relim()
+        self.spectrum.ax.autoscale()
         self.spectrum.draw()
-        
-    def change_to_spec_flux_angstrom(self):
-        self.spectrum_button.setText('spec_flux_angstrom')
-        self.spectrum.data_plot[0].set_ydata(self.model.spec_flux_angstrom)
-        self.spectrum.draw()
-        
+
     def plot_spectrum(self):
         self.spectrum.ax.clear()
         self.spectrum.ax.set_title('Spectrum')
         self.spectrum.ax.set_xlabel('Wavelength (A)')
         self.spectrum.ax.set_ylabel('Intensity')
-        self.spectrum.data_plot = self.spectrum.ax.plot(self.model.spec_angstrom, self.model.spec_flux_angstrom, label='b')
+        self.spectrum.dataplot = self.spectrum.ax.plot(self.model.spec_angstrom, self.model.spec_flux_angstrom, label='b')
         self.spectrum.draw()
-        
+
+    def change_graph_to_ws(self):
+        self.change_graph(self.model.ws, 'Ws', '')
+
+    def change_graph_to_t_rads(self):
+        self.change_graph(self.model.t_rads, 't_rads', '(K)')
+
+    def change_graph(self, data, name, unit):
+        self.graph_button.setText(name)
+        self.graph.dataplot[0].set_ydata(data)
+        self.graph.ax1.relim()
+        self.graph.ax1.autoscale()
+        self.graph.ax1.set_title(name + ' vs Shell')
+        self.graph.ax1.set_ylabel(name + ' ' + unit)
+        normalizer = colors.Normalize(vmin=data.min(), vmax=data.max())
+        color_map = plt.cm.ScalarMappable(norm=normalizer, cmap=plt.cm.jet)
+        color_map.set_array(data)
+        self.graph.cb.set_clim(vmin=data.min(), vmax=data.max())
+        self.graph.cb.update_normal(color_map)
+        if unit == '(K)':
+            unit = 'T (K)'
+        self.graph.cb.set_label(unit)
+        for i, item in enumerate(data):
+            self.shells[i].set_facecolor(color_map.to_rgba(item))
+        self.graph.draw()
+
     def plot_model(self):
         self.graph.ax1.clear()
         self.graph.ax1.set_title('t_rads vs Shell')
         self.graph.ax1.set_xlabel('Shell Number')
         self.graph.ax1.set_ylabel('t_rads (K)')
         self.graph.ax1.yaxis.get_major_formatter().set_powerlimits((0, 1))
-        self.graph.ax1.plot(range(len(self.model.t_rads)), self.model.t_rads)
+        self.graph.dataplot = self.graph.ax1.plot(range(len(self.model.t_rads)), self.model.t_rads)
         self.graph.ax2.clear()
         self.graph.ax2.set_title('Shell View')
         self.graph.ax2.set_xlabel('Distance from Center (cm)')
@@ -119,16 +165,16 @@ class ModelViewer(QtGui.QWidget):
         else:
             self.graph.cb = self.graph.figure.colorbar(t_rad_color_map)
             self.graph.cb.set_label('T (K)')
-        normalizing_factor = 0.2 * (self.model.r_outer[-1] - self.model.r_inner[0]) / self.model.r_inner[0]
-        #normalizing_factor = 8e-16
+        self.graph.normalizing_factor = 0.2 * (self.model.r_outer[-1] - self.model.r_inner[0]) / self.model.r_inner[0]
+        #self.graph.normalizing_factor = 8e-16
         for i, t_rad in enumerate(self.model.t_rads):
-            r_inner = self.model.r_inner[i] * normalizing_factor
-            r_outer = self.model.r_outer[i] * normalizing_factor
+            r_inner = self.model.r_inner[i] * self.graph.normalizing_factor
+            r_outer = self.model.r_outer[i] * self.graph.normalizing_factor
             self.shells.append(Shell(i, (0,0), r_inner, r_outer, facecolor=t_rad_color_map.to_rgba(t_rad),
                                      picker=self.graph.shell_picker))
             self.graph.ax2.add_patch(self.shells[i])
-        self.graph.ax2.set_xlim(0, self.model.r_outer[-1] * normalizing_factor)
-        self.graph.ax2.set_ylim(0, self.model.r_outer[-1] * normalizing_factor)
+        self.graph.ax2.set_xlim(0, self.model.r_outer[-1] * self.graph.normalizing_factor)
+        self.graph.ax2.set_ylim(0, self.model.r_outer[-1] * self.graph.normalizing_factor)
         #self.graph.gs.tight_layout(self.graph.figure)
         self.graph.draw()
 
