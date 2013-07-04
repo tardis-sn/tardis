45e091ac8457ebd4b44d143c521aba6a5000859c
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-07-04 17:45:54 -0400

    cleaned up the config reader and made it Quantity aware.

--- tardis/tests/test_config_reader.py
+++ tardis/tests/test_config_reader.py
@@ -18,3 +18,13 @@ def test_quantity_parser_malformed_quantity1():
 def test_quantity_parser_malformed_quantity2():
     with pytest.raises(config_reader.TardisMalformedQuantityError):
         q1 = config_reader.parse2quantity('5 abcd')
+
+
+def test_config_namespace_attribute_test():
+    namespace = config_reader.TardisConfigurationNameSpace({'param1':1})
+    assert namespace.param1 == 1
+
+def test_config_namespace_attribute_test():
+    namespace = config_reader.TardisConfigurationNameSpace({'param1':1})
+    with pytest.raises(AttributeError):
+        assert namespace.param2 == 1
