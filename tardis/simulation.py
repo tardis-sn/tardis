a74c3784247602e578db3f80cf00b522432b7dd5
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-07-06 22:08:39 -0400

    implemented history writing functionality. No reader is implemented as of yet.

--- tardis/simulation.py
+++ tardis/simulation.py
@@ -1,19 +1,25 @@
 import logging
 import time
-
+from pandas import HDFStore
 # Adding logging support
 logger = logging.getLogger(__name__)
 
 
-def run_radial1d(radial1d_model, save_history=None):
+def run_radial1d(radial1d_model, history_fname=None):
+    if history_fname:
+        history_buffer = HDFStore(history_fname)
+
     start_time = time.time()
     radial1d_model.simulate(update_radiation_field=False, enable_virtual=True, initialize_j_blues=True)
+    if history_fname:
+        radial1d_model.to_hdf5(history_buffer, path='model%d' % radial1d_model.iterations_executed, close_h5=False)
+
     while radial1d_model.iterations_remaining > 0:
         logger.info('Remaining run %d', radial1d_model.iterations_remaining)
         radial1d_model.simulate(update_radiation_field=True, enable_virtual=False)
-        if save_history is not None:
-            save_history.store(radial1d_model)
 
+        if history_fname:
+            radial1d_model.to_hdf5(history_buffer, path='model%d' % radial1d_model.iterations_executed, close_h5=False)
 
     #Finished second to last loop running one more time
     logger.info('Doing last run')
@@ -22,9 +28,10 @@ def run_radial1d(radial1d_model, save_history=None):
 
     radial1d_model.simulate(enable_virtual=True, update_radiation_field=False)
 
-    if save_history is not None:
-        save_history.store(radial1d_model)
-        save_history.finalize()
+    if history_fname:
+        radial1d_model.to_hdf5(history_fname, path='model%d' % radial1d_model.iterations_executed)
+
+
 
     logger.info("Finished in %d iterations and took %.2f s", radial1d_model.iterations_executed, time.time()-start_time)
 
