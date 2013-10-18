98ff9ea048b586f6cada2823bca0721074be7654
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-10-17 22:33:36 -0400

    added favicon

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = ''
+html_favicon = 'tardis_logo.ico'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
