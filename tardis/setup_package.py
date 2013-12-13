758dd8aa159c5ebe7e6402ead9ab4fc6bd6220db
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-12-13 12:12:27 -0500

    fixed setup.py to new astropy setup

--- tardis/setup_package.py
+++ tardis/setup_package.py
@@ -1,4 +1,11 @@
 #setting the right include
 
-def get_package_data():
-    return {'tardis.montecarlo_multizone':['randomkit/*.c']}
+from setuptools import Extension
+
+randomkit_files = ['tardis/randomkit/rk_isaac.c', 'tardis/randomkit/rk_mt.c', 'tardis/randomkit/rk_primitive.c',
+                   'tardis/randomkit/rk_sobol.c']
+
+def get_extensions():
+    return [Extension('tardis.montecarlo',
+                        ['tardis/montecarlo.pyx'] + randomkit_files)]
+    #return {'tardis.montecarlo_multizone':['randomkit/*.c']}
