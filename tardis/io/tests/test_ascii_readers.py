71152e17f1cccdac0b4ab13890190222d3e130c1
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-01-10 15:32:19 +0100

    make the test files visible

--- tardis/io/tests/test_ascii_readers.py
+++ tardis/io/tests/test_ascii_readers.py
@@ -3,10 +3,11 @@ from astropy import units as u
 from tardis import io
 from numpy.testing import assert_almost_equal
 
-test_data_directory = os.path.join(os.path.dirname(__file__), 'data')
+test_data_directory = os.path.dirname(__file__)
 
 def test_simple_ascii_density_reader():
-    time_model, data = io.read_simple_ascii_density(os.path.join(test_data_directory, 'tardis_simple_ascii_density_test.dat'))
+    time_model, data = io.read_simple_ascii_density(os.path.join(test_data_directory,
+                                                                 'tardis_simple_ascii_density_test.dat'))
 
     assert time_model.unit.physical_type == 'time'
     assert_almost_equal(time_model.to(u.day).value, 1.0)
