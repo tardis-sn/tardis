71e444aee252c3cec50a53b4b3d8fc530b3a6e3f
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-01-10 16:54:52 +0100

    moving config_reader

--- tardis/io/__init__.py
+++ tardis/io/__init__.py
@@ -1,2 +1,4 @@
 #readin model_data
 from tardis.io.model_reader import read_simple_ascii_density, read_simple_ascii_abundances
+
+from tardis.io.config_reader import parse_abundance_dict_to_dataframe
