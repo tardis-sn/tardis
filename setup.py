f60b380ff0a167b08e850bc2f777ae03c9cd269a
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-02-03 14:33:26 -0500

    changed the release number to dev

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ LICENSE = 'BSD (3 clause)'
 URL = 'http://tardis.readthedocs.org/'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.9'
+VERSION = '0.9.dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
