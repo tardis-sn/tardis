7a962a23721ec090bec90909fd061cd885f7ad78
Michael.Klauser <michael@klauser.biz>
2014-02-18 18:27:39 +0100

    delete the default config parser from master for pr

--- tardis/io/default_conf_test/default_test_load.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'michi'
-import yaml
-
-f = open('default_conf_test/conf_def.yaml')
-default = yaml.safe_load(f)
-f = open('default_conf_test/conf_tes.yaml')
-config = yaml.safe_load(f)
