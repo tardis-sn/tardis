55cfebe908ec8cf7085b81ace9ae01c2cc0dd47a
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-03-10 16:58:01 -0400

    added simple atomic dataset test + some documentation

--- tardis/tests/conftest.py
+++ tardis/tests/conftest.py
@@ -1,5 +1,4 @@
 #py.test configuration
 
 def pytest_addoption(parser):
-    parser.addoption("--atomic-data-set", default=None, help="filename for atomic dataset")
-
+    parser.addoption("--atomic-dataset", dest='atomic-dataset', default=None, help="filename for atomic dataset")
--- tardis/tests/test_tardis_full.py
+++ tardis/tests/test_tardis_full.py
@@ -2,28 +2,28 @@ import pytest
 import os
 import yaml
 from tardis import io, model, simulation
-from numpy.testing import assert_almost_equal, assert_array_almost_equal
+from tardis.io.config_reader import TARDISConfiguration
+from numpy.testing import assert_array_almost_equal
 
-@pytest.mark.skipif(not pytest.config.getvalue("atomic_database"),
+@pytest.mark.skipif(not pytest.config.getvalue("atomic-dataset"),
                     reason='--atomic_database was not specified')
 class TestSimpleRun():
     """
-    Very simple run with excitation and ionization set to
+    Very simple run
     """
+
     @classmethod
-    @pytest.fixture(scope = "class", autouse = True)
-    def setup(self, pure_kurucz_filename):
-        self.atom_data_filename = os.path.expanduser(pure_kurucz_filename)
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self):
+        self.atom_data_filename = pytest.config.getvalue('atomic-dataset')
         assert os.path.exists(self.atom_data_filename)
         self.config_yaml = yaml.load(open('tardis/io/tests/data/tardis_configv1_verysimple.yml'))
         self.config_yaml['atom_data'] = self.atom_data_filename
 
-        self.config = io.config_reader.TARDISConfiguration.from_config_dict(self.config_yaml)
-        assert self.config.atom_data.uuid1 == 'ca2684c53cc511e39f0ec8bcc8a04795', 'requiring specific atom dataset'
-        assert self.config.atom_data.md5 == '4cccdc9b4c09faf3e348a2c710fb1715', 'requiring specific atom dataset'
+        self.config = TARDISConfiguration.from_config_dict(self.config_yaml)
         self.model = model.Radial1DModel(self.config)
         simulation.run_radial1d(self.model)
 
 
     def test_structure(self):
-        assert_array_almost_equal(self.config.v_inner, 0)
+        assert_array_almost_equal(self.config.v_inner, 0.0)
