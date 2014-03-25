22046bb72d817283440c3a92beb2f36c7380ad52
Michi <michael@klauser.biz>
2014-03-25 15:45:33 +0100

    new abundances type

--- tardis/io/default_config_parser.py
+++ tardis/io/default_config_parser.py
@@ -407,6 +407,27 @@ class PropertyTypeRangeSampled(PropertyTypeRange):
         elif isinstance(value, dict):
             return [value['start'], value['end'], value['sample']]
     
+class PropertyTypeAbundances(PropertyType):
+    
+    elements = { 'neut': 0, 'h': 1, 'he': 2, 'li': 3, 'be': 4, 'b': 5, 'c': 6, 'n': 7, 'o': 8, 'f': 9, 'ne': 10, 'na': 11, 'mg': 12, 'al': 13, 'si': 14, 'p': 15, 's': 16, 'cl': 17, 'ar': 18, 'k': 19,    'ca': 20, 'sc': 21, 'ti': 22, 'v': 23, 'cr': 24, 'mn': 25, 'fe': 26, 'co': 27, 'ni': 28, 'cu': 29, 'zn': 30, 'ga': 31, 'ge': 32, 'as': 33, 'se': 34, 'br': 35, 'kr': 36, 'rb': 37, 'sr': 38, 'y': 39,  'zr': 40, 'nb': 41, 'mo': 42, 'tc': 43, 'ru': 44, 'rh': 45, 'pd': 46, 'ag': 47, 'cd': 48, 'in': 49, 'sn': 50, 'sb': 51, 'te': 52, 'i': 53, 'xe': 54, 'cs': 55, 'ba': 56, 'la': 57, 'ce': 58, 'pr': 59, 'nd': 60, 'pm': 61, 'sm': 62, 'eu': 63, 'gd': 64, 'tb': 65, 'dy': 66, 'ho': 67, 'er': 68, 'tm': 69, 'yb': 70, 'lu': 71, 'hf': 72, 'ta': 73, 'w': 74, 're': 75, 'os': 76, 'ir': 77, 'pt': 78, 'au': 79, 'hg': 80, 'tl': 81, 'pb': 82, 'bi': 83, 'po': 84, 'at': 85, 'rn': 86, 'fr': 87, 'ra': 88, 'ac': 89, 'th': 90, 'pa': 91, 'u': 92, 'np': 93, 'pu': 94, 'am': 95, 'cm': 96, 'bk': 97, 'cf': 98, 'es': 99, 'fm': 100, 'md': 101, 'no': 102, 'lr': 103, 'rf': 104, 'db': 105, 'sg': 106, 'bh': 107, 'hs': 108, 'mt': 109, 'ds':110, 'rg':111, 'cn':112 }
+
+    def check_type(self, _value):
+        value = dict((k.lower(), v) for k,v in _value.items())
+        if set(value).issubset(set(self.elements)):
+            return True
+        else:
+            return False
+        
+    def to_type(self, _value):
+        if isinstance(_value, dict):
+            value = dict((k.lower(), v) for k,v in _value.items())
+            abundances = dict.fromkeys(self.elements.copy(), 0.0)
+            for k in value:
+                abundances[k] = value[k]
+            return abundances
+        else:
+            raise ConfigError
+            
     
     
 
@@ -460,6 +481,9 @@ class DefaultParser(object):
         
         self.__types['container-property'] = PropertyTypeContainer
         self.__register_leaf('container-property')
+        
+        self.__types['abundance_set'] = PropertyTypeAbundances
+        self.__register_leaf('abundance_set')
         
         self.__mandatory = False
         self.__default_value = None
@@ -656,6 +680,7 @@ class Container(DefaultParser):
         #self.__register_leaf('quantity')
         #self.__register_leaf('string')
 
+        self.__type = None
         self.__allowed_value = None
         self.__allowed_type = None
         self.__config_value = None
@@ -684,6 +709,7 @@ class Container(DefaultParser):
                 raise ValueError('Wrong container type')
             else:
                 type_dict = container_dict['type']
+                self.__type = container_dict['type']
         except KeyError:
             raise ValueError('No container type specified')
 
@@ -771,6 +797,7 @@ class Container(DefaultParser):
         Return the configuration
         :return:
         """
+        self.__conf['type'] = self.__type
         return self.__conf
 
 
