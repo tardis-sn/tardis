026af3a79ff78c3f14ad047c38e3494d13bfd3fb
Michi <michael@klauser.biz>
2014-04-15 13:04:23 +0200

    new test of the montecarlo function

--- tardis/montecarlo.pyx
+++ tardis/montecarlo.pyx
@@ -43,23 +43,6 @@ cdef extern from "randomkit/randomkit.h":
     float_type_t rk_double(rk_state *state)
 cdef rk_state mt_state
 
-########### Test wrapper section #################
-# This should be moved to an external file
-def binary_search_wrapper(np.ndarray x, float_type_t x_insert, int_type_t imin, int_type_t imax):
-    cdef float_type_t* x_pointer
-    x_pointer = <float_type_t*> x.data
-    return binary_search(x_pointer, x_insert, imin, imax)
-
-def line_search_wrapper(np.ndarray nu, float_type_t nu_insert,
-                        int_type_t number_of_lines):
-    cdef float_type_t* nu_pointer
-    nu_pointer = <float_type_t*> nu.data
-    return line_search(nu_pointer, nu_insert, number_of_lines)
-
-
-##################################################
-
-
 
 
 cdef np.ndarray x
