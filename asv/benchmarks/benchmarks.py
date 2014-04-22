4982bd62a06c37de77de0ce6552f98b39ed55922
Vytautas Jančauskas <unaudio@gmail.com>
2014-04-22 21:50:15 +0300

    Implemented asv based benchmarking for the binary_search function

--- asv/benchmarks/benchmarks.py
+++ asv/benchmarks/benchmarks.py
@@ -1,6 +1,10 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import numpy as np
+from tardis.tests import montecarlo_test_wrappers as montecarlo
+
+LINE_SIZE = 10000000
 
 class TimeSuite:
     """
@@ -8,29 +12,8 @@ class TimeSuite:
     of iterating over dictionaries in Python.
     """
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+        self.line = np.arange(LINE_SIZE, 1, -1).astype(np.float64)
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def time_binarysearch(self):
+        for _ in range(LINE_SIZE):
+            montecarlo.binary_search_wrapper(self.line, np.random.random() * LINE_SIZE, 0, LINE_SIZE - 1)
