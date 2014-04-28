d93559b0f8db12b84e2473570d98c0c6e335d304
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-04-28 12:47:48 +0300

    inserted tests for fix for #133

---
+++ tardis/tests/test_plasma_simple.py
@@ -0,0 +1,24 @@
+from astropy import constants as const, units as u
+import os
+import tardis
+from tardis import plasma_array, atomic
+import pytest
+#from numpy.testing import assert_allclose
+data_path = os.path.join(tardis.__path__[0], 'tests', 'data')
+helium_test_db = os.path.join(data_path, 'chianti_he_db.h5')
+
+
+class TestNebularPlasma(object):
+
+    def setup(self):
+        atom_data = atomic.AtomData.from_hdf5(helium_test_db)
+        self.plasma = plasma_array.BasePlasmaArray.from_abundance(
+            {'He':1.0}, 1e-15*u.Unit('g/cm3'), atom_data, 10 * u.day,
+            ionization_mode='nebular', excitation_mode='dilute-lte')
+
+    def test_high_temperature(self):
+        with pytest.raises(ValueError) as excinfo:
+            self.plasma.update_radiationfield([100000.], [1.])
+
+        assert excinfo.value.message.startswith('t_rads outside of zeta '
+                                                'factor interpolation')
