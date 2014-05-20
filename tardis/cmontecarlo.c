c465e938e3c02e2f04ba6bc9cde4321bf85c7284
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-21 00:58:13 +0300

    Reimplement compute_distance2outer and compute_distance2inner in C

--- tardis/cmontecarlo.c
+++ tardis/cmontecarlo.c
@@ -30,3 +30,21 @@ npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, np
     }
   return imin;
 }
+
+npy_float64 compute_distance2outer(npy_float64 r, npy_float64 mu, npy_float64 r_outer)
+{
+  return sqrt(r_outer * r_outer + ((mu * mu - 1.0) * r * r)) - (r * mu);
+}
+
+npy_float64 compute_distance2inner(npy_float64 r, npy_float64 mu, npy_float64 r_inner)
+{
+  npy_float64 check = r_inner * r_inner + (r * r * (mu * mu - 1.0));
+  if (check < 0.0) 
+    {
+      return MISS_DISTANCE;
+    }
+  else
+    {
+      return mu < 0.0 ? -r * mu - sqrt(check) : MISS_DISTANCE;
+    }
+}
--- tardis/cmontecarlo.h
+++ tardis/cmontecarlo.h
@@ -4,6 +4,8 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
+#define MISS_DISTANCE 1e99
+
 /** Look for a place to insert a value in an inversely sorted float array.
  *
  * @param x an inversely (largest to lowest) sorted float array
@@ -14,5 +16,9 @@
  * @return index of the next boundary to the left
  */
 npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, npy_int64 imax);
+
+npy_float64 compute_distance2outer(npy_float64 r, npy_float64 mu, npy_float64 r_outer);
+
+npy_float64 compute_distance2inner(npy_float64 r, npy_float64 mu, npy_float64 r_inner);
 
 #endif // TARDIS_CMONTECARLO_H
