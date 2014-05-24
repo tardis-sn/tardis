ad0b5ff1d9d224e9d7b6b6c5bdaa78f921fde0f0
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-05-24 17:57:20 -0400

    still adding files to see if we can get tardis to work with the new package template

--- tardis/conftest.py
+++ tardis/conftest.py
@@ -1,4 +1,6 @@
 #py.test configuration
 
+from astropy.tests.pytest_plugins import *
+
 def pytest_addoption(parser):
     parser.addoption("--atomic-dataset", dest='atomic-dataset', default=None, help="filename for atomic dataset")
