38fd9c5cb2719a9c5bf902efe39e073dfd8b2654
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-24 11:41:40 +0300

    Make sure functions are inlined

--- tardis/cmontecarlo.h
+++ tardis/cmontecarlo.h
@@ -1,3 +1,4 @@
+
 #ifndef TARDIS_CMONTECARLO_H
 #define TARDIS_CMONTECARLO_H
 
@@ -15,10 +16,10 @@
  *
  * @return index of the next boundary to the left
  */
-npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, npy_int64 imax);
+inline npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, npy_int64 imax);
 
-npy_float64 compute_distance2outer(npy_float64 r, npy_float64 mu, npy_float64 r_outer);
+inline npy_float64 compute_distance2outer(npy_float64 r, npy_float64 mu, npy_float64 r_outer);
 
-npy_float64 compute_distance2inner(npy_float64 r, npy_float64 mu, npy_float64 r_inner);
+inline npy_float64 compute_distance2inner(npy_float64 r, npy_float64 mu, npy_float64 r_inner);
 
 #endif // TARDIS_CMONTECARLO_H
