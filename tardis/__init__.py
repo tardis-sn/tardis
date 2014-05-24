dbfdde6153436e790c1a5384d21d21b4aacacf48
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-05-24 17:45:18 -0400

    added the _astropy_helpers script to the init

--- tardis/__init__.py
+++ tardis/__init__.py
@@ -1,5 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import logging
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
 logger = logging.getLogger('tardis')
 logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
