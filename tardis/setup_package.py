798b362d93f2ed37a3495e44018cf49efdf45e99
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-05-24 17:02:36 -0400

    fixed the numpy dependencies

--- tardis/setup_package.py
+++ tardis/setup_package.py
@@ -1,14 +1,21 @@
 #setting the right include
-
 from setuptools import Extension
+import numpy as np
 
-randomkit_files = ['tardis/randomkit/rk_isaac.c', 'tardis/randomkit/rk_mt.c', 'tardis/randomkit/rk_primitive.c',
+randomkit_files = ['tardis/randomkit/rk_isaac.c', 'tardis/randomkit/rk_mt.c',
+                   'tardis/randomkit/rk_primitive.c',
                    'tardis/randomkit/rk_sobol.c']
 
 def get_extensions():
     return [Extension('tardis.montecarlo',
-                        ['tardis/montecarlo.pyx', 'tardis/cmontecarlo.c'] + randomkit_files, include_dirs=['tardis/randomkit']), Extension('tardis.tests.montecarlo_test_wrappers', ['tardis/tests/montecarlo_test_wrappers.pyx', 'tardis/cmontecarlo.c'] + randomkit_files, include_dirs=['tardis/randomkit'])]
-    #return {'tardis.montecarlo_multizone':['randomkit/*.c']}
+                        ['tardis/montecarlo.pyx', 'tardis/cmontecarlo.c'] +
+                        randomkit_files,
+                        include_dirs=['tardis/randomkit', np.get_include()]),
+            Extension('tardis.tests.montecarlo_test_wrappers',
+                      ['tardis/tests/montecarlo_test_wrappers.pyx',
+                       'tardis/cmontecarlo.c'] + randomkit_files,
+                      include_dirs=['tardis/randomkit', np.get_include()])]
+
 
 def get_package_data():
     return {
