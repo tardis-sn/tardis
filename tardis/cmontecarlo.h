5440e2cdc7fb34fe80b856e0404b9678827c7e99
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-27 00:02:51 +0300

    Ported line_search to C

--- tardis/cmontecarlo.h
+++ tardis/cmontecarlo.h
@@ -18,6 +18,16 @@
  */
 inline npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, npy_int64 imax);
 
+/** Insert a value in to an array of line frequencies
+ *
+ * @param nu array of line frequencies
+ * @param nu_insert value of nu key
+ * @param number_of_lines number of lines in the line list
+ *
+ * @return index of the next line ot the red. If the key value is redder than the reddest line returns number_of_lines.
+ */
+inline npy_int64 line_search(npy_float64 *nu, npy_float64 nu_insert, npy_int64 number_of_lines);
+
 /** Calculate the distance to the outer boundary.
  *
  * @return distance to the outer boundary
