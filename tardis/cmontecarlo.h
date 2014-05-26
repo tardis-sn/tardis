387094cf94f5a5afa46ec01c1304f21d279a6151
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-26 23:41:32 +0300

    Added doxygen comments to compute_distance2outer and compute_distance2inner

--- tardis/cmontecarlo.h
+++ tardis/cmontecarlo.h
@@ -18,8 +18,16 @@
  */
 inline npy_int64 binary_search(npy_float64 *x, npy_float64 x_insert, npy_int64 imin, npy_int64 imax);
 
+/** Calculate the distance to the outer boundary.
+ *
+ * @return distance to the outer boundary
+ */
 inline npy_float64 compute_distance2outer(npy_float64 r, npy_float64 mu, npy_float64 r_outer);
 
+/** Calculate the distance to the inner boundary.
+ *
+ * @return distance to the inner boundary
+ */
 inline npy_float64 compute_distance2inner(npy_float64 r, npy_float64 mu, npy_float64 r_inner);
 
 #endif // TARDIS_CMONTECARLO_H
