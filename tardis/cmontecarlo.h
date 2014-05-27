844a0ab8055486fac6e5b27aa61f68efd42a34dd
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-27 11:09:00 +0300

    Started porting StorageModel to C

--- tardis/cmontecarlo.h
+++ tardis/cmontecarlo.h
@@ -7,6 +7,53 @@
 
 #define MISS_DISTANCE 1e99
 
+typedef struct StorageModel
+{
+  npy_float64 *packet_nus;
+  npy_float64 *packet_mus;
+  npy_float64 *packet_energies;
+  npy_float64 *output_nus;
+  npy_float64 *output_energies;
+  npy_int64 *last_line_interaction_in_id;
+  npy_int64 *last_line_interaction_out_id;
+  npy_int64 *last_line_interaction_shell_id;
+  npy_int64 *last_interaction_type;
+  npy_int64 no_of_packets;
+  npy_int64 no_of_shells;
+  npy_float64 *r_inner;
+  npy_float64 *r_outer;
+  npy_float64 *v_inner;
+  npy_float64 time_explosion;
+  npy_float64 inverse_time_explosion;
+  npy_float64 *electron_densities;
+  npy_float64 *inverse_electron_densities;
+  npy_float64 *line_list_nu;
+  npy_float64 *line_lists_tau_sobolevs;
+  npy_int64 line_lists_tau_sobolevs_nd;
+  npy_float64 *line_lists_j_blues;
+  npy_int64 line_lists_j_blues_nd;
+  npy_int64 no_of_lines;
+  npy_int64 line_interaction_id;
+  npy_float64 *transition_probabilities;
+  npy_int64 transition_probabilities_nd;
+  npy_int64 *line2macro_level_upper;
+  npy_int64 *macro_block_references;
+  npy_int64 *transition_type;
+  npy_int64 *destination_level_id;
+  npy_int64 *transition_line_id;
+  npy_float64 *js;
+  npy_float64 *nubars;
+  npy_float64 spectrum_start_nu;
+  npy_float64 spectrum_delta_nu;
+  npy_float64 spectrum_end_nu;
+  npy_float64 *spectrum_virt_nu;
+  npy_float64 sigma_thomson;
+  npy_float64 inverse_sigma_thomson;
+  npy_float64 inner_boundary_albedo;
+  npy_int64 reflective_inner_boundary;
+  npy_int64 current_packet_id;
+} storage_model_t;
+
 /** Look for a place to insert a value in an inversely sorted float array.
  *
  * @param x an inversely (largest to lowest) sorted float array
