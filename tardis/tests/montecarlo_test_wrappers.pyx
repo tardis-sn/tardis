88cd3f29a78599c6c2d9939f132bcb9770e0ea6f
Vytautas Jančauskas <unaudio@gmail.com>
2014-05-27 23:20:55 +0300

    Added a wrapper for compute_distance2electron

--- tardis/tests/montecarlo_test_wrappers.pyx
+++ tardis/tests/montecarlo_test_wrappers.pyx
@@ -22,3 +22,6 @@ def compute_distance2inner_wrapper(float_type_t r, float_type_t mu, float_type_t
 
 def compute_distance2line_wrapper(float_type_t r, float_type_t mu, float_type_t nu, float_type_t nu_line, float_type_t t_exp, float_type_t inverse_t_exp, float_type_t last_line, float_type_t next_line, int_type_t cur_zone_id):
     return compute_distance2line(r, mu, nu, nu_line, t_exp, inverse_t_exp, last_line, next_line, cur_zone_id)
+
+def compute_distance2electron_wrapper(float_type_t r, float_type_t mu, float_type_t tau_event, float_type_t inverse_ne):
+    return compute_distance2electron(r, mu, tau_event, inverse_ne)
