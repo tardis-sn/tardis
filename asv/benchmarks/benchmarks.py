c537a9d4fb9d84009284f09629ede03dfcc0e29b
Vytautas Jancauskas <vytasj@localhost.localdomain>
2014-05-30 11:48:59 +0300

    Added benchmarks for distance2line and distance2electron

--- asv/benchmarks/benchmarks.py
+++ asv/benchmarks/benchmarks.py
@@ -31,3 +31,12 @@ class TimeSuite:
             montecarlo.compute_distance2inner_wrapper(1.5, -1.0, 1.0)
             montecarlo.compute_distance2inner_wrapper(0.0, 0.0, 0.0)
             montecarlo.compute_distance2inner_wrapper(1.2, -0.7, 1.0)
+
+    def time_compute_distance2line(self):
+        for _ in range(1000000):
+            montecarlo.compute_distance2line_wrapper(2.20866912e+15, -0.251699059004, 1.05581082105e+15, 1.06020910733e+15, 1693440.0, 5.90513983371e-07, 1.0602263591e+15, 1.06011723237e+15, 2)
+            montecarlo.compute_distance2line_wrapper(2.23434667994e+15, -0.291130548401, 1.05581082105e+15, 1.06733618121e+15, 1693440.0, 5.90513983371e-07, 1.06738407486e+15, 1.06732933961e+15, 3)
+
+    def time_compute_distance2electron(self):
+        for _ in range(1000000):
+            montecarlo.compute_distance2electron_wrapper(0.0, 0.0, 2.0, 2.0)
