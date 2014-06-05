87339fe1e9fe78a49b3f3b5b4412cbe4ef6530d7
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-06-05 16:11:18 -0400

    Update setup.py
    Added tardis-sn as package name

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ for root, dirs, files in os.walk(PACKAGENAME):
                     os.path.relpath(root, PACKAGENAME), filename))
 package_info['package_data'][PACKAGENAME].extend(c_files)
 
-setup(name=PACKAGENAME,
+setup(name=PACKAGENAME + '-sn',
       version=VERSION,
       description=DESCRIPTION,
       scripts=scripts,
