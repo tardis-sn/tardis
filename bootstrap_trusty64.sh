3efbf02da37e5e021a2877b74a40d2a3caffc4cc
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-10-24 16:43:50 -0400

    restructuring TRAVIS to be easier to read

--- bootstrap_trusty64.sh
+++ bootstrap_trusty64.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install -y python-virtualenv python-numpy python-pandas python-scipy python-h5py python-yaml python-astropy cython
-chown -R
 virtualenv --system-site-packages tardis.ve
+chmod -R a+rwX tardis.ve
 source tardis.ve/bin/activate
 pip install -r /vagrant/pip-requirements
