108b1effc962a14dc1dee2e32053a04d3ea17255
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-11-06 13:47:17 -0500

    added various bug reporting techniques

--- tardis/conftest.py
+++ tardis/conftest.py
@@ -27,3 +27,105 @@ def pytest_addoption(parser):
                   default=False)
 
     parser.addoption("--atomic-dataset", dest='atomic-dataset', default=None, help="filename for atomic dataset")
+
+def pytest_report_header(config):
+
+    stdoutencoding = getattr(sys.stdout, 'encoding') or 'ascii'
+
+    s = "\n"
+    if six.PY2:
+        args = [x.decode('utf-8') for x in config.args]
+    elif six.PY3:
+        args = config.args
+    s += "Running tests in {0}.\n\n".format(" ".join(args))
+
+    from platform import platform
+    plat = platform()
+    if isinstance(plat, bytes):
+        plat = plat.decode(stdoutencoding, 'replace')
+    s += "Platform: {0}\n\n".format(plat)
+    s += "Executable: {0}\n\n".format(sys.executable)
+    s += "Full Python Version: \n{0}\n\n".format(sys.version)
+
+    s += "encodings: sys: {0}, locale: {1}, filesystem: {2}".format(
+        sys.getdefaultencoding(),
+        locale.getpreferredencoding(),
+        sys.getfilesystemencoding())
+    if sys.version_info < (3, 3, 0):
+        s += ", unicode bits: {0}".format(
+            int(math.log(sys.maxunicode, 2)))
+    s += '\n'
+
+    s += "byteorder: {0}\n".format(sys.byteorder)
+    s += "float info: dig: {0.dig}, mant_dig: {0.dig}\n\n".format(
+        sys.float_info)
+
+    import numpy
+    s += "Numpy: {0}\n".format(numpy.__version__)
+
+    try:
+        import scipy
+        s += "Scipy: {0}\n".format(scipy.__version__)
+    except:
+        s += "Scipy: not available\n"
+
+    try:
+        import astropy
+    except:
+        s += "astropy: not available\n"
+    else:
+        s += "astropy: {0}\n".format(astropy.__version__)
+
+    try:
+        import yaml
+    except:
+        s += "yaml: not available\n"
+    else:
+        s += "yaml: {0}\n".format(yaml.__version__)
+
+
+    try:
+        import cython
+    except:
+        s += "cython: not available\n"
+    else:
+        s += "cython: {0}\n".format(cython.__version__)
+
+
+
+    try:
+        import h5py.version
+        s += "h5py: {0}\n".format(h5py.version.version)
+    except:
+        s += "h5py: not available\n"
+
+
+    try:
+        import matplotlib
+        s += "Matplotlib: {0}\n".format(matplotlib.__version__)
+    except:
+        s += "Matplotlib: not available\n"
+
+    try:
+        import IPython
+    except:
+        s += "ipython: not available\n"
+    else:
+        s += "ipython: {0}\n".format(IPython.__version__)
+
+
+    special_opts = ["remote_data", "pep8"]
+    opts = []
+    for op in special_opts:
+        if getattr(config.option, op, None):
+            opts.append(op)
+    if opts:
+        s += "Using Astropy options: {0}.\n".format(" ".join(opts))
+
+    if six.PY3 and (config.getini('doctest_rst') or config.option.doctest_rst):
+        s += "Running doctests in .rst files is not supported on Python 3.x\n"
+
+    if not six.PY3:
+        s = s.encode(stdoutencoding, 'replace')
+
+    return s
