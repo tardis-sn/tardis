fdc5e0a3674cdec9f2bd945ff7483bb72c917b8f
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-11-06 15:39:45 +0000

    revamp of the installation instructions

--- bootstrap_trusty64.sh
+++ bootstrap_trusty64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python-virtualenv python-numpy python-pandas python-scipy python-h5py python-yaml cython git
+apt-get install -y python-virtualenv python-numpy python-pandas python-scipy python-h5py python-yaml ipython python-matplotlib cython git
 virtualenv --system-site-packages tardis.ve
 chmod -R a+rwX tardis.ve
 source tardis.ve/bin/activate
