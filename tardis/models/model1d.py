d1ccbbd13150e32552f9ec3350ae382bde01eb70
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-12-04 12:06:01 +0100

    cleanup

--- tardis/models/model1d.py
+++ tardis/models/model1d.py
@@ -19,11 +19,7 @@ class Radial1D(BaseModel):
         super(Radial1D, self).__init__()
         self._register_attribute('radius', radius)
 
-
-    @property
-    def mass(self):
-        return self.volume * self.density
-
+    
 
 class RadialHomologous1D(Radial1D):
     """
@@ -41,32 +37,6 @@ class RadialHomologous1D(Radial1D):
         self.temperature = temperature
         self.density
 
-    @property
-    def radius(self):
-        return self._radius
-
-    @property
-    def velocity(self):
-        return self._velocity
-
-    @property
-    def t_0(self):
-        return self.t_0
-
-    @radius.setter
-    def radius(self, value):
-        self._radius = u.Quantity(value, unit=u.cm)
-        self._velocity = self._radius / self._t_0
-
-    @velocity.setter
-    def velocity(self, value):
-        self._velocity = u.Quantity(value, unit=u.cm / u.s)
-        self._radius = self.t_0 * self._velocity
-
-    @t_0.setter
-    def t_0(self, value):
-        self._t_0 = u.Quantity(value, u.s)
-        self._radius = self.velocity * self._t_0
 
 
 
