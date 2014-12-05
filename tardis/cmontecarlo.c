80cc0bdb5865fd5d8d27f6ff14142e3f8b8bbc0d
Stuart Sim <ssim@mso.anu.edu.au>
2014-12-05 15:13:59 +0000

    Deleting odd/old binary search

--- tardis/cmontecarlo.c
+++ tardis/cmontecarlo.c
@@ -73,51 +73,6 @@ reverse_binary_search (double *x, double x_insert,
   return ret_val;
 }
 
-inline tardis_error_t
-binary_search (double *x, double x_insert, int64_t imin,
-	       int64_t imax, int64_t * result)
-{
-  /*
-     Have in mind that *x points to a sorted array.
-     Like [1,2,3,4,5,...]
-   */
-  int imid;
-  tardis_error_t ret_val = TARDIS_ERROR_OK;
-  if (x_insert < x[imin] || x_insert > x[imax])
-    {
-      ret_val = TARDIS_ERROR_BOUNDS_ERROR;
-    }
-  else
-    {
-      while (imax >= imin)
-	{
-	  imid = (imin + imax) / 2;
-	  if (x[imid] == x_insert)
-	    {
-	      *result = imid;
-	      break;
-	    }
-	  else if (x[imid] < x_insert)
-	    {
-	      imin = imid + 1;
-	    }
-	  else
-	    {
-	      imax = imid - 1;
-	    }
-	}
-      if (imax - imid == 2 && x_insert < x[imin + 1])
-	{
-	  *result = imin;
-	}
-      else
-	{
-	  *result = imin;
-	}
-    }
-  return ret_val;
-}
-
 INLINE double
 rpacket_doppler_factor (rpacket_t * packet, storage_model_t * storage)
 {
