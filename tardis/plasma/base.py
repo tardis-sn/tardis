b1d320111ee1e01ab85682fda3a37e7425289b73
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-02-03 20:59:50 +0100

    first attempt to do the networkx plasma

--- tardis/plasma/base.py
+++ tardis/plasma/base.py
@@ -1,7 +1,12 @@
-
+import networkx as nx
 
 class BasePlasma(object):
-        pass
+        def __init__(self, plasma_properties):
+            self._build_graph(plasma_properties)
+
+        def _build_graph(self, plasma_properties):
+            for plasma_property in plasma_properties:
+                self.graph = nx.DiGraph()
 
 
 class PlasmaMissingModule(Exception):
@@ -13,23 +18,5 @@ class StandardPlasma(BasePlasma):
                  delta_treatment=None, nlte_config=None, ionization_mode='lte',
                  excitation_mode='lte'):
 
-        self.number_densities = number_densities
-        self.atom_data = atom_data
-        self.time_explosion = time_explosion
-        self.nlte_config = nlte_config
-        self.delta_treatment = delta_treatment
-        self.electron_densities = self.number_densities.sum(axis=0)
-
-    module_dict = {'partition_function':pf, 'level_populations':lp}
-
-    def setup(self):
-        for key in self.module_dict:
-            current_module = self.module_dict[key]
-            current_dependencies = current_module.get_dependencies()
-            for key in current_dependencies:
-                if key not in self.module_dict:
-                    raise PlasmaMissingModule('Module {0} missing')
-                current_module.register_depency()
-    def evaluate(self):
-
+        pass
 
