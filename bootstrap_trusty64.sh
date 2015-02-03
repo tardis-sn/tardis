dc074b261e3bb4d3cdd7d858acddcb0567ab41b7
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-02-03 17:45:51 +0100

    updated setup.py to 0.4.2 package-template[not all]. added git to vagrant

--- bootstrap_trusty64.sh
+++ bootstrap_trusty64.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo apt-get install git --yes
+
+sudo apt-get install git
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b
 
