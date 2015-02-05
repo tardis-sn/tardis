e05877b2db3e50c10bc0391dc7893bbf733faa69
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-02-05 17:38:35 +0100

    added test with atomic data dependency

--- tardis/plasma/tests/simple_networkx_test.py
+++ tardis/plasma/tests/simple_networkx_test.py
@@ -6,5 +6,5 @@ from tardis.plasma.plasma_properties import (BetaRadiation,
 from tardis.plasma.plasma_input import TRadiative, AtomicData
 
 
-def test_simple_networkx_test1():
+def test_simple_networkx_test1(atomic_data):
     bp = BasePlasma([TRadiative, BetaRadiation, LevelBoltzmannFactor, AtomicLevels, AtomicLines, AtomicData], t_rad=5000, atomic_data=None)
