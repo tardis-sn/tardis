4acbdf96408308acb942b355b771c8a26a35a5ae
Michi <michael@klauser.biz>
2015-02-23 16:13:37 +0100

    bla

--- tardis/montecarlo/src/cmontecarlo_mainloop.c
+++ tardis/montecarlo/src/cmontecarlo_mainloop.c
@@ -26,69 +26,75 @@ montecarlo_parallel_loop(storage_model_t * storage, int64_t openmp_threads,
 #ifdef OPENMP
 
 #pragma omp parallel (shared(storage,openmp_threads, virtual_packet_flag), private(reabsorbed,packet))
-{
+	{
+		int64_t npacket;
+		int64_t reabsorbed = 0;
+		int64_t num_threads = 0;
+		int64_t threads_num = 0;
+		int64_t spectrum_virt_nu_len = 0;
+		int64_t line_lists_j_blues_len = 0;
+		RPacket *packet = NULL;
+		RPacket *big_packet = NULL;
+		double *big_thread_spectrum_virt_nu = NULL;
+		double *big_thread_line_lists_j_blues = NULL;
+
+		npacket = storage->no_of_packets;
+		spectrum_virt_nu_len = storage->spectrum_virt_nu_len;
+		line_lists_j_blues_len = storage->line_lists_j_blues_len;
+
+		omp_set_num_threads(openmp_threads);
+
 #pragma single
-{
-	int64_t npacket;
-	int64_t reabsorbed = 0;
-	int64_t num_threads = 0;
-	int64_t threads_num = 0;
-	int64_t spectrum_virt_nu_len = 0;
-	int64_t line_lists_j_blues_len = 0;
-	RPacket *packet = NULL;
-	RPacket *big_packet = NULL;
-	double *big_thread_spectrum_virt_nu = NULL;
-	double *big_thread_line_lists_j_blues = NULL;
+		{
+			num_threads = omp_get_num_threads();
+			big_packet =
+			    (RPacket *) malloc(sizeof(RPacket) * num_threads);
+			big_thread_spectrum_virt_nu =
+			    (double *)malloc(sizeof(double) *
+					     (spectrum_virt_nu_len *
+					      num_threads));
+			big_thread_line_lists_j_blues =
+			    (double *)malloc(sizeof(double) *
+					     (line_lists_j_blues_len *
+					      num_threads));
 
-	npacket = storage->no_of_packets;
-	spectrum_virt_nu_len = storage->spectrum_virt_nu_len;
-	line_lists_j_blues_len = storage->line_lists_j_blues_len;
-
-	omp_set_num_threads(openmp_threads);
-
-	num_threads = omp_get_num_threads();
-	big_packet = (RPacket *) malloc(sizeof(RPacket) * num_threads);
-	big_thread_spectrum_virt_nu =
-	    (double *)malloc(sizeof(double) *
-			     (spectrum_virt_nu_len * num_threads));
-	big_thread_line_lists_j_blues =
-	    (double *)malloc(sizeof(double) *
-			     (line_lists_j_blues_len * num_threads));
-
-}
+		}
 #pragma omp for
-	for (int ip = 0; ip <= npacket; ip++) {
-		threads_num = omp_get_thread_num();
-		packet = (RPacket *) (big_packet + threads_num);
-		rpacket_set_id(packet, ip);
-		packet->thread_spectrum_virt_nu =
-		    (double *)(big_thread_spectrum_virt_nu + threads_num);
-		packet->thread_line_lists_j_blues =
-		    (double *)(big_thread_line_lists_j_blues + threads_num);
-		rpacket_init(packet, &storage, ip, virtual_packet_flag);
+		for (int ip = 0; ip <= npacket; ip++) {
+			threads_num = omp_get_thread_num();
+			packet = (RPacket *) (big_packet + threads_num);
+			rpacket_set_id(packet, ip);
+			packet->thread_spectrum_virt_nu =
+			    (double *)(big_thread_spectrum_virt_nu +
+				       threads_num);
+			packet->thread_line_lists_j_blues =
+			    (double *)(big_thread_line_lists_j_blues +
+				       threads_num);
+			rpacket_init(packet, &storage, ip, virtual_packet_flag);
 
-		if (!(npacket % (npacket / 20))) {
-			printf("%d\n", npacket);
-		}
-		if (virtual_packet_flag > 0) {
-			reabsorbed =
-			    montecarlo_one_packet(&storage, &packet, -1);
-		} else {
-			reabsorbed =
-			    montecarlo_one_packet(&storage, &packet, 0);
-		}
+			if (!(npacket % (npacket / 20))) {
+				printf("%d\n", npacket);
+			}
+			if (virtual_packet_flag > 0) {
+				reabsorbed =
+				    montecarlo_one_packet(&storage, &packet,
+							  -1);
+			} else {
+				reabsorbed =
+				    montecarlo_one_packet(&storage, &packet, 0);
+			}
 
-		move_data_packet2storage(storage, packet, ip, reabsorbed);
+			move_data_packet2storage(storage, packet, ip,
+						 reabsorbed);
+		}
+		free(packet);
+		packet = NULL;
+
 	}
-	free(packet);
-	packet = NULL;
-
-}
 #else
 	printf
 	    ('CRITICAL - Tardis was build without openmp support. Fallback to none parallel run');
 	montecarlo_serial_loop(storage, 0, virtual_packet_flag);
-
 
 #endif
 
