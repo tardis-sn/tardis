88cc64bf869c1db4bd8000a7475f264663430aac
Adrian <andarms@outlook.com>
2015-03-04 19:44:42 -0500

    coping project to tardis/web

---
+++ web/main.py
@@ -0,0 +1,30 @@
+import os
+
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler, StaticFileHandler
+from tornado.httpserver import HTTPServer
+
+TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "templates")
+STATIC_PATH = os.path.join(os.path.dirname(__file__), "static")
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def make_app():
+    return Application([
+            (r"/", MainHandler),
+            (r'/static/(.*)', StaticFileHandler, {'path': STATIC_PATH})
+        ],
+        template_path = TEMPLATE_PATH)
+
+def main():
+    app = make_app()
+    http_server = HTTPServer(app)
+    port = int(os.environ.get("PORT", 8888))
+    http_server.listen(port)
+    IOLoop.instance().start()
+    
+
+if __name__ == "__main__":
+    main()
