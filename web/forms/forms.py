1fe14c1609b474edc48f9bc0f6c4d4136f0a108c
mavidser <mavidser@gmail.com>
2015-03-08 23:53:58 +0530

    Form generation from the config file is now complete.

--- web/forms/forms.py
+++ web/forms/forms.py
@@ -1,5 +1,4 @@
 #TODO: quantity_range_sampled
-#TODO: Add support for container-property
 
 from wtforms import validators, fields
 from wtforms_tornado import Form
@@ -28,80 +27,140 @@ field_type = {
     'quantity': fields.TextField,
     'quantity_range_sampled': fields.TextField,
     'float': fields.TextField,
+    'int': fields.TextField,
     'string': fields.TextField,
     'list': fields.TextField,
     'bool': fields.BooleanField,
+    'container-declaration': fields.SelectField
 }
+
+association_dict = {}
 
 #------------------------------------------------------------------------------
 
-def createField(schema,parent=None):
+def createField(schema,parent='',name=None):
     description = {}
     validation = []
-    description['help_text'] = schema['help']
+    # print schema
+    if 'help' in schema:
+        description['help_text'] = schema['help']
     if 'default' in schema and schema['default']!='None':
         description['default'] = schema['default']
     else:
         description['default'] = ''
+    if name in association_dict and parent==association_dict[name]['parent']:
+        try:
+            description['association']=association_dict[name]['association']
+        except:
+            pass
+
     if parent:
         description['parent']=parent
     if 'mandatory' in schema and schema['mandatory']:
         validation = [validators.DataRequired()]
+    # print schema
+    # print
     if 'file' in schema and schema['file']:
         form_field = fields.FileField
     elif 'allowed_value' in schema:
         form_field = fields.SelectField
-        choices = [(i,i) for i in schema['allowed_value'].split()]
+        choices = [(i,i) for i in schema['allowed_value']]
         return form_field(choices=choices,validators=validation,description=description)
     else:
         form_field = field_type[schema['property_type']]
+        if schema['property_type']=='container-declaration':
+            choices=[(i,i) for i in schema['containers']]
+            children = [i  for i in schema if (i[0]=='_' or i[0]=='+')]
+            for i in children:
+                for j in schema[i]:
+                    print j
+                    if j in association_dict:
+                        association_dict[j]['association'] = association_dict[j]['association']+" "+i[1:]
+                    else:
+                        association_dict[j] = {}
+                        if parent:
+                            association_dict[j]['parent'] = parent
+                            association_dict[j]['association'] = i[1:] + " " +parent+'_'+ name
+                        else:
+                            association_dict[j]['parent'] = parent
+                            association_dict[j]['association'] = i[1:] + " " +name
+
+            return form_field(choices=choices,validators=validation,description=description)
     return form_field(validators=validation,description=description)
 
-def autofill_fields(yml_field,single_item):
+def populate_fields(yml_field,single_item,parent=''):
     def run(cls):
-        print yml_field
-        print
+        # print parent
+        global association_dict
         if single_item:
             setattr(cls, single_item, createField(yml_field))
         else:
             for i in yml_field:
-                if 'property_type' in yml_field[i]:
-                    setattr(cls, i, createField(yml_field[i]))
-                else:
-                    setattr(cls, i, fields.BooleanField())
-                    for j in yml_field[i]:
-                        setattr(cls, j, createField(yml_field[i][j],parent=i))
+                try:
+                    association = association_dict[i]['association']
+                except:
+                    association = ''
+                if i=='property_type' and yml_field[i]=='container-property':
+                    pass
+                elif 'property_type' in yml_field[i]:
 
+                    if 'property_type' in yml_field[i] and yml_field[i]['property_type']=='container-property':
+                        if association:
+                            setattr(cls, i, fields.BooleanField(description={'association':association}))
+                        else:
+                            setattr(cls, i, fields.BooleanField())
+                        # print parent,i
+                        populate_fields(yml_field[i],single_item,parent=i)(cls)
+                        association_dict = {}
+                        try:
+                            association.pop()
+                        except: pass
+                    else:
+                        name = i
+                        if parent:
+                            print 'd',i,parent
+                            setattr(cls, parent+'_'+i, createField(yml_field[i],parent=parent,name=name))
+                        else:
+                            # print 'hoo'
+                            setattr(cls, i, createField(yml_field[i],parent=parent,name=name))
+                else:
+                    if parent:
+                        setattr(cls, i, fields.BooleanField(description={'association':association,'parent':parent}))
+                    else:
+                        setattr(cls, i, fields.BooleanField(description={'association':association}))
+                    populate_fields(yml_field[i],single_item,parent=i)(cls)
         return cls
     return run
 
 #------------------------------------------------------------------------------
 
-@autofill_fields(config_data['supernova'],single_item=False)
+@populate_fields(config_data['supernova'],single_item=False)
 class SupernoveForm(Form):
     pass
 
-@autofill_fields(config_data['atom_data'],single_item='atom_data')
+@populate_fields(config_data['atom_data'],single_item='atom_data')
 class AtomForm(Form):
     pass
 
-@autofill_fields(config_data['plasma'],single_item=False)
+@populate_fields(config_data['plasma'],single_item=False)
 class PlasmaForm(Form):
     pass
 
-@autofill_fields(config_data['spectrum'],single_item='spectrum')
-class SpectrumForm(Form):
-    pass
-
+@populate_fields(config_data['model']['abundances'],single_item=False)
 class AbundanceForm(Form):
-    pass
+    uniform_abundances = fields.TextAreaField(description={'help_text':'Insert Uniform abundances of all the shells, in the format: C: 0.01 O: 0.01 etc...'})
 
+@populate_fields(config_data['model']['structure'],single_item=False)
 class StructureForm(Form):
     pass
 
+@populate_fields(config_data['montecarlo'],single_item=False)
 class MonteCarloForm(Form):
     pass
 
+@populate_fields(config_data['spectrum'],single_item='spectrum')
+class SpectrumForm(Form):
+    pass
 #------------------------------------------------------------------------------
 
 class TardisForm(Form):
