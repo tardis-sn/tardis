da30d4e7f9e6632210ce8abcc95f6538fcb8b8f9
Vidit Bhargava <viditvineet@hotmail.com>
2015-03-10 00:03:40 +0530

    Added a few tests for atomic.py

--- tardis/tests/test_atomic.py
+++ tardis/tests/test_atomic.py
@@ -26,6 +26,40 @@ def test_levels_h5_readin():
     assert data['g'][4] == 1
     assert data['metastable'][4] == False
 
+def test_lines_h5_readin():
+    data=atomic.read_lines_data(atomic.default_atom_h5_path)
+    assert data['line_id'][7] == 20
+    assert data['wavelength'][7] == 71.533
+    assert data['atomic_number'][7] == 14
+    assert data['ion_number'][7] == 5
+    assert data['level_number_lower'][7] == 1.0
+    assert data['level_number_upper'][7] == 32.0
+    si_frequency_ul = data['f_ul'][7]
+    si_frequency_lu = data['f_lu'][7]
+    testing.assert_almost_equal(si_frequency_ul,0.0772574,decimal=7)
+    testing.assert_almost_equal(si_frequency_lu,0.1545148,decimal=7)
+    
+def test_collision_h5_readin():
+    with pytest.raises(ValueError) as excinfo:
+        atomic.read_collision_data(None)
+    assert 'fname can not' in str(excinfo.value)
+    test='../doesnt_exist.h5'
+    with pytest.raises(IOError) as excinfo:
+        atomic.read_collision_data(test)
+    assert 'HDF5 File' in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        atomic.read_collision_data(atomic.default_atom_h5_path)
+    assert 'NLTE' in str(excinfo.value)
+    path=os.path.join(os.path.dirname(__file__), 'data', 'chianti_he_db.h5')
+    collision_data, temperature=atomic.read_collision_data(path)
+    assert collision_data[0][0] == 2
+    assert collision_data[0][1] == 0
+    assert collision_data[0][2] == 18
+    assert collision_data[0][3] == 2
+    assert collision_data[0][4] == 1.0
+    delta_e = collision_data[0][5]
+    testing.assert_almost_equal(delta_e,35484.25114336,decimal=7)
+
 
 def test_atom_levels():
     atom_data = atomic.AtomData.from_hdf5(atomic.default_atom_h5_path)
