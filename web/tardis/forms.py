96011708f070c31ebcb0fd810a2f8a375b0e18aa
Aditya Narayan <narayanaditya95@gmail.com>
2015-03-14 10:19:13 +0530

    Removed obsolete import:Django-crispy form

--- web/tardis/forms.py
+++ web/tardis/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 
-from crispy_forms.helper import FormHelper
-
 
 class DynamicForm(forms.Form):
     """
@@ -11,11 +9,6 @@ class DynamicForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.legend, fields = kwargs.pop('legend'), kwargs.pop('fields')
         super(DynamicForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-8'
         if isinstance(fields, forms.Field):
             fields = {'Input': fields}
         for field_name, field in fields.iteritems():
--- web/tardis/settings.py
+++ web/tardis/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'crispy_forms',
     'tardis',
 )
 
