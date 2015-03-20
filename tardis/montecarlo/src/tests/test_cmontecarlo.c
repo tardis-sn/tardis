1c9da2f90e8ea705e24653c8c74abffb797a87f6
kaushik94 <kaushik.varanasi1@gmail.com>
2015-03-20 21:13:11 +0530

    pushing test forcefully :(

---
+++ tardis/montecarlo/src/tests/test_cmontecarlo.c
@@ -0,0 +1,21 @@
+#include "stdlib.h"
+
+#include "../cmontecarlo.h"
+#include "test_macros.h"
+
+
+void test_rpacket_get_set()
+{
+	double nu = 10.3;
+	rpacket_t * packet = (rpacket_t * ) malloc(sizeof(rpacket_t));
+	rpacket_set_nu(packet, nu);
+	double ret = rpacket_get_nu( packet );
+	ASSERT_EQUAL_FLOAT(ret, nu);
+}
+
+int main()
+{
+
+	test_rpacket_get_set();
+	return 0;
+}
