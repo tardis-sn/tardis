eefee5e36c27d85508b6daf5b9ac7a1811396344
jasminu <j.michajlow@student.uw.edu.pl>
2015-03-27 19:47:56 +0100

    - test_reverse_binary_search re-done basing on previous implementations. - a new test proposition to be discussed.

--- tardis/tests/test_montecarlo.py
+++ tardis/tests/test_montecarlo.py
@@ -1,31 +1,40 @@
 import numpy as np
-from tardis import montecarlo
+from tardis.montecarlo import montecarlo
 import pytest
 
 test_line_list = np.array([10, 9, 8, 7, 6, 5, 5, 4, 3, 2, 1]).astype(np.float64)
 
-# @pytest.mark.parametrize(("insert_value", "expected_insert_position"), [
-#     (9.5, 0),
-#     (8.5, 1),
-#     (7.5, 2),
-#     (6.5, 3),
-#     (5.5, 4),
-#     (5.2, 4),
-#     (4.5, 6),
-#     (3.5, 7),
-#     (2.5, 8),
-#     (1.5, 9)])
-# def test_binary_search(insert_value, expected_insert_position):
-#     insert_position = montecarlo.binary_search_wrapper(test_line_list, insert_value, 0, len(test_line_list))
-#     assert insert_position == expected_insert_position
+empty_list = np.array([]).astype(np.float64)
 
+@pytest.mark.parametrize(("insert_value", "expected_insert_position"), [
+    (9.5, 0),
+    (8.5, 1),
+    (7.5, 2),
+    (6.5, 3),
+    (5.5, 4),
+    (5.2, 4),
+    (4.5, 6),
+    (3.5, 7),
+    (2.5, 8),
+    (1.5, 9)])
+def test_reverse_binary_search(insert_value, expected_insert_position):
+    print(dir(montecarlo))
+    insert_position = montecarlo.reverse_binary_search_wrapper(test_line_list, insert_value, 0, len(test_line_list))
+    assert insert_position == expected_insert_position
 
-# @pytest.mark.parametrize(("insert_value"), [
-#     (10.5),
-#     (0.5)])
-# def test_binary_search_out_of_bounds(insert_value, capsys):
-#     with pytest.raises(ValueError):
-#         insert_position = montecarlo.binary_search_wrapper(test_line_list, insert_value, 0, len(test_line_list)-1)
+@pytest.mark.parametrize(("insert_value"), [
+    (10.5),
+    (0.5)])
+def test_reverse_binary_search_out_of_bounds(insert_value, capsys):
+    with pytest.raises(ValueError):
+        insert_position = montecarlo.reverse_binary_search_wrapper(test_line_list, insert_value, 0, len(test_line_list)-1)
+
+#this test acutally fails right now. The reverse_binary_search reads from array not checking whether
+#the range (imin,imax) is not empty. Is it fine?
+# def test_reverse_binary_search_does_not_fail_on_empty_array():
+#     insert_position = montecarlo.reverse_binary_search_wrapper(empty_list, 1.0, 0, len(test_line_list)-1)
+#     assert insert_position == 1
+
 
 # @pytest.mark.parametrize(("insert_value", "expected_insert_position"), [
 #     (10.5, 0),
