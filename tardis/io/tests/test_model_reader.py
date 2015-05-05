f2a75efdbd10d70c41d071afaf4359a31cc53585
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-05-05 15:42:18 +0200

    added model reader test

--- tardis/io/tests/test_model_reader.py
+++ tardis/io/tests/test_model_reader.py
@@ -1,6 +1,25 @@
+import tardis
 from tardis.io.model_reader import read_artis_density
+
+from astropy import units as u
+import numpy as np
 
 import pytest
 
-def test_simple_read_artis_density():
-    assert True
+import os
+
+data_path = os.path.join(tardis.__path__[0], 'io', 'tests', 'data')
+
+@pytest.fixture
+def artis_density_fname():
+    return os.path.join(data_path, 'artis_model.dat')
+
+def test_simple_read_artis_density(artis_density_fname):
+    (time_of_model, index, v_inner, v_outer,
+     mean_density) = read_artis_density(artis_density_fname)
+
+    assert np.isclose(0.00114661 * u.day, time_of_model, atol=1e-7 * u.day)
+    assert len(mean_density) == 69
+    assert len(mean_density) == len(v_inner)
+
+
