f3f5fe18805d24cd33508c409a34e3e5fce04ecc
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-05-06 15:04:44 +0200

    bringing the new atomicdata class in line with tardisatomic restructure

--- tardis/atomic/base.py
+++ tardis/atomic/base.py
@@ -1,24 +1,25 @@
-from tardis.atomic.plugins import BaseAtomicDataPlugin
+from tardis.atomic.plugins import BaseAtomicDataType
 import pandas as pd
 
 class AtomicData(object):
 
-    plugins = BaseAtomicDataPlugin.__subclasses__()
+    all_data_types = BaseAtomicDataType.__subclasses__()
 
     @classmethod
     def from_hdf(cls, fname):
         return cls(pd.HDFStore(fname, mode='r'))
 
-    def __init__(self, hdf_store):
-        self.plugins = [plugin()
-                        for plugin in BaseAtomicDataPlugin.__subclasses__()]
-        self.name2plugin = {item.name:item for item in self.plugins}
+    def __init__(self, hdf_store, data_types):
+        self.data_types = [data_type()
+                        for data_type in self.all_data_types]
+
+        self.name2data_type = {item.name:item for item in self.data_types}
 
 
 
     def __getattr__(self, item):
-        if item in self.name2plugin:
-            return self.name2plugin[item].data
+        if item in self.name2data_type:
+            return self.name2date_type[item].data
         else:
             super(AtomicData, self).__getattribute__(item)
 
--- tardis/atomic/plugins.py
+++ tardis/atomic/plugins.py
@@ -2,8 +2,10 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 import pandas as pd
 
 
-class BaseAtomicData(object):
+class BaseAtomicDataType(object):
     __metaclass__ = ABCMeta
+
+    _value = None
 
     @abstractproperty
     def hdf_name(self):
@@ -12,11 +14,18 @@ class BaseAtomicData(object):
 
     @abstractmethod
     def load_hdf(self, hdf_store):
-        raise NotImplementedError
+        self._value = self.hdf_name[self.hdf_name]
 
+    @property
+    def value(self):
+        if self._value is None:
+            raise ValueError('{0} is not available please load first'.format(
+                self.__class__.__name__))
+        else:
+            return self._value
 
     def save_hdf(self, hdf_store):
-        hdf_store.put(self.hdf_name, self.data, format='table',
+        hdf_store.put(self.hdf_name, self._value, format='table',
                       data_columns=True)
 
 
@@ -25,14 +34,15 @@ class BaseAtomicData(object):
         return getattr(self, '_name', self.hdf_name)
 
 
-class Atoms(BaseAtomicData):
+class Atoms(BaseAtomicDataType):
     hdf_name = 'atoms'
 
-class Ions(BaseAtomicData):
+
+class Ions(BaseAtomicDataType):
     hdf_name = 'ions'
 
-class Levels(BaseAtomicData):
+class Levels(BaseAtomicDataType):
     hdf_name = 'levels'
 
-class Lines(BaseAtomicData):
+class Lines(BaseAtomicDataType):
     hdf_name = 'lines'
