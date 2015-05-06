32794139e113a91de02ad82d0618212559a46f7b
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-05-06 12:24:42 +0200

    new data classes for atomic data

--- tardis/atomic/plugins.py
+++ tardis/atomic/plugins.py
@@ -2,7 +2,7 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 import pandas as pd
 
 
-class BaseAtomicDataPlugin(object):
+class BaseAtomicData(object):
     __metaclass__ = ABCMeta
 
     @abstractproperty
@@ -25,47 +25,14 @@ class BaseAtomicDataPlugin(object):
         return getattr(self, '_name', self.hdf_name)
 
 
-class Atoms():
-
+class Atoms(BaseAtomicData):
     hdf_name = 'atoms'
 
-
-
-
-
-class Ions(DB2HDF):
-
+class Ions(BaseAtomicData):
     hdf_name = 'ions'
 
-    def __init__(self, **kwargs):
-        self.exclude_species = kwargs.get('exclude_species', [])
-        self.max_ionization_energy = kwargs.get('max_ionization_energy', np.inf)
-
-
-    def load_sql(self):
-        ion_data = self.atomic_db.session.query(Ion, Atom).join("atom").values(
-            Atom.atomic_number, Ion.ion_number,
-            Ion.ionization_energy)
-        self.data = self._to_data_frame(ion_data)
-
-
-    def to_hdf(self, file_or_buf):
-        raise NotImplementedError()
-
-
-class Levels(DB2HDF):
-
+class Levels(BaseAtomicData):
     hdf_name = 'levels'
 
-    def __init__(self, exclude_species=[], max_ionization_energy=np.inf):
-        self.exclude_species = exclude_species
-        self.max_ionization_energy = max_ionization_energy
-
-    def load_sql(self):
-        self.data = None
-
-    def to_hdf(self, file_or_buf):
-        raise NotImplementedError()
-
-class MyData(BaseAtomicDataPlugin):
-    hdf_name = 'blah'
+class Lines(BaseAtomicData):
+    hdf_name = 'lines'
