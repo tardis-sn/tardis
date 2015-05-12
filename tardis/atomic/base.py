1bcd6876b10d90cd39c506c1f1377950046136b2
Christian Vogl <cvogl@mpa-garching.mpg.de>
2015-05-12 18:04:15 +0200

    Bug fix for the new Atomic

--- tardis/atomic/base.py
+++ tardis/atomic/base.py
@@ -9,9 +9,9 @@ class AtomicData(object):
     def from_hdf(cls, fname):
         return cls(pd.HDFStore(fname, mode='r'))
 
-    def __init__(self, hdf_store, data_types):
+    def __init__(self, hdf_store, data_types=self.all_data_types):
         self.data_types = [data_type()
-                        for data_type in self.all_data_types]
+                           for data_type in data_types]
 
         self.name2data_type = {item.name:item for item in self.data_types}
 
@@ -19,8 +19,34 @@ class AtomicData(object):
 
     def __getattr__(self, item):
         if item in self.name2data_type:
-            return self.name2date_type[item].data
+            return self.name2date_type[item].value
         else:
             super(AtomicData, self).__getattribute__(item)
+
+
+class AtomicDataLeg(AtomicData):
+    _atom_data = None
+
+    def __init__(self, hdf_store, data_types):
+        super_object = super(AtomicDataLeg, self)
+        super_object.__init__(hdf_store, data_types)
+
+
+    @property
+    def atom_data(self):
+        return  # The new atom data with old shape
+
+
+    @atom_data.setter
+    def atom_data(self, value):
+        """
+        This function returns the basic atomic data in the same way as the old atomic.py
+
+
+        :param value:
+        :return: numpy
+        """
+        # Do stuff with value
+        self._atom_data = value
 
 
