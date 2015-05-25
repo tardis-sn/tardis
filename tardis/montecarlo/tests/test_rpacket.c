f8b8a5204c3bb1daee80b7590e91fcef1d624268
kaushik94 <kaushik.varanasi1@gmail.com>
2015-05-26 04:59:11 +0530

    raising python assertion

--- tardis/montecarlo/tests/test_rpacket.c
+++ tardis/montecarlo/tests/test_rpacket.c
@@ -1,12 +1,15 @@
 #include "../src/rpacket.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <Python.h>
 
 void test_rpacket_get_nu(){
 	rpacket_t * rp = (rpacket_t *) malloc(sizeof(rpacket_t));
 	double value = 10.2;
 	rpacket_set_nu(rp, value);
-	assert(value == rpacket_get_nu(rp));
+	if( value != rpacket_get_nu(rp) ){
+		PyErr_Format(PyExc_AssertionError, "value %g not %g", value, rpacket_get_nu(rp));
+	}
 }
 
 int main(){
