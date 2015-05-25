eb29cac807f1c528ba21a3ed571e01bf71664b67
Aoife Boyle <aboyle19@qub.ac.uk>
2015-05-25 15:47:42 +0100

    Added atomic plasma property tests

--- tardis/plasma/tests/test_simple_plasma.py
+++ tardis/plasma/tests/test_simple_plasma.py
@@ -1,6 +1,14 @@
+import pytest
 from tardis.plasma.standard_plasmas import LTEPlasma
+import os
+import tardis
 
-
+@pytest.fixture
+def included_he_atomic_data():
+    from tardis import atomic
+    atomic_db_fname = os.path.join(tardis.__path__[0], 'tests', 'data',
+                                   'chianti_he_db.h5')
+    return atomic.AtomData.from_hdf5(atomic_db_fname)
 
 #def test_simple_networkx_test1(atomic_data, abundance, t_rad):
 #    modules = [TRadiative, BetaRadiation, LevelBoltzmannFactor,
