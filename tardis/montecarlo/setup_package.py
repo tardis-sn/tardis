3c8aab4611e7e478cfc04b78f33e985c30b734cf
kaushik94 <kaushik.varanasi1@gmail.com>
2015-05-26 03:05:53 +0530

    getter function

--- tardis/montecarlo/setup_package.py
+++ tardis/montecarlo/setup_package.py
@@ -8,12 +8,18 @@ from glob import glob
 
 def get_extensions():
     sources = ['tardis/montecarlo/montecarlo.pyx']
+    test_sources = []
     sources += [os.path.relpath(fname) for fname in glob(
         os.path.join(os.path.dirname(__file__), 'src', '*.c'))]
     sources += [os.path.relpath(fname) for fname in glob(
         os.path.join(os.path.dirname(__file__), 'src/randomkit', '*.c'))]
-
+    test_sources += [os.path.relpath(fname) for fname in glob(os.path.join(os.path.dirname(__file__), 'tests', '*.c'))]
     return [Extension('tardis.montecarlo.montecarlo', sources,
                       include_dirs=['tardis/montecarlo/src',
                                     'tardis/montecarlo/src/randomkit',
+                                    np.get_include()]),
+    		Extension('tardis.montecarlo.tests.test_cmontecarlo', test_sources,
+    				  include_dirs=['tardis/montecarlo/src',
+                                    'tardis/montecarlo/src/randomkit',
+                                    'tardis/montecarlo/tests',
                                     np.get_include()])]
