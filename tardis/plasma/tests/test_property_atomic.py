40ec50eafd9470aefc131378590a6227133e73c3
Aoife Boyle <aboyle19@qub.ac.uk>
2015-05-25 18:05:02 +0100

    Added atomic plasma property tests

--- tardis/plasma/tests/test_property_atomic.py
+++ tardis/plasma/tests/test_property_atomic.py
@@ -5,52 +5,45 @@ import tardis
 
 from tardis.plasma.properties.atomic import Levels, Lines, LinesLowerLevelIndex, LinesUpperLevelIndex, AtomicMass, IonizationData
 
-@pytest.fixture
-def included_he_atomic_data():
-    from tardis import atomic
-    atomic_db_fname = os.path.join(tardis.__path__[0], 'tests', 'data',
-                                   'chianti_he_db.h5')
-    return atomic.AtomData.from_hdf5(atomic_db_fname)
-
-def test_levels_property():
+def test_levels_property(included_he_atomic_data):
 
     levels_module = Levels(None)
     selected_atoms = [2]
-    levels = levels_module.calculate(included_he_atomic_data(), selected_atoms)
+    levels = levels_module.calculate(included_he_atomic_data, selected_atoms)
     assert np.isclose(levels.ix[2].ix[0].ix[1]['energy'], 3.17545416e-11)
 
-def test_lines_property():
+def test_lines_property(included_he_atomic_data):
 
     lines_module = Lines(None)
     selected_atoms = [2]
-    lines = lines_module.calculate(included_he_atomic_data(), selected_atoms)
+    lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     assert np.isclose(lines.ix[564954]['wavelength'], 10833.307)
 
-def test_lines_lower_level_index_property():
+def test_lines_lower_level_index_property(included_he_atomic_data):
     
     selected_atoms = [2]
     levels_module = Levels(None)
     lines_module = Lines(None)
-    levels = levels_module.calculate(included_he_atomic_data(), selected_atoms)
-    lines = lines_module.calculate(included_he_atomic_data(), selected_atoms)
+    levels = levels_module.calculate(included_he_atomic_data, selected_atoms)
+    lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     lines_lower_module = LinesLowerLevelIndex(None)
     lines_lower_level_index = lines_lower_module.calculate(levels, lines)
     assert lines_lower_level_index[9]==0
 
-def test_lines_upper_level_index_property():
+def test_lines_upper_level_index_property(included_he_atomic_data):
     
     selected_atoms = [2]
     levels_module = Levels(None)
     lines_module = Lines(None)
-    levels = levels_module.calculate(included_he_atomic_data(), selected_atoms)
-    lines = lines_module.calculate(included_he_atomic_data(), selected_atoms)
+    levels = levels_module.calculate(included_he_atomic_data, selected_atoms)
+    lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     lines_upper_module = LinesUpperLevelIndex(None)
     lines_upper_level_index = lines_upper_module.calculate(levels, lines)
     assert lines_upper_level_index[9]==30
 
-def test_ionization_data_property():
+def test_ionization_data_property(included_he_atomic_data):
     
     ionization_data_module = IonizationData(None)
     selected_atoms = [2]
-    ionization_data = ionization_data_module.calculate(included_he_atomic_data(), selected_atoms)
+    ionization_data = ionization_data_module.calculate(included_he_atomic_data, selected_atoms)
     assert np.isclose(float(ionization_data.ix[2].ix[1]),  3.9393336e-11)
--- tardis/plasma/tests/test_simple_plasma.py
+++ tardis/plasma/tests/test_simple_plasma.py
@@ -3,13 +3,6 @@ from tardis.plasma.standard_plasmas import LTEPlasma
 import os
 import tardis
 
-@pytest.fixture
-def included_he_atomic_data():
-    from tardis import atomic
-    atomic_db_fname = os.path.join(tardis.__path__[0], 'tests', 'data',
-                                   'chianti_he_db.h5')
-    return atomic.AtomData.from_hdf5(atomic_db_fname)
-
 #def test_simple_networkx_test1(atomic_data, abundance, t_rad):
 #    modules = [TRadiative, BetaRadiation, LevelBoltzmannFactor,
 #               AtomicLevels, AtomicLines, AtomicData, Abundance, SelectedAtoms,
@@ -22,5 +15,5 @@ def included_he_atomic_data():
 def test_simple_lte_plasma(included_he_atomic_data, abundance, t_rad, density,
                            time_explosion):
     lte_plasma = LTEPlasma(t_rad, abundance, density, time_explosion,
-                           included_he_atomic_data())
+                           included_he_atomic_data)
 
