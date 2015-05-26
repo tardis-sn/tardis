e13e0e1e5195410c058f2ea64fdfba8d8a6290b5
Aoife Boyle <aboyle19@qub.ac.uk>
2015-05-26 11:47:41 +0100

    Added atomic plasma property tests

--- tardis/plasma/tests/test_partition_function.py
+++ tardis/plasma/tests/test_partition_function.py
@@ -1,18 +1,21 @@
 import numpy as np
+import pytest
 
 from tardis.plasma.properties import LevelBoltzmannFactor, LTEPartitionFunction
 
+@pytest.mark.xfail
+def test_level_boltzmann_factor1(standard_lte_plasma_he_db):
+    level_boltzmann_factor_module = LevelBoltzmannFactor(None)
+    levels = standard_lte_plasma_he_db.levels
+    factor = level_boltzmann_factor_module.calculate(levels, 
+    standard_lte_plasma_he_db.beta_rad)
+    assert np.allclose(factor.loc[2, 0, 0], levels.loc[2, 0, 0].g)
+    assert np.allclose(factor.loc[2, 1, 0], levels.loc[2, 1, 0].g)
+    assert np.allclose(factor.loc[2, 0, 10], 7.6218092841240705e-12)
 
-#def test_level_boltzmann_factor1(standard_lte_plasma_he_db):
-#    level_boltzmann_factor_module = LevelBoltzmannFactor(None)
-#    levels = standard_lte_plasma_he_db.levels
-#    factor = level_boltzmann_factor_module.calculate(levels, standard_lte_plasma_he_db.beta_rad)
-#    assert np.allclose(factor.loc[2, 0, 0], levels.loc[2, 0, 0].g)
-#    assert np.allclose(factor.loc[2, 1, 0], levels.loc[2, 1, 0].g)
-#    assert np.allclose(factor.loc[2, 0, 10], 7.6218092841240705e-12)
-
-#def test_lte_partition_function1(standard_lte_plasma_he_db):
-#    partition_function = LTEPartitionFunction.calculate(
-#        standard_lte_plasma_he_db.levels,
-#        standard_lte_plasma_he_db.level_boltzmann_factor)
-#    assert 0
+@pytest.mark.xfail
+def test_lte_partition_function1(standard_lte_plasma_he_db):
+    partition_function = LTEPartitionFunction.calculate(
+        standard_lte_plasma_he_db.levels,
+        standard_lte_plasma_he_db.level_boltzmann_factor)
+    assert 0
--- tardis/plasma/tests/test_property_atomic.py
+++ tardis/plasma/tests/test_property_atomic.py
@@ -1,26 +1,25 @@
+import os
+
 import pytest
 import numpy as np
-import os
-import tardis
 
-from tardis.plasma.properties.atomic import Levels, Lines, LinesLowerLevelIndex, LinesUpperLevelIndex, AtomicMass, IonizationData
+import tardis
+from tardis.plasma.properties.atomic import (Levels, Lines, 
+LinesLowerLevelIndex, LinesUpperLevelIndex, AtomicMass, IonizationData)
 
 def test_levels_property(included_he_atomic_data):
-
     levels_module = Levels(None)
     selected_atoms = [2]
     levels = levels_module.calculate(included_he_atomic_data, selected_atoms)
     assert np.isclose(levels.ix[2].ix[0].ix[1]['energy'], 3.17545416e-11)
 
 def test_lines_property(included_he_atomic_data):
-
     lines_module = Lines(None)
     selected_atoms = [2]
     lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     assert np.isclose(lines.ix[564954]['wavelength'], 10833.307)
 
 def test_lines_lower_level_index_property(included_he_atomic_data):
-    
     selected_atoms = [2]
     levels_module = Levels(None)
     lines_module = Lines(None)
@@ -28,10 +27,9 @@ def test_lines_lower_level_index_property(included_he_atomic_data):
     lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     lines_lower_module = LinesLowerLevelIndex(None)
     lines_lower_level_index = lines_lower_module.calculate(levels, lines)
-    assert lines_lower_level_index[9]==0
+    assert lines_lower_level_index[9] == 0
 
 def test_lines_upper_level_index_property(included_he_atomic_data):
-    
     selected_atoms = [2]
     levels_module = Levels(None)
     lines_module = Lines(None)
@@ -39,11 +37,11 @@ def test_lines_upper_level_index_property(included_he_atomic_data):
     lines = lines_module.calculate(included_he_atomic_data, selected_atoms)
     lines_upper_module = LinesUpperLevelIndex(None)
     lines_upper_level_index = lines_upper_module.calculate(levels, lines)
-    assert lines_upper_level_index[9]==30
+    assert lines_upper_level_index[9] == 30
 
 def test_ionization_data_property(included_he_atomic_data):
-    
     ionization_data_module = IonizationData(None)
     selected_atoms = [2]
-    ionization_data = ionization_data_module.calculate(included_he_atomic_data, selected_atoms)
-    assert np.isclose(float(ionization_data.ix[2].ix[1]),  3.9393336e-11)
+    ionization_data = ionization_data_module.calculate(included_he_atomic_data,
+        selected_atoms)
+    assert np.isclose(float(ionization_data.ix[2].ix[1]), 3.9393336e-11)
