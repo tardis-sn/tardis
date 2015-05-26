6c07f2936ec3c1a9a38489346214508dcc220e51
Aoife Boyle <aboyle19@qub.ac.uk>
2015-05-26 12:18:24 +0100

    Added atomic plasma property tests

--- tardis/plasma/tests/test_simple_plasma.py
+++ tardis/plasma/tests/test_simple_plasma.py
@@ -1,19 +1,22 @@
-import pytest
-from tardis.plasma.standard_plasmas import LTEPlasma
 import os
+
+import pytest
+
 import tardis
+from tardis.plasma.standard_plasmas import LTEPlasma
 
-#def test_simple_networkx_test1(atomic_data, abundance, t_rad):
-#    modules = [TRadiative, BetaRadiation, LevelBoltzmannFactor,
-#               AtomicLevels, AtomicLines, AtomicData, Abundance, SelectedAtoms,
-#               AtomicMass]
-#    bp = BasePlasma(modules, t_rad=t_rad, atomic_data=atomic_data,
-#                    abundance=abundance)
-#    assert bp.t_rad[0] == 5000
+@pytest.mark.xfail
+def test_simple_networkx_test1(atomic_data, abundance, t_rad):
+    modules = [TRadiative, BetaRadiation, LevelBoltzmannFactor,
+               AtomicLevels, AtomicLines, AtomicData, Abundance, SelectedAtoms,
+               AtomicMass]
+    bp = BasePlasma(modules, t_rad=t_rad, atomic_data=atomic_data,
+                    abundance=abundance)
+    assert bp.t_rad[0] == 5000
 
-
-#def test_simple_lte_plasma(included_he_atomic_data, abundance, t_rad, density,
-#                           time_explosion):
-#    lte_plasma = LTEPlasma(t_rad, abundance, density, time_explosion,
-#                           included_he_atomic_data)
+@pytest.mark.xfail
+def test_simple_lte_plasma(included_he_atomic_data, abundance, t_rad, density,
+                           time_explosion):
+    lte_plasma = LTEPlasma(t_rad, abundance, density, time_explosion,
+                           included_he_atomic_data)
 
