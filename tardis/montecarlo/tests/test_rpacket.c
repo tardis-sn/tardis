3b16b688c7c90580546dadc1013ec08e583884eb
kaushik94 <kaushik.varanasi1@gmail.com>
2015-05-28 19:48:00 +0530

    C API

--- tardis/montecarlo/tests/test_rpacket.c
+++ tardis/montecarlo/tests/test_rpacket.c
@@ -5,17 +5,12 @@
 #include "../src/rpacket.h"
 
 
-void test_rpacket_get_nu(){
+static PyObject *
+test_rpacket_get_nu(){
 	rpacket_t * rp = (rpacket_t *) malloc(sizeof(rpacket_t));
 	double value = 10.2;
 	rpacket_set_nu(rp, value);
 	if( value != rpacket_get_nu(rp) ){
 		PyErr_Format(PyExc_AssertionError, "value %g not %g", value, rpacket_get_nu(rp));
 	}
-}
-
-int main(){
-
-	test_rpacket_get_nu();
-	return 0;
 }
