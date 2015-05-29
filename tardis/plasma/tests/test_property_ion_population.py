c927b64f1e5c7676cc8673980d418fbe87037841
Aoife Boyle <aboyle19@qub.ac.uk>
2015-05-29 12:51:51 +0100

    Finished plasma property tests for now.

--- tardis/plasma/tests/test_property_ion_population.py
+++ tardis/plasma/tests/test_property_ion_population.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-def test_phi_saha_lte(t_rad, beta_rad, g_electron, ionization_data,
+def test_phi_saha_lte(beta_rad, g_electron, ionization_data,
         phi_saha_lte):
     assert(phi_saha_lte.shape == (2,20))
     assert np.all(np.isclose(g_electron * 4 * np.exp(
         -ionization_data.ionization_energy.ix[2].ix[1] * beta_rad),
         phi_saha_lte.ix[2].ix[1]))
 
-def test_ion_number_density(phi_saha_lte, ion_number_density):
-    ion_numbers = ion_number_density.index.get_level_values(1).values
+def test_ion_number_density(phi_saha_lte, ion_number_density_lte):
+    ion_numbers = ion_number_density_lte.index.get_level_values(1).values
     ion_numbers = ion_numbers.reshape((ion_numbers.shape[0], 1))
-    n_electron_from_ions = (ion_number_density.values * ion_numbers).sum(
+    n_electron_from_ions = (ion_number_density_lte.values * ion_numbers).sum(
         axis=0)
-    n_electron_from_saha = (ion_number_density.ix[2].ix[0]/
-        ion_number_density.ix[2].ix[1]) * phi_saha_lte.ix[2].ix[1]
+    n_electron_from_saha = (ion_number_density_lte.ix[2].ix[0]/
+        ion_number_density_lte.ix[2].ix[1]) * phi_saha_lte.ix[2].ix[1]
     tolerance = 0.01 * n_electron_from_ions
     assert np.allclose(n_electron_from_ions, n_electron_from_saha,
         atol=tolerance)
--- tardis/plasma/tests/test_property_level_population.py
+++ tardis/plasma/tests/test_property_level_population.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-from tardis.plasma.properties.level_population import (LevelPopulationLTE,
-LevelNumberDensity)
-
 def test_level_population_lte(level_population_lte):
     assert np.allclose(level_population_lte.ix[2].ix[0].ix[0], 1.0)
     assert np.allclose(level_population_lte.ix[2].ix[0].sum(), 1.0)
 
-def test_level_number_density(level_population_lte, ion_number_density):
-    level_number_density_module = LevelNumberDensity(None)
-    level_number_density = level_number_density_module.calculate(
-        level_population_lte, ion_number_density)
-    assert np.allclose(level_number_density.ix[2].ix[0].ix[0],
-        ion_number_density.ix[2].ix[0])
-    assert np.allclose(level_number_density.sum(), ion_number_density.sum())
+def test_level_number_density_lte(level_number_density_lte,
+    ion_number_density_lte):
+    assert np.allclose(level_number_density_lte.ix[2].ix[0].ix[0],
+        ion_number_density_lte.ix[2].ix[0])
+    assert np.allclose(level_number_density_lte.sum(),
+        ion_number_density_lte.sum())
