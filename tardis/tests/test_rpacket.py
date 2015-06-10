e24ace01d1d2dd5cb2a9ce97c4e9bc502cc3f3ed
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2015-06-10 10:14:46 -1000

    converted the tests to fixtures tests

--- tardis/tests/test_rpacket.py
+++ tardis/tests/test_rpacket.py
@@ -12,87 +12,107 @@ test_path = os.path.join(path[0], 'montecarlo', 'montecarlo.so')
 
 tests = CDLL(test_path)
 
-np.random.seed(26061963)
+SEED = 26061963
 
+def get_random_float(size=10):
+    from sys import float_info as floats
+    MAX_FLOAT, MIN_FLOAT = floats[0], floats[3]
+    np.random.seed(SEED)
+    return np.random.uniform(MIN_FLOAT, MAX_FLOAT, size=size)
 
-@pytest.fixture(params=np.random.random(size=10))
+@pytest.fixture(params=get_random_float())
 def random_double_value(request):
-	return request.params
+    return c_double(request.param)
+
+
+def get_random_unsigned_int(size=10):
+    MAX_INT, MIN_INT = 10000, -10000
+    np.random.seed(SEED)
+    return np.random.randint(0, MAX_INT, size=size)
+
+@pytest.fixture(params=get_random_unsigned_int())
+def random_unsigned_int(request):
+    return request.param
+
+def get_random_int(size=10):
+    MAX_INT, MIN_INT = 10000, -10000
+    np.random.seed(SEED)
+    return np.random.randint(MIN_INT, MAX_INT, size=size)
+
+@pytest.fixture(params=get_random_int())
+def random_int(request):
+    return request.param
+
 
 
 
 # Testing functions with float(C double) valued parameters
 
 def test_rpacket_get_nu(random_double_value):
-	assert tests.test_rpacket_get_nu(random_double_value)
+    assert tests.test_rpacket_get_nu(random_double_value)
 
-@pytest.fixture
-def test_rpacket_get_mu(double_value):
-	assert tests.test_rpacket_get_mu(double_value)
 
-@pytest.fixture
-def test_rpacket_get_energy(double_value):
-	assert tests.test_rpacket_get_energy(double_value)
+def test_rpacket_get_mu(random_double_value):
+    assert tests.test_rpacket_get_mu(random_double_value)
 
-@pytest.fixture
-def test_rpacket_get_r(double_value):
-	assert tests.test_rpacket_get_r(double_value)
 
-@pytest.fixture
-def test_rpacket_get_tau_event(double_value):
-	assert tests.test_rpacket_get_tau_event(double_value)
+def test_rpacket_get_energy(random_double_value):
+    assert tests.test_rpacket_get_energy(random_double_value)
 
-@pytest.fixture
-def test_rpacket_get_nu_line(double_value):
-	assert tests.test_rpacket_get_nu_line(double_value)
 
-@pytest.fixture
-def test_rpacket_get_d_boundary(double_value):
-	assert tests.test_rpacket_get_d_boundary(double_value)
+def test_rpacket_get_r(random_double_value):
+    assert tests.test_rpacket_get_r(random_double_value)
 
-@pytest.fixture
-def test_rpacket_get_d_electron(double_value):
-	assert tests.test_rpacket_get_d_electron(double_value)
 
-@pytest.fixture
-def test_rpacket_get_d_line(double_value):
-	assert tests.test_rpacket_get_d_line(double_value)
+def test_rpacket_get_tau_event(random_double_value):
+    assert tests.test_rpacket_get_tau_event(random_double_value)
+
+
+def test_rpacket_get_nu_line(random_double_value):
+    assert tests.test_rpacket_get_nu_line(random_double_value)
+
+
+def test_rpacket_get_d_boundary(random_double_value):
+    assert tests.test_rpacket_get_d_boundary(random_double_value)
+
+
+def test_rpacket_get_d_electron(random_double_value):
+    assert tests.test_rpacket_get_d_electron(random_double_value)
+
+def test_rpacket_get_d_line(random_double_value):
+    assert tests.test_rpacket_get_d_line(random_double_value)
 
 
 # Testing functions with Unsigned Integer valued parameters
-@pytest.fixture
-def test_rpacket_get_current_shell_id(unsigned_int_value):
-	assert tests.test_rpacket_get_current_shell_id(unsigned_int_value)
 
-@pytest.fixture
-def test_rpacket_get_next_line_id(unsigned_int_value):
-	assert tests.test_rpacket_get_next_line_id(unsigned_int_value)
+def test_rpacket_get_current_shell_id(random_unsigned_int):
+    assert tests.test_rpacket_get_current_shell_id(random_unsigned_int)
+
+
+def test_rpacket_get_next_line_id(random_unsigned_int):
+    assert tests.test_rpacket_get_next_line_id(random_unsigned_int)
 
 
 # Testing functions with Integer valued parameters
-@pytest.fixture
-def test_rpacket_get_recently_crossed_boundary(int_value):
-	assert tests.test_rpacket_get_recently_crossed_boundary(int_value)
+def test_rpacket_get_recently_crossed_boundary(random_int):
+    assert tests.test_rpacket_get_recently_crossed_boundary(random_int)
 
-@pytest.fixture
-def test_rpacket_get_virtual_packet_flag(int_value):
-	assert tests.test_rpacket_get_virtual_packet_flag(int_value)
+def test_rpacket_get_virtual_packet_flag(random_int):
+    assert tests.test_rpacket_get_virtual_packet_flag(random_int)
 
-@pytest.fixture
-def test_rpacket_get_virtual_packet(int_value):
-	assert tests.test_rpacket_get_virtual_packet(int_value)
+def test_rpacket_get_virtual_packet(random_int):
+    assert tests.test_rpacket_get_virtual_packet(random_int)
 
-@pytest.fixture
-def test_rpacket_get_next_shell_id(int_value):
-	assert tests.test_rpacket_get_next_shell_id(int_value)
+def test_rpacket_get_next_shell_id(random_int):
+    assert tests.test_rpacket_get_next_shell_id(random_int)
 
 
 # Testing functions without any parameters
 def test_rpacket_get_last_line():
-	assert tests.test_rpacket_get_last_line()
+    assert tests.test_rpacket_get_last_line()
 
 def test_rpacket_get_close_line():
-	assert tests.test_rpacket_get_close_line()
+    assert tests.test_rpacket_get_close_line()
 
 def test_rpacket_get_status():
-	assert tests.test_rpacket_get_status()
+    assert tests.test_rpacket_get_status()
