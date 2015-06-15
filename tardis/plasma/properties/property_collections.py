f0b60341d16e2f0f885bd88f1bb254619aa5159a
Aoife Boyle <aboyle19@qub.ac.uk>
2015-06-15 20:08:38 +0100

    Added dilute-lte and nebular ionisation to the new plasma.

--- tardis/plasma/properties/property_collections.py
+++ tardis/plasma/properties/property_collections.py
@@ -1,24 +1,28 @@
-from tardis.plasma.properties import (BetaRadiation, LevelBoltzmannFactor,
-    Levels, Lines, AtomicMass, LTEPartitionFunction,
-    LevelPopulationLTE, LevelNumberDensity, PhiSahaLTE, GElectron,
+from tardis.plasma.properties import (BetaRadiation, LevelBoltzmannFactorLTE,
+    Levels, Lines, AtomicMass, PartitionFunction,
+    LevelPopulation, LevelNumberDensity, PhiSahaLTE, GElectron,
     IonizationData, NumberDensity, IonNumberDensity, LinesLowerLevelIndex,
     LinesUpperLevelIndex, TauSobolev, TRadiative, AtomicData, Abundance,
     Density, TimeExplosion, ElectronDensity, BetaSobolev, JBlues,
-    TransitionProbabilities, StimulatedEmissionFactor, SelectedAtoms)
+    TransitionProbabilities, StimulatedEmissionFactor, SelectedAtoms,
+    PhiGeneral, PhiSahaNebular, LevelBoltzmannFactorDiluteLTE, DilutionFactor,
+    ZetaData)
 
 class PlasmaPropertyCollection(list):
     pass
 
 basic_inputs = PlasmaPropertyCollection([TRadiative, Abundance, Density,
-    TimeExplosion, AtomicData, JBlues])
+    TimeExplosion, AtomicData, JBlues, DilutionFactor])
 basic_properties = PlasmaPropertyCollection([BetaRadiation,
-    LevelBoltzmannFactor, Levels, Lines, AtomicMass,
+    Levels, Lines, AtomicMass, LevelPopulation, PartitionFunction,
     GElectron, IonizationData, NumberDensity, LinesLowerLevelIndex,
     LinesUpperLevelIndex, TauSobolev, LevelNumberDensity, IonNumberDensity,
-    ElectronDensity, StimulatedEmissionFactor, SelectedAtoms])
-lte_ionization_properties = PlasmaPropertyCollection([LTEPartitionFunction,
-    PhiSahaLTE])
-lte_excitation_properties = PlasmaPropertyCollection([LTEPartitionFunction,
-    LevelPopulationLTE])
+    ElectronDensity, StimulatedEmissionFactor, SelectedAtoms, PhiGeneral])
+lte_ionization_properties = PlasmaPropertyCollection([PhiSahaLTE])
+lte_excitation_properties = PlasmaPropertyCollection([LevelBoltzmannFactorLTE])
 macro_atom_properties = PlasmaPropertyCollection([BetaSobolev,
     TransitionProbabilities])
+nebular_ionization_properties = PlasmaPropertyCollection([PhiSahaNebular,
+    ZetaData])
+dilute_lte_excitation_properties = PlasmaPropertyCollection([
+    LevelBoltzmannFactorDiluteLTE])
