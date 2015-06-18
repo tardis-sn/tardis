3a811b1192469d03ca134460704bc9ee4da3b286
Aoife Boyle <aboyle19@qub.ac.uk>
2015-06-18 13:16:22 +0100

    Added electron temperature property.

--- tardis/plasma/base.py
+++ tardis/plasma/base.py
@@ -183,7 +183,7 @@ class StandardPlasma(BasePlasma):
 
     def __init__(self, number_densities, atom_data, time_explosion,
                  delta_treatment=None, nlte_config=None, ionization_mode='lte',
-                 excitation_mode='lte', w=None):
+                 excitation_mode='lte', w=None, link_t_rad_t_electron=0.9):
 
         pass
 
