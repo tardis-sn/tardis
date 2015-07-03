f41718a359d87b6099bf30e79d00480a5591f817
Christian Vogl <cvogl@mpa-garching.mpg.de>
2015-07-03 20:07:44 +0200

    -migrated switch for turning off continuum processes from cmontecarlo.c to montecarlo.pyx -disabled passing of continuum dummy data if continuum processes are switched off

--- tardis/montecarlo/montecarlo.pyx
+++ tardis/montecarlo/montecarlo.pyx
@@ -22,6 +22,10 @@ cdef extern from "src/cmontecarlo.h":
         TARDIS_PACKET_STATUS_EMITTED = 1
         TARDIS_PACKET_STATUS_REABSORBED = 2
 
+    ctypedef enum ContinuumProccessesStatus:
+        CONTINUUM_OFF = 0
+        CONTINUUM_ON = 1
+
     ctypedef struct rpacket_t:
         double nu
         double mu
@@ -99,6 +103,7 @@ cdef extern from "src/cmontecarlo.h":
         double *t_electrons
         double *l_pop
         double *l_pop_r
+        ContinuumProccessesStatus cont_status
 
     int_type_t montecarlo_one_packet(storage_model_t *storage, rpacket_t *packet, int_type_t virtual_mode)
     int rpacket_init(rpacket_t *packet, storage_model_t *storage, int packet_index, int virtual_packet_flag)
@@ -164,17 +169,23 @@ def montecarlo_radial1d(model, int_type_t virtual_packet_flag=0):
     storage.electron_densities = <double*> electron_densities.data
     cdef np.ndarray[double, ndim=1] inverse_electron_densities = 1.0 / electron_densities
     storage.inverse_electron_densities = <double*> inverse_electron_densities.data
-    # Continuum list
-    cdef np.ndarray[double, ndim=1] continuum_list_nu = np.array(
-        [9.0e14, 8.223e14, 6.0e14, 3.5e14, 3.0e14])  # Dummy data
-    storage.continuum_list_nu = <double*> continuum_list_nu.data
-    storage.no_of_edges = continuum_list_nu.size
-    cdef np.ndarray[double, ndim =1] chi_bf_tmp_partial = np.zeros(continuum_list_nu.size)
-    storage.chi_bf_tmp_partial = <double*> chi_bf_tmp_partial.data
-    cdef np.ndarray[double, ndim=1] l_pop = np.ones(storage.no_of_shells * continuum_list_nu.size, dtype=np.float64)
-    storage.l_pop = <double*> l_pop.data
-    cdef np.ndarray[double, ndim=1] l_pop_r = np.ones(storage.no_of_shells * continuum_list_nu.size, dtype=np.float64)
-    storage.l_pop_r = <double*> l_pop_r.data
+    # Switch for continuum processes
+    storage.cont_status = CONTINUUM_OFF
+    # Continuum data
+    cdef np.ndarray[double, ndim=1] continuum_list_nu
+    cdef np.ndarray[double, ndim =1] chi_bf_tmp_partial
+    cdef np.ndarray[double, ndim=1] l_pop
+    cdef np.ndarray[double, ndim=1] l_pop_r
+    if storage.cont_status == CONTINUUM_ON:
+        continuum_list_nu = np.array([9.0e14, 8.223e14, 6.0e14, 3.5e14, 3.0e14])  # Dummy data
+        storage.continuum_list_nu = <double*> continuum_list_nu.data
+        storage.no_of_edges = continuum_list_nu.size
+        chi_bf_tmp_partial = np.zeros(continuum_list_nu.size)
+        storage.chi_bf_tmp_partial = <double*> chi_bf_tmp_partial.data
+        l_pop = np.ones(storage.no_of_shells * continuum_list_nu.size, dtype=np.float64)
+        storage.l_pop = <double*> l_pop.data
+        l_pop_r = np.ones(storage.no_of_shells * continuum_list_nu.size, dtype=np.float64)
+        storage.l_pop_r = <double*> l_pop_r.data
     # Line lists
     cdef np.ndarray[double, ndim=1] line_list_nu = model.atom_data.lines.nu.values
     storage.line_list_nu = <double*> line_list_nu.data
