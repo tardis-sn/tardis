5afd37813d291855b2c758a2b8d411ab7e020898
Aoife Boyle <aboyle19@qub.ac.uk>
2015-07-13 12:59:30 +0100

    Organisation of property files, improved graph.

--- tardis/plasma/properties/level_population.py
+++ tardis/plasma/properties/level_population.py
@@ -1,33 +1,33 @@
 import logging
 
-import numpy as np
-
 from tardis.plasma.properties.base import ProcessingPlasmaProperty
 
 logger = logging.getLogger(__name__)
 
 __all__ = ['LevelPopulationFraction', 'LevelNumberDensity']
 
-
 class LevelPopulationFraction(ProcessingPlasmaProperty):
-
-        outputs = ('level_population_fraction',)
-        latexformula = (r'$N_{i, j, k} = \frac{g_{i, j, k} '
+    """
+    Outputs:
+    level_population_fraction : Pandas DataFrame
+    """
+    outputs = ('level_population_fraction',)
+    latexformula = (r'$N_{i, j, k} = \frac{g_{i, j, k} '
                          r'e^{-\beta_\textrm{rad} E_{i, j, k}}}{Z_{i, j}}$')
 
-        @staticmethod
-        def calculate(levels, partition_function, level_boltzmann_factor):
-
-            partition_function_broadcast = partition_function.ix[
-                levels.index.droplevel(2)].values
-
-            return level_boltzmann_factor / partition_function_broadcast
+    def calculate(self, levels, partition_function, level_boltzmann_factor):
+        partition_function_broadcast = partition_function.ix[
+            levels.index.droplevel(2)].values
+        return level_boltzmann_factor / partition_function_broadcast
 
 class LevelNumberDensity(ProcessingPlasmaProperty):
+    """
+    Outputs:
+    level_number_density : Pandas DataFrame
+    """
     outputs = ('level_number_density',)
 
-    @staticmethod
-    def calculate(level_population_fraction, ion_number_density):
+    def calculate(self, level_population_fraction, ion_number_density):
         ion_number_density_broadcast = ion_number_density.ix[
             level_population_fraction.index.droplevel(2)].values
         return level_population_fraction * ion_number_density_broadcast
