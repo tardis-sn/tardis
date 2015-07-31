a8c38a650849171277cfa32949c64c528aa2f39c
Aoife Boyle <aboyle19@qub.ac.uk>
2015-07-31 10:33:00 +0100

    Combined NLTE options into single property.

--- tardis/plasma/properties/partition_function.py
+++ tardis/plasma/properties/partition_function.py
@@ -57,156 +57,12 @@ class LevelBoltzmannFactorNoNLTE(ProcessingPlasmaProperty):
     @staticmethod
     def calculate(general_level_boltzmann_factor):
         return general_level_boltzmann_factor
-'''
-class LevelBoltzmannFactorNLTECoronal(ProcessingPlasmaProperty):
 
-    outputs = ('level_boltzmann_factor',)
-
-    @staticmethod
-    def calculate(t_electron, lines, atomic_data, nlte_data,
-            general_level_boltzmann_factor, nlte_species,
-            previous_electron_densities):
-        """
-        Calculating the NLTE level populations for specific ions
-        """
-
-        beta_sobolevs = np.ones((len(lines), len(t_electron)))
-        j_blues = np.zeros((len(lines), len(t_electron)))
-
-        for species in nlte_species:
-            logger.info('Calculating rates for species %s', species)
-            number_of_levels = atomic_data.levels.energy.ix[species].count()
-
-            lnl = nlte_data.lines_level_number_lower[species]
-            lnu = nlte_data.lines_level_number_upper[species]
-
-            lines_index = nlte_data.lines_idx[species]
-            A_uls = nlte_data.A_uls[species]
-            B_uls = nlte_data.B_uls[species]
-            B_lus = nlte_data.B_lus[species]
-
-            r_lu_index = lnu * number_of_levels + lnl
-            r_ul_index = lnl * number_of_levels + lnu
-
-            r_ul_matrix = np.zeros((number_of_levels, number_of_levels,
-                len(t_electron)), dtype=np.float64)
-            r_ul_matrix_reshaped = r_ul_matrix.reshape((number_of_levels**2,
-                len(t_electron)))
-            r_ul_matrix_reshaped[r_ul_index] = A_uls[np.newaxis].T + \
-                B_uls[np.newaxis].T * j_blues[lines_index]
-            r_ul_matrix_reshaped[r_ul_index] *= beta_sobolevs[lines_index]
-
-            r_lu_matrix = np.zeros_like(r_ul_matrix)
-            r_lu_matrix_reshaped = r_lu_matrix.reshape((number_of_levels**2,
-                len(t_electron)))
-            r_lu_matrix_reshaped[r_lu_index] = B_lus[np.newaxis].T * \
-                j_blues[lines_index] * beta_sobolevs[lines_index]
-
-            if atomic_data.has_collision_data:
-                if previous_electron_densities is None:
-                    collision_matrix = r_ul_matrix.copy()
-                    collision_matrix.fill(0.0)
-                else:
-                    collision_matrix = nlte_data.get_collision_matrix(species,
-                        t_electron) * previous_electron_densities.values
-            else:
-                collision_matrix = r_ul_matrix.copy()
-                collision_matrix.fill(0.0)
-
-            rates_matrix = r_lu_matrix + r_ul_matrix + collision_matrix
-
-            for i in xrange(number_of_levels):
-                rates_matrix[i, i] = -rates_matrix[:, i].sum(axis=0)
-
-            rates_matrix[0, :, :] = 1.0
-
-            x = np.zeros(rates_matrix.shape[0])
-            x[0] = 1.0
-            for i in xrange(len(t_electron)):
-                level_boltzmann_factor = \
-                    np.linalg.solve(rates_matrix[:, :, i], x)
-                general_level_boltzmann_factor[i].ix[species] = \
-                    level_boltzmann_factor
-        return general_level_boltzmann_factor
-
-class LevelBoltzmannFactorNLTEClassicalNebular(ProcessingPlasmaProperty):
-    outputs = ('level_boltzmann_factor',)
-
-    @staticmethod
-    def calculate(t_electron, lines, atomic_data, nlte_data,
-            general_level_boltzmann_factor, nlte_species, j_blues,
-            previous_beta_sobolevs, lte_j_blues, previous_electron_densities):
-        """
-        Calculating the NLTE level populations for specific ions
-        """
-        beta_sobolevs = np.ones((len(lines), len(t_electron)))
-
-        if len(j_blues)==0:
-            j_blues = lte_j_blues
-        else:
-            j_blues = pd.DataFrame(j_blues, index=lines.index, columns =
-                range(len(t_electron)))
-
-        for species in nlte_species:
-            logger.info('Calculating rates for species %s', species)
-            number_of_levels = atomic_data.levels.energy.ix[species].count()
-
-            lnl = nlte_data.lines_level_number_lower[species]
-            lnu = nlte_data.lines_level_number_upper[species]
-
-            lines_index = nlte_data.lines_idx[species]
-            A_uls = nlte_data.A_uls[species]
-            B_uls = nlte_data.B_uls[species]
-            B_lus = nlte_data.B_lus[species]
-
-            j_blues_index = lines.index[lines_index]
-
-            r_lu_index = lnu * number_of_levels + lnl
-            r_ul_index = lnl * number_of_levels + lnu
-
-            r_ul_matrix = np.zeros((number_of_levels, number_of_levels,
-                len(t_electron)), dtype=np.float64)
-            r_ul_matrix_reshaped = r_ul_matrix.reshape((number_of_levels**2,
-                len(t_electron)))
-            r_ul_matrix_reshaped[r_ul_index] = A_uls[np.newaxis].T + \
-                B_uls[np.newaxis].T * j_blues.ix[j_blues_index]
-            r_ul_matrix_reshaped[r_ul_index] *= beta_sobolevs[lines_index]
-
-            r_lu_matrix = np.zeros_like(r_ul_matrix)
-            r_lu_matrix_reshaped = r_lu_matrix.reshape((number_of_levels**2,
-                len(t_electron)))
-            r_lu_matrix_reshaped[r_lu_index] = B_lus[np.newaxis].T * \
-                j_blues.ix[j_blues_index] * beta_sobolevs[lines_index]
-
-            if atomic_data.has_collision_data:
-                if previous_electron_densities is None:
-                    collision_matrix = r_ul_matrix.copy()
-                    collision_matrix.fill(0.0)
-                else:
-                    collision_matrix = nlte_data.get_collision_matrix(species,
-                        t_electron) * previous_electron_densities.values
-            else:
-                collision_matrix = r_ul_matrix.copy()
-                collision_matrix.fill(0.0)
-
-            rates_matrix = r_lu_matrix + r_ul_matrix + collision_matrix
-
-            for i in xrange(number_of_levels):
-                rates_matrix[i, i] = -rates_matrix[:, i].sum(axis=0)
-
-            rates_matrix[0, :, :] = 1.0
-
-            x = np.zeros(rates_matrix.shape[0])
-            x[0] = 1.0
-            for i in xrange(len(t_electron)):
-                level_boltzmann_factor = \
-                    np.linalg.solve(rates_matrix[:, :, i], x)
-                general_level_boltzmann_factor[i].ix[species] = \
-                    level_boltzmann_factor
-        return general_level_boltzmann_factor
-'''
 class LevelBoltzmannFactorNLTE(ProcessingPlasmaProperty):
     outputs = ('level_boltzmann_factor',)
+
+    def calculate(self):
+        pass
 
     def __init__(self, plasma_parent, classical_nebular=False,
         coronal_approximation=False):
@@ -219,64 +75,32 @@ class LevelBoltzmannFactorNLTE(ProcessingPlasmaProperty):
             self.calculate = self._calculate_general
         self._update_inputs()
 
-    def _calculate_classical_nebular(self, general_level_boltzmann_factor):
-        return general_level_boltzmann_factor
-    def _calculate_coronal_approximation(self, general_level_boltzmann_factor):
-        return general_level_boltzmann_factor
-    def _calculate_general(self, general_level_boltzmann_factor):
-        return general_level_boltzmann_factor
-    def _main_nlte_calculation(self):
-        pass
-'''
-    @staticmethod
-    def calculate(t_electron, lines, atomic_data, nlte_data,
-            general_level_boltzmann_factor, nlte_species, j_blues,
-            previous_beta_sobolevs, lte_j_blues, previous_electron_densities):
-        """
-        Calculating the NLTE level populations for specific ions
-        """
-        if previous_beta_sobolevs is None:
-            beta_sobolevs = np.ones((len(lines), len(t_electron)))
-        else:
-            beta_sobolevs = previous_beta_sobolevs
-
-        if len(j_blues)==0:
-            j_blues = lte_j_blues
-        else:
-            j_blues = pd.DataFrame(j_blues, index=lines.index, columns =
-                range(len(t_electron)))
-
+    def _main_nlte_calculation(self, nlte_species, atomic_data, nlte_data,
+        t_electron, j_blues, beta_sobolevs, general_level_boltzmann_factor,
+        previous_electron_densities):
         for species in nlte_species:
             logger.info('Calculating rates for species %s', species)
             number_of_levels = atomic_data.levels.energy.ix[species].count()
-
             lnl = nlte_data.lines_level_number_lower[species]
             lnu = nlte_data.lines_level_number_upper[species]
-
             lines_index = nlte_data.lines_idx[species]
             A_uls = nlte_data.A_uls[species]
             B_uls = nlte_data.B_uls[species]
             B_lus = nlte_data.B_lus[species]
-
-            j_blues_index = lines.index[lines_index]
-
             r_lu_index = lnu * number_of_levels + lnl
             r_ul_index = lnl * number_of_levels + lnu
-
             r_ul_matrix = np.zeros((number_of_levels, number_of_levels,
                 len(t_electron)), dtype=np.float64)
             r_ul_matrix_reshaped = r_ul_matrix.reshape((number_of_levels**2,
                 len(t_electron)))
             r_ul_matrix_reshaped[r_ul_index] = A_uls[np.newaxis].T + \
-                B_uls[np.newaxis].T * j_blues.ix[j_blues_index]
+                B_uls[np.newaxis].T * j_blues.ix[lines_index]
             r_ul_matrix_reshaped[r_ul_index] *= beta_sobolevs[lines_index]
-
             r_lu_matrix = np.zeros_like(r_ul_matrix)
             r_lu_matrix_reshaped = r_lu_matrix.reshape((number_of_levels**2,
                 len(t_electron)))
             r_lu_matrix_reshaped[r_lu_index] = B_lus[np.newaxis].T * \
-                j_blues.ix[j_blues_index] * beta_sobolevs[lines_index]
-
+                j_blues.ix[lines_index] * beta_sobolevs[lines_index]
             if atomic_data.has_collision_data:
                 if previous_electron_densities is None:
                     collision_matrix = r_ul_matrix.copy()
@@ -287,14 +111,10 @@ class LevelBoltzmannFactorNLTE(ProcessingPlasmaProperty):
             else:
                 collision_matrix = r_ul_matrix.copy()
                 collision_matrix.fill(0.0)
-
             rates_matrix = r_lu_matrix + r_ul_matrix + collision_matrix
-
             for i in xrange(number_of_levels):
                 rates_matrix[i, i] = -rates_matrix[:, i].sum(axis=0)
-
             rates_matrix[0, :, :] = 1.0
-
             x = np.zeros(rates_matrix.shape[0])
             x[0] = 1.0
             for i in xrange(len(t_electron)):
@@ -303,7 +123,51 @@ class LevelBoltzmannFactorNLTE(ProcessingPlasmaProperty):
                 general_level_boltzmann_factor[i].ix[species] = \
                     level_boltzmann_factor
         return general_level_boltzmann_factor
-'''
+
+    def _calculate_classical_nebular(self, t_electron, lines, atomic_data,
+        nlte_data, general_level_boltzmann_factor, nlte_species, j_blues,
+        previous_beta_sobolevs, lte_j_blues, previous_electron_densities):
+        beta_sobolevs = np.ones((len(lines), len(t_electron)))
+        if len(j_blues)==0:
+            j_blues = lte_j_blues
+        else:
+            j_blues = pd.DataFrame(j_blues, index=lines.index, columns =
+                range(len(t_electron)))
+        general_level_boltzmann_factor = self._main_nlte_calculation(
+            nlte_species, atomic_data, nlte_data, t_electron, j_blues,
+            beta_sobolevs, general_level_boltzmann_factor,
+            previous_electron_densities)
+        return general_level_boltzmann_factor
+
+    def _calculate_coronal_approximation(self, t_electron, lines, atomic_data,
+        nlte_data, general_level_boltzmann_factor, nlte_species,
+        previous_electron_densities):
+        beta_sobolevs = np.ones((len(lines), len(t_electron)))
+        j_blues = np.zeros((len(lines), len(t_electron)))
+        general_level_boltzmann_factor = self._main_nlte_calculation(
+            nlte_species, atomic_data, nlte_data, t_electron, j_blues,
+            beta_sobolevs, general_level_boltzmann_factor,
+            previous_electron_densities)
+        return general_level_boltzmann_factor
+
+    def _calculate_general(self, t_electron, lines, atomic_data, nlte_data,
+        general_level_boltzmann_factor, nlte_species, j_blues,
+        previous_beta_sobolevs, lte_j_blues, previous_electron_densities):
+        if previous_beta_sobolevs is None:
+            beta_sobolevs = np.ones((len(lines), len(t_electron)))
+        else:
+            beta_sobolevs = previous_beta_sobolevs
+        if len(j_blues)==0:
+            j_blues = lte_j_blues
+        else:
+            j_blues = pd.DataFrame(j_blues, index=lines.index, columns =
+                range(len(t_electron)))
+        general_level_boltzmann_factor = self._main_nlte_calculation(
+            nlte_species, atomic_data, nlte_data, t_electron, j_blues,
+            beta_sobolevs, general_level_boltzmann_factor,
+            previous_electron_densities)
+        return general_level_boltzmann_factor
+
 class PartitionFunction(ProcessingPlasmaProperty):
     outputs = ('partition_function',)
     latex_outputs = '$Z_{i, j}$'
