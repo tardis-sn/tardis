975809fd01211f0f015cc806a97518d0af4fae41
Aoife Boyle <aboyle19@qub.ac.uk>
2015-08-12 16:08:44 +0100

    Re-added NLTE level population test.

--- tardis/plasma/tests/test_plasmas_full.py
+++ tardis/plasma/tests/test_plasmas_full.py
@@ -53,5 +53,13 @@ class TestPlasmas():
             np.loadtxt(data_path('plasma_comparison_nlte_trads.dat'),
                 unpack=True)
         new_plasma_t_rads = self.nlte_model.t_rads / u.Unit('K')
+        old_plasma_levels = \
+            np.loadtxt(data_path('plasma_comparison_nlte_levels.dat'),
+                unpack=True)
+        new_plasma_levels = \
+            self.lte_model.plasma_array.get_value(
+            'level_number_density').ix[2].ix[1][10].values
         np.testing.assert_allclose(
             new_plasma_t_rads, old_plasma_t_rads, atol=150)
+        np.testing.assert_allclose(
+            new_plasma_levels, old_plasma_levels, rtol=0.05)
