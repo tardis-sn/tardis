eb8f95f4fe5adc3c076b6954706e5e1c68c239cb
Aoife Boyle <aboyle19@qub.ac.uk>
2015-08-16 12:23:17 +0100

    Correction.

--- tardis/plasma/tests/test_property_nlte.py
+++ tardis/plasma/tests/test_property_nlte.py
@@ -9,6 +9,6 @@ def test_he_nlte_plasma(number_density, atomic_data, time_explosion,
         helium_treatment='recomb-nlte')
     he_nlte_plasma.update_radiationfield(t_rad, w, j_blues, nlte_config=None)
     assert np.all(he_nlte_plasma.get_value(
-        'level_number_density').ix[2].ix[0].ix[0])==0.0
+        'level_number_density').ix[2].ix[0].ix[0]==0.0)
     assert np.allclose(he_nlte_plasma.get_value(
         'ion_number_density').ix[2].ix[1], number_density.ix[2])
