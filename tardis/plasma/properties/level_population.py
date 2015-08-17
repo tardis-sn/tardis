366b4799d5fe45dc8ae8441f7154a16590a67c9f
Aoife Boyle <aboyle19@qub.ac.uk>
2015-08-17 12:51:25 +0100

    Removed repetition of level_number_density calculation.

--- tardis/plasma/properties/level_population.py
+++ tardis/plasma/properties/level_population.py
@@ -4,7 +4,7 @@ from tardis.plasma.properties.base import ProcessingPlasmaProperty
 
 logger = logging.getLogger(__name__)
 
-__all__ = ['LevelNumberDensity', 'LevelNumberDensityHeNLTE']
+__all__ = ['LevelNumberDensity']
 
 class LevelNumberDensity(ProcessingPlasmaProperty):
     """
@@ -15,7 +15,18 @@ class LevelNumberDensity(ProcessingPlasmaProperty):
     latex_name = ('N_{i,j,k}',)
     latex_formula = ('N_{i,j}\\dfrac{bf_{i,j,k}}{Z_{i,j}}',)
 
-    def calculate(self, level_boltzmann_factor, ion_number_density,
+    def calculate():
+        pass
+
+    def __init__(self, plasma_parent, helium_treatment='dilute-lte'):
+        super(LevelNumberDensity, self).__init__(plasma_parent)
+        if helium_treatment=='recomb-nlte':
+            self.calculate = self._calculate_helium_recomb_nlte
+        elif helium_treatment=='dilute-lte':
+            self.calculate = self._calculate_dilute_lte
+        self._update_inputs()
+
+    def _calculate_dilute_lte(self, level_boltzmann_factor, ion_number_density,
         levels, partition_function):
         partition_function_broadcast = partition_function.ix[
             levels.droplevel(2)].values
@@ -25,24 +36,9 @@ class LevelNumberDensity(ProcessingPlasmaProperty):
             level_population_fraction.index.droplevel(2)].values
         return level_population_fraction * ion_number_density_broadcast
 
-class LevelNumberDensityHeNLTE(ProcessingPlasmaProperty):
-    """
-    Outputs:
-    level_number_density : Pandas DataFrame
-    """
-    outputs = ('level_number_density',)
-    latex_name = ('N_{i,j,k}',)
-    latex_formula = ('N_{i,j}\\dfrac{bf_{i,j,k}}{Z_{i,j}}',)
-
-    def calculate(self, level_boltzmann_factor, ion_number_density,
-        levels, partition_function, helium_population):
-        partition_function_broadcast = partition_function.ix[
-            levels.droplevel(2)].values
-        level_population_fraction = level_boltzmann_factor /\
-            partition_function_broadcast
-        ion_number_density_broadcast = ion_number_density.ix[
-            level_population_fraction.index.droplevel(2)].values
-        level_number_density = \
-            level_population_fraction * ion_number_density_broadcast
+    def _calculate_helium_recomb_nlte(self, level_boltzmann_factor,
+        ion_number_density, levels, partition_function, helium_population):
+        level_number_density = self._calculate_dilute_lte(
+            level_boltzmann_factor, ion_number_density, levels,
+            partition_function)
         level_number_density.ix[2].update(helium_population)
-        return level_number_density
