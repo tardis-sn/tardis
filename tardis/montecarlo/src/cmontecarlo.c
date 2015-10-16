10912b081cab1025c05763b53410d1099fe09030
JSmyth94 <jsmyth78@qub.ac.uk>
2015-10-16 11:46:43 +0100

    f as function of v fix 3

--- tardis/montecarlo/src/cmontecarlo.c
+++ tardis/montecarlo/src/cmontecarlo.c
@@ -603,11 +603,11 @@ montecarlo_line_scatter (rpacket_t * packet, storage_model_t * storage,
   double random_clump = rk_double(mt_state); // Generates a random number to determine clumpiness.
   double f_0 = 0.4; // True "clumping" factor.
   double R = 50.0; // Ratio of cloud density to intercloud density
-  double v_inner = storage->v_inner;
+  double v_in = storage->*v_inner;
   double v_initial = 1.10000000e+09;
   double k = -2.0;
 
-  double f = f_0*pow((v_inner/v_initial),k);
+  double f = f_0*pow((v_in/v_initial),k);
 
   double tau_line =
     storage->line_lists_tau_sobolevs[line2d_idx];
