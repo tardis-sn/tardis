757d983ef16ee1ac038f0b1d1c1dfe9a8b8515a9
JSmyth94 <jsmyth78@qub.ac.uk>
2015-10-23 09:39:47 +0100

    Config master read check

--- tardis/io/config_reader.py
+++ tardis/io/config_reader.py
@@ -551,6 +551,8 @@ def parse_clumping_section(clumping_dict):
     else:
         clumping_config_dict['density_ratio'] = 1.0
 
+        print "Parse was successful, filling factor is: %d" % clumping_config_dict['filling_factor']
+
 
     return clumping_config_dict
 
