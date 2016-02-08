256efe8dcf2f242c760e66ecaa07a578d33d2a6f
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2016-02-08 11:11:26 +0100

    removed unnecessary FAIL macro in abbrev.h

--- tardis/montecarlo/src/abbrev.h
+++ tardis/montecarlo/src/abbrev.h
@@ -3,14 +3,12 @@
 
 #include <stdlib.h>
 
-#define FAIL() abort()
-
 /**
  * @brief safe malloc; checks for NULL and aborts if encountered
  */
 static inline void* safe_malloc(size_t size) {
   void *mem = malloc(size);
-  if (mem == NULL && size != 0) FAIL();
+  if (mem == NULL && size != 0) abort();
   return mem;
 }
 
@@ -19,7 +17,7 @@ static inline void* safe_malloc(size_t size) {
  */
 static inline void* safe_realloc(void *ptr, size_t size) {
   void *mem = realloc(ptr, size);
-  if (mem == NULL && size != 0) FAIL();
+  if (mem == NULL && size != 0) abort();
   return mem;
 }
 
