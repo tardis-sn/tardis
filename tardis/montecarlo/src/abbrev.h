533c81917d4db65c4cb4c84c8041773441a2b950
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2016-02-08 10:41:59 +0100

    minor update of docstrings in abbrev.h

--- tardis/montecarlo/src/abbrev.h
+++ tardis/montecarlo/src/abbrev.h
@@ -3,14 +3,18 @@
 
 #define FAIL() abort()
 
-/* safe malloc */
-
+/**
+ * @brief safe malloc; checks for NULL and aborts if encountered
+ */
 static inline void* safe_malloc(size_t size) {
   void *mem = malloc(size);
   if (mem == NULL && size != 0) FAIL();
   return mem;
 }
 
+/**
+ * @brief safe realloc; checks for NULL and aborts if encountered
+ */
 static inline void* safe_realloc(void *ptr, size_t size) {
   void *mem = realloc(ptr, size);
   if (mem == NULL && size != 0) FAIL();
