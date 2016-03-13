e7f4a7bfc98705d3f62d3b97f7c35668c00dbb51
Prabhjot Singh <prabhjot.nith@gmail.com>
2016-03-13 16:40:06 +0530

    to_hdf function written to MonteCarloRunner class
    as the first objective TEP-002

--- tardis/montecarlo/base.py
+++ tardis/montecarlo/base.py
@@ -117,6 +117,25 @@ class MontecarloRunner(object):
         return ['scatter', 'downbranch', 'macroatom'].index(
             line_interaction_type)
 
+    def to_hdf(self, path_or_buf):
+        """
+            path_or_buf may include path like "model/plasma/levelpopulation/hdf_file.h5"
+        """
+        filename = path_or_buf[(path_or_buf.rfind('/')+1):]
+        path = path_or_buf[:-(len(filename))]
+        
+        if not os.path.exists(path):
+            try:
+                os.makedirs(path)
+            except:
+                raise OSError("Can't create destination directory (%s)!" % (path)) 
+ 
+        try:
+            hdf = pd.HDFStore(filename)
+        except TypeError:
+            hdf = filename
+        
+        hdf.close()
 
     @property
     def output_nu(self):
