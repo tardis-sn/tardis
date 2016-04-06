2e5e5e704c8f479fddda6e3272ae47b7444b583a
Karan Desai <karandesai_96@live.com>
2016-04-06 13:16:04 +0530

    Unit tests for untested methods of tardis/atomic.py
    * Refactor data_path() and write its test.
    
    - Also, reorganization of import statements and
      minor PEP8 ixes around data_path.
    
    * Add/ refactor tests for 4 methods in atomic.py
    
    - Renamed all four tests as 'test_' + 'methodname'.
    
    - Refactored these methods:
        - test_read_basic_atom_data
        - test_read_ionization_data
        - test_read_levels_data
        - test_read_lines_data
    
    - These tests comprise of calling the methods and
      performing assertions with values of any one row
      of obtained astropy.table.Table
    
    * Fix test_read_lines_data, write two new tests.
    
    - Newly added tests are:
        - test_read_synpp_refs
        - test_read_zeta_data
    
    - Fixed assert statements in test_read_lines_data.
    
    * Write test for read_collision_data()
    
    * Write test for read_macro_atom_data()
    
    - Format of the test has been kept similar
      to the test_read_collision_data() which
      was added just before this commit.
    
    * Replace datapaths with fixtures.
    
    - Minor cosmetic changes: wrapping lines within 80
      characters for keeping code consistent with PEP8
      guidelines.

--- tardis/tests/test_atomic.py
+++ tardis/tests/test_atomic.py
@@ -1,30 +1,127 @@
-from tardis import atomic
-from numpy import testing
-import pytest
 import os
+import pytest
+from numpy import testing
+from tardis import atomic
 
-def test_atomic_h5_readin():
-    data = atomic.read_basic_atom_data(atomic.default_atom_h5_path)
+
+@pytest.fixture(scope="module")
+def default_atom_h5_path():
+    return atomic.default_atom_h5_path
+
+
+@pytest.fixture(scope="module")
+def chianti_he_db_h5_path():
+    return os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 'data', 'chianti_he_db.h5')
+
+
+def test_data_path():
+    data_path = atomic.data_path('test')
+    assert data_path.split('/')[-3:] == ['tardis', 'data', 'test']
+
+
+def test_read_basic_atom_data(default_atom_h5_path):
+    data = atomic.read_basic_atom_data(default_atom_h5_path)
     assert data['atomic_number'][13] == 14
     assert data['symbol'][13] == "Si"
-    si_mass = data['mass'][13]
-    testing.assert_almost_equal(si_mass, 28.085, decimal=4)
-    pass
+    testing.assert_almost_equal(data['mass'][13], 28.085, decimal=4)
 
-def test_ionization_h5_readin():
-    data = atomic.read_ionization_data(atomic.default_atom_h5_path)
-    hi_ionization = data['ionization_energy'][0]
-    testing.assert_almost_equal(hi_ionization, 13.59844, decimal=4)
 
-def test_levels_h5_readin():
-    data = atomic.read_levels_data(atomic.default_atom_h5_path)
+def test_read_ionization_data(default_atom_h5_path):
+    data = atomic.read_ionization_data(default_atom_h5_path)
+    assert data['atomic_number'][0] == 1
+    assert data['ion_number'][0] == 1
+    testing.assert_almost_equal(data['ionization_energy'][0], 13.59844, decimal=4)
+
+
+def test_read_levels_data(default_atom_h5_path):
+    data = atomic.read_levels_data(default_atom_h5_path)
     assert data['atomic_number'][4] == 14
     assert data['ion_number'][4] == 0
     assert data['level_number'][4] == 4
-    si_energy = data['energy'][4]
-    testing.assert_almost_equal(si_energy, 1.90865, decimal=4)
+    testing.assert_almost_equal(data['energy'][4], 1.90865, decimal=4)
     assert data['g'][4] == 1
     assert data['metastable'][4] == False
+
+
+def test_read_lines_data(default_atom_h5_path):
+    data = atomic.read_lines_data(default_atom_h5_path)
+    assert data['line_id'][0] == 8
+    assert data['atomic_number'][0] == 14
+    assert data['ion_number'][0] == 5
+    testing.assert_almost_equal(data['wavelength'][0], 66.772, decimal=4)
+    testing.assert_almost_equal(data['f_ul'][0], 0.02703, decimal=4)
+    testing.assert_almost_equal(data['f_lu'][0], 0.04054, decimal=4)
+    assert data['level_number_lower'][0] == 0.0
+    assert data['level_number_upper'][0] == 36.0
+
+
+def test_read_synpp_refs(chianti_he_db_h5_path):
+    data = atomic.read_synpp_refs(chianti_he_db_h5_path)
+    assert data['atomic_number'][0] == 1
+    assert data['ion_number'][0] == 0
+    testing.assert_almost_equal(data['wavelength'][0], 6562.7973633, decimal=4)
+    assert data['line_id'][0] == 564995
+
+
+def test_read_zeta_data(default_atom_h5_path, chianti_he_db_h5_path):
+    data = atomic.read_zeta_data(chianti_he_db_h5_path)
+    testing.assert_almost_equal(data[2000][1][1], 0.339, decimal=4)
+    testing.assert_almost_equal(data[2000][1][2], 0.000, decimal=4)
+
+    with pytest.raises(ValueError):
+        atomic.read_zeta_data(None)
+
+    with pytest.raises(IOError):
+        atomic.read_zeta_data('fakepath')
+
+    with pytest.raises(ValueError):
+        atomic.read_zeta_data(default_atom_h5_path)
+
+
+def test_read_collision_data(default_atom_h5_path, chianti_he_db_h5_path):
+    data = atomic.read_collision_data(chianti_he_db_h5_path)
+    assert data[0]['atomic_number'][0] == 2
+    assert data[0]['ion_number'][0] == 0
+    assert data[0]['level_number_upper'][0] == 18
+    assert data[0]['level_number_lower'][0] == 2
+    assert data[0]['g_ratio'][0] == 1.0
+    testing.assert_almost_equal(data[0]['delta_e'][0], 35484.251143, decimal=4)
+    assert data[1][0] == 2000.0
+    assert data[1][1] == 4000.0
+
+    with pytest.raises(ValueError):
+        atomic.read_zeta_data(None)
+
+    with pytest.raises(IOError):
+        atomic.read_zeta_data('fakepath')
+
+    with pytest.raises(ValueError):
+        atomic.read_zeta_data(default_atom_h5_path)
+
+
+def test_read_macro_atom_data(default_atom_h5_path, chianti_he_db_h5_path):
+    data = atomic.read_macro_atom_data(chianti_he_db_h5_path)
+    assert data[0]['atomic_number'][0] == 2
+    assert data[0]['ion_number'][0] == 0
+    assert data[0]['source_level_number'][0] == 0.0
+    assert data[0]['destination_level_number'][0] == 48.0
+    assert data[0]['transition_type'][0] == 1
+    assert data[0]['transition_probability'][0] == 0.0
+    assert data[0]['transition_line_id'][0] == 564957
+
+    assert data[1]['count_down'][0] == 0
+    assert data[1]['count_up'][0] == 7
+    assert data[1]['count_total'][0] == 7
+
+    with pytest.raises(ValueError):
+        atomic.read_macro_atom_data(None)
+
+    with pytest.raises(IOError):
+        atomic.read_macro_atom_data('fakepath')
+
+    with pytest.raises(ValueError):
+        atomic.read_macro_atom_data(default_atom_h5_path)
 
 
 def test_atom_levels():
