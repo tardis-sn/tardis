a21e5966d09ff954a87e8af39c0b5fcf68e87769
Karan Desai <karandesai_96@live.com>
2016-05-10 18:31:23 +0530

    Add tests_slow directory to setup_package.py
    - This makes sure that they are included in the
      directoy structure after the build is executed.

--- tardis/tests/setup_package.py
+++ tardis/tests/setup_package.py
@@ -1,4 +1,6 @@
 def get_package_data():
     return {
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.h5',
-                                            'data/*.dat', 'data/*.npy']}
+                                            'data/*.dat', 'data/*.npy',
+                                            'tests_slow/w7/*.yml',
+                                            'tests_slow/w7/*.dat']}
