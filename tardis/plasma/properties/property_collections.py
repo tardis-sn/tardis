aeabd1fac38501f9395674df0d46b4c164825f3e
Aoife Boyle <aboyle19@qub.ac.uk>
2016-05-31 10:21:05 +0200

    Undoing last 2 commits

--- tardis/plasma/properties/property_collections.py
+++ tardis/plasma/properties/property_collections.py
@@ -10,14 +10,13 @@ basic_properties = PlasmaPropertyCollection([BetaRadiation,
     Levels, Lines, AtomicMass, PartitionFunction,
     GElectron, IonizationData, NumberDensity, LinesLowerLevelIndex,
     LinesUpperLevelIndex, TauSobolev, IonNumberDensity,
-    StimulatedEmissionFactor, SelectedAtoms, ElectronTemperature,
-    RadiationFieldCorrection, ZetaData])
+    StimulatedEmissionFactor, SelectedAtoms, ElectronTemperature])
 lte_ionization_properties = PlasmaPropertyCollection([PhiSahaLTE])
 lte_excitation_properties = PlasmaPropertyCollection([LevelBoltzmannFactorLTE])
 macro_atom_properties = PlasmaPropertyCollection([BetaSobolev,
     TransitionProbabilities])
 nebular_ionization_properties = PlasmaPropertyCollection([PhiSahaNebular,
-    ZetaData, BetaElectron])
+    ZetaData, BetaElectron, RadiationFieldCorrection])
 dilute_lte_excitation_properties = PlasmaPropertyCollection([
     LevelBoltzmannFactorDiluteLTE])
 non_nlte_properties = PlasmaPropertyCollection([LevelBoltzmannFactorNoNLTE])
@@ -25,7 +24,8 @@ nlte_properties = PlasmaPropertyCollection([
     LevelBoltzmannFactorNLTE, NLTEData, LTEJBlues, PreviousElectronDensities,
     PreviousBetaSobolev, BetaSobolev])
 helium_nlte_properties = PlasmaPropertyCollection([HeliumNLTE,
-    ZetaData, BetaElectron, LevelNumberDensityHeNLTE])
+    RadiationFieldCorrection, ZetaData,
+    BetaElectron, LevelNumberDensityHeNLTE])
 helium_lte_properties = PlasmaPropertyCollection([LevelNumberDensity])
 helium_numerical_nlte_properties = PlasmaPropertyCollection([
     HeliumNumericalNLTE])
