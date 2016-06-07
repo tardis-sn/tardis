52115b223719ec9fd2637e91a9eedefdff19c713
Karan Desai <karandesai_96@live.com>
2016-06-07 23:01:57 +0530

    Add a check in pytest_unconfigure for integration tests.

--- tardis/tests/tests_slow/conftest.py
+++ tardis/tests/tests_slow/conftest.py
@@ -39,34 +39,36 @@ def pytest_configure(config):
 
 @remote_data
 def pytest_unconfigure(config):
-    # Html report created by pytest-html plugin is read here, uploaded to
-    # dokuwiki and finally deleted.
-    if dokuwiki_available:
-        githash = tardis.__githash__
-        report_content = open(config.option.htmlpath, 'rb').read()
-        report_content = report_content.replace("<!DOCTYPE html>", "")
+    integration_tests_configpath = config.getvalue("integration-tests")
+    if integration_tests_configpath is not None:
+        # Html report created by pytest-html plugin is read here, uploaded to
+        # dokuwiki and finally deleted.
+        if dokuwiki_available:
+            githash = tardis.__githash__
+            report_content = open(config.option.htmlpath, 'rb').read()
+            report_content = report_content.replace("<!DOCTYPE html>", "")
 
-        report_content = (
-            "Test executed on commit "
-            "[[https://www.github.com/tardis-sn/tardis/commit/{0}|{0}]]\n\n"
-            "{1}".format(githash, report_content)
-        )
+            report_content = (
+                "Test executed on commit "
+                "[[https://www.github.com/tardis-sn/tardis/commit/{0}|{0}]]\n\n"
+                "{1}".format(githash, report_content)
+            )
 
-        # These steps are already performed by `integration_tests_config` but
-        # all the fixtures are teared down and no longer usable, when this
-        # method is being called by pytest, hence they are called as functions.
-        integration_tests_config = config.option.integration_tests_config
-        try:
-            doku_conn = dokuwiki.DokuWiki(
-                url=integration_tests_config["dokuwiki"]["url"],
-                user=integration_tests_config["dokuwiki"]["username"],
-                password=integration_tests_config["dokuwiki"]["password"])
-        except gaierror, dokuwiki.DokuWikiError:
-            print "Dokuwiki connection not established, report upload failed!"
-        else:
-            # Upload report on dokuwiki. Temporary link due to prototyping purposes.
-            doku_conn.pages.set("reports:{0}".format(githash[:7]), report_content)
-            print "Uploaded report on Dokuwiki."
+            # These steps are already performed by `integration_tests_config` but
+            # all the fixtures are teared down and no longer usable, when this
+            # method is being called by pytest, hence they are called as functions.
+            integration_tests_config = config.option.integration_tests_config
+            try:
+                doku_conn = dokuwiki.DokuWiki(
+                    url=integration_tests_config["dokuwiki"]["url"],
+                    user=integration_tests_config["dokuwiki"]["username"],
+                    password=integration_tests_config["dokuwiki"]["password"])
+            except gaierror, dokuwiki.DokuWikiError:
+                print "Dokuwiki connection not established, report upload failed!"
+            else:
+                # Upload report on dokuwiki. Temporary link due to prototyping purposes.
+                doku_conn.pages.set("reports:{0}".format(githash[:7]), report_content)
+                print "Uploaded report on Dokuwiki."
 
     # Remove the local report file. Keeping the report saved on local filesystem
     # is not desired, hence deleted.
