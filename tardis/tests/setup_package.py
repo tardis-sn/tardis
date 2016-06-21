486ef748c83eae362e6bc4fe03b71dbc3c352b48
Karan Desai <karandesai_96@live.com>
2016-06-21 09:27:40 +0530

    Add config, abundances and densities profile for abn_tom setup.

--- tardis/tests/setup_package.py
+++ tardis/tests/setup_package.py
@@ -3,4 +3,6 @@ def get_package_data():
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.h5',
                                             'data/*.dat', 'data/*.npy',
                                             'tests_slow/w7/*.yml',
-                                            'tests_slow/w7/*.dat']}
+                                            'tests_slow/w7/*.dat',
+                                            'tests_slow/at/*.yml',
+                                            'tests_slow/at/*.dat']}
