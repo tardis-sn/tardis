c0f865dbced2e1452f83c42038b4c347e1fe60fa
Karan Desai <karandesai_96@live.com>
2016-06-23 21:21:32 +0530

    Modify setup_package.py to include setup dirs implicitly.

--- tardis/tests/setup_package.py
+++ tardis/tests/setup_package.py
@@ -2,7 +2,5 @@ def get_package_data():
     return {
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc', 'data/*.h5',
                                             'data/*.dat', 'data/*.npy',
-                                            'tests_slow/w7/*.yml',
-                                            'tests_slow/w7/*.dat',
-                                            'tests_slow/at/*.yml',
-                                            'tests_slow/at/*.dat']}
+                                            'tests_slow/*/*.yml',
+                                            'tests_slow/*/*.dat']}
