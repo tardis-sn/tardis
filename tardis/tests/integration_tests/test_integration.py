cbd1d02070ca525104d492921a19b320c811491a
Mikhail Mishin <mishinma1805@gmail.com>
2016-06-27 11:48:16 +0300

    fix import in test_w7

--- tardis/tests/integration_tests/test_integration.py
+++ tardis/tests/integration_tests/test_integration.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from numpy.testing import assert_allclose
 from astropy.tests.helper import assert_quantity_allclose
 
-from tardis.atomic import AtomData
+from tardis.io.atomic import AtomData
 from tardis.simulation import Simulation
 from tardis.io.config_reader import Configuration
 
@@ -93,7 +93,7 @@ class TestIntegration(object):
         )
 
         # Load atom data file separately, pass it for forming tardis config.
-        self.atom_data = AtomData.from_hdf5(atom_data_filepath)
+        self.atom_data = AtomData.from_hdf(atom_data_filepath)
 
         # Check whether the atom data file in current run and the atom data
         # file used in obtaining the reference data are same.
