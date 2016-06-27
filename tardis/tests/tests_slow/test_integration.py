ed4854e22d49b6e02db6d7ffb52c97e93f5ca5d2
Mikhail Mishin <mishinma1805@gmail.com>
2016-06-27 11:48:16 +0300

    fix import in test_w7

--- tardis/tests/tests_slow/test_integration.py
+++ tardis/tests/tests_slow/test_integration.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from numpy.testing import assert_allclose
 from astropy.tests.helper import assert_quantity_allclose
 
-from tardis.atomic import AtomData
+
+from tardis.io.atomic import AtomData
 from tardis.simulation.base import run_radial1d
 from tardis.model import Radial1DModel
 from tardis.io.config_reader import Configuration
