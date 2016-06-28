c80097b8db79ef80d264cad6d8f6568b2b307ae6
Fotis Tsamis <ftsamis@gmail.com>
2016-06-29 01:13:04 +0300

    Introduce the basics of the new model/base.py.

--- tardis/model/base.py
+++ tardis/model/base.py
@@ -1,13 +1,63 @@
+import numpy as np
+
+
 class Radial1DModel(object):
-    def __init__(self, velocity, density, abundance, t_rad,
-                 v_boundary_inner, v_boundary_outer, w=None):
+    def __init__(self, velocity, density, abundance, t_inner, t_radiative,
+                 v_boundary_inner, v_boundary_outer, time_explosion,
+                 dilution_factor=None):
         self.velocity = velocity
         self.density = density
         self.abundance = abundance
-        self.t_rad = t_rad
+        self.t_inner = t_inner
+        self.t_radiative = t_radiative
         self.v_boundary_inner = v_boundary_inner
         self.v_boundary_outer = v_boundary_outer
-        self.w = w
+        self.time_explosion = time_explosion
+        if dilution_factor is None:
+            self.dilution_factor = 0.5 * (1 - np.sqrt(
+                1 - (self.r_inner[0] ** 2 / self.r_middle ** 2).to(1).value))
+        else:
+            self.dilution_factor = dilution_factor
+
+    @property
+    def w(self):
+        return self.dilution_factor
+
+    @w.setter
+    def w(self, value):
+        self.dilution_factor = value
+
+    @property
+    def t_rad(self):
+        return self.t_radiative
+
+    @t_rad.setter
+    def t_rad(self, value):
+        self.t_radiative = value
+
+    @property
+    def r_inner(self):
+        return self.time_explosion * self.v_inner
+
+    @property
+    def r_outer(self):
+        return self.time_explosion * self.v_outer
+
+    @property
+    def r_middle(self):
+        return 0.5 * self.r_inner + 0.5 * self.r_outer
+
+    @property
+    def v_inner(self):
+        return self.velocity[:-1]
+
+    @property
+    def v_outer(self):
+        return self.velocity[1:]
+
+    @property
+    def v_middle(self):
+        return 0.5 * self.v_inner + 0.5 * self.v_outer
 
     @classmethod
     def from_config(cls, config):
