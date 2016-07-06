9b59071fb0616e90e69f491da02b796059484c95
Karan Desai <karandesai_96@live.com>
2016-07-06 09:14:19 +0530

    Add an argument parser for integration test runner.

--- tardis/tests/tests_slow/runner.py
+++ tardis/tests/tests_slow/runner.py
@@ -1,10 +1,22 @@
+import argparse
+import os
 import time
-import subprocess
+
+
+parser = argparse.ArgumentParser(description="Run slow integration tests")
+parser.add_argument("--yaml", dest="yaml_filepath",
+                    help="Path to YAML config file for integration tests.")
+parser.add_argument("--atomic-dataset", dest="atomic_dataset",
+                    help="Path to atomic dataset.")
+
+test_command = (
+    "python setup.py test --test-path=tardis/tests/tests_slow/test_integration.py "
+    "--args=\"-rs --integration-tests={0} --atomic-dataset={1} --remote-data\""
+)
 
 
 if __name__ == "__main__":
+    args = parser.parse_args()
     while True:
-        subprocess.call([
-            "python", "setup.py", "test", "--test-path=tardis/tests/test_util.py",
-        ])
+        os.system(test_command.format(args.yaml_filepath, args.atomic_dataset))
         time.sleep(20)
