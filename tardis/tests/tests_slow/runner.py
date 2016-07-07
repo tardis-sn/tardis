54249a1f91af3d0c462e5464d64635b9cd5ca168
Karan Desai <karandesai_96@live.com>
2016-07-07 09:44:53 +0530

    Use subprocess calls instead of os.system.

--- tardis/tests/tests_slow/runner.py
+++ tardis/tests/tests_slow/runner.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 import json
 import logging
-import os
+import subprocess
 import time
 
 import requests
@@ -12,15 +12,10 @@ from tardis import __githash__ as tardis_githash
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(description="Run slow integration tests")
-parser.add_argument("--yaml", dest="yaml_filepath",
+parser.add_argument("--integration-tests", dest="yaml_filepath",
                     help="Path to YAML config file for integration tests.")
 parser.add_argument("--atomic-dataset", dest="atomic_dataset",
                     help="Path to atomic dataset.")
-
-test_command = (
-    "python setup.py test --test-path=tardis/tests/tests_slow/test_integration.py "
-    "--args=\"-rs --integration-tests={0} --atomic-dataset={1} --remote-data\""
-)
 
 
 if __name__ == "__main__":
@@ -33,9 +28,17 @@ if __name__ == "__main__":
         gh_tardis_githash = gh_master_head_data['commit']['sha']
 
         if gh_tardis_githash != tardis_githash:
-            os.system("git pull origin master")
-            os.system(test_command.format(args.yaml_filepath,
-                                          args.atomic_dataset))
+            subprocess.call([
+                "git", "pull", "https://www.github.com/tardis-sn/tardis", "master"
+            ])
+
+            subprocess.call([
+                "python", "setup.py", "test",
+                "--test-path=tardis/tests/tests_slow/test_integration.py", "--args",
+                "-rs --integration-tests={0} --atomic-dataset={1} --remote-data".format(
+                    args.yaml_filepath, args.atomic_dataset
+                )
+            ])
         else:
             checked = datetime.datetime.now()
             logger.info("Up-to-date. Checked on {0} {1}".format(
