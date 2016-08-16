596c2eaa276019d26e57da9fad8319c8ca5ac4c9
Fotis Tsamis <ftsamis@gmail.com>
2016-08-16 15:03:46 +0300

    Simulation: Log the plasma state when advancing the simulation.

--- tardis/simulation/base.py
+++ tardis/simulation/base.py
@@ -1,6 +1,7 @@
 import time
 import logging
 import numpy as np
+import pandas as pd
 
 from tardis.montecarlo.base import MontecarloRunner
 from tardis.model.base import Radial1DModel
@@ -71,14 +72,21 @@ class Simulation(object):
 
         # calculate_next_plasma_state equivalent
         # FIXME: Should convergence strategy have its own class?
-        self.model.t_rad = self.damped_converge(
+        next_t_rad = self.damped_converge(
             self.model.t_rad, estimated_t_rad,
             self.convergence_strategy.t_rad.damping_constant)
-        self.model.w = self.damped_converge(
+        next_w = self.damped_converge(
             self.model.w, estimated_w, self.convergence_strategy.w.damping_constant)
-        self.model.t_inner = self.damped_converge(
+        next_t_inner = self.damped_converge(
             self.model.t_inner, estimated_t_inner,
             self.convergence_strategy.t_inner.damping_constant)
+
+        self.log_plasma_state(self.model.t_rad, self.model.w,
+                              self.model.t_inner, next_t_rad, next_w,
+                              next_t_inner)
+        self.model.t_rad = next_t_rad
+        self.model.w = next_w
+        self.model.t_inner = next_t_inner
 
         # model.calculate_j_blues() equivalent
         # model.update_plasmas() equivalent
@@ -128,6 +136,48 @@ class Simulation(object):
         logger.info("Simulation finished in {0:d} iterations "
                     "and took {1:.2f} s".format(
                         self.iterations_executed, time.time() - start_time))
+
+    def log_plasma_state(self, t_rad, w, t_inner, next_t_rad, next_w,
+                         next_t_inner, log_sampling=5):
+        """
+        Logging the change of the plasma state
+
+        Parameters
+        ----------
+        t_rad: ~astropy.units.Quanity
+            current t_rad
+        w: ~astropy.units.Quanity
+            current w
+        next_t_rad: ~astropy.units.Quanity
+            next t_rad
+        next_w: ~astropy.units.Quanity
+            next_w
+        log_sampling: ~int
+            the n-th shells to be plotted
+
+        Returns
+        -------
+
+        """
+
+        plasma_state_log = pd.DataFrame(index=np.arange(len(t_rad)),
+                                           columns=['t_rad', 'next_t_rad',
+                                                    'w', 'next_w'])
+        plasma_state_log['t_rad'] = t_rad
+        plasma_state_log['next_t_rad'] = next_t_rad
+        plasma_state_log['w'] = w
+        plasma_state_log['next_w'] = next_w
+
+        plasma_state_log.index.name = 'Shell'
+
+        plasma_state_log = str(plasma_state_log[::log_sampling])
+
+        plasma_state_log = ''.join(['\t%s\n' % item for item in
+                                    plasma_state_log.split('\n')])
+
+        logger.info('Plasma stratification:\n%s\n', plasma_state_log)
+        logger.info('t_inner {0:.3f} -- next t_inner {1:.3f}'.format(
+            t_inner, next_t_inner))
 
     def log_run_results(self, emitted_luminosity, absorbed_luminosity):
         logger.info("Luminosity emitted = {0:.5e} "
