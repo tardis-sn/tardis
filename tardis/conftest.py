91c256f9a5998c82914d426c09cffe711aba9d64
Mikhail Mishin <mishinma1805@gmail.com>
2016-08-18 17:42:41 +0300

    Change the default UUID

--- tardis/conftest.py
+++ tardis/conftest.py
@@ -56,7 +56,7 @@ try:
 except NameError:   # Needed to support Astropy <= 1.0.0
     pass
 
-DEFAULT_UUID = '06709f04d369f07c58e01a7d0f34bd10'
+DEFAULT_UUID = '94af35cb55d22ec5020914ea9409a1cb'
 
 # -------------------------------------------------------------------------
 # Initialization
@@ -90,7 +90,7 @@ def atomic_data_fname():
 @pytest.fixture
 def kurucz_atomic_data(atomic_data_fname):
     atomic_data = AtomData.from_hdf(atomic_data_fname)
-
+    
     if atomic_data.md5 != DEFAULT_UUID:
         pytest.skip('Need default Kurucz atomic dataset (md5="{}"'.format(DEFAULT_UUID))
     else:
