ee320fe8c9d4eaf90448bedb132c4179f9e1e01a
Karan Desai <karandesai_96@live.com>
2016-08-20 11:42:12 +0530

    Add LocalPlotSaver class to facilitate saving of plots locally.

--- tardis/tests/integration_tests/plot_helpers.py
+++ tardis/tests/integration_tests/plot_helpers.py
@@ -1,10 +1,11 @@
+import os
 import tempfile
 
 from pytest_html import extras
-import tardis
+from tardis import __githash__ as tardis_githash
 
 
-thumbnail_html = """
+thumbnail_html_remote = """
 <div class="image" style="float: left">
     <a href="#">
         <img src= "{dokuwiki_url}lib/exe/fetch.php?media=reports:{githash}:{name}.png" />
@@ -14,7 +15,7 @@ thumbnail_html = """
 
 
 class PlotUploader(object):
-    def __init__(self, request):
+    def __init__(self, request, dokuwiki_url):
         """A helper class to collect plots from integration tests and upload
         them to DokuWiki.
 
@@ -26,7 +27,7 @@ class PlotUploader(object):
         self.request = request
         self._plots = list()
         self.plot_html = list()
-        self.dokuwiki_url = self.request.config.dokureport.dokuwiki_url
+        self.dokuwiki_url = dokuwiki_url
 
     def add(self, plot, name):
         """Accept a plot figure and add it to ``self._plots``.
@@ -40,7 +41,7 @@ class PlotUploader(object):
         self._plots.append((plot, name))
 
     def upload(self, report):
-        """Upload the content in self._plots to dokuwiki.
+        """Upload the content of self._plots to dokuwiki.
 
         Parameters
         ----------
@@ -62,18 +63,89 @@ class PlotUploader(object):
             plot.savefig(plot_file.name)
 
             self.request.config.dokureport.doku_conn.medias.add(
-                "reports:{0}:{1}.png".format(tardis.__githash__[0:7], name),
+                "reports:{0}:{1}.png".format(tardis_githash[:7], name),
                 plot_file.name
             )
 
             self.plot_html.append(extras.html(
-                thumbnail_html.format(
+                thumbnail_html_remote.format(
                     dokuwiki_url=self.dokuwiki_url,
-                    githash=tardis.__githash__[0:7],
+                    githash=tardis_githash[:7],
                     name=name)
                 )
             )
             plot_file.close()
+
+    def get_extras(self):
+        """Return ``self.plot_html`` which is further added into html report.
+
+        Returns
+        -------
+        list
+             List of strings containing raw html snippet to embed images.
+        """
+        return self.plot_html
+
+
+thumbnail_html_local = """
+<div class="image" style="float: left">
+    <a href="#">
+        <img src= "assets/{name}.png" />
+    </a>
+</div>
+"""
+
+
+class LocalPlotSaver(object):
+    def __init__(self, request, assets_dirpath):
+        """A helper class to collect plots from integration tests and save
+        them in a particular directory locally.
+
+        Parameters
+        ----------
+        request : _pytest.python.RequestObject
+
+        """
+        self.request = request
+        self._plots = list()
+        self.plot_html = list()
+        self.assets_dirpath = assets_dirpath
+
+    def add(self, plot, name):
+        """Accept a plot figure and add it to ``self._plots``.
+
+        Parameters
+        ----------
+        plot : matplotlib.pyplot.figure
+        name : str
+
+        """
+        self._plots.append((plot, name))
+
+    def upload(self, report):
+        """Upload (save) content of ``self._plots`` to ``self.assets_dirpath``.
+
+        Parameters
+        ----------
+        report : _pytest.runner.TestReport
+
+        """
+
+        for plot, name in self._plots:
+            axes = plot.axes[0]
+
+            if report.passed:
+                axes.text(0.8, 0.8, 'passed', transform=axes.transAxes,
+                          bbox={'facecolor': 'green', 'alpha': 0.5, 'pad': 10})
+            else:
+                axes.text(0.8, 0.8, 'failed', transform=axes.transAxes,
+                          bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+
+            plot.savefig(os.path.join(self.assets_dirpath, "{0}.png".format(name)))
+
+            self.plot_html.append(extras.html(
+                thumbnail_html_local.format(name=name))
+            )
 
     def get_extras(self):
         """Return ``self.plot_html`` which is further added into html report.
