7a87fedad0d95f8769c057e37660d7a37a6a4d74
Tobychev <tobychev@live.com>
2016-08-30 18:13:59 +0200

    Refactored name of variable, added some constant factors to the integral

--- tardis/montecarlo/src/integrator.c
+++ tardis/montecarlo/src/integrator.c
@@ -14,24 +14,24 @@
 #define PLEN    2
 #define SHELLEN 3
 
-
+#define M_PI acos(-1.0)
 #define C_INV 3.33564e-11
 
-indexpair_t find_nu_limits_for_crossing_and_p(double nu, double p,int cr_idx, int no_of_shell_cr, double inv_t, double* Rs, double* line_nu, int len)
+indexpair_t find_nu_limits_for_crossing_and_p(double nu, double p, int cr_idx, int no_of_cr_shells, double inv_t, const double* Rs, const double* line_nu, int len)
 {
     double blu_R, red_R, z_blu, z_red, z_cr, nu_blu, nu_red;
     indexpair_t pair;
 
-    if (no_of_shell_cr > 1)
+    if (no_of_cr_shells > 1)
     {
         assert(Rs[0] > Rs[1]); // Decreasing order
         
-        blu_R = get_r(cr_idx-1,no_of_shell_cr,Rs);
-        red_R = get_r(cr_idx,no_of_shell_cr,Rs);
+        blu_R = get_r(cr_idx-1,no_of_cr_shells,Rs);
+        red_R = get_r(cr_idx,no_of_cr_shells,Rs);
         z_blu = sqrt( blu_R*blu_R - p*p );
         z_red = sqrt( red_R*red_R - p*p );
-        nu_blu = nu * (1 + get_cr_sign(cr_idx,no_of_shell_cr)*z_blu*C_INV*inv_t);
-        nu_red = nu * (1 + get_cr_sign(cr_idx,no_of_shell_cr)*z_red*C_INV*inv_t);
+        nu_blu = nu * (1 + get_cr_sign(cr_idx,no_of_cr_shells)*z_blu*C_INV*inv_t);
+        nu_red = nu * (1 + get_cr_sign(cr_idx,no_of_cr_shells)*z_red*C_INV*inv_t);
     }
     else 
     {
@@ -51,65 +51,66 @@ indexpair_t find_nu_limits_for_crossing_and_p(double nu, double p,int cr_idx, in
     return pair;
 }
 
-double get_r(int cr_idx, int no_of_shell_cr, double* Rs)
+double get_r(int cr_idx, int no_of_cr_shells, const double* Rs)
 {
-    if (cr_idx < no_of_shell_cr){
+    if (cr_idx < no_of_cr_shells){
         return Rs[cr_idx];}
-    else if (cr_idx < 2*no_of_shell_cr){
-        return Rs[(2*no_of_shell_cr - 1) - cr_idx];}
+    else if (cr_idx < 2*no_of_cr_shells){
+        return Rs[(2*no_of_cr_shells - 1) - cr_idx];}
 }
 
-int get_cr_sign(int cr_idx, int no_of_shell_cr)
+int get_cr_sign(int cr_idx, const int no_of_cr_shells)
 {
-    if (cr_idx < no_of_shell_cr){
+    if (cr_idx < no_of_cr_shells){
         return -1;}
-    else if (cr_idx < 2*no_of_shell_cr){
+    else if (cr_idx < 2*no_of_cr_shells){
         return 1;}
 }
 
-int get_cr_start(int no_of_shell_cr, double p, double R_ph)
+int get_cr_start(int no_of_cr_shells, double p, double R_ph)
 {
     if (p >= R_ph) {
         return 0;}
     else if (p < R_ph) {
-        return no_of_shell_cr+1;
+        return no_of_cr_shells+1;
     }
 }
 
-int get_sh_idx(int cr_idx, int no_of_shell_cr)
+int get_sh_idx(int cr_idx, int no_of_cr_shells)
 {
-    if (cr_idx < no_of_shell_cr){
+    if (cr_idx < no_of_cr_shells){
        return cr_idx;}
-    else if (cr_idx < 2*no_of_shell_cr){
-        return ((2*no_of_shell_cr - 1) - cr_idx);}
+    else if (cr_idx < 2*no_of_cr_shells){
+        return ((2*no_of_cr_shells - 1) - cr_idx);}
 
 }
-int get_num_shell_cr(double p, double* Rs, int len)
+int get_num_shell_cr(double p, const double* Rs, int len)
 {
     assert(Rs[0] > Rs[1]);
 
     int num;
-    for(int num = 0; num < len; ++num)
+    for(num = 0; num < len; ++num)
     {
         if( p > Rs[num]) {
             break;}
     }
+    printf("\n");
     return num;
 }
 
 
-double integrate_intensity(double* I_nu,double* ps, int len)
+double integrate_intensity(const double* I_nu, const double* ps, int len)
 {
     double result = 0.0;
     double h = (ps[0]-ps[len-1])/len;
-    result =  I_nu[0]*h/2.0;
+    result =  I_nu[0]*h*ps[0]/2.0;
 
     for (int idx = 1; idx < len-1; ++idx)
     {
-        result += I_nu[idx]*h;
+        result += I_nu[idx]*h*ps[idx];
     }
-    result += I_nu[len-1]*h/2.0;
-    return result;
+    result += I_nu[len-1]*h*ps[len-1]/2.0;
+    return result*8*M_PI*M_PI;
 }
 
 void debug_print_arg(double* arg,int len)
@@ -131,13 +132,12 @@ void debug_print_2d_arg(double* arg,int len1, int len2)
     }
 }
 
-
-void integrate_source_functions(double* L_nu, double* line_nu, double* taus, double* att_S_ul, double* I_BB, double* nus, 
-              double* ps, double* Rs, double R_ph, int64_t* lens)
+void integrate_source_functions(double* L_nu, const double* line_nu, const double* taus, const double* att_S_ul, const double* I_BB, 
+        const double* nus, const double* ps, const double* Rs, double R_ph, const int64_t* lens)
 {
     double* I_nu  = calloc(lens[PLEN], sizeof(double));
     double  inv_t = 1.8432e+42;
-    int no_of_shell_cr;
+    int no_of_cr_shells;
     indexpair_t nu_lims;
     for (int nu_idx = 0; nu_idx < lens[NULEN]; ++nu_idx)
     {
@@ -148,18 +148,21 @@ void integrate_source_functions(double* L_nu, double* line_nu, double* taus, dou
             {
                 I_nu[p_idx] = I_BB[nu_idx];
             }           
-            no_of_shell_cr = get_num_shell_cr(ps[p_idx],Rs,lens[SHELLEN]);
-            for (int cr_idx = get_cr_start(no_of_shell_cr, ps[p_idx], R_ph); cr_idx < 2*no_of_shell_cr; ++cr_idx)
+            no_of_cr_shells = get_num_shell_cr(ps[p_idx],Rs,lens[SHELLEN]);
+            printf("no_sh %d\n", no_of_cr_shells);
+            printf("start %d, end %d\n", get_cr_start(no_of_cr_shells, ps[p_idx], R_ph),  2*no_of_cr_shells);
+            for (int cr_idx = get_cr_start(no_of_cr_shells, ps[p_idx], R_ph); cr_idx < 2*no_of_cr_shells; ++cr_idx)
             {
-                nu_lims = find_nu_limits_for_crossing_and_p(nus[nu_idx], ps[p_idx], cr_idx, no_of_shell_cr, inv_t, Rs, line_nu, lens[LINELEN]);
+                nu_lims = find_nu_limits_for_crossing_and_p(nus[nu_idx], ps[p_idx], cr_idx, no_of_cr_shells, inv_t, Rs, line_nu, lens[LINELEN]);
+                printf("Inner loop from %d to %d; ",nu_lims.start,nu_lims.end);
                 for (int k_idx = nu_lims.start; k_idx < nu_lims.end; ++k_idx)
                 {
-                    I_nu[p_idx] = I_nu[p_idx] * exp(-taus[k_idx * lens[SHELLEN] + get_sh_idx(cr_idx,no_of_shell_cr)]) 
-                                    + att_S_ul[k_idx * lens[SHELLEN] + get_sh_idx(cr_idx,no_of_shell_cr)];
+                    I_nu[p_idx] = I_nu[p_idx] * exp(-taus[k_idx * lens[SHELLEN] + get_sh_idx(cr_idx,no_of_cr_shells)]) 
+                                    + att_S_ul[k_idx * lens[SHELLEN] + get_sh_idx(cr_idx,no_of_cr_shells)];
                 }
             }
         }
         L_nu[nu_idx] = integrate_intensity(I_nu, ps, lens[PLEN]); 
     }
-    debug_print_arg(L_nu, lens[NULEN]);
+    printf("\n\n");
 }
