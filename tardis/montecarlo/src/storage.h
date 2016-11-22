2fc4884d9a5b212d603e11931dd434c0c6ed3303
Christian Vogl <cvogl@mpa-garching.mpg.de>
2016-11-22 16:14:41 +0100

    Add correct handling of bound-free absorption events

--- tardis/montecarlo/src/storage.h
+++ tardis/montecarlo/src/storage.h
@@ -78,6 +78,7 @@ typedef struct StorageModel
   int64_t virt_packet_count;
   int64_t virt_array_size;
   int64_t kpacket2macro_level;
+  int64_t *cont_edge2macro_level;
   double *photo_ion_estimator;
   double *stim_recomb_estimator;
   int64_t *photo_ion_estimator_statistics;
