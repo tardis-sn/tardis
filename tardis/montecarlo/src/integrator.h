632480912874a174f35ae55a51b81eb726455659
yeganer <yeganer@gmx.de>
2017-05-19 14:38:21 +0200

    Apply coding standards for identation

--- tardis/montecarlo/src/integrator.h
+++ tardis/montecarlo/src/integrator.h
@@ -3,32 +3,14 @@
 
 #include "storage.h"
 
-typedef struct IndexPair
-{
-    int start;
-    int end;
-} indexpair_t;
+double
+integrate_intensity(const double* I_nu, const double h, int N);
 
-indexpair_t find_nu_limits_for_crossing_and_p(double nu, double p, int cr_idx, int no_of_cr_shells, double inv_t, const double* Rs, const double* line_nu, int len);
+int64_t
+populate_z(const storage_model_t *storage, const double p, double *oz, int64_t *oshell_id);
 
-int get_cr_sign(int cr_idx, int no_of_cr_shells);
-int get_cr_start(int no_of_cr_shells, double p, double R_ph);
-int get_sh_idx(int cr_idx, int no_of_cr_shells);
-int get_num_shell_cr(double p, const double* Rs, int len);
-
-double get_r(int cr_idx, int no_of_cr_shells, const double* Rs);
-double integrate_intensity(const double* I_nu, const double h, int N);
-
-void debug_print_arg(double* arg,int len);
-void debug_print_2d_arg(double* arg,int len1, int len2);
-void integrate_source_functions(double* L_nu, const double* line_nu, const double* taus, const double* att_S_ul, const double* I_BB, 
-        const double* nus, const double* ps, const double* Rs, double R_ph, const int64_t* lens);
-
-double *_formal_integral(
-        storage_model_t *storage, double T, double *nu, int64_t nu_size, double *att_S_ul, int N);
-/*
-double recursive_update_luminosity(
-    double nu_start, double nu_end, double *line_nu, double *tau, double *att_S_ul, double initial);
-    */
+double
+*_formal_integral(
+                  storage_model_t *storage, double T, double *nu, int64_t nu_size, double *att_S_ul, int N);
 
 #endif
