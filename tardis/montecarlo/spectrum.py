d07c86edae1c4f722fad5959189c640ca2814d91
yeganer <yeganer@gmx.de>
2017-06-03 15:15:29 +0200

    Refactor TARDISSpectrum
    The refactor changes the way a spectrum is handled. Instead of creating
    a spectrum and updating it after each montecarlo iteration, we create a
    new spectrum from the MontecarloRunner whenever it is needed.
    
    Benefits:
    - TARDISSpectrum is now a general purpose spectrum class that is not
    linked to the Iteration sheme anymore.
    - It is now possible to save spectra for all iterations without them
    being updated each iteration.
    - This class can now also be used for spectra created by the formal
    integral approach (upcoming PR #726)

--- tardis/montecarlo/spectrum.py
+++ tardis/montecarlo/spectrum.py
@@ -5,58 +5,70 @@ from astropy import constants, units as u
 from tardis.io.util import to_hdf
 
 
+def require_field(name):
+    def _require_field(f):
+        def wrapper(self, *args, **kwargs):
+            if not getattr(self, name, None):
+                raise AttributeError(
+                        '{} is required as attribute of {} to calculate {}'.format(
+                            name,
+                            self.__class__.__name__,
+                            f.__name__
+                            )
+                        )
+            else:
+                return f(self, *args, **kwargs)
+        return wrapper
+    return _require_field
+
+
 class TARDISSpectrum(object):
     """
     TARDIS Spectrum object
     """
 
-    def __init__(self, frequency, distance=None):
-        self._frequency = frequency
-        self.wavelength = self.frequency.to('angstrom', u.spectral())
-
+    def __init__(self, frequency, luminosity, distance=None):
         self.distance = distance
 
-
-
+        self._frequency = frequency
         self.delta_frequency = frequency[1] - frequency[0]
+        self.wavelength = self.frequency.to('angstrom', u.spectral())
 
-        self._flux_nu = np.zeros_like(frequency.value) * u.Unit('erg / (s Hz cm^2)')
-        self._flux_lambda = np.zeros_like(frequency.value) * u.Unit('erg / (s Angstrom cm^2)')
+        self._luminosity = luminosity
 
-        self.luminosity_density_nu = np.zeros_like(self.frequency) * u.Unit('erg / (s Hz)')
-        self.luminosity_density_lambda = np.zeros_like(self.frequency) * u.Unit('erg / (s Angstrom)')
+        self.luminosity_density_nu = (
+                luminosity / self.delta_frequency).to('erg / (s Hz)')
+        self.luminosity_density_lambda = u.Quantity(
+                self.f_nu_to_f_lambda(self.luminosity_density_nu.value),
+                'erg / (s Angstrom)'
+                )
+
+        if self.distance is not None:
+            self._flux_nu = (
+                    self.luminosity_density_nu /
+                    (4 * np.pi * self.distance.to('cm')**2))
+
+            self._flux_lambda = u.Quantity(
+                    self.f_nu_to_f_lambda(self.flux_nu.value),
+                    'erg / (s Angstrom cm^2)'
+                    )
 
     @property
     def frequency(self):
         return self._frequency[:-1]
 
-
     @property
+    @require_field('distance')
     def flux_nu(self):
-        if self.distance is None:
-            raise AttributeError('supernova distance not supplied - flux calculation impossible')
-        else:
-            return self._flux_nu
+        return self._flux_nu
 
     @property
+    @require_field('distance')
     def flux_lambda(self):
-        if self.distance is None:
-            raise AttributeError('supernova distance not supplied - flux calculation impossible')
         return self._flux_lambda
-
-    def update_luminosity(self, spectrum_luminosity):
-        self.luminosity_density_nu = (spectrum_luminosity / self.delta_frequency).to('erg / (s Hz)')
-        self.luminosity_density_lambda = self.f_nu_to_f_lambda(self.luminosity_density_nu.value) \
-                                         * u.Unit('erg / (s Angstrom)')
-
-        if self.distance is not None:
-            self._flux_nu = (self.luminosity_density_nu / (4 * np.pi * self.distance.to('cm')**2))
-
-            self._flux_lambda = self.f_nu_to_f_lambda(self.flux_nu.value) * u.Unit('erg / (s Angstrom cm^2)')
 
     def f_nu_to_f_lambda(self, f_nu):
         return f_nu * self.frequency.value**2 / constants.c.cgs.value / 1e8
-
 
     def plot(self, ax, mode='wavelength'):
         if mode == 'wavelength':
