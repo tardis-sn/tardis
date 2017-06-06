25659bab391303a4bde80e39680b2ebe73b70d22
Vaibhav Gupta <vg3095@gmail.com>
2017-06-06 09:06:27 +0530

    Replaced values in test_partition.py with values from HDF file

--- tardis/plasma/tests/test_partition_function.py
+++ tardis/plasma/tests/test_partition_function.py
@@ -1,19 +1,35 @@
 import numpy as np
+import pandas as pd
+import os
+
+def data_path():
+    return os.path.join('tardis', 'plasma', 'tests', 'data', 'ref_data.hdf')
+
+
+def plasma_compare_data(path):
+    fpath = data_path()
+    with pd.HDFStore(fpath, 'r') as plasma:
+        data = plasma[path]
+    return data
 
 def test_level_boltzmann_factor_lte(level_boltzmann_factor_lte, levels):
-    assert np.allclose(level_boltzmann_factor_lte.ix[2].ix[0].ix[0], 1)
-    assert np.allclose(level_boltzmann_factor_lte.ix[2].ix[1].ix[0], 2)
+    expected = plasma_compare_data('/plasma/level_boltzmann_factor_lte')
+    assert np.allclose(level_boltzmann_factor_lte.ix[2].ix[0].ix[0], expected.ix[2].ix[0].ix[0])
+    assert np.allclose(level_boltzmann_factor_lte.ix[2].ix[1].ix[0], expected.ix[2].ix[1].ix[0])
     assert np.allclose(level_boltzmann_factor_lte.ix[2].ix[0].ix[10],
-        7.6218092841240705e-12)
+        expected.ix[2].ix[0].ix[10])
 
 def test_level_boltzmann_factor_dilute_lte(level_boltzmann_factor_dilute_lte,
     levels):
-    assert np.allclose(level_boltzmann_factor_dilute_lte.ix[2].ix[0].ix[0], 1)
-    assert np.allclose(level_boltzmann_factor_dilute_lte.ix[2].ix[1].ix[0], 2)
+    expected = plasma_compare_data('/plasma/level_boltzmann_factor_dilute_lte')
+    assert np.allclose(level_boltzmann_factor_dilute_lte.ix[2].ix[0].ix[0], expected.ix[2].ix[0].ix[0])
+    assert np.allclose(level_boltzmann_factor_dilute_lte.ix[2].ix[1].ix[0], expected.ix[2].ix[1].ix[0])
     assert np.allclose(level_boltzmann_factor_dilute_lte.ix[2].ix[0].ix[10],
-        3.8109046420620353e-12)
+        expected.ix[2].ix[0].ix[10])
 
 def test_lte_partition_function(partition_function, levels):
-    assert np.allclose(partition_function.ix[2].ix[0], 1.0)
-    assert np.allclose(partition_function.ix[2].ix[1], 2.0)
-    assert np.allclose(partition_function.ix[2].ix[2], 1.0)
+    expected = plasma_compare_data('/plasma/partition_function')
+    assert np.allclose(partition_function.ix[2].ix[0], expected.ix[2].ix[0])
+    assert np.allclose(partition_function.ix[2].ix[1], expected.ix[2].ix[1])
+    assert np.allclose(partition_function.ix[2].ix[2], expected.ix[2].ix[2])
+
