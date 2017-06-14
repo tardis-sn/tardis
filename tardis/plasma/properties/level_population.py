578235d7582a5c3d83285ef23db36dfc410f5179
yeganer <yeganer@gmx.de>
2017-06-14 11:46:43 +0200

    [PEP8]

--- tardis/plasma/properties/level_population.py
+++ tardis/plasma/properties/level_population.py
@@ -8,11 +8,13 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['LevelNumberDensity', 'LevelNumberDensityHeNLTE']
 
+
 class LevelNumberDensity(ProcessingPlasmaProperty):
     """
     Attributes:
     level_number_density : Pandas DataFrame, dtype float
-                           Index atom number, ion number, level number. Columns are zones.
+                           Index atom number, ion number, level number.
+                           Columns are zones.
     """
     outputs = ('level_number_density',)
     latex_name = ('N_{i,j,k}',)
@@ -31,18 +33,20 @@ class LevelNumberDensity(ProcessingPlasmaProperty):
                             index=partition_function.index)
         self._ion2level_idx = indexer.ix[levels.droplevel(2)].values
 
-    def calculate(self, level_boltzmann_factor, ion_number_density,
-        levels, partition_function):
+    def calculate(
+            self, level_boltzmann_factor, ion_number_density,
+            levels, partition_function):
         """
-        Reduces non-metastable level populations by a factor of W compared to LTE in the case of dilute-lte excitation.
+        Reduces non-metastable level populations by a factor of W
+        compared to LTE in the case of dilute-lte excitation.
         """
         if self.initialize_indices:
             self._initialize_indices(levels, partition_function)
             self.initialize_indices = False
         partition_function_broadcast = partition_function.values[
             self._ion2level_idx]
-        level_population_fraction = (level_boltzmann_factor.values
-                                     / partition_function_broadcast)
+        level_population_fraction = (level_boltzmann_factor.values /
+                                     partition_function_broadcast)
         ion_number_density_broadcast = ion_number_density.values[
             self._ion2level_idx]
         level_number_density = (level_population_fraction *
@@ -50,18 +54,22 @@ class LevelNumberDensity(ProcessingPlasmaProperty):
         return pd.DataFrame(level_number_density,
                             index=level_boltzmann_factor.index)
 
+
 class LevelNumberDensityHeNLTE(LevelNumberDensity):
     """
     Attributes:
     level_number_density : Pandas DataFrame, dtype float
-                           Index atom number, ion number, level number. Columns are zones.
+                           Index atom number, ion number, level number.
+                           Columns are zones.
     """
 
-    def calculate(self, level_boltzmann_factor,
-        ion_number_density, levels, partition_function,
-                               helium_population_updated):
+    def calculate(
+            self, level_boltzmann_factor,
+            ion_number_density, levels, partition_function,
+            helium_population_updated):
         """
-        If one of the two helium NLTE methods is used, this updates the helium level populations to the appropriate
+        If one of the two helium NLTE methods is used, this updates
+        the helium level populations to the appropriate
         values.
         """
         level_number_density = super(LevelNumberDensityHeNLTE, self).calculate(
