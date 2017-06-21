57d5283562cc2ff44a0d4f563327ffcc51ec3655
Vaibhav Gupta <vg3095@gmail.com>
2017-06-21 13:59:28 +0530

    Create a common Simulation fixture under conftest.py for HDF tests

--- tardis/model/tests/test_density.py
+++ tardis/model/tests/test_density.py
@@ -8,19 +8,19 @@ from numpy.testing import assert_almost_equal
 ###
 
 @pytest.fixture(scope="module", autouse=True)
-def to_hdf_buffer(hdf_file_path,homologous_density):
-    homologous_density.to_hdf(hdf_file_path)
+def to_hdf_buffer(hdf_file_path,simulation_verysimple):
+    simulation_verysimple.model.homologous_density.to_hdf(hdf_file_path)
 
-def test_hdf_density_0(hdf_file_path, homologous_density):
-    actual = homologous_density.density_0
+def test_hdf_density_0(hdf_file_path, simulation_verysimple):
+    actual = simulation_verysimple.model.homologous_density.density_0
     if hasattr(actual, 'cgs'):
         actual = actual.cgs.value
     path = os.path.join('homologous_density','density_0')
     expected = pd.read_hdf(hdf_file_path, path)
     assert_almost_equal(actual, expected.values)
 
-def test_hdf_time_0(hdf_file_path, homologous_density):
-    actual = homologous_density.time_0
+def test_hdf_time_0(hdf_file_path, simulation_verysimple):
+    actual = simulation_verysimple.model.homologous_density.time_0
     if hasattr(actual, 'cgs'):
         actual = actual.cgs.value
     path = os.path.join('homologous_density','scalars')
