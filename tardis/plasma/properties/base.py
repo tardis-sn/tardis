d9309129247e417a4fcec63015e8fd04ab27afa0
Vaibhav Gupta <vg3095@gmail.com>
2017-06-22 16:30:30 +0530

    Delete BasePlasmaProperty hdf method

--- tardis/plasma/properties/base.py
+++ tardis/plasma/properties/base.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 import numpy as np
 import pandas as pd
 
-from tardis.io.util import to_hdf
+from tardis.io.util import PlasmaWriterMixin
 
 __all__ = ['BasePlasmaProperty', 'BaseAtomicDataProperty',
            'HiddenPlasmaProperty', 'Input', 'ArrayInput', 'DataFrameInput',
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 import os
 
-class BasePlasmaProperty(object):
+class BasePlasmaProperty(PlasmaWriterMixin):
     """
     Attributes
     ----------
@@ -65,25 +65,7 @@ class BasePlasmaProperty(object):
                     ''))
         return latex_label.replace('\\', r'\\')
 
-    def to_hdf(self, path_or_buf, path):
-        """
-        Stores plugin value to an HDFStore
 
-        Parameters
-        ----------
-        path_or_buf:
-            Path or buffer to the HDF store
-        path: ~str
-            path to store the modules data under
-            - will be joined to <path>/output_name
-
-        Returns
-        -------
-            : None
-
-        """
-        outputs = {name: getattr(self, name) for name in self.outputs}
-        to_hdf(path_or_buf, path, outputs)
 
 
 class ProcessingPlasmaProperty(BasePlasmaProperty):
