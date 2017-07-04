67287327fd656156483d01554ca0af462bd9ee5a
Vaibhav Gupta <vg3095@gmail.com>
2017-07-04 22:31:35 +0530

    Added test for as_atomic_numbers method

--- tardis/io/tests/test_decay.py
+++ tardis/io/tests/test_decay.py
@@ -18,3 +18,18 @@ def test_simple_decay(simple_abundance_model):
     assert_almost_equal(decayed_abundance.ix[27, 56][1], 0.4423791)
     assert_almost_equal(decayed_abundance.ix[28, 56][0], 1.1086e-05)
     assert_almost_equal(decayed_abundance.ix[28, 56][1], 1.1086e-05)
+
+@pytest.fixture
+def raw_abundance_simple():
+    abundances = pd.DataFrame([[0.2, 0.2], [0.1, 0.1]], index=[28, 30])
+    abundances.index.rename('atomic_number', inplace=True)
+    return abundances
+
+def test_abundance_merge(simple_abundance_model, raw_abundance_simple):
+    decayed_df = simple_abundance_model.decay(100)
+    merged_df = simple_abundance_model.as_atomic_numbers(raw_abundance_simple, 100, normalize=False)
+
+    assert_almost_equal(merged_df.loc[28][0], raw_abundance_simple.loc[28][0] + decayed_df.loc[28, 56][0])
+    assert_almost_equal(merged_df.loc[28][1], raw_abundance_simple.loc[28][1] + decayed_df.loc[28, 56][1])
+    assert_almost_equal(merged_df.loc[30][1], raw_abundance_simple.loc[30][1])
+    assert_almost_equal(merged_df.loc[26][0], decayed_df.loc[26, 56][0])
