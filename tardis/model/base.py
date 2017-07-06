9e122be73a3ddd8875a2a08f667db9cbfe1a4a75
Vaibhav Gupta <vg3095@gmail.com>
2017-07-06 18:42:14 +0530

    Move decay of isotope_abundance under Model abundance property

--- tardis/model/base.py
+++ tardis/model/base.py
@@ -188,6 +188,9 @@ class Radial1DModel(HDFWriterMixin):
             # abundance has one element less than velocity
             # ix stop index is inclusive
             stop -= 2
+        if not self.raw_isotope_abundance.empty:
+            self._abundance = self.raw_isotope_abundance.decay(
+                self.time_explosion).as_atomic_numbers(self.raw_abundance)
         abundance = self._abundance.ix[:, start:stop]
         abundance.columns = range(len(abundance.columns))
         return abundance
