4695e89c728308f75812c0108a4c496033617db6
Vaibhav Gupta <vg3095@gmail.com>
2017-08-22 18:49:57 +0530

    Update write_dot/tex plasma tests

--- tardis/plasma/tests/test_plasma_dot_tex.py
+++ tardis/plasma/tests/test_plasma_dot_tex.py
@@ -1,19 +1,20 @@
-import filecmp
 import os
 import pygraphviz as pgv
-
+import pytest
 
 data_path = os.path.join('tardis', 'plasma', 'tests', 'data')
 
 
 def test_write_dot(tmpdir, simulation_verysimple):
-    fname = str(tmpdir.mkdir('test').join('plasma.dot'))
+    fname = str(tmpdir.mkdir('test_dot').join('plasma.dot'))
     simulation_verysimple.plasma.write_to_dot(fname)
-    assert pgv.AGraph(fname) == pgv.AGraph(
-        os.path.join(data_path, 'plasma_ref.dot'))
+    actual = pgv.AGraph(fname).to_string()
+    expected = pgv.AGraph(os.path.join(data_path, 'plasma_ref.dot')).to_string()
+    assert actual == expected
 
 
 def test_write_tex(tmpdir, simulation_verysimple):
-    fname = str(tmpdir.mkdir('test').join('plasma.tex'))
-    simulation_verysimple.plasma.write_to_dot(fname)
-    assert filecmp.cmp(fname, os.path.join(data_path, 'plasma_ref.tex'))
+    fname = str(tmpdir.mkdir('test_tex').join('plasma.tex'))
+    simulation_verysimple.plasma.write_to_tex(fname)
+    with open(fname, 'r') as fp1, open(os.path.join(data_path, 'plasma_ref.tex'), 'r') as fp2:
+        assert fp1.readline() ==fp2.readline()
