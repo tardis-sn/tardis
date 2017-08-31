ebd9d0adb1d096b5afc968b2c3be879860785f2c
yeganer <yeganer@gmx.de>
2017-08-31 10:24:56 +0200

    Add travis caching

---
+++ travis/before_install.sh
@@ -0,0 +1,19 @@
+conda env create -f tardis_env27.yml
+source activate tardis
+if [[ $SETUP_CMD == *coverage* ]]; then 
+    conda install -c conda-forge git-lfs=2.2.1 -y
+    git lfs install --skip-smudge
+    git clone $TARDIS_REF_DATA_URL $HOME/tardis-refdata
+    cd $HOME/tardis-refdata
+
+    # Checkout PR version, not master
+    git fetch origin pull/3/head:carsus-ref
+    git checkout carsus-ref
+
+    # Pull the files
+    git lfs pull --include="atom_data/kurucz_cd23_chianti_H_He.h5" origin
+    git lfs pull --include="atom_data/chianti_He.h5" origin
+    git lfs pull --include="plasma_reference/" origin
+    git lfs pull --include="unit_test_data.h5" origin
+    cd $TRAVIS_BUILD_DIR
+fi
