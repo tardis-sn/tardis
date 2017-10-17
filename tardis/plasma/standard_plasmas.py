2ae1cdd13dc2ebd628d64bf6fa09ca5240fcd5c8
Laud Bentil <laud.bentil@meltwater.org>
2017-10-17 13:25:53 +0000

    tardis/plasma/standard_plasmas.py: old database file warning
    displays warning when using old version of database files - raised exception with error message

--- tardis/plasma/standard_plasmas.py
+++ tardis/plasma/standard_plasmas.py
@@ -77,10 +77,8 @@ def assemble_plasma(config, model, atom_data=None):
         try:
             atom_data = atomic.AtomData.from_hdf(atom_data_fname)
         except:
-            logger.warn(
-            'Error might be from the use of an old-format of the atomic database,\n please refer to '+ \
+            raise ValueError('Error might be from the use of an old-format of the atomic database,\n please see '+ \
             'https://github.com/tardis-sn/tardis-refdata/tree/master/atom_data for the most recent version.')
-            raise
 
     atom_data.prepare_atom_data(
         model.abundance.index,
