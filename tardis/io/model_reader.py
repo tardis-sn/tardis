3c8b6fdbfcb42964df32a9b46109e8452712fc0b
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2018-07-20 14:02:25 +0200

    Update cmfgen_density reader

--- tardis/io/model_reader.py
+++ tardis/io/model_reader.py
@@ -251,10 +251,14 @@ def read_cmfgen_density(fname):
     Reading a density file of the following structure (example; lines starting with a hash will be ignored):
     The first density describes the mean density in the center of the model and is not used.
     The file consists of a header row and next row contains unit of the respective attributes
-    velocity densities electron_densities temperature
-    km/s g/cm^3 /cm^3 K
-    871.66905 4.2537191e-09 2.5953807e+14 7.6395577
-    877.44269 4.2537191e-09 2.5953807e+14 7.6395577
+    Note that the first column has to contain a running index
+
+    Example:
+
+    index velocity densities electron_densities temperature
+    - km/s g/cm^3 /cm^3 K
+    0 871.66905 4.2537191e-09 2.5953807e+14 7.6395577
+    1 877.44269 4.2537191e-09 2.5953807e+14 7.6395577
 
     Rest columns contain abundances of elements and isotopes
 
@@ -290,11 +294,11 @@ def read_cmfgen_density(fname):
             elif row_index == 2:
                 quantities = line.split()
 
-    velocity = u.Quantity(df['velocity'].values, quantities[0]).to('cm/s')
-    temperature = u.Quantity(df['temperature'].values, quantities[1])[1:]
-    mean_density = u.Quantity(df['densities'].values, quantities[2])[1:]
+    velocity = u.Quantity(df['velocity'].values, quantities[1]).to('cm/s')
+    temperature = u.Quantity(df['temperature'].values, quantities[2])[1:]
+    mean_density = u.Quantity(df['densities'].values, quantities[3])[1:]
     electron_densities = u.Quantity(
-        df['electron_densities'].values, quantities[3])[1:]
+        df['electron_densities'].values, quantities[4])[1:]
 
     return time_of_model, velocity, mean_density, electron_densities, temperature
 
