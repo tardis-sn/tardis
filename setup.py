fdd04a7f610a1e5d26b74e42ebfd472abb87888c
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-07-26 12:05:31 +0200

    update the text to be correct

--- setup.py
+++ setup.py
@@ -60,11 +60,11 @@ if not RELEASE:
 # invoking any other functionality from distutils since it can potentially
 # modify distutils' behavior.
 cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
-add_command_option('install', 'with-openmp', 'compile TARDIS without OpenMP',
+add_command_option('install', 'with-openmp', 'compile TARDIS with OpenMP',
+                   is_bool=True, )
+add_command_option('build', 'with-openmp', 'compile TARDIS with OpenMP',
                    is_bool=True)
-add_command_option('build', 'with-openmp', 'compile TARDIS without OpenMP',
-                   is_bool=True)
-add_command_option('develop', 'with-openmp', 'compile TARDIS without OpenMP',
+add_command_option('develop', 'with-openmp', 'compile TARDIS with OpenMP',
                    is_bool=True)
 add_command_option('install', 'with-vpacket-logging', 'compile TARDIS with virtual packet logging',
                    is_bool=True)
