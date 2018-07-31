9aad9f9b81b1a7145ea3de2f753f631f7c4f2ba4
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-07-31 17:20:28 +0200

    remove wrong comment from atomic. use stdout for logging

--- tardis/__init__.py
+++ tardis/__init__.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import logging
+import logging, sys
 from tardis.io.util import ColoredLogger
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
@@ -17,5 +17,5 @@ logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 console_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(console_formatter)
-logger.addHandler(console_handler)
+logger.addHandler(sys.stdout)
 logging.getLogger('py.warnings').addHandler(console_handler)
