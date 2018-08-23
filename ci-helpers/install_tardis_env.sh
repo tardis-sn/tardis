076bfbe6d00476594edad6055d0a4672371415bc
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-23 15:00:15 +0200

    caching tardis env

--- ci-helpers/install_tardis_env.sh
+++ ci-helpers/install_tardis_env.sh
@@ -1,3 +1,8 @@
-export PYTHONIOENCODING=UTF8
+cd $TRAVIS_BUILD_DIR
 if test -e $HOME/miniconda/envs/tardis; then
     echo "TARDIS env already installed.";
+else
+   conda env create -f tardis_env27.yml
+fi
+
+source activate tardis
