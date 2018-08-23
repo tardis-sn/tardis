5c1a8a0a3eeef4a44cf2fdf9527c615210e86ead
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-23 14:52:02 +0200

    remove cached miniconda

--- ci-helpers/install_miniconda.sh
+++ ci-helpers/install_miniconda.sh
@@ -1,10 +1,16 @@
 export PYTHONIOENCODING=UTF8
 if test -e $HOME/miniconda/bin; then
-     echo "miniconda already installed.";
+    echo "miniconda already installed.";
+    export PATH=$HOME/miniconda/bin:$PATH
+    hash -r
+    #conda update --yes conda
+
 else
     wget $MINICONDA_URL -O miniconda.sh
     chmod +x miniconda.sh
+    rm -r $HOME/miniconda
     bash miniconda.sh -b -p $HOME/miniconda
+
     export PATH=$HOME/miniconda/bin:$PATH
     hash -r
     conda update --yes conda
