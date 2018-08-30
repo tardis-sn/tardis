0e8dafb2d63d5b4466a3b95e0f164795652e962b
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-30 16:58:25 +0200

    Add Travis Caching and separate scripts for CI (#866)
    * add caching
    
    * remove cached miniconda
    
    * swap coveralls for codecov
    
    * adding new ci helper
    
    * caching tardis env
    
    * add refdata to caching
    
    * fetch reference data
    
    * ref data avail
    
    * remove the fetch reference data caching - to be done in next PR
    
    * update ref_data_home
    
    * update comments from unoebauer
    
    * update refdata path
    
    * add single quotes
    
    * remove git lfs from separate install
    
    * update clone refdata
    
    * remove space after equal
    
    * update travis yaml

---
+++ ci-helpers/install_tardis_env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+cd $TRAVIS_BUILD_DIR
+if test -e $HOME/miniconda/envs/tardis; then
+    echo "TARDIS env already installed.";
+    # Also check for tardis_env27.yml change
+else
+   conda env create -f tardis_env27.yml
+   #trouble with building due to segfault at cython (https://github.com/cython/cython/issues/2199)
+   #remove if we can get normal cython through conda
+   git clone https://github.com/cython/cython
+   cd cython
+   git checkout c485b1b77264c3c75d090a3c526de24966830d42
+   CFLAGS="$CFLAGS -D CYTHON_CLINE_IN_TRACEBACK=0" python setup.py install
+   cd ..
+fi
+
+source activate tardis
