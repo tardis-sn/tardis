27732bf8c307e700564c330956985d3c2b5f92f5
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-30 16:44:45 +0200

    update travis yaml

--- ci-helpers/install_tardis_env.sh
+++ ci-helpers/install_tardis_env.sh
@@ -5,6 +5,13 @@ if test -e $HOME/miniconda/envs/tardis; then
     # Also check for tardis_env27.yml change
 else
    conda env create -f tardis_env27.yml
+   #trouble with building due to segfault at cython (https://github.com/cython/cython/issues/2199)
+   #remove if we can get normal cython through conda
+   git clone https://github.com/cython/cython
+   cd cython
+   git checkout c485b1b77264c3c75d090a3c526de24966830d42
+   CFLAGS="$CFLAGS -D CYTHON_CLINE_IN_TRACEBACK=0" python setup.py install
+   cd ..
 fi
 
 source activate tardis
