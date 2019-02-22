cfb8af7029c71ad7dd0550f0e4cdda0914b90b78
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-02-22 10:44:51 -0500

    fix relative imports

--- tardis/model/base.py
+++ tardis/model/base.py
@@ -9,7 +9,7 @@ from tardis.util.base import quantity_linspace
 from tardis.io.model_reader import read_density_file, read_abundances_file, read_uniform_abundances
 from tardis.io.util import HDFWriterMixin
 from tardis.io.decay import IsotopeAbundances
-from density import HomologousDensity
+from tardis.model.density import HomologousDensity
 
 logger = logging.getLogger(__name__)
 
