dd8e5e2b56a2e16726d9339cb6f6be861297c034
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-02-22 11:28:57 -0500

    fix .ix in atomic to .loc and .iloc

--- tardis/io/atomic.py
+++ tardis/io/atomic.py
@@ -306,13 +306,13 @@ class AtomData(object):
         tmp_lines_lower2level_idx = self.lines.index.droplevel('level_number_upper')
 
         self.lines_lower2level_idx = (
-                self.levels_index.ix[tmp_lines_lower2level_idx].
+                self.levels_index.loc[tmp_lines_lower2level_idx].
                 astype(np.int64).values)
 
         tmp_lines_upper2level_idx = self.lines.index.droplevel('level_number_lower')
 
         self.lines_upper2level_idx = (
-                self.levels_index.ix[tmp_lines_upper2level_idx].
+                self.levels_index.loc[tmp_lines_upper2level_idx].
                 astype(np.int64).values)
 
         self.atom_ion_index = None
@@ -351,11 +351,11 @@ class AtomData(object):
 
             self.macro_atom_references.loc[:, "references_idx"] = np.arange(len(self.macro_atom_references))
 
-            self.macro_atom_data.loc[:, "lines_idx"] = self.lines_index.ix[
+            self.macro_atom_data.loc[:, "lines_idx"] = self.lines_index.loc[
                 self.macro_atom_data['transition_line_id']
             ].values
 
-            self.lines_upper2macro_reference_idx = self.macro_atom_references.ix[
+            self.lines_upper2macro_reference_idx = self.macro_atom_references.loc[
                 tmp_lines_upper2level_idx, 'references_idx'
             ].astype(np.int64).values
 
