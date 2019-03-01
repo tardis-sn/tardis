012c69450b99c640b763e593e64cd463bc645f50
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-03-01 13:29:26 -0500

    New template setup from astropy (#891)
    * add new setup.cfg from package template
    
    * add new setup.py
    
    * remove duplicate ah section
    
    * add new setup.cfg from package template
    
    * fix the setup.py to use the new version.

--- setup.py
+++ setup.py
@@ -1,54 +1,74 @@
-#!/usr/bin/env python
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
 import glob
 import os
 import sys
-
-import ah_bootstrap
-from setuptools import setup
-
-#A dirty hack to get around some early import/configurations ambiguities
-if sys.version_info[0] >= 3:
-    import builtins
-else:
-    import builtins as builtins
-builtins._ASTROPY_SETUP_ = True
-
-from astropy_helpers.setup_helpers import (register_commands, adjust_compiler,
-                                           get_debug_option, get_package_info,
-                                           add_command_option)
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
-
-# Get some values from the setup.cfg
 try:
     from configparser import ConfigParser
 except ImportError:
-    from configparser import ConfigParser
+    from ConfigParser import ConfigParser
 
+# Get some values from the setup.cfg
 conf = ConfigParser()
 conf.read(['setup.cfg'])
 metadata = dict(conf.items('metadata'))
 
 PACKAGENAME = metadata.get('package_name', 'packagename')
-DESCRIPTION = metadata.get('description', 'Astropy affiliated package')
-AUTHOR = metadata.get('author', '')
+DESCRIPTION = metadata.get('description', 'Astropy Package Template')
+AUTHOR = metadata.get('author', 'Astropy Developers')
 AUTHOR_EMAIL = metadata.get('author_email', '')
 LICENSE = metadata.get('license', 'unknown')
-URL = metadata.get('url', 'http://astropy.org')
+URL = metadata.get('url', 'http://docs.astropy.org/projects/package-template/')
+__minimum_python_version__ = metadata.get("minimum_python_version", "3.6")
 
-# Get the long description from the package's docstring
-#__import__(PACKAGENAME)
-#package = sys.modules[PACKAGENAME]
-LONG_DESCRIPTION = "" #package.__doc__
+# Enforce Python version check - this is the same check as in __init__.py but
+# this one has to happen before importing ah_bootstrap.
+if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
+    sys.stderr.write("ERROR: packagename requires Python {} or later\n".format(__minimum_python_version__))
+    sys.exit(1)
+
+# Import ah_bootstrap after the python version validation
+
+import ah_bootstrap
+from setuptools import setup
+
+import builtins
+builtins._ASTROPY_SETUP_ = True
+
+from astropy_helpers.setup_helpers import (register_commands, get_debug_option,
+                                           get_package_info, add_command_option)
+from astropy_helpers.git_helpers import get_git_devstr
+from astropy_helpers.version_helpers import generate_version_py
+
+
+# order of priority for long_description:
+#   (1) set in setup.cfg,
+#   (2) load LONG_DESCRIPTION.rst,
+#   (3) load README.rst,
+#   (4) package docstring
+readme_glob = 'README*'
+_cfg_long_description = metadata.get('long_description', '')
+if _cfg_long_description:
+    LONG_DESCRIPTION = _cfg_long_description
+
+elif os.path.exists('LONG_DESCRIPTION.rst'):
+    with open('LONG_DESCRIPTION.rst') as f:
+        LONG_DESCRIPTION = f.read()
+
+elif len(glob.glob(readme_glob)) > 0:
+    with open(glob.glob(readme_glob)[0]) as f:
+        LONG_DESCRIPTION = f.read()
+
+else:
+    # Get the long description from the package's docstring
+    __import__(PACKAGENAME)
+    package = sys.modules[PACKAGENAME]
+    LONG_DESCRIPTION = package.__doc__
 
 # Store the package name in a built-in variable so it's easy
 # to get from other parts of the setup infrastructure
 builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
-# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '3.dev'
+# VERSION should be PEP440 compatible (http://www.python.org/dev/peps/pep-0440)
+VERSION = metadata.get('version', '0.0.dev')
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
@@ -56,10 +76,12 @@ RELEASE = 'dev' not in VERSION
 if not RELEASE:
     VERSION += get_git_devstr(False)
 
+
 # Populate the dict of setup command overrides; this should be done before
 # invoking any other functionality from distutils since it can potentially
 # modify distutils' behavior.
 cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
+
 add_command_option('install', 'with-openmp', 'compile TARDIS with OpenMP',
                    is_bool=True, )
 add_command_option('build', 'with-openmp', 'compile TARDIS with OpenMP',
@@ -95,12 +117,6 @@ package_info['package_data'][PACKAGENAME].append('io/schemas/*')
 # Define entry points for command-line scripts
 entry_points = {}
 
-for hook in [('prereleaser', 'middle'), ('releaser', 'middle'),
-             ('postreleaser', 'before'), ('postreleaser', 'middle')]:
-    hook_ep = 'zest.releaser.' + '.'.join(hook)
-    hook_name = 'astropy.release.' + '.'.join(hook)
-    hook_func = 'astropy.utils.release:' + '_'.join(hook)
-    entry_points[hook_ep] = ['%s = %s' % (hook_name, hook_func)]
 
 entry_points['console_scripts'] = [
     'tardis_test_runner = tardis.tests.integration_tests.runner:run_tests',
@@ -119,6 +135,7 @@ for root, dirs, files in os.walk(PACKAGENAME):
             c_files.append(
                 os.path.join(
                     os.path.relpath(root, PACKAGENAME), filename))
+
 package_info['package_data'][PACKAGENAME].extend(c_files)
 
 setup(name=PACKAGENAME + '-sn',
@@ -133,7 +150,7 @@ setup(name=PACKAGENAME + '-sn',
       long_description=LONG_DESCRIPTION,
       cmdclass=cmdclassd,
       zip_safe=False,
-      use_2to3=True,
+      use_2to3=False,
       entry_points=entry_points,
       **package_info
 )
