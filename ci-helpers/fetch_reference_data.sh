908c8c082521c6231fe1f37be2a324b4e5d13d58
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-03-08 21:18:50 -0500

    Azure pipeline for TARDIS (#894)
    * enable pull request
    
    * add display variable step
    
    * add a few more variables
    
    * set build directory variable
    
    * set variable
    
    * add tardis home
    
    * try second url variable
    
    * switching out build_dir definitions
    
    * change https to http
    
    * add install conda env script
    
    * add reference data fetcher
    
    * fix env creator
    
    * fix variable hell
    
    * fix source activate
    
    * fix source problems
    
    * add ref-data test
    
    * add moria to pool
    
    * add mac build
    
    * remove task
    
    * remove activation of conda activate
    
    * make conda variable

--- ci-helpers/fetch_reference_data.sh
+++ ci-helpers/fetch_reference_data.sh
@@ -4,8 +4,8 @@ git lfs install --skip-smudge
 if test -e $REF_DATA_HOME; then
     echo "Ref data available"
 else
-    echo git clone $REF_DATA_GITHUBURL $REF_DATA_HOME
-    git clone $REF_DATA_GITHUBURL $REF_DATA_HOME
+    echo git clone $REF_DATA_GITHUB_URL $REF_DATA_HOME
+    git clone $REF_DATA_GITHUB_URL $REF_DATA_HOME
 
 cd $REF_DATA_HOME
 # Use the following to get the ref-data from the master;
@@ -18,4 +18,4 @@ git lfs pull --include="atom_data/chianti_He.h5" origin
 git lfs pull --include="plasma_reference/" origin
 git lfs pull --include="unit_test_data.h5" origin
 echo MD5 `md5sum unit_test_data.h5`; fi
-cd $TRAVIS_BUILD_DIR
+cd $TARDIS_BUILD_DIR
