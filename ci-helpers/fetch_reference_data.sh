6e58992c324202a97a0af9f3543ee27629935470
Christian Vogl <cvogl@mpa-garching.mpg.de>
2019-04-02 18:05:36 +0200

    Update refdata path

--- ci-helpers/fetch_reference_data.sh
+++ ci-helpers/fetch_reference_data.sh
@@ -9,11 +9,11 @@ else
 
 cd $REF_DATA_HOME
 # Use the following to get the ref-data from the master;
-git fetch origin
-git checkout origin/master
+#git fetch origin
+#git checkout origin/master
 # Use the following to get the ref-data from a specific pull request
-#git fetch origin pull/11/head:update-ref
-#git checkout origin/update-ref
+git fetch origin pull/20/head:update-ref
+git checkout origin/update-ref
 git lfs pull --include="atom_data/kurucz_cd23_chianti_H_He.h5" origin
 git lfs pull --include="atom_data/chianti_He.h5" origin
 git lfs pull --include="plasma_reference/" origin
