750fb86bf9e05b667ad68490b5d7834e2d535e7e
sarafina325 <31808841+sarafina325@users.noreply.github.com>
2019-04-12 11:08:47 -0700

    Parsing STELLA files for TARDIS (#915)
    * Add simple first reader for stella data.
    
    * Add notebook explaining STELLA to TARDIS
    
    * Remove last row of STELLA data
    
    * Add STELLA notebook to Example Models section
    
    * Add mesa.stella.dat file

---
+++ tardis/io/parsers/stella.py
@@ -0,0 +1,14 @@
+import re
+import pandas as pd
+from astropy import units as u
+import numpy as np
+
+def read_stella_data(filename):
+    with open(filename) as fh:
+        col = fh.readlines()[5]
+    col_names = re.split('\s{3,}', col.strip())
+    col_names = [re.sub('\s\(.+\)', '', col_name).replace(' ', '_') for col_name in col_names]
+    data = pd.read_csv(filename, skiprows=7, delim_whitespace=True, names = col_names)
+    # drop last row of data
+    data = data.iloc[0:-1]
+    return data
