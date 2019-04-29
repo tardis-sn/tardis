f2f06f0277e85e2e8277b61a4329f86629fb78a5
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-04-29 18:30:43 -0400

    add tardis_internal_config and downloader

---
+++ tardis/io/data_repo.py
@@ -0,0 +1,28 @@
+from tqdm.autonotebook import tqdm
+
+def download_from_url(url, dst):
+    """
+    kindly used from https://gist.github.com/wy193777/0e2a4932e81afc6aa4c8f7a2984f34e2
+    @param: url to download file
+    @param: dst place to put the file
+    """
+
+    file_size = int(requests.head(url).headers["Content-Length"])
+    if os.path.exists(dst):
+        first_byte = os.path.getsize(dst)
+    else:
+        first_byte = 0
+    if first_byte >= file_size:
+        return file_size
+    header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
+    pbar = tqdm(
+        total=file_size, initial=first_byte,
+        unit='B', unit_scale=True, desc=url.split('/')[-1])
+    req = requests.get(url, headers=header, stream=True)
+    with(open(dst, 'ab')) as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                pbar.update(1024)
+    pbar.close()
+    return file_size
