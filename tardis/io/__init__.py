02f50ef73fb68878e7970476e6bf299eda9ac490
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-04-29 18:38:35 -0400

    update the configuration stuff

--- tardis/io/__init__.py
+++ tardis/io/__init__.py
@@ -2,3 +2,4 @@
 from tardis.io.model_reader import read_simple_ascii_density, read_simple_ascii_abundances, read_density_file
 
 from tardis.io.util import parse_abundance_dict_to_dataframe
+from tardis.io.config_internal import get_internal_configuration, get_data_dir
