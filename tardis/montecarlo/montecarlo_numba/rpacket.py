50fa9c093dd86b4fbccae907957c252825f1b8dd
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-05-01 19:11:11 -0400

    all sorts of nonsense
    Co-authored-by: Christian Vogl <cvogl@mpa-garching.mpg.de>
    Co-authored-by: Alice Harpole <aliceharpole@gmail.com>
    Co-authored-by: Yssavo Camacho-Neves <yic6@physics.rutgers.edu>

--- tardis/montecarlo/montecarlo_numba/rpacket.py
+++ tardis/montecarlo/montecarlo_numba/rpacket.py
@@ -1,10 +1,11 @@
 import numpy as np
 from enum import Enum
-from numba import int64, float64
+from numba import int64, float64, boolean
 from numba import jitclass, njit
 from tardis.montecarlo.montecarlo_numba.compute_distance import (compute_distance2boundary,
-                                                                 compute_distance2continuum)
-
+                                                                 compute_distance2continuum,
+                                                                 compute_distance2line)
+from tardis.montecarlo.montecarlo_numba import njit_dict
 from astropy import constants as const
 
 C_SPEED_OF_LIGHT = const.c.to('cm/s').value
@@ -27,7 +28,9 @@ rpacket_spec = [
     ('energy', float64),
     ('tau_event', float64),
     ('nu_line', float64),
- #   ('d_line', float64)  # Distance to electron event. 
+    ('last_line', boolean),
+    ('next_line_id', int64),
+    ('d_line', float64),  # Distance to line event. 
     ('d_electron', float64), #/**< Distance to line event. */
     ('d_boundary', float64), # distance to boundary 
     ('current_shell_id', int64),
@@ -37,9 +40,14 @@ rpacket_spec = [
     ('distance', float64)
 ]
 
-@njit
+@njit(**njit_dict)
 def get_tau_event():
     return  np.random.exponential()
+
+
+@njit(**njit_dict)
+def get_random_mu():
+    return 2.0 * np.random.random() - 1.0
 
 
 @jitclass(rpacket_spec)
@@ -55,19 +63,19 @@ class RPacket(object):
         self.delta_shell_id = 0
         self.d_boundary = -1.0
         self.d_electron = -1.0
+        self.d_line = -1.0
         self.distance = 0.0
         #self.nu_line = nu_line
     
     def compute_distances(self, storage):
-        compute_distance2continuum(self, storage)
+        compute_distance2line(self, storage)
         compute_distance2boundary(self, storage)
-        if self.d_boundary < self.d_electron:
+        if self.d_boundary < self.d_line:
             next_interaction = BOUNDARY
             self.distance = self.d_boundary
         else:
-            next_interaction = ESCATTERING
-            # TODO: Fixme
-            self.distance = self.d_boundary
+            next_interaction = LINE
+            self.distance = self.d_line
 
         self.next_interaction = next_interaction
 
@@ -99,3 +107,68 @@ class RPacket(object):
             self.status = EMITTED
         else:
             self.status = REABSORBED
+    
+    def set_line(self, storage):
+        packet.line_id = np.searchsorted(storage.line_list_nu, nu)
+        if packet.line_id > len(storage.line_list_nu):
+            packet.last_line = True
+        else:
+            packet.nu_line = storage.line_list_nu[packet.line_id]
+            packet.last_line = False
+
+ # if (rpacket_get_virtual_packet (packet) > 0)
+ #   {
+#      rpacket_set_tau_event (packet,
+#                             rpacket_get_tau_event (packet) + tau_line);
+#      rpacket_set_next_line_id (packet, next_line_id + 1);
+#      test_for_close_line (packet, storage);
+#    } 
+    def transform_energy(self, storage_model):
+        """
+        Transform from the LabFrame to the ComovingFrame. Then change the angle 
+        and transform back conserving energy in the ComovingFrame.        
+        """
+        old_doppler_factor = self.get_doppler_factor(storage_model)
+        self.mu = get_random_mu()
+        inverse_doppler_factor = 1. / self.get_doppler_factor(storage_model)
+        comov_energy = self.energy * old_doppler_factor
+        self.energy = comov_energy * inverse_doppler_factor
+
+    def line_scatter(self, storage_model):
+        next_line_id = self.next_line_id
+        tau_line = storage_model.line_lists_tau_sobolevs[next_line_id, self.current_shell_id]
+        # TODO: Fixme
+        tau_continuum = 0.0
+        tau_combined = tau_line + tau_continuum
+        
+        if (next_line_id + 1 == storage.no_of_lines):
+            self.last_line = True
+        if (rpacket_get_tau_event (packet) < tau_combined): # Line absorption occurs
+            self.move_packet(storage_model, self.distance)
+            self.transform_energy(storage_model)
+\            #TODO: Fixme
+            self.line_emission(storage)
+        else:
+            self.tau_event -= tau_line
+            self.next_line_id = next_line_id + 1
+            # ???
+            self.nu_line = storage_model.line_list_nu[self.next_line_id] 
+            #test_for_close_line (packet, storage);
+
+    def line_emission(self, storage_model):
+        emission_line_id = packet.next_line_id
+        inverse_doppler_factor = self.get_doppler_factor(storage)
+        self.nu = storage_model.line_list_nu[emission_line_id] * inverse_doppler_factor 
+        #packet.nu_line =  storage->line_list_nu[emission_line_id]);
+        self.next_line_id = emission_line_id + 1
+        self.tau_event = get_tau_event()
+#void test_for_close_line (rpacket_t * packet, const storage_model_t * storage)
+#{
+#  if (!rpacket_get_last_line (packet) &&
+#      fabs (storage->line_list_nu[rpacket_get_next_line_id (packet)] -
+#            rpacket_get_nu_line (packet)) < (rpacket_get_nu_line (packet)*
+#                                             1e-7))
+#    {
+#      rpacket_set_close_line (packet, true);
+#    }
+#}
