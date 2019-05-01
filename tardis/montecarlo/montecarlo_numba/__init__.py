90bfb229708460588c55ef581143de1c65b28700
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-05-01 16:40:27 -0400

    final numba test - seems to work.
    Co-authored-by: Christian Vogl <cvogl@mpa-garching.mpg.de>
    Co-authored-by: Alice Harpole <aliceharpole@gmail.com>
    Co-authored-by: Yssavo Camacho-Neves <yic6@physics.rutgers.edu>

--- tardis/montecarlo/montecarlo_numba/__init__.py
+++ tardis/montecarlo/montecarlo_numba/__init__.py
@@ -1,1 +1,6 @@
+from llvmlite import binding
+binding.set_option("tmp", "-non-global-value-max-name-size=2048")
+
 from tardis.montecarlo.montecarlo_numba.rpacket import RPacket
+from tardis.montecarlo.montecarlo_numba.base import montecarlo_radial1d
+
