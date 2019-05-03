89e4821734fe84fdf9a88f44fe0e216af5a07dbf
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-05-03 14:59:49 -0400

    rewrite problem

--- tardis/montecarlo/montecarlo_numba/__init__.py
+++ tardis/montecarlo/montecarlo_numba/__init__.py
@@ -1,6 +1,6 @@
 from llvmlite import binding
 binding.set_option("tmp", "-non-global-value-max-name-size=2048")
-njit_dict = {'fastmath': True}
+njit_dict = {'fastmath': False}
 
 from tardis.montecarlo.montecarlo_numba.rpacket import RPacket
 from tardis.montecarlo.montecarlo_numba.base import montecarlo_radial1d
