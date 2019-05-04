af2c440ba60f240db2ebb77e726d0c41fc27c8b4
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-05-04 09:13:10 -0400

    adding electron scattering

--- tardis/montecarlo/montecarlo_numba/single_packet_loop.py
+++ tardis/montecarlo/montecarlo_numba/single_packet_loop.py
@@ -1,8 +1,8 @@
 from numba import njit
 import numpy as np
 from enum import Enum
-from tardis.montecarlo.montecarlo_numba.rpacket import (
-    ESCATTERING, BOUNDARY, LINE, IN_PROCESS, REABSORBED)
+from tardis.montecarlo.montecarlo_numba.rpacket import (IN_PROCESS, REABSORBED)
+from tardis.montecarlo.montecarlo_numba.rpacket import InteractionType
 
 @njit
 def single_packet_loop(storage_model, r_packet):
@@ -10,9 +10,9 @@ def single_packet_loop(storage_model, r_packet):
 
     while r_packet.status == IN_PROCESS:
         distance, interaction_type, delta_shell = r_packet.trace_packet(storage_model)
-        if interaction_type == BOUNDARY:
+        if interaction_type == InteractionType.BOUNDARY:
             r_packet.move_packet_across_shell_boundary(distance, delta_shell, storage_model.no_of_shells)
-        elif interaction_type == LINE:
+        elif interaction_type == InteractionType.LINE:
             r_packet.move_packet(distance)
             r_packet.transform_energy(storage_model)
             r_packet.line_emission(storage_model)
