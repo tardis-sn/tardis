60c4392be40c1519dab292432daff73cff653f85
Marc Williamson <marxwillia@gmail.com>
2019-05-29 13:59:36 -0400

    Used pytest fixtures

--- tardis/io/tests/test_csvy_reader.py
+++ tardis/io/tests/test_csvy_reader.py
@@ -1,27 +1,33 @@
+import tardis
 from tardis.io.parsers import csvy
 import pytest
 import os
 from astropy import units as u
 import numpy.testing as npt
 
-test_data_directory = os.path.dirname(__file__)
 
-def data_path(filename):
-    data_dir = os.path.dirname(__file__)
-    return os.path.join(data_dir, 'data', filename)
+DATA_PATH = os.path.join(tardis.__path__[0], 'io', 'tests', 'data')
 
-def test_csvy_finds_csv_first_line():
-    yaml_dict, csv = csvy.load_csvy(data_path('csvy_full.csvy'))
+@pytest.fixture
+def csvy_full_fname():
+    return os.path.join(DATA_PATH, 'csvy_full.dat')
+
+@pytest.fixture
+def csvy_nocsv_fname():
+    return os.path.join(DATA_PATH, 'csvy_nocsv.dat')
+
+def test_csvy_finds_csv_first_line(csvy_full_fname):
+    yaml_dict, csv = csvy.load_csvy(csvy_full_fname)
     assert csv['velocity'][0] == 10000
 
-def test_csv_colnames_equiv_datatype_fields():
-    yaml_dict, csv = csvy.load_csvy(data_path('csvy_full.csvy'))
+def test_csv_colnames_equiv_datatype_fields(csvy_full_fname):
+    yaml_dict, csv = csvy.load_csvy(csvy_full_fname)
     datatype_names = [od['name'] for od in yaml_dict['datatype']['fields']]
     for key in csv.columns:
         assert key in datatype_names
     for name in datatype_names:
         assert name in csv.columns
 
-def test_csvy_nocsv_data_is_none():
-    yaml_dict, csv = csvy.load_csvy(data_path('csvy_nocsv.csvy'))
+def test_csvy_nocsv_data_is_none(csvy_nocsv_fname):
+    yaml_dict, csv = csvy.load_csvy(csvy_nocsv_fname)
     assert csv is None
