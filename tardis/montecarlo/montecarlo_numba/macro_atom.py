7b8c86acdd916b29c4f0dcabf66c4cfd08ca2a55
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-06-27 12:59:20 -0400

    debugged macro_atom - very close now
    Co-authored-by: Christian Vogl <cvogl@mpa-garching.mpg.de>

--- tardis/montecarlo/montecarlo_numba/macro_atom.py
+++ tardis/montecarlo/montecarlo_numba/macro_atom.py
@@ -46,10 +46,13 @@ def macro_atom(r_packet, numba_plasma):
                 transition_id, r_packet.current_shell_id]
 
             probability += transition_probability
+
             if probability > probability_event:
-                current_transition_type = numba_plasma.transition_type[
-                    transition_id]
-                break
+              activation_level_id = numba_plasma.destination_level_id[
+                transition_id]
+              current_transition_type = numba_plasma.transition_type[
+                  transition_id]
+              break
 
         else:
             raise MacroAtomError(
@@ -61,54 +64,3 @@ def macro_atom(r_packet, numba_plasma):
         return numba_plasma.transition_line_id[transition_id]
     else:
         raise MacroAtomError('MacroAtom currently only allows BB transitions')
-"""
-#void
-#macro_atom (rpacket_t * packet, const storage_model_t * storage, rk_state *mt_state)
-#{
-  int emit = 0, i = 0, offset = -1;
-  uint64_t activate_level = rpacket_get_macro_atom_activation_level (packet);
-  while (emit >= 0)
-    {
-      double event_random = rk_double (mt_state);
-      i = storage->macro_block_references[activate_level] - 1;
-      double p = 0.0;
-      offset = storage->transition_probabilities_nd *
-                             rpacket_get_current_shell_id (packet);
-      do
-        {
-          ++i;
-          p += storage->transition_probabilities[offset + i];
-        }
-      while (p <= event_random);
-      emit = storage->transition_type[i];
-      activate_level = storage->destination_level_id[i];
-    }
-  switch (emit)
-    {
-      case BB_EMISSION:
-        line_emission (packet, storage, storage->transition_line_id[i], mt_state);
-        break;
-
-      case BF_EMISSION:
-        rpacket_set_current_continuum_id (packet, storage->transition_line_id[i]);
-        storage->last_line_interaction_out_id[rpacket_get_id (packet)] =
-          rpacket_get_current_continuum_id (packet);
-
-        continuum_emission (packet, storage, mt_state, sample_nu_free_bound, 3);
-        break;
-
-      case FF_EMISSION:
-        continuum_emission (packet, storage, mt_state, sample_nu_free_free, 4);
-        break;
-
-      case ADIABATIC_COOLING:
-        storage->last_interaction_type[rpacket_get_id (packet)] = 5;
-        rpacket_set_status (packet, TARDIS_PACKET_STATUS_REABSORBED);
-        break;
-
-      default:
-        fprintf (stderr, "This process for macro-atom deactivation should not exist! (emit = %d)\n", emit);
-        exit(1);
-    }
-}
-"""
