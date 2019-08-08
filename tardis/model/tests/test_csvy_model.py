d26ba34afe62bd801cfe5859a587a942760ec80f
Marc Williamson <marcwilliamson@Marcs-MacBook-Pro.local>
2019-08-08 16:40:32 +0200

    Modified test to have separate config files for csvy_model vs old model

--- tardis/model/tests/test_csvy_model.py
+++ tardis/model/tests/test_csvy_model.py
@@ -9,20 +9,24 @@ import pytest
 
 DATA_PATH = os.path.join(tardis.__path__[0],'model','tests','data')
 
-@pytest.fixture(scope="module", params=['config_csvy_full.yml',
-                                        'config_csvy_nocsv_branch85.yml',
-                                        'config_csvy_nocsv_uniform.yml',
-                                        'config_csvy_nocsv_powerlaw.yml',
-                                        'config_csvy_nocsv_exponential.yml',
-                                        'config_csvy_full_rad.yml'])
-def full_filename(request):
-    return os.path.join(DATA_PATH, request.param)
+@pytest.fixture(scope="module", params=[('config_csvy_full.yml','config_csvy_full_old.yml'),
+                                        ('config_csvy_nocsv_branch85.yml','config_csvy_nocsv_branch85_old.yml'),
+                                        ('config_csvy_nocsv_uniform.yml','config_csvy_nocsv_uniform_old.yml'),
+                                        ('config_csvy_nocsv_powerlaw.yml','config_csvy_nocsv_powerlaw_old.yml'),
+                                        ('config_csvy_nocsv_exponential.yml','config_csvy_nocsv_exponential_old.yml'),
+                                        ('config_csvy_full_rad.yml','config_csvy_full_rad_old.yml')])
+def filename(request):
+    fn_tup = request.param
+    fn = os.path.join(DATA_PATH, fn_tup[0])
+    fnold = os.path.join(DATA_PATH, fn_tup[1])
+    return fn, fnold
 
-
-def test_compare_models(full_filename):
-    tardis_config = Configuration.from_yaml(full_filename)
+def test_compare_models(filename):
+    fn, fnold = filename
+    tardis_config = Configuration.from_yaml(fn)
+    tardis_config_old = Configuration.from_yaml(fnold)
     csvy_model = Radial1DModel.from_csvy(tardis_config)
-    config_model = Radial1DModel.from_config(tardis_config)
+    config_model = Radial1DModel.from_config(tardis_config_old)
     csvy_model_props = csvy_model.get_properties().keys()
     config_model_props = config_model.get_properties().keys()
     npt.assert_array_equal(csvy_model_props, config_model_props)
