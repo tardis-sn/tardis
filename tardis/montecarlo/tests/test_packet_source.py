0717806c3e50bfba6d85691ead048c85ce0b1634
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2019-12-20 15:20:33 +0100

    tests for new packet source

--- tardis/montecarlo/tests/test_packet_source.py
+++ tardis/montecarlo/tests/test_packet_source.py
@@ -17,3 +17,10 @@ def test_bb_packet_source(data_path):
     packets = bb.create_blackbody_packet_nus(10000, 100)
     reference_packets = np.load(os.path.join(data_path, 'mc_packets_100_t10000.npy'))
     assert np.all(np.isclose(packets, reference_packets))
+
+def test_uniform_packet_energies(data_path):
+    bb = BlackBodySimpleSource(2508)
+    unif_energies = bb.create_uniform_packet_energies(100)
+    ref_energies = 0.01 * np.ones(100)
+    assert np.all(np.isclose(unif_energies, ref_energies))
+
