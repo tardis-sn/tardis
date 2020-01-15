2688238e43a9aad054d96df1dd2c432a36c73bfb
Marc Williamson <marcwilliamson@Marcs-MacBook-Pro.local>
2020-01-15 17:28:45 -0500

    Moved binary files to tardis-refdata unit_tests_data.h5

--- tardis/montecarlo/tests/test_packet_source.py
+++ tardis/montecarlo/tests/test_packet_source.py
@@ -13,9 +13,10 @@ def data_path():
     return os.path.join(tardis.__path__[0], 'montecarlo', 'tests', 'data')
 
 
-def test_bb_packet_sampling(data_path):
+def test_bb_packet_sampling(tardis_ref_data):
     bb = BlackBodySimpleSource(2508)
-    ref_df = pd.read_hdf('test_bb_sampling.h5')
+    #ref_df = pd.read_hdf('test_bb_sampling.h5')
+    ref_df = tardis_ref_data['/packet_unittest/blackbody']
     nus = bb.create_blackbody_packet_nus(10000, 100)
     mus = bb.create_zero_limb_darkening_packet_mus(100)
     unif_energies = bb.create_uniform_packet_energies(100)
