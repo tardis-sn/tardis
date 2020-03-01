bc7da8096985e81202b031e070101152b6910d5b
shreyansjain012@gmail.com <shreyansjain012@gmail.com>
2020-03-01 16:23:31 +0530

    removed cython boiler plate code

--- tardis/plasma/properties/util/macro_atom.py
+++ tardis/plasma/properties/util/macro_atom.py
@@ -1,41 +1,33 @@
-# module for fast macro_atom calculations
-
-# cython: profile=False
-# cython: boundscheck=False
-# cython: cdivision=True
-# cython: wraparound=False
-# cython: language_level=3
-
 import numpy as np
-
-cimport numpy as np
-
-ctypedef np.int64_t int_type_t
-
+import numba
 from astropy import constants
 
+# cimport numpy as np
 
-cdef extern from "math.h":
-    double exp(double)
+# ctypedef np.int64_t int_type_t
 
 
-cdef double h_cgs = constants.h.cgs.value
-cdef double c = constants.c.cgs.value
-cdef double kb = constants.k_B.cgs.value
-cdef double inv_c2 = 1 / (c ** 2)
+
+# cdef extern from "math.h":
+#     double exp(double)
+
+
+h_cgs = constants.h.cgs.value
+c = constants.c.cgs.value
+kb = constants.k_B.cgs.value
+inv_c2 = 1 / (c ** 2)
 
 
 def calculate_transition_probabilities(
-        double [:] transition_probability_coef,
-        double [:, ::1] beta_sobolev, double [:, ::1] j_blues,
-        double [:, ::1] stimulated_emission_factor,
-        int_type_t [:] transition_type,
-        int_type_t [:] lines_idx,
-        int_type_t [:] block_references,
-        double [:, ::1] transition_probabilities):
+        transition_probability_coef,
+        beta_sobolev, j_blues,
+        stimulated_emission_factor,
+        transition_type,
+        lines_idx,
+        block_references,
+        transition_probabilities):
 
-    cdef int i, j, k, line_idx
-    cdef np.ndarray[double, ndim=1] norm_factor = np.zeros(transition_probabilities.shape[1])
+    norm_factor = np.zeros(transition_probabilities.shape[1])
 
     for i in range(transition_probabilities.shape[0]):
         line_idx = lines_idx[i]
