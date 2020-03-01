f4a3da821aa7d079cf73075f54a6562d08a53ba9
ismail <ismail.gadzhiev@yahoo.com>
2020-03-01 19:03:08 +0300

    Added numba version of black_body_intensity

--- tardis/util/base.py
+++ tardis/util/base.py
@@ -3,7 +3,7 @@ import os
 import re
 from collections import OrderedDict
 
-import numexpr as ne
+from numba import njit
 import numpy as np
 import pandas as pd
 import yaml
@@ -243,21 +243,19 @@ def create_synpp_yaml(radial1d_mdl, fname, shell_no=0, lines_db=None):
         yaml.dump(yaml_reference, stream=f, explicit_start=True)
 
 
+@njit(parallel=True, error_model='numpy')
 def intensity_black_body(nu, T):
     """
     Calculate the intensity of a black-body according to the following formula
-
     .. math::
         I(\\nu, T) = \\frac{2h\\nu^3}{c^2}\frac{1}
         {e^{h\\nu \\beta_\\textrm{rad}} - 1}
-
     Parameters
     ----------
     nu: float
         Frequency of light
     T: float
         Temperature in kelvin
-
     Returns
     -------
     Intensity: float
@@ -265,8 +263,7 @@ def intensity_black_body(nu, T):
     """
     beta_rad = 1 / (k_B_cgs * T)
     coefficient = 2 * h_cgs / c_cgs ** 2
-    intensity = ne.evaluate('coefficient * nu**3 / '
-                            '(exp(h_cgs * nu * beta_rad) -1 )')
+    intensity = coefficient * nu ** 3 / (np.exp(h_cgs * nu * beta_rad) - 1)
     return intensity
 
 
