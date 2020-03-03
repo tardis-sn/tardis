e9a391d09f41116581c37446c79b08b27db59efe
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-04 00:44:44 +0530

    Update test_datatable.py
    Added testing for series dot multiplication and scalar multiplication

--- tardis/tests/test_datatable.py
+++ tardis/tests/test_datatable.py
@@ -10,11 +10,19 @@ import pandas as pd
 from tardis.datatable import DataTable
 import pytest
 
+
+
 def test_datatable():
+    """
+    Creating datatable and checking various operations
+    """
     df = pd.DataFrame( data = pd.np.random.randint(0, 100, (10, 5)) , columns = list('ABCED') )
     unit = pd.Series(['m', 's', 't', 'm/s', 'kg'])
     datatable = DataTable (df, units=['m', 's', 't', 'm/s', 'kg']) 
     
+    """
+    Assertions for units while copying, slicing and get unit function
+    """
     assert unit.equals(datatable.units)
      
     datatable2 = datatable.copy()
@@ -25,9 +33,21 @@ def test_datatable():
     
     assert datatable.get_unit("C") == 't'
     
+    """
+    Assertion while arithmetic operations
+    """
     datatable[columns[0]] += 1
     assert unit.equals(datatable.units)
     
     datatable[columns[3]] *= 2
     assert unit.equals(datatable.units)
     
+    """
+    Unit testing for scalar and series multiplication 
+    """
+    result = datatable.series_dot_product([1,2,3,4,5,6,7,8,9,10],"kg",datatable.columns)
+    assert result.units.equals(pd.Series(['mkg','skg','tkg','m/skg','kgkg']))
+
+    datatable.scalar_mul(2,"s",datatable.columns)
+    assert datatable.units.equals(pd.Series(['ms','ss','ts','m/ss','kgs']))
+    assert datatable.shape == datatable2.shape
