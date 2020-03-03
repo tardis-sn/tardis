cbb7dca5c8594df919440cda78d0e54339a9ab99
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-04 00:43:11 +0530

    Added Series Dot Multiplication and Scalar Multiplication
    Extended features of class DataTable by adding the above mentioned features

--- tardis/datatable.py
+++ tardis/datatable.py
@@ -90,3 +90,86 @@ class DataTable(pd.DataFrame):
                 print( "The unit for the following attribute is not set." )
         else :
             print( "The unit for the following attribute is not set." )
+            
+    
+    def scalar_mul(self, const, unit, attr):
+        
+        """
+        Multiplies a constant having unit to entire datatable
+        
+        Parameters
+        ----------
+        const: int
+            value of the scalar
+            
+        unit: string
+            unit of the scalar
+        
+        attr: array of string or int
+            list of columns to which scalar is multiplied
+        
+        """        
+        new_units = []
+        for i in range(len(self.units)):
+            if( list(self.columns) [i] in attr):
+                new_units.append(self.units[i]+unit)
+            else:
+                new_units.append(self.units[i])
+        
+        for i in attr:
+            if(i not in self.columns):
+                print("Wrong Attributes entered")
+                break
+            self[i] = const*self[i]
+            
+        self.units = pd.Series(new_units)   
+        
+        
+    def series_dot_product(self, series, unit, attr):
+        
+        """
+        Multiplies a constant having unit to some columns of datatable
+        
+        Parameters
+        ----------
+        series: Series or array
+            values of the series
+            
+        unit: string
+            unit of the scalar
+            
+        attr: array of string or int
+            list of columns to which series is multiplied
+            
+        Returns
+        ----------
+        object of class DataTable
+        
+        """
+        new_units = []
+        for i in range(len(self.units)):
+            if(list(self.columns)[i] in attr):
+                new_units.append(self.units[i]+unit)
+            else:
+                new_units.append(self.units[i])
+        
+        if self.shape[0] != len(list(series)):
+            print("The series dimension is not correct")
+        
+        else:
+            data2 = []
+            for i in attr:
+                if(i not in self.columns):
+                    print("Wrong Attributes entered")
+                    break
+                else:
+                    res = 0
+                    for j in range(self.shape[0]):
+                        res += self[i][j]*series[j]
+                    data2.append(res)
+        
+            dataframe = pd.DataFrame(data = [data2] ,columns=self.columns)
+            result = DataTable(dataframe)
+            result.units = pd.Series(new_units)
+            return result
+        
