2d43290a3bc09dbf1a791d32d6c6f39e06bb39be
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-03 17:03:06 +0530

    Update test_datatable.py

--- tardis/tests/test_datatable.py
+++ tardis/tests/test_datatable.py
@@ -11,12 +11,6 @@ from tardis.datatable import DataTable
 import pytest
 
 
-@pytest.mark.parametrize ( (" df" , "unit ") ,
-                         (pd.DataFrame( 
-                                 data = pd.np.random.randint(0, 100, (10, 5)) , 
-                                 columns = list('ABCED') ), 
-                         pd.Series(['m', 's', 't', 'm/s', 'kg']) ) )
-
 def test_datatable(df, unit):
     assert 1==1
     
