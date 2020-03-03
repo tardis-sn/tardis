28071cba5478a7957dff62f0aa1cc3e442d15bcc
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-03 11:57:49 +0530

    Fixed typo

--- tardis/tests/test_datatable.py
+++ tardis/tests/test_datatable.py
@@ -11,29 +11,28 @@ from datatable import DataTable
 import pytest
 
 
-def test_datatable():
-    unit = pd.Series(['m', 's', 't', 'm/s', 'kg'])
-    df = pd.DataFrame( 
-            data = pd.np.random.randint(0, 100, (10, 5)) , 
-            columns = list('ABCED') 
-            )
-    
+@pytest.mark.parametrize ( (" df" , "unit ") ,
+                         [pd.DataFrame( 
+                                 data = pd.np.random.randint(0, 100, (10, 5)) , 
+                                 columns = list('ABCED') ), 
+                         ['m', 's', 't', 'm/s', 'kg'] ] )
+
+def test_datatable(df, unit):
     datatable = DataTable (df, units=['m', 's', 't', 'm/s', 'kg']) 
     
     assert unit.equals(datatable.units)
-
+     
     datatable2 = datatable.copy()
     assert unit.equals(datatable2.units)
-
+    
     columns = datatable.columns
     assert datatable[[columns[1], columns[2]]].units.equals(unit)
-
+    
     assert datatable.get_unit("C") == 't'
-
+    
     datatable[columns[0]] += 1
     assert unit.equals(datatable.units)
-
+    
     datatable[columns[3]] *= 2
     assert unit.equals(datatable.units)
-
-
+    
