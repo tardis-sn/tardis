acc2114065058dd6b836d62b1eb1a3d83bc4819f
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-04 13:34:58 +0530

    Update test_datatable.py

--- tardis/tests/test_datatable.py
+++ tardis/tests/test_datatable.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 from tardis.datatable import DataTable
 import pytest
-
+import astropy.units as u
 
 
 def test_datatable():
@@ -17,8 +17,8 @@ def test_datatable():
     Creating datatable and checking various operations
     """
     df = pd.DataFrame( data = pd.np.random.randint(0, 100, (10, 5)) , columns = list('ABCED') )
-    unit = pd.Series(['m', 's', 't', 'm/s', 'kg'])
-    datatable = DataTable (df, units=['m', 's', 't', 'm/s', 'kg']) 
+    unit = pd.Series([u.meter, u.second, u.kg, u.meter/u.second, u.Ohm])
+    datatable = DataTable (df, units=[u.meter, u.second, u.kg, u.meter/u.second, u.Ohm]) 
     
     """
     Assertions for units while copying, slicing and get unit function
@@ -31,7 +31,7 @@ def test_datatable():
     columns = datatable.columns
     assert datatable[[columns[1], columns[2]]].units.equals(unit)
     
-    assert datatable.get_unit("C") == 't'
+    assert datatable.get_unit("C") == u.kg
     
     """
     Assertion while arithmetic operations
@@ -45,9 +45,14 @@ def test_datatable():
     """
     Unit testing for scalar and series multiplication 
     """
-    result = datatable.series_dot_product([1,2,3,4,5,6,7,8,9,10],"kg",datatable.columns)
-    assert result.units.equals(pd.Series(['mkg','skg','tkg','m/skg','kgkg']))
-
-    datatable.scalar_mul(2,"s",datatable.columns)
-    assert datatable.units.equals(pd.Series(['ms','ss','ts','m/ss','kgs']))
+    
+    datatable.scalar_mul(2,u.second,datatable.columns)
+    assert datatable.units.equals(pd.Series([u.meter*u.second, u.second*u.second, u.kg*u.second,
+                                          u.meter*u.second/u.second, u.Ohm*u.second]))
     assert datatable.shape == datatable2.shape
+    
+    df = pd.DataFrame([[0, 1, -2, -1], [1, 1, 1, 1]])
+    datatable3 = DataTable (df, units=[u.meter, u.second])
+    s = pd.Series([1, 1, 2, 1])
+    result = datatable3.dot(s, u.second, datatable3.columns)
+    assert result.units.equals(pd.Series([u.meter*u.second, u.second*u.second]))
