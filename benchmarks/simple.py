8ae72125dd4036b35b6b95f3c0ca4d9d4c33c067
yashasvi-goel <yashasvigoel@gmail.com>
2020-03-14 20:25:05 +0530

    updates

--- benchmarks/simple.py
+++
@@ -1,5 +0,0 @@
-def time_xrange():
-        import os
-#        os.system('ls')
-        os.system('tardis /mnt/0CAA0733AA0718BA/tardis/tardis_example/tardis_example.yml /mnt/0CAA0733AA0718BA/tardis/tardis_example/output_spectrum.dat')
-time_xrange.timeout=10000.0
