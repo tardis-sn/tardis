a68536e7055ca94208d51db5ca1791a2a99ce62c
jordi5 <jordi.eguren.brown@gmail.com>
2020-03-19 12:37:43 -0300

    Revert "	modified:   test_gui.py"
    This reverts commit b640779b8d5618dc52476471ef991c51c3a8fb07.
    
    	modified:   tardis/gui/tests/test_gui.py

--- tardis/gui/tests/test_gui.py
+++ tardis/gui/tests/test_gui.py
@@ -3,8 +3,11 @@ import pytest
 from tardis.io.config_reader import Configuration
 from tardis.simulation import Simulation
 import astropy.units as u
-import tardis.gui as gui
-from PyQt5 import QtWidgets
+import tardis.gui
+#from tardis.gui import interface
+#from tardis.gui.widgets import Tardis 
+#from tardis.gui.datahandler import SimpleTableModel
+#from PyQt5 import QtWidgets
 
 
 
@@ -38,12 +41,12 @@ def simulation_one_loop(
 
     return simulation
 
-@pytest.mark.skipif('QT_API' not in os.environ, reason="enviroment variable QT_API is not set")
+@pytest.mark.skip('test')#if('QT_API' not in os.environ, reason="enviroment variable QT_API is not set")
 def test_gui(simulation_one_loop):
 
     simulation = simulation_one_loop
     app = QtWidgets.QApplication([])
-    tablemodel = gui.datahandler.SimpleTableModel
-    win = gui.widgets.Tardis(tablemodel)
+    tablemodel = SimpleTableModel
+    win = Tardis(tablemodel)
     win.show_model(simulation)
     app.quit()
