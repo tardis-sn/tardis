84b2f4d23dcb2160ea653f147ec0430085415f45
Arjun Savel <asavel@berkeley.edu>
2020-05-29 10:27:07 -0400

    Moving log_decorator from base.py

--- tardis/montecarlo/montecarlo_numba/base.py
+++ tardis/montecarlo/montecarlo_numba/base.py
@@ -2,7 +2,6 @@ from numba import prange, njit, jit
 import logging
 import numpy as np
 
-# from tardis.montecarlo import __init__
 from tardis.montecarlo.montecarlo_numba.r_packet import RPacket, PacketStatus
 from tardis.montecarlo.montecarlo_numba.numba_interface import (
     PacketCollection, VPacketCollection, NumbaModel, numba_plasma_initialize,
@@ -90,42 +89,3 @@ def montecarlo_main_loop(packet_collection, numba_model, numba_plasma,
     packet_collection.packets_output_nu[:] = output_nus[:]
     
     return v_packets_energy_hist
-
-def log_decorator(func):
-    """
-    Decorator to log functions while in debug mode, i.e., when
-    `debug_montecarlo` is True in the config. Works for
-    `@jit'd and `@njit`'d functions, but with a significant speed
-    penalty.
-
-    Questions:
-        - stdout or print to file?
-
-    TODO: How do log *args?
-    TODO: How to pass kwargs to @jit, @njit?
-    TODO: in nopython mode: do I need a context manager?
-    TODO: Buffer?
-    TODO: make numpy docstring.
-    TODO: have this know debug_mode from the config.
-
-    :param func: function to be logged.
-    :return: either the function itself, if debug_mode is true, or
-    """
-    if __init__.DEBUG_MODE:
-        logger.setLevel(logging.DEBUG)
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
-        console_formatter = logging.Formatter(
-            '%(name)s - %(levelname)s - %(message)s')
-        console_handler.setFormatter(console_formatter)
-        logger.addHandler(console_handler)
-
-        def wrapper(*args, **kwargs):
-            logger.debug(f'Func: {func}. Input: {(args, kwargs)}')
-            result = func(*args)
-            logger.debug(f'Output: {result}.')
-            return result
-
-        return wrapper
-    else:
-        return func
