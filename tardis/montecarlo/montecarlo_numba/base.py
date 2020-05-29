69f2726beb1e926e23d3457b506b0f14d819066e
Arjun Savel <asavel@berkeley.edu>
2020-05-29 08:30:52 -0400

    Added basic logging capabilities to montecarlo_numba

--- tardis/montecarlo/montecarlo_numba/base.py
+++ tardis/montecarlo/montecarlo_numba/base.py
@@ -1,6 +1,7 @@
 from numba import prange, njit, jit
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import logging
 import numpy as np
 from tardis.montecarlo.montecarlo_numba.r_packet import RPacket, PacketStatus
 from tardis.montecarlo.montecarlo_numba.numba_interface import (
@@ -10,6 +11,8 @@ from tardis.montecarlo.montecarlo_numba.numba_interface import (
 from tardis.montecarlo.montecarlo_numba.single_packet_loop import (
     single_packet_loop)
 from tardis.montecarlo.montecarlo_numba import njit_dict
+
+logger = logging.getLogger(__name__)
 
 
 def montecarlo_radial1d(model, plasma, runner, montecarlo_configuration):
@@ -87,3 +90,32 @@ def montecarlo_main_loop(packet_collection, numba_model, numba_plasma,
     packet_collection.packets_output_nu[:] = output_nus[:]
     
     return v_packets_energy_hist
+
+def log_decorator(func):
+    """
+    Decorator to log functions while in debug mode, i.e., when
+    `debug_montecarlo` is True in the config. Works for
+    `@jit'd and `@njit`'d functions, but with a significant speed
+    penalty.
+
+    TODO: How do log *args?
+    TODO: How to pass kwargs to @jit, @njit?
+    TODO: in nopython mode: do I need a context manager?
+    TODO: Buffer?
+
+    :param func: function to be logged.
+    :return: either the function itself, if debug_mode is true, or
+    """
+    if DEBUG_MODE:
+        logging.basicConfig(level=logging.DEBUG,
+                                filename='myapp.log',
+                                format='%(asctime)s %(levelname)s:%(message)s')
+        def wrapper(A):
+    #         logging.debug(*args)
+            result = func(A)
+            if A % 10 == 0:
+                logging.debug(result)
+            return result
+        return wrapper
+    else:
+        return func
