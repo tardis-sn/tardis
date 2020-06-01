ded0d39daae50ad0716f629293c9953f57f12414
Arjun Savel <asavel@berkeley.edu>
2020-06-01 16:16:51 -0400

    Added crude buffer

--- tardis/montecarlo/montecarlo_numba/montecarlo_logger.py
+++ tardis/montecarlo/montecarlo_numba/montecarlo_logger.py
@@ -1,7 +1,9 @@
 import logging
 from functools import wraps
 
-DEBUG_MODE = True
+DEBUG_MODE = False
+BUFFER = 10
+ticker = 1
 
 def log_decorator(func):
     """
@@ -26,9 +28,12 @@ def log_decorator(func):
     # to ensure that calling `help` on the decorated function still works
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logger.debug(f'Func: {func.__name__}. Input: {(args, kwargs)}')
         result = func(*args, **kwargs)
-        logger.debug(f'Output: {result}.')
+        global ticker
+        ticker += 1
+        if ticker % BUFFER == 0:  # without a buffer, performance suffers
+            logger.debug(f'Func: {func.__name__}. Input: {(args, kwargs)}')
+            logger.debug(f'Output: {result}.')
         return result
 
     if DEBUG_MODE:
