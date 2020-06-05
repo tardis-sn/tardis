5aa18095580e23219b01963824efa10477655ac9
Arjun Savel <asavel@berkeley.edu>
2020-06-05 13:24:31 -0700

    Add jitclass arg for single packet seed in montecarloconfiguration

--- tardis/montecarlo/montecarlo_numba/numba_interface.py
+++ tardis/montecarlo/montecarlo_numba/numba_interface.py
@@ -153,9 +153,9 @@ monte_carlo_configuration_spec = [
     ('line_interaction_type', int64),
     ('number_of_vpackets', int64),
     ('temporary_v_packet_bins', int64),
-    ('full_relativity', boolean)
+    ('full_relativity', boolean),
+    ('single_packet_seed', int64)
 ]
-
 
 @jitclass(monte_carlo_configuration_spec)
 class MonteCarloConfiguration(object):
