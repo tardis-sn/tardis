4f0acca14f9f4e02a4ebaf338729cd75ad7a159e
Arjun Savel <asavel@berkeley.edu>
2020-06-05 12:19:30 -0700

    Move close_line_threshold check past montecarloexception

--- tardis/montecarlo/montecarlo_numba/r_packet.py
+++ tardis/montecarlo/montecarlo_numba/r_packet.py
@@ -89,15 +89,18 @@ def calculate_distance_line(r_packet, comov_nu, nu_line, time_explosion,
         return MISS_DISTANCE
 
     nu_diff = comov_nu - nu_line
-    if np.abs(nu_diff / comov_nu) < CLOSE_LINE_THRESHOLD:
-        nu_diff = 0.0
+
     if nu_diff <= 0:
         raise MonteCarloException(f'nu difference is less than 0.0; current '
-                                  f'packet nu={r_packet.nu}, '\
-                                  f'packet seed={r_packet.index}, '\
+                                  f'packet nu={r_packet.nu}, '
+                                  f'packet seed={r_packet.index}, '
                                   f'nu line: {nu_line}',
-                                    f'shell id: {r_packet.current_shell_id}')
+                                  f'shell id: {r_packet.current_shell_id}',
+                                  f'packet = {r_packet}')
 
+    # for numerical reasons, if line is too close we set the distance to 0.
+    if np.abs(nu_diff / comov_nu) < CLOSE_LINE_THRESHOLD:
+        nu_diff = 0.0
     if montecarlo_configuration.full_relativity:
         nu_r = nu_line / nu
         ct = C_SPEED_OF_LIGHT * time_explosion
