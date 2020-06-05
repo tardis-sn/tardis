8ebb33ca3659a207e7e64ebc2c25354c35ae013d
Arjun Savel <asavel@berkeley.edu>
2020-06-05 13:46:22 -0700

    Exceptions args must be compile-time constants in nopython mode

--- tardis/montecarlo/montecarlo_numba/r_packet.py
+++ tardis/montecarlo/montecarlo_numba/r_packet.py
@@ -91,12 +91,7 @@ def calculate_distance_line(r_packet, comov_nu, nu_line, time_explosion,
     nu_diff = comov_nu - nu_line
 
     if nu_diff <= 0:
-        raise MonteCarloException(f'nu difference is less than 0.0; current '
-                                  f'packet nu={r_packet.nu}, '
-                                  f'packet seed={r_packet.index}, '
-                                  f'nu line: {nu_line}',
-                                  f'shell id: {r_packet.current_shell_id}',
-                                  f'packet = {r_packet}')
+        raise MonteCarloException
 
     # for numerical reasons, if line is too close we set the distance to 0.
     if np.abs(nu_diff / comov_nu) < CLOSE_LINE_THRESHOLD:
