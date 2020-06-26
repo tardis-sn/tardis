05cad63b23b5c5b2087a14368f78303c27678977
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2020-06-26 15:14:42 -0400

    Code style black (#1201)
    * add pyproject with black settings
    
    * black codestyle for all except model, montecarlo
    
    * Update tardis/io/atom_data/util.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/io/atom_data/util.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/io/config_reader.py
    
    * Update tardis/io/config_reader.py
    
    * Update tardis/io/decay.py
    
    * Update tardis/io/util.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/plasma/properties/general.py
    
    Co-authored-by: Wolfgang Kerzendorf <wkerzendorf@gmail.com>
    
    * Update tardis/plasma/properties/general.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/plasma/properties/nlte.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/plasma/properties/partition_function.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>
    
    * Update tardis/plasma/properties/partition_function.py
    
    Co-authored-by: Wolfgang Kerzendorf <wkerzendorf@gmail.com>
    
    * Delete properties.py
    
    * Update tardis/plasma/standard_plasmas.py
    
    Co-authored-by: Wolfgang Kerzendorf <wkerzendorf@gmail.com>
    
    * Delete test_montecarlo.py
    
    Co-authored-by: Jaladh Singhal <jaladhsinghal@gmail.com>

--- tardis/io/parsers/blondin_toymodel.py
+++ tardis/io/parsers/blondin_toymodel.py
@@ -9,8 +9,8 @@ from astropy import units as u
 
 from tardis.util.base import parse_quantity
 
-PATTERN_REMOVE_BRACKET = re.compile(r'\[.+\]')
-T0_PATTERN = re.compile('tend = (.+)\n')
+PATTERN_REMOVE_BRACKET = re.compile(r"\[.+\]")
+T0_PATTERN = re.compile("tend = (.+)\n")
 
 
 def read_blondin_toymodel(fname):
@@ -32,64 +32,95 @@ def read_blondin_toymodel(fname):
     blondin_csv: pandas.DataFrame
         DataFrame containing the csv part of the toymodel
     """
-    with open(fname, 'r') as fh:
+    with open(fname, "r") as fh:
         for line in fh:
             if line.startswith("#idx"):
                 break
         else:
             raise ValueError(
-                'File {0} does not conform to Toy Model format as it does '
-                'not contain #idx')
-    columns = [PATTERN_REMOVE_BRACKET.sub('', item) for item in
-               line[1:].split()]
+                "File {0} does not conform to Toy Model format as it does "
+                "not contain #idx"
+            )
+    columns = [
+        PATTERN_REMOVE_BRACKET.sub("", item) for item in line[1:].split()
+    ]
 
-    raw_blondin_csv = pd.read_csv(fname, delim_whitespace=True, comment='#',
-                                  header=None, names=columns)
-    raw_blondin_csv.set_index('idx', inplace=True)
+    raw_blondin_csv = pd.read_csv(
+        fname, delim_whitespace=True, comment="#", header=None, names=columns
+    )
+    raw_blondin_csv.set_index("idx", inplace=True)
 
-    blondin_csv = raw_blondin_csv.loc[:,
-                  ['vel', 'dens', 'temp', 'X_56Ni0', 'X_Ti', 'X_Ca', 'X_S',
-                   'X_Si', 'X_O', 'X_C']]
-    rename_col_dict = {'vel': 'velocity', 'dens': 'density',
-                       'temp': 't_electron'}
+    blondin_csv = raw_blondin_csv.loc[
+        :,
+        [
+            "vel",
+            "dens",
+            "temp",
+            "X_56Ni0",
+            "X_Ti",
+            "X_Ca",
+            "X_S",
+            "X_Si",
+            "X_O",
+            "X_C",
+        ],
+    ]
+    rename_col_dict = {
+        "vel": "velocity",
+        "dens": "density",
+        "temp": "t_electron",
+    }
     rename_col_dict.update({item: item[2:] for item in blondin_csv.columns[3:]})
-    rename_col_dict['X_56Ni0'] = 'Ni56'
+    rename_col_dict["X_56Ni0"] = "Ni56"
     blondin_csv.rename(columns=rename_col_dict, inplace=True)
     blondin_csv.iloc[:, 3:] = blondin_csv.iloc[:, 3:].divide(
-        blondin_csv.iloc[:, 3:].sum(axis=1), axis=0)
+        blondin_csv.iloc[:, 3:].sum(axis=1), axis=0
+    )
 
     # changing velocities to outer boundary
-    new_velocities = 0.5 * (blondin_csv.velocity.iloc[
-                            :-1].values + blondin_csv.velocity.iloc[1:].values)
+    new_velocities = 0.5 * (
+        blondin_csv.velocity.iloc[:-1].values
+        + blondin_csv.velocity.iloc[1:].values
+    )
     new_velocities = np.hstack(
-        (new_velocities, [2 * new_velocities[-1] - new_velocities[-2]]))
-    blondin_csv['velocity'] = new_velocities
+        (new_velocities, [2 * new_velocities[-1] - new_velocities[-2]])
+    )
+    blondin_csv["velocity"] = new_velocities
 
-    with open(fname, 'r') as fh:
+    with open(fname, "r") as fh:
         t0_string = T0_PATTERN.findall(fh.read())[0]
 
-    t0 = parse_quantity(t0_string.replace('DAYS', 'day'))
+    t0 = parse_quantity(t0_string.replace("DAYS", "day"))
     blondin_dict = {}
-    blondin_dict['model_density_time_0'] = str(t0)
-    blondin_dict['description'] = 'Converted {0} to csvy format'.format(fname)
-    blondin_dict['tardis_model_config_version'] = 'v1.0'
-    blondin_dict_fields = [dict(name='velocity', unit='km/s',
-                                desc='velocities of shell outer bounderies.')]
+    blondin_dict["model_density_time_0"] = str(t0)
+    blondin_dict["description"] = "Converted {0} to csvy format".format(fname)
+    blondin_dict["tardis_model_config_version"] = "v1.0"
+    blondin_dict_fields = [
+        dict(
+            name="velocity",
+            unit="km/s",
+            desc="velocities of shell outer bounderies.",
+        )
+    ]
     blondin_dict_fields.append(
-        dict(name='density', unit='g/cm^3', desc='mean density of shell.'))
+        dict(name="density", unit="g/cm^3", desc="mean density of shell.")
+    )
     blondin_dict_fields.append(
-        dict(name='t_electron', unit='K', desc='electron temperature.'))
+        dict(name="t_electron", unit="K", desc="electron temperature.")
+    )
 
     for abund in blondin_csv.columns[3:]:
         blondin_dict_fields.append(
-            dict(name=abund, desc='Fraction {0} abundance'.format(abund)))
-    blondin_dict['datatype'] = {'fields': blondin_dict_fields}
+            dict(name=abund, desc="Fraction {0} abundance".format(abund))
+        )
+    blondin_dict["datatype"] = {"fields": blondin_dict_fields}
 
     return blondin_dict, blondin_csv
 
 
-def convert_blondin_toymodel(in_fname, out_fname, v_inner, v_outer,
-                             conversion_t_electron_rad=None):
+def convert_blondin_toymodel(
+    in_fname, out_fname, v_inner, v_outer, conversion_t_electron_rad=None
+):
     """
 
     Parameters
@@ -109,25 +140,29 @@ def convert_blondin_toymodel(in_fname, out_fname, v_inner, v_outer,
         outer boundary velocity. If float will be interpreted as km/s
     """
     blondin_dict, blondin_csv = read_blondin_toymodel(in_fname)
-    blondin_dict['v_inner_boundary'] = str(u.Quantity(v_inner, u.km / u.s))
-    blondin_dict['v_outer_boundary'] = str(u.Quantity(v_outer, u.km / u.s))
+    blondin_dict["v_inner_boundary"] = str(u.Quantity(v_inner, u.km / u.s))
+    blondin_dict["v_outer_boundary"] = str(u.Quantity(v_outer, u.km / u.s))
 
     if conversion_t_electron_rad is not None:
-        blondin_dict['datatype']['fields'].append({
-            'desc':
-                'converted radiation temperature '
-                'using multiplicative factor={0}'.format(
-                    conversion_t_electron_rad),
-            'name': 't_rad', 'unit': 'K'})
+        blondin_dict["datatype"]["fields"].append(
+            {
+                "desc": "converted radiation temperature "
+                "using multiplicative factor={0}".format(
+                    conversion_t_electron_rad
+                ),
+                "name": "t_rad",
+                "unit": "K",
+            }
+        )
 
-        blondin_csv['t_rad'] = (conversion_t_electron_rad *
-                                blondin_csv.t_electron)
+        blondin_csv["t_rad"] = (
+            conversion_t_electron_rad * blondin_csv.t_electron
+        )
 
-
-    csvy_file = '---\n{0}\n---\n{1}'.format(
+    csvy_file = "---\n{0}\n---\n{1}".format(
         yaml.dump(blondin_dict, default_flow_style=False),
-        blondin_csv.to_csv(index=False))
+        blondin_csv.to_csv(index=False),
+    )
 
-    with open(out_fname, 'w') as fh:
+    with open(out_fname, "w") as fh:
         fh.write(csvy_file)
-
--- tardis/util/base.py
+++ tardis/util/base.py
@@ -23,52 +23,66 @@ e_charge_gauss = constants.e.gauss.value
 logger = logging.getLogger(__name__)
 tardis_dir = os.path.realpath(tardis.__path__[0])
 
-ATOMIC_SYMBOLS_DATA = pd.read_csv(get_internal_data_path('atomic_symbols.dat'), delim_whitespace=True,
-                                  names=['atomic_number', 'symbol']).set_index('atomic_number').squeeze()
+ATOMIC_SYMBOLS_DATA = (
+    pd.read_csv(
+        get_internal_data_path("atomic_symbols.dat"),
+        delim_whitespace=True,
+        names=["atomic_number", "symbol"],
+    )
+    .set_index("atomic_number")
+    .squeeze()
+)
 
 ATOMIC_NUMBER2SYMBOL = OrderedDict(ATOMIC_SYMBOLS_DATA.to_dict())
-SYMBOL2ATOMIC_NUMBER = OrderedDict((y, x) for x, y in ATOMIC_NUMBER2SYMBOL.items())
+SYMBOL2ATOMIC_NUMBER = OrderedDict(
+    (y, x) for x, y in ATOMIC_NUMBER2SYMBOL.items()
+)
 
-synpp_default_yaml_fname = get_internal_data_path('synpp_default.yaml')
+synpp_default_yaml_fname = get_internal_data_path("synpp_default.yaml")
 
 
-NUMERAL_MAP = tuple(zip(
-    (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
-    ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-))
+NUMERAL_MAP = tuple(
+    zip(
+        (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+        ("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"),
+    )
+)
+
 
 class MalformedError(Exception):
     pass
 
 
 class MalformedSpeciesError(MalformedError):
-
     def __init__(self, malformed_element_symbol):
         self.malformed_element_symbol = malformed_element_symbol
 
     def __str__(self):
-        return ('Expecting a species notation (e.g. "Si 2", "Si II", "Fe IV") '
-                '- supplied {0}'.format(self.malformed_element_symbol))
+        return (
+            'Expecting a species notation (e.g. "Si 2", "Si II", "Fe IV") '
+            "- supplied {0}".format(self.malformed_element_symbol)
+        )
 
 
 class MalformedElementSymbolError(MalformedError):
-
     def __init__(self, malformed_element_symbol):
         self.malformed_element_symbol = malformed_element_symbol
 
     def __str__(self):
-        return ('Expecting an atomic symbol (e.g. Fe) - supplied {0}').format(
-            self.malformed_element_symbol)
+        return ("Expecting an atomic symbol (e.g. Fe) - supplied {0}").format(
+            self.malformed_element_symbol
+        )
 
 
 class MalformedQuantityError(MalformedError):
-
     def __init__(self, malformed_quantity_string):
         self.malformed_quantity_string = malformed_quantity_string
 
     def __str__(self):
-        return ('Expecting a quantity string(e.g. "5 km/s") for keyword '
-                '- supplied {0}').format(self.malformed_quantity_string)
+        return (
+            'Expecting a quantity string(e.g. "5 km/s") for keyword '
+            "- supplied {0}"
+        ).format(self.malformed_quantity_string)
 
 
 def int_to_roman(i):
@@ -90,7 +104,8 @@ def int_to_roman(i):
         count = i // integer
         result.append(numeral * count)
         i -= integer * count
-    return ''.join(result)
+    return "".join(result)
+
 
 def roman_to_int(roman_string):
     """
@@ -110,22 +125,29 @@ def roman_to_int(roman_string):
     NUMERALS_SET = set(list(zip(*NUMERAL_MAP))[1])
     roman_string = roman_string.upper()
     if len(set(list(roman_string.upper())) - NUMERALS_SET) != 0:
-        raise ValueError('{0} does not seem to be a roman numeral'.format(
-            roman_string))
+        raise ValueError(
+            "{0} does not seem to be a roman numeral".format(roman_string)
+        )
     i = result = 0
     for integer, numeral in NUMERAL_MAP:
-        while roman_string[i:i + len(numeral)] == numeral:
+        while roman_string[i : i + len(numeral)] == numeral:
             result += integer
             i += len(numeral)
     if result < 1:
-        raise ValueError('Can not interpret Roman Numeral {0}'.format(roman_string))
+        raise ValueError(
+            "Can not interpret Roman Numeral {0}".format(roman_string)
+        )
     return result
 
 
 def calculate_luminosity(
-        spec_fname, distance, wavelength_column=0,
-        wavelength_unit=u.angstrom, flux_column=1,
-        flux_unit=u.Unit('erg / (Angstrom cm2 s)')):
+    spec_fname,
+    distance,
+    wavelength_column=0,
+    wavelength_unit=u.angstrom,
+    flux_column=1,
+    flux_unit=u.Unit("erg / (Angstrom cm2 s)"),
+):
     """
     Calculates luminosity of star.
 
@@ -153,13 +175,15 @@ def calculate_luminosity(
     wavelength.max() : float
         Maximum value of wavelength of light
     """
-    #BAD STYLE change to parse quantity
+    # BAD STYLE change to parse quantity
     distance = u.Unit(distance)
 
-    wavelength, flux = np.loadtxt(spec_fname, usecols=(wavelength_column, flux_column), unpack=True)
+    wavelength, flux = np.loadtxt(
+        spec_fname, usecols=(wavelength_column, flux_column), unpack=True
+    )
 
     flux_density = np.trapz(flux, wavelength) * (flux_unit * wavelength_unit)
-    luminosity = (flux_density * 4 * np.pi * distance**2).to('erg/s')
+    luminosity = (flux_density * 4 * np.pi * distance ** 2).to("erg/s")
 
     return luminosity.value, wavelength.min(), wavelength.max()
 
@@ -184,66 +208,79 @@ def create_synpp_yaml(radial1d_mdl, fname, shell_no=0, lines_db=None):
         If the current dataset does not contain necessary reference files
     """
 
-    logger.warning('Currently only works with Si and a special setup')
+    logger.warning("Currently only works with Si and a special setup")
     if radial1d_mdl.atom_data.synpp_refs is not None:
         raise ValueError(
-            'The current atom dataset does not contain the '
-            'necessary reference files (please contact the authors)')
+            "The current atom dataset does not contain the "
+            "necessary reference files (please contact the authors)"
+        )
 
-    radial1d_mdl.atom_data.synpp_refs['ref_log_tau'] = -99.0
+    radial1d_mdl.atom_data.synpp_refs["ref_log_tau"] = -99.0
     for key, value in radial1d_mdl.atom_data.synpp_refs.iterrows():
         try:
-            radial1d_mdl.atom_data.synpp_refs['ref_log_tau'].loc[key] = np.log10(
-                radial1d_mdl.plasma.tau_sobolevs[0].loc[value['line_id']])
+            radial1d_mdl.atom_data.synpp_refs["ref_log_tau"].loc[
+                key
+            ] = np.log10(
+                radial1d_mdl.plasma.tau_sobolevs[0].loc[value["line_id"]]
+            )
         except KeyError:
             pass
 
-
     relevant_synpp_refs = radial1d_mdl.atom_data.synpp_refs[
-        radial1d_mdl.atom_data.synpp_refs['ref_log_tau'] > -50]
+        radial1d_mdl.atom_data.synpp_refs["ref_log_tau"] > -50
+    ]
 
     with open(synpp_default_yaml_fname) as stream:
         yaml_reference = yaml.load(stream, Loader=yaml.CLoader)
 
     if lines_db is not None:
-        yaml_reference['opacity']['line_dir'] = os.path.join(lines_db, 'lines')
-        yaml_reference['opacity']['line_dir'] = os.path.join(lines_db, 'refs.dat')
+        yaml_reference["opacity"]["line_dir"] = os.path.join(lines_db, "lines")
+        yaml_reference["opacity"]["line_dir"] = os.path.join(
+            lines_db, "refs.dat"
+        )
 
-    yaml_reference['output']['min_wl'] = float(
-        radial1d_mdl.runner.spectrum.wavelength.to('angstrom').value.min())
-    yaml_reference['output']['max_wl'] = float(
-        radial1d_mdl.runner.spectrum.wavelength.to('angstrom').value.max())
+    yaml_reference["output"]["min_wl"] = float(
+        radial1d_mdl.runner.spectrum.wavelength.to("angstrom").value.min()
+    )
+    yaml_reference["output"]["max_wl"] = float(
+        radial1d_mdl.runner.spectrum.wavelength.to("angstrom").value.max()
+    )
 
+    # raise Exception("there's a problem here with units what units does synpp expect?")
+    yaml_reference["opacity"]["v_ref"] = float(
+        (
+            radial1d_mdl.tardis_config.structure.v_inner[0].to("km/s")
+            / (1000.0 * u.km / u.s)
+        ).value
+    )
+    yaml_reference["grid"]["v_outer_max"] = float(
+        (
+            radial1d_mdl.tardis_config.structure.v_outer[-1].to("km/s")
+            / (1000.0 * u.km / u.s)
+        ).value
+    )
 
-    #raise Exception("there's a problem here with units what units does synpp expect?")
-    yaml_reference['opacity']['v_ref'] = float(
-        (radial1d_mdl.tardis_config.structure.v_inner[0].to('km/s') /
-         (1000. * u.km / u.s)).value)
-    yaml_reference['grid']['v_outer_max'] = float(
-        (radial1d_mdl.tardis_config.structure.v_outer[-1].to('km/s') /
-         (1000. * u.km / u.s)).value)
+    # pdb.set_trace()
 
-    #pdb.set_trace()
-
-    yaml_setup = yaml_reference['setups'][0]
-    yaml_setup['ions'] = []
-    yaml_setup['log_tau'] = []
-    yaml_setup['active'] = []
-    yaml_setup['temp'] = []
-    yaml_setup['v_min'] = []
-    yaml_setup['v_max'] = []
-    yaml_setup['aux'] = []
+    yaml_setup = yaml_reference["setups"][0]
+    yaml_setup["ions"] = []
+    yaml_setup["log_tau"] = []
+    yaml_setup["active"] = []
+    yaml_setup["temp"] = []
+    yaml_setup["v_min"] = []
+    yaml_setup["v_max"] = []
+    yaml_setup["aux"] = []
 
     for species, synpp_ref in relevant_synpp_refs.iterrows():
-        yaml_setup['ions'].append(100 * species[0] + species[1])
-        yaml_setup['log_tau'].append(float(synpp_ref['ref_log_tau']))
-        yaml_setup['active'].append(True)
-        yaml_setup['temp'].append(yaml_setup['t_phot'])
-        yaml_setup['v_min'].append(yaml_reference['opacity']['v_ref'])
-        yaml_setup['v_max'].append(yaml_reference['grid']['v_outer_max'])
-        yaml_setup['aux'].append(1e200)
+        yaml_setup["ions"].append(100 * species[0] + species[1])
+        yaml_setup["log_tau"].append(float(synpp_ref["ref_log_tau"]))
+        yaml_setup["active"].append(True)
+        yaml_setup["temp"].append(yaml_setup["t_phot"])
+        yaml_setup["v_min"].append(yaml_reference["opacity"]["v_ref"])
+        yaml_setup["v_max"].append(yaml_reference["grid"]["v_outer_max"])
+        yaml_setup["aux"].append(1e200)
 
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         yaml.dump(yaml_reference, stream=f, explicit_start=True)
 
 
@@ -269,8 +306,9 @@ def intensity_black_body(nu, T):
     """
     beta_rad = 1 / (k_B_cgs * T)
     coefficient = 2 * h_cgs / c_cgs ** 2
-    intensity = ne.evaluate('coefficient * nu**3 / '
-                            '(exp(h_cgs * nu * beta_rad) -1 )')
+    intensity = ne.evaluate(
+        "coefficient * nu**3 / " "(exp(h_cgs * nu * beta_rad) -1 )"
+    )
     return intensity
 
 
@@ -295,10 +333,10 @@ def species_tuple_to_string(species_tuple, roman_numerals=True):
     atomic_number, ion_number = species_tuple
     element_symbol = ATOMIC_NUMBER2SYMBOL[atomic_number]
     if roman_numerals:
-        roman_ion_number = int_to_roman(ion_number+1)
-        return '{0} {1}'.format(str(element_symbol), roman_ion_number)
+        roman_ion_number = int_to_roman(ion_number + 1)
+        return "{0} {1}".format(str(element_symbol), roman_ion_number)
     else:
-        return '{0} {1:d}'.format(element_symbol, ion_number)
+        return "{0} {1:d}".format(element_symbol, ion_number)
 
 
 def species_string_to_tuple(species_string):
@@ -322,15 +360,17 @@ def species_string_to_tuple(species_string):
     """
 
     try:
-        element_symbol, ion_number_string = re.match(r'^(\w+)\s*(\d+)',
-                                                     species_string).groups()
+        element_symbol, ion_number_string = re.match(
+            r"^(\w+)\s*(\d+)", species_string
+        ).groups()
     except AttributeError:
         try:
             element_symbol, ion_number_string = species_string.split()
         except ValueError:
             raise MalformedSpeciesError(
                 'Species string "{0}" is not of format <element_symbol><number>'
-                ' (e.g. Fe 2, Fe2, ..)'.format(species_string))
+                " (e.g. Fe 2, Fe2, ..)".format(species_string)
+            )
 
     atomic_number = element_symbol2atomic_number(element_symbol)
 
@@ -342,11 +382,14 @@ def species_string_to_tuple(species_string):
         except ValueError:
             raise MalformedSpeciesError(
                 "Given ion number ('{}') could not be parsed".format(
-                    ion_number_string))
+                    ion_number_string
+                )
+            )
 
     if ion_number > atomic_number:
         raise ValueError(
-            'Species given does not exist: ion number > atomic number')
+            "Species given does not exist: ion number > atomic number"
+        )
 
     return atomic_number, ion_number - 1
 
@@ -472,15 +515,18 @@ def quantity_linspace(start, stop, num, **kwargs):
     ValueError
         If start and stop values have no unit attribute.
     """
-    if not (hasattr(start, 'unit') and hasattr(stop, 'unit')):
-        raise ValueError('Both start and stop need to be quantities with a '
-                         'unit attribute')
+    if not (hasattr(start, "unit") and hasattr(stop, "unit")):
+        raise ValueError(
+            "Both start and stop need to be quantities with a " "unit attribute"
+        )
 
-    return (np.linspace(start.value, stop.to(start.unit).value, num, **kwargs)
-            * start.unit)
+    return (
+        np.linspace(start.value, stop.to(start.unit).value, num, **kwargs)
+        * start.unit
+    )
 
 
-def convert_abundances_format(fname, delimiter=r'\s+'):
+def convert_abundances_format(fname, delimiter=r"\s+"):
     """
     Changes format of file containing abundances into data frame
 
@@ -496,10 +542,9 @@ def convert_abundances_format(fname, delimiter=r'\s+'):
     DataFrame
         Corresponding data frame
     """
-    df = pd.read_csv(fname, delimiter=delimiter, comment='#', header=None)
+    df = pd.read_csv(fname, delimiter=delimiter, comment="#", header=None)
     # Drop shell index column
     df.drop(df.columns[0], axis=1, inplace=True)
     # Assign header row
-    df.columns = [nucname.name(i)
-                  for i in range(1, df.shape[1] + 1)]
+    df.columns = [nucname.name(i) for i in range(1, df.shape[1] + 1)]
     return df
