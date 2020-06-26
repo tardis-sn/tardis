eea405d45916a1dc274e2b5288d5f262f32ebb97
Jaladh Singhal <jaladhsinghal@gmail.com>
2020-06-27 00:18:37 +0530

    Update tardis/plasma/standard_plasmas.py
    Co-authored-by: Wolfgang Kerzendorf <wkerzendorf@gmail.com>

--- tardis/plasma/standard_plasmas.py
+++ tardis/plasma/standard_plasmas.py
@@ -146,10 +146,7 @@ def assemble_plasma(config, model, atom_data=None):
         )
         property_kwargs[JBluesDetailed] = {"w_epsilon": config.plasma.w_epsilon}
     else:
-        raise ValueError(
-            "radiative_rates_type type unknown - %s",
-            config.plasma.radiative_rates_type,
-        )
+        raise ValueError(f"radiative_rates_type type unknown - {config.plasma.radiative_rates_type}")
 
     if config.plasma.excitation == "lte":
         plasma_modules += lte_excitation_properties
