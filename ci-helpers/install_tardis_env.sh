74ae0fe8f14fc63b64997be0070067654ff3094a
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2020-07-19 20:00:40 +0530

    [MRG] Using Mamba in Pipelines (#1210)
    * testing mamba installation. Simply install mamba from conda and try to create env
    
    * added mamba after conda installation
    
    * check if mamba installation succesful
    
    * installing mamba before activating
    
    * fix syntax errors
    
    * Pin mamba to 0.4.0 and make pipelines exit on error with any bash command
    
    * move tardis_env.sh into pipelines file
    
    * fetch reference data fetching from bash scripts to azure pipelines file
    
    * fix syntax error
    
    * Added packet data
    
    * Adding variable shellopts to check
    
    * added shellopts=errexit so that build fails on non-zero error code
    
    * testing if pipeline fails
    
    * reverting changes, testing succesful
    
    * remove unused lines
    
    * testing, this should fail the pipeline
    
    * Made all single lines where necessary
    Added a step to fail build on error as suggested by epassaro
    improved documentation to steps
    
    * added back the git lfs pull code
    
    * removed git lfs codes
    
    * added git lfs to pipelines to make it faster

--- ci-helpers/install_tardis_env.sh
+++ ci-helpers/install_tardis_env.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 cd $TARDIS_BUILD_DIR
+
+# install mamba
+if test -e $HOME/miniconda/bin/mamba; then
+    echo "Mamba installed Succesfuly"
+
+else
+    echo "Mamba not installed"
+    conda install mamba=0.4.0 -c conda-forge -y
+    echo "Installed Mamba to correct location"
+fi
+
 if test -e  $HOME/miniconda/envs/tardis; then
     echo "TARDIS env already installed.";
     # Also check for tardis_env3.yml change
 else
-   conda env create -f tardis_env3.yml
+   # conda env create -f tardis_env3.yml
+   echo "Creating TARDIS environment using Mamba"
+   mamba env create -f tardis_env3.yml
    #trouble with building due to segfault at cython (https://github.com/cython/cython/issues/2199)
    #remove if we can get normal cython through conda
 fi
