02fce37467eb6ba3f050146dc31391e165aedff4
Ezequiel Pássaro <epassaro15@gmail.com>
2020-07-19 20:14:33 -0300

    Rename remote (3)

--- ci-helpers/fetch_reference_data.sh
+++ ci-helpers/fetch_reference_data.sh
@@ -5,22 +5,22 @@ if test -e $REF_DATA_DIR; then
     echo "Reference data available"
 
 else
-    git clone $REF_DATA_URL $REF_DATA_DIR
+    git clone $REF_DATA_URL $REF_DATA_DIR -o upstream
     cd $REF_DATA_DIR
 
     # Use the following to get the ref-data from the master;
-    git fetch origin
-    git checkout origin/master
+    git fetch upstream
+    git checkout upstream/master
 
     # Use the following to get the ref-data from a specific pull request
-    # git fetch origin pull/20/head:update-ref
+    # git fetch upstream pull/20/head:update-ref
     # git checkout update-ref
 
-    git lfs pull --include="atom_data/kurucz_cd23_chianti_H_He.h5" origin
-    git lfs pull --include="atom_data/chianti_He.h5" origin
-    git lfs pull --include="plasma_reference/" origin
-    git lfs pull --include="unit_test_data.h5" origin
-    git lfs pull --include="packet_unittest.h5" origin
+    git lfs pull --include="atom_data/kurucz_cd23_chianti_H_He.h5" upstream
+    git lfs pull --include="atom_data/chianti_He.h5" upstream
+    git lfs pull --include="plasma_reference/" upstream
+    git lfs pull --include="unit_test_data.h5" upstream
+    git lfs pull --include="packet_unittest.h5" upstream
 
     echo MD5: `md5sum unit_test_data.h5`
     echo MD5: `md5sum atom_data/kurucz_cd23_chianti_H_He.h5`
