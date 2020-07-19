bd67fc1dd7c42652f4ca942c5688df1e1af84414
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2020-07-19 20:00:40 +0530

    [MRG] Using Mamba in Pipelines (#1210)
    * testing mamba installation. Simply install mamba from conda and try to create env
    
    * added mamba after conda installation
    
    * check if mamba installation succesful
    
    * installing mamba before activating
    
    * fix syntax errors
    
    * Pin mamba to 0.4.0 and make pipelines exit on error with any bash command
    
    * move tardis_env.sh into pipelines file
    
    * fetch reference data fetching from bash scripts to azure pipelines file
    
    * fix syntax error
    
    * Added packet data
    
    * Adding variable shellopts to check
    
    * added shellopts=errexit so that build fails on non-zero error code
    
    * testing if pipeline fails
    
    * reverting changes, testing succesful
    
    * remove unused lines
    
    * testing, this should fail the pipeline
    
    * Made all single lines where necessary
    Added a step to fail build on error as suggested by epassaro
    improved documentation to steps
    
    * added back the git lfs pull code
    
    * removed git lfs codes
    
    * added git lfs to pipelines to make it faster

--- ci-helpers/install_miniconda.sh
+++ ci-helpers/install_miniconda.sh
@@ -6,6 +6,7 @@ if test -e $HOME/miniconda/bin; then
     hash -r
     #conda update --yes conda
 
+
 else
     wget $MINICONDA_URL -O miniconda.sh
     chmod +x miniconda.sh
@@ -15,6 +16,7 @@ else
     export PATH=$HOME/miniconda/bin:$PATH
     hash -r
     conda update --yes conda
+
 fi
 
 source $HOME/miniconda/etc/profile.d/conda.sh
