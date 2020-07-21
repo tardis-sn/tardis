f21c4fd7caa74499697634584424816cd21ff31e
jordi5 <jordi.eguren.brown@gmail.com>
2020-07-20 23:12:19 -0300

    make tests and test files more descriptive

--- tardis/model/tests/test_csvy_model.py
+++ tardis/model/tests/test_csvy_model.py
@@ -10,22 +10,18 @@ import pytest
 
 DATA_PATH = os.path.join(tardis.__path__[0],'model','tests','data')
 
-@pytest.fixture(scope="module", params=[('config_csvy_full.yml','config_csvy_full_old.yml'),
-                                        ('config_csvy_nocsv_branch85.yml','config_csvy_nocsv_branch85_old.yml'),
-                                        ('config_csvy_nocsv_uniform.yml','config_csvy_nocsv_uniform_old.yml'),
-                                        ('config_csvy_nocsv_powerlaw.yml','config_csvy_nocsv_powerlaw_old.yml'),
-                                        ('config_csvy_nocsv_exponential.yml','config_csvy_nocsv_exponential_old.yml'),
-                                        ('config_csvy_full_rad.yml','config_csvy_full_rad_old.yml')])
-def filename(request):
-    fn_tup = request.param
-    fn = os.path.join(DATA_PATH, fn_tup[0])
-    fnold = os.path.join(DATA_PATH, fn_tup[1])
-    return fn, fnold
+@pytest.fixture(scope="module", params=['model_full', 'branch85', 'uniform', 'powerlaw', 'exponential', 'radiative'])
+def model_config_files(request):
+    filename = request.param
+    csvy_config_file = os.path.join(DATA_PATH, filename,'_csvy.yml')
+    old_config_file = os.path.join(DATA_PATH, filename,'_old_config.yml')
+    return csvy_config_file, old_config_file
 
-def test_compare_models(filename):
-    fn, fnold = filename
-    tardis_config = Configuration.from_yaml(fn)
-    tardis_config_old = Configuration.from_yaml(fnold)
+def test_compare_models(model_config_files):
+    """Compare models produced by .from_config and .from_csvy to check that velocities, densities and abundances(pre and post decay) are the same"""
+    csvy_config_file, old_config_file = model_config_files
+    tardis_config = Configuration.from_yaml(csvy_config_file)
+    tardis_config_old = Configuration.from_yaml(old_config_file)
     csvy_model = Radial1DModel.from_csvy(tardis_config)
     config_model = Radial1DModel.from_config(tardis_config_old)
     csvy_model_props = csvy_model.get_properties().keys()
@@ -42,28 +38,36 @@ def test_compare_models(filename):
                 config_model_val = config_model_val.value
                 csvy_model_val = csvy_model_val.value
             npt.assert_array_almost_equal(csvy_model_val, config_model_val)
-    pd.testing.assert_frame_equal(csvy_model.abundance,config_model.abundance,check_less_precise = True)
-    pd.testing.assert_frame_equal(csvy_model.raw_abundance,config_model.raw_abundance)
-    pd.testing.assert_frame_equal(csvy_model.raw_isotope_abundance,config_model.raw_isotope_abundance)
+
+
+    assert csvy_model.raw_abundance.shape == config_model.raw_abundance.shape
+    assert csvy_model.raw_isotope_abundance.shape == config_model.raw_abundance.shape
+    assert csvy_model.abundance.shape == config_model.abundance.shape
+    npt.assert_array_almost_equal(csvy_model.raw_abundance.to_numpy(),config_model.raw_abundance.to_numpy())
+    npt.assert_array_almost_equal(csvy_model.raw_isotope_abundance.to_numpy(),config_model.raw_isotope_abundance.to_numpy())
+    npt.assert_array_almost_equal(csvy_model.abundance.to_numpy(),config_model.abundance.to_numpy())
 
 @pytest.fixture(scope='module')
-def csvy_hard_coded():
-    csvypath = os.path.join(DATA_PATH, 'csvy_hard_coded.yml')
+def csvy_model_abundances():
+    csvypath = os.path.join(DATA_PATH, 'csvy_model_abundance_dataframes.yml')
     config = Configuration.from_yaml(csvypath)
     csvy_hard_coded = Radial1DModel.from_csvy(config)
     return csvy_hard_coded 
  
-def test_csvy_input_abundance_storage(csvy_hard_coded):
-
-    #test abundance
+def test_read_csvy_abundances(csvy_model_abundances):
+    """test if model read abundances and isotope abundances and constructs dataframes appropiatly before applying decay"""
+    #rows in input_abundances represent element abundance in each of the 6 shells
     abundance_index = pd.Index([1,2],name = 'atomic_number')
     input_abundance = pd.DataFrame([[0.0,0.33,0.3,0.5,0.4,0.2], 
                                     [0.98,0.64,0.6,0.4,0.55,0.79]],
                                    index = abundance_index)
-
-    pd.testing.assert_frame_equal(csvy_hard_coded.raw_abundance,input_abundance,check_names = True)
     
-    #test isotopes
+    model_abundance_shape = csvy_model_abundances.raw_abundance.shape
+    input_shape = input_abundance.shape
+    assert model_abundance_shape == input_shape
+    npt.assert_array_almost_equal(input_abundance.to_numpy(),csvy_model_abundances.raw_abundance.to_numpy())
+    
+    #rows in input_isotopes represent isotope abundance in each of the 6 shells
     arrays = [[28],[56]]
     isotope_index = pd.MultiIndex.from_arrays(
             arrays, names=["atomic_number", "mass_number"]
@@ -72,13 +76,40 @@ def test_csvy_input_abundance_storage(csvy_hard_coded):
                                   columns=np.arange(6),
                                   index = isotope_index)
     
-    pd.testing.assert_frame_equal(csvy_hard_coded.raw_isotope_abundance,input_isotopes,check_names = True)
+    model_isotopes_shape = csvy_model_abundances.raw_isotope_abundance.shape
+    input_isotopes_shape = input_isotopes.shape
+    assert model_isotopes_shape == input_isotopes_shape
+    npt.assert_array_almost_equal(input_isotopes.to_numpy(),csvy_model_abundances.raw_isotope_abundance.to_numpy())
     
 
-#test decay
-def test_decay_dataframe(csvy_hard_coded):
+
+def test_csvy_model_decay(csvy_model_abundances):
+    """Compare model decay of abundance and hand decayed abundances:
+    Ni_halflife = 6.075 * u.d
+    Co_halflife = 77.233 * u.d
+
+    lambda_Ni = np.log(2) / Ni_halflife
+    lambda_Co = np.log(2) / Co_halflife
+
+    t = 4 * u.d means 4 days have passed since the explosion
+
+    def N1(N0, lambda1, t=4.0 * u.d):
+        return N0 * np.exp(-lambda1 * t)
+
+    
+    def N2(N1_0, lambda_1, lambda_2, t=4.0 * u.d):
+        return (lambda_1 * N1_0 
+                * (np.exp(-lambda_1 * t) / (lambda_2 - lambda_1)
+                + np.exp(-lambda_2 * t) / (lambda_1 - lambda_2)))
+
+     if the original Ni56 abundance for a given shell is 0.05, after 4 days:
+
+     cobalt_abundace_after_4_days = N2(0.05, lambda_Ni, lambda_Co)
+     nickel_abundace_after_4_days = N1(0.05, lambda_Ni)
+     iron_abundance_after_4_days = 0.05 - cobalt_abundace_after_4_days - nickel_abundace_after_4_days"""
+
     decay_index = pd.Index([1, 2, 26, 27, 28],name = 'atomic_number')
-    ref_abund = pd.DataFrame([
+    hand_decayed_abundance = pd.DataFrame([
                       [0.0,0.33,0.3,0.5,0.4,0.2], 
                       [0.98,0.64,0.6,0.4,0.55,0.79],
                       [0.00013977843354947162, 0.00020966765032420787, 0.0006988921677473642, 0.0006988921677473642, 0.0003494460838736821, 6.988921677473581e-05],
@@ -86,4 +117,7 @@ def test_decay_dataframe(csvy_hard_coded):
                       [0.012671293342497312, 0.019006940013745966, 0.06335646671248656, 0.06335646671248656, 0.03167823335624328, 0.006335646671248656]
                             ], index = decay_index)
 
-    pd.testing.assert_frame_equal(csvy_hard_coded.abundance,ref_abund,check_names = True, check_less_precise = True)
+    model_decayed_abundance_shape = csvy_model_abundances.abundance.shape
+    hand_decayed_abundance_shape = hand_decayed_abundance.shape
+    assert model_decayed_abundance_shape == hand_decayed_abundance_shape
+    npt.assert_array_almost_equal(decayed_abundance.to_numpy(),csvy_model_abundances.abundance.to_numpy())
