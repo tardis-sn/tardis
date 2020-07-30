bfbadbb12bfc03203988c051b51e24d1da31ef73
Arjun Savel <asavel@berkeley.edu>
2020-07-30 10:24:58 -0400

    Bring rpacket creation outside of if/else

--- tardis/montecarlo/montecarlo_numba/base.py
+++ tardis/montecarlo/montecarlo_numba/base.py
@@ -56,22 +56,16 @@ def montecarlo_main_loop(packet_collection, numba_model, numba_plasma,
     for i in prange(len(output_nus)):
         if montecarlo_configuration.single_packet_seed != -1:
             i = montecarlo_configuration.single_packet_seed
-            r_packet = RPacket(numba_model.r_inner[0],
-                               packet_collection.packets_input_mu[i],
-                               packet_collection.packets_input_nu[i],
-                               packet_collection.packets_input_energy[i],
-                               i)
             np.random.seed(i)
         else:
-            r_packet = RPacket(numba_model.r_inner[0],
-                               packet_collection.packets_input_mu[i],
-                               packet_collection.packets_input_nu[i],
-                               packet_collection.packets_input_energy[i],
-                               i)
             seed = packet_seeds[i]
             np.random.seed(seed)
 
-
+        r_packet = RPacket(numba_model.r_inner[0],
+                           packet_collection.packets_input_mu[i],
+                           packet_collection.packets_input_nu[i],
+                           packet_collection.packets_input_energy[i],
+                           i)
         vpacket_collection = VPacketCollection(
             spectrum_frequency, montecarlo_configuration.number_of_vpackets,
             montecarlo_configuration.temporary_v_packet_bins)
