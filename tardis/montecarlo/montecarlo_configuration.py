1df427cf7200d2c31d0538eae7deb2268e6201d3
Arjun Savel <asavel@berkeley.edu>
2020-08-20 12:07:17 -0400

    Add scatter-disabling functionality to global module

--- tardis/montecarlo/montecarlo_configuration.py
+++ tardis/montecarlo/montecarlo_configuration.py
@@ -1,3 +1,5 @@
+from tardis import constants as const
+
 full_relativity = True
 single_packet_seed = -1
 temporary_v_packet_bins = None
@@ -5,3 +7,5 @@ number_of_vpackets = 0
 montecarlo_seed = 0
 line_interaction_type = None
 packet_seeds = []
+SIGMA_THOMSON = const.sigma_T.to('cm^2').value
+disable_line_scattering = False
