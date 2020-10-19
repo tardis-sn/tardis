e01305a7058f5133de7c0f2113728ae7b183406a
Andrew Fullard <andrewgfullard@gmail.com>
2020-10-19 14:50:58 -0400

    Fixed vpacket trace within shell test

--- tardis/montecarlo/montecarlo_numba/tests/test_vpacket.py
+++ tardis/montecarlo/montecarlo_numba/tests/test_vpacket.py
@@ -12,19 +12,14 @@ from tardis.montecarlo.montecarlo_numba.numba_interface import Estimators
 from tardis.montecarlo.montecarlo_numba import macro_atom
 C_SPEED_OF_LIGHT = const.c.to('cm/s').value
 
-from numpy.testing import (
-        assert_equal,
-        assert_almost_equal,
-        assert_array_equal,
-        assert_allclose
-        )
+import numpy.testing as npt
 
 
 @pytest.fixture(scope="function")
 def v_packet():
     return vpacket.VPacket(
         r = 7.5e14,
-        nu = 0.4,
+        nu = 4e15,
         mu = 0.3,
         energy = 0.9,
         current_shell_id = 0,
@@ -33,41 +28,28 @@ def v_packet():
         is_close_line = 0
     )
 
-@pytest.fixture(scope="function")
-def model():
-    return numba_interface.NumbaModel(
-        r_inner = np.array([6.912e14, 8.64e14], dtype=np.float64),
-        r_outer = np.array([8.64e14, 1.0368e15], dtype=np.float64),
-        time_explosion = 5.2e7
-    )
+def v_packet_initialize_line_id(v_packet, numba_plasma, numba_model):
+        inverse_line_list_nu = numba_plasma.line_list_nu[::-1]
+        doppler_factor = r_packet.get_doppler_factor(v_packet.r, v_packet.mu,
+                                            numba_model.time_explosion)
+        comov_nu = v_packet.nu * doppler_factor
+        next_line_id = (len(numba_plasma.line_list_nu) -
+                        np.searchsorted(inverse_line_list_nu, comov_nu))
+        v_packet.next_line_id = next_line_id
 
-@pytest.fixture(scope="function")
-def plasma():
-    return numba_interface.NumbaPlasma(
-        electron_density=1.0e9*np.ones(2, dtype=np.float64),
-        line_list_nu=np.array([
-            1.26318289e+16,
-            1.26318289e+16,
-            1.23357675e+16,
-            1.23357675e+16,
-            1.16961598e+16], dtype=np.float64),
-        tau_sobolev=np.ones((2, 1000), dtype=np.float64),
-        transition_probabilities=np.zeros((2, 2), dtype=np.float64),
-        line2macro_level_upper=np.zeros(2, dtype=np.int64),
-        macro_block_references=np.zeros(2, dtype=np.int64),
-        transition_type=np.zeros(2, dtype=np.int64),
-        destination_level_id=np.zeros(2, dtype=np.int64),
-        transition_line_id=np.zeros(2, dtype=np.int64)
-    )
+#@pytest.mark.xfail(reason='To be implemented')
+def test_trace_vpacket_within_shell(v_packet, verysimple_numba_model, verysimple_numba_plasma):
+    #Give the vpacket a reasonable line ID
+    v_packet_initialize_line_id(v_packet, verysimple_numba_plasma, verysimple_numba_model)
 
+    tau_trace_combined, distance_boundary, delta_shell = vpacket.trace_vpacket_within_shell(
+                                                                                    v_packet,
+                                                                                    verysimple_numba_model,
+                                                                                    verysimple_numba_plasma)
 
-@pytest.mark.xfail(reason='To be implemented')
-def test_trace_vpacket_within_shell(v_packet, model, plasma):
-    #tau_trace_combined, distance_boundary, delta_shell = vpacket.trace_vpacket_within_shell(
-    #                                                                                v_packet,
-    #                                                                                model,
-    #                                                                                plasma)
-    assert False
+    npt.assert_almost_equal(tau_trace_combined, 8164850.891288479)
+    npt.assert_almost_equal(distance_boundary, 843684056256104.1)
+    assert delta_shell == 1
 
 
 @pytest.mark.xfail(reason='To be implemented')
