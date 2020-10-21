38a2129271d08c0b4481c5aac8c7fe35b1e4f0c0
Andrew Fullard <andrewgfullard@gmail.com>
2020-10-21 11:21:26 -0400

    Change numba error mode to numpy

--- tardis/montecarlo/montecarlo_numba/__init__.py
+++ tardis/montecarlo/montecarlo_numba/__init__.py
@@ -1,6 +1,6 @@
 from llvmlite import binding
 binding.set_option("tmp", "-non-global-value-max-name-size=2048")
-njit_dict = {'fastmath': True}
+njit_dict = {'fastmath': True, 'error_mode':'numpy'}
 
 from tardis.montecarlo.montecarlo_numba.r_packet import RPacket
 from tardis.montecarlo.montecarlo_numba.base import montecarlo_radial1d
