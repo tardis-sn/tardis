de14601734720c2d999d42428cb81609b185bb18
Andrew Fullard <andrewgfullard@gmail.com>
2020-10-29 14:12:34 -0400

    Added utility functions for gamma-rays
    Added utility functions and rearranged a lot

---
+++ tardis/energy_input/tests/test_util.py
@@ -0,0 +1,50 @@
+import pytest
+import astropy.units as u
+import numpy.testing as npt
+import numpy as np
+
+import tardis.energy_input.util as util
+from tardis import constants as const
+
+#this is terrible practice
+R_ELECTRON = 2.8179403227e-15
+
+@pytest.mark.parametrize(
+    ["energy", "expected"],
+    [(511.0e3, 1.0),
+    (255.5e3, 0.5),
+    (0.0, 0.0),
+    (511.0e10, 1e7)]
+)
+def test_kappa_calculation(energy, expected):
+    kappa = util.kappa_calculation(energy)
+    npt.assert_almost_equal(kappa, expected)
+
+@pytest.mark.parametrize(
+    ["energy", "theta_C"],
+    [(511.0e3, 1.0),
+    (255.5e3, np.pi),
+    (0.0, 2.0 * np.pi),
+    (511.0e10, np.pi / 2.0)]
+)
+def test_klein_nishina(energy, theta_C):
+    actual = util.klein_nishina(energy, theta_C)
+    
+    kappa = util.kappa_calculation(energy) 
+    
+    expected = R_ELECTRON / 2 * \
+            (1. + kappa * (1. - np.cos(theta_C))) ** -2. * \
+            (1. + np.cos(theta_C) ** 2. + \
+            (kappa ** 2. * (1. - np.cos(theta_C)) ** 2.) / (1. + kappa * (1. - np.cos(theta_C))))
+
+    npt.assert_almost_equal(actual, expected)
+
+
+@pytest.mark.xfail(reason="To be implemented")
+def test_compton_theta_distribution():
+    assert False
+
+
+@pytest.mark.xfail(reason="To be removed")
+def test_euler_rodrigues():
+    assert False
---
+++ tardis/energy_input/util.py
@@ -0,0 +1,54 @@
+import astropy.units as u
+import numpy as np
+
+R_ELECTRON = 2.8179403227e-15
+
+def kappa_calculation(energy):
+    k = energy / (511.0e3 * u.eV)
+    return k.value
+
+def euler_rodrigues(theta, direction):
+    a = np.cos(theta / 2)
+    b = direction.x * np.sin(theta / 2)
+    c = direction.y * np.sin(theta / 2)
+    d = direction.z * np.sin(theta / 2)
+    
+    er11 = a ** 2. + b ** 2. - c ** 2. - d ** 2.
+    er12 = 2. * (b * c - a * d)
+    er13 = 2. * (b * d + a * d)
+    
+    er21 = 2. * (b * c + a * d)
+    er22 = a ** 2. + c ** 2. - b ** 2. - d ** 2.
+    er23 = 2. * (c * d - a * b)
+    
+    er31 = 2. * (b * d - a * c)
+    er32 = 2. * (c * d + a * b)
+    er33 = a ** 2. + d ** 2. - b ** 2. - c ** 2.
+    
+    return np.array([[er11, er12, er13],
+                     [er21, er22, er23],
+                     [er31, er32, er33]])
+
+def klein_nishina(energy, theta_C):
+    kappa = kappa_calculation(energy) 
+    return R_ELECTRON / 2 * \
+            (1. + kappa * (1. - np.cos(theta_C))) ** -2. * \
+            (1. + np.cos(theta_C) ** 2. + \
+            (kappa ** 2. * (1. - np.cos(theta_C)) ** 2.) / (1. + kappa * (1. - np.cos(theta_C))))
+
+def compton_theta_distribution(energy, sample_resolution = 100):
+    dtheta = np.pi / sample_resolution
+    
+    theta_distribution = np.zeros(sample_resolution)
+    theta_angles = np.ones(sample_resolution) * np.pi
+    
+    theta = 0.
+    for i in range(sample_resolution - 1):
+        theta_distribution[i + 1] = \
+                theta_distribution[i] + klein_nishina(energy, theta)
+        theta_angles[i] = theta
+        theta += dtheta
+
+    norm_theta_distribution = theta_distribution / np.max(theta_distribution)
+    
+    return theta_angles, norm_theta_distribution
