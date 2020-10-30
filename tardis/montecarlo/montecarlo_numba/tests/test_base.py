a340ee9cda31de9d97b195d463439457292ccbe8
Andrew Fullard <andrewgfullard@gmail.com>
2020-10-30 14:50:38 -0400

    Improved C comparison test (still fails)

--- tardis/montecarlo/montecarlo_numba/tests/test_base.py
+++ tardis/montecarlo/montecarlo_numba/tests/test_base.py
@@ -6,8 +6,11 @@ import numpy as np
 import copy
 
 import tardis.montecarlo.montecarlo_numba.base as base
-from tardis.montecarlo.montecarlo_numba.numba_interface import PacketCollection
+from tardis.montecarlo.montecarlo_numba.numba_interface import PacketCollection, VPacketCollection
 from tardis.montecarlo import montecarlo_configuration as montecarlo_configuration
+import tardis.montecarlo.montecarlo_numba.numba_interface as numba_interface
+import tardis.montecarlo.montecarlo_numba.r_packet as r_packet
+import tardis.montecarlo.montecarlo_numba.single_packet_loop as spl
 
 @pytest.fixture()
 def read_c_test(tardis_ref_path):
@@ -30,13 +33,50 @@ def c_test_packet_collection(read_c_test):
         output_nu, output_energy
     )
 
+@pytest.fixture(scope="function")
+def model():
+    return numba_interface.NumbaModel(
+        r_inner = np.array([6.912e14, 8.64e14], dtype=np.float64),
+        r_outer = np.array([8.64e14, 1.0368e15], dtype=np.float64),
+        time_explosion = 5.2e7
+    )
+
+@pytest.fixture(scope="function")
+def estimators():
+    return numba_interface.Estimators(
+        j_estimator = np.array([0.0, 0.0], dtype=np.float64),
+        nu_bar_estimator = np.array([0.0, 0.0], dtype=np.float64),
+        j_blue_estimator = np.array([[1.e-10], [1.e-10]], dtype=np.float64),
+        Edotlu_estimator = np.array([[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]], dtype=np.float64)
+    )
+
+@pytest.fixture(scope="function")
+def plasma():
+    return numba_interface.NumbaPlasma(
+        electron_density = np.array([1.0e9, 1.0e9], dtype=np.float64),
+        line_list_nu = np.array([
+            1.26318289e+16,
+            1.26318289e+16,
+            1.23357675e+16,
+            1.23357675e+16,
+            1.16961598e+16], dtype=np.float64),
+        tau_sobolev = np.ones((500, 2), dtype=np.float64) * 1.0e-5,
+        transition_probabilities = np.array([[0], [0]], dtype=np.float64),
+        line2macro_level_upper = np.array([0, 0], dtype=np.int64),
+        macro_block_references = np.array([0, 0], dtype=np.int64),
+        transition_type = np.array([0, 0], dtype=np.int64),
+        destination_level_id = np.array([0, 0], dtype=np.int64),
+        transition_line_id = np.array([0, 0], dtype=np.int64)
+    )
+
+
 @pytest.mark.xfail(reason='To be implemented')
 def test_montecarlo_radial1d():
     assert False
 
 #@pytest.mark.xfail(reason='To be implemented')
 def test_montecarlo_main_loop(
-    c_test_packet_collection, verysimple_numba_model, verysimple_numba_plasma, verysimple_estimators,
+    c_test_packet_collection, model, plasma, estimators,
     nb_simulation_verysimple, set_seed_fixture
 ):
     montecarlo_configuration.single_packet_seed = 0
@@ -49,11 +89,39 @@ def test_montecarlo_main_loop(
         np.zeros(len(c_test_packet_collection.packets_input_nu), dtype=np.float64)
     )
 
-    set_seed_fixture(23111963)
-    base.montecarlo_main_loop(
-        output_packet_collection, verysimple_numba_model, verysimple_numba_plasma, verysimple_estimators,
-        nb_simulation_verysimple.runner.spectrum_frequency.value, 0, [23111963]
+    vpacket_collection = VPacketCollection(
+        np.array([0, 0], dtype=np.float64), 0, np.inf, 
+        0, 0
     )
 
-    npt.assert_allclose(output_packet_collection.packets_output_nu, c_test_packet_collection.packets_output_nu)
-    npt.assert_allclose(output_packet_collection.packets_output_energy, c_test_packet_collection.packets_output_energy)
+    output_nus = np.empty_like(output_packet_collection.packets_output_nu)
+    output_energies = np.empty_like(output_packet_collection.packets_output_nu)
+
+    set_seed_fixture(23111963)
+    seed = 23111963
+    for i in range(len(c_test_packet_collection.packets_input_nu)):
+        packet = r_packet.RPacket(model.r_inner[0],
+                           output_packet_collection.packets_input_mu[i],
+                           output_packet_collection.packets_input_nu[i],
+                           output_packet_collection.packets_input_energy[i],
+                           seed,
+                           i, 0)
+
+    
+        spl.single_packet_loop(
+            packet, model, plasma, estimators, vpacket_collection 
+        )
+
+        output_nus[i] = packet.nu
+
+        if packet.status == r_packet.PacketStatus.REABSORBED:
+            output_energies[i] = -packet.energy
+        elif packet.status == r_packet.PacketStatus.EMITTED:
+            output_energies[i] = packet.energy
+
+    # np.savetxt('scatter_output_energy.txt', output_energies)
+    output_packet_collection.packets_output_energy[:] = output_energies[:]
+    output_packet_collection.packets_output_nu[:] = output_nus[:]
+
+    npt.assert_allclose(output_packet_collection.packets_output_nu, c_test_packet_collection.packets_output_nu, rtol=1e-12)
+    npt.assert_allclose(output_packet_collection.packets_output_energy, c_test_packet_collection.packets_output_energy, rtol=1e-12)
