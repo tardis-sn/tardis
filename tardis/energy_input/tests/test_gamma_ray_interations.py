769f2fd759291958a0762c09e78138c593688076
Andrew Fullard <andrewgfullard@gmail.com>
2020-11-09 14:37:01 -0500

    Function documentation, some tests

--- tardis/energy_input/tests/test_gamma_ray_interations.py
+++ tardis/energy_input/tests/test_gamma_ray_interations.py
@@ -1,16 +1,31 @@
 import pytest
+import numpy.testing as npt
+
+from tardis.energy_input.gamma_ray_interactions import compton_scatter, \
+    pair_creation, photoabsorption, scatter_type
 
 @pytest.mark.xfail(reason="To be implemented")
 def test_compton_scatter():
     assert False
 
-@pytest.mark.xfail(reason="To be implemented")
-def test_pair_creation():
-    assert False
 
-@pytest.mark.xfail(reason="To be implemented")
-def test_photoabsorption():
-    assert False
+def test_pair_creation(basic_gamma_ray):
+    initial_mu = basic_gamma_ray.direction.mu
+
+    actual = pair_creation(basic_gamma_ray)
+
+    npt.assert_almost_equal(actual.energy, 511.0e3)
+    assert actual.direction.mu != initial_mu
+
+
+def test_photoabsorption(basic_gamma_ray):
+    
+    actual = photoabsorption(basic_gamma_ray)
+    expected = basic_gamma_ray.energy
+
+    npt.assert_almost_equal(actual, expected)
+    assert basic_gamma_ray.status == "Absorbed"
+
 
 @pytest.mark.xfail(reason="To be implemented")
 def test_scatter_type():
--- tardis/energy_input/util.py
+++ tardis/energy_input/util.py
@@ -4,10 +4,36 @@ import numpy as np
 R_ELECTRON = 2.8179403227e-15
 
 def kappa_calculation(energy):
+    """
+    Calculates kappa for various other calculations
+    i.e. energy normalized to 511 KeV
+
+    Parameters
+    ----------
+    energy : dtype float
+
+    Returns
+    -------
+    kappa : dtype float
+
+    """
     k = energy / (511.0e3 * u.eV)
     return k.value
 
 def euler_rodrigues(theta, direction):
+    """
+    Calculates the Euler-Rodrigues rotation matrix
+
+    Parameters
+    ----------
+    theta : dtype float
+    direction : SphericalVector object
+
+    Returns
+    -------
+    rotation matrix : Two dimensional Numpy array, dtype float
+
+    """
     a = np.cos(theta / 2)
     b = direction.x * np.sin(theta / 2)
     c = direction.y * np.sin(theta / 2)
@@ -30,6 +56,19 @@ def euler_rodrigues(theta, direction):
                      [er31, er32, er33]])
 
 def quadratic(b, c):
+    """
+    Solves the reduced quadratic equation
+
+    Parameters
+    ----------
+    b : dtype float
+    c : dtype float
+
+    Returns
+    -------
+    x1, x2 : dtype float
+
+    """
     delta = b ** 2 - 4.0 * c
     if delta > 0:
         delta = np.sqrt(delta)
@@ -46,6 +85,19 @@ def quadratic(b, c):
     return x1, x2
 
 def klein_nishina(energy, theta_C):
+    """
+    Calculates the Klein-Nishina equation
+
+    Parameters
+    ----------
+    energy : dtype float
+    theta_C : dtype float
+
+    Returns
+    -------
+     : dtype float
+
+    """
     kappa = kappa_calculation(energy) 
     return R_ELECTRON / 2 * \
             (1. + kappa * (1. - np.cos(theta_C))) ** -2. * \
@@ -53,6 +105,21 @@ def klein_nishina(energy, theta_C):
             (kappa ** 2. * (1. - np.cos(theta_C)) ** 2.) / (1. + kappa * (1. - np.cos(theta_C))))
 
 def compton_theta_distribution(energy, sample_resolution = 100):
+    """
+    Calculates the distribution of theta angles 
+    for Compton Scattering
+
+    Parameters
+    ----------
+    energy : dtype float
+    sample_resolution : dtype int
+
+    Returns
+    -------
+    theta_angles : One dimensional Numpy array, dtype float
+    norm_theta_distribution : One dimensional Numpy array, dtype float
+
+    """
     dtheta = np.pi / sample_resolution
     
     theta_distribution = np.zeros(sample_resolution)
