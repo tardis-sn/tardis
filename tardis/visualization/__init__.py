50f13ba63499d5371af790025c659483ef52bfd1
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-02-24 21:41:59 +0530

    Renamed widgets subpackage to visualization and restructured modules within it (#1462)
    * Reorganized widgets & tools modules
    
    * Changed import paths as per new subpackage structure
    
    * Updated imports in demo notebooks
    
    * Added EOF newline to init files

---
+++ tardis/visualization/__init__.py
@@ -0,0 +1,8 @@
+"""Visualization tools and widgets for TARDIS."""
+
+from tardis.visualization.widgets.shell_info import (
+    shell_info_from_simulation,
+    shell_info_from_hdf,
+)
+from tardis.visualization.widgets.line_info import LineInfoWidget
+from tardis.visualization.tools.sdec_plot import SDECPlotter
--- tardis/widgets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Visualization tools and widgets for TARDIS."""
-
-from tardis.widgets.shell_info import (
-    shell_info_from_simulation,
-    shell_info_from_hdf,
-)
-from tardis.widgets.line_info import LineInfoWidget
-from tardis.widgets.sdec_plot import SDECPlotter
