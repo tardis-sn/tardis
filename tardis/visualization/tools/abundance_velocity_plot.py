c96f0e176a09b6116bbdda83c6a29e0844dad1cd
antreev-brar <antreev.brar@gmail.com>
2021-03-19 09:55:34 +0530

    introduced_AVPlotter&AVData_class

--- tardis/visualization/tools/abundance_velocity_plot.py
+++ tardis/visualization/tools/abundance_velocity_plot.py
@@ -13,66 +13,144 @@ import plotly.graph_objects as go
 import numpy as np
     
 
-def abundance_velocity_matplot(sim):
-    """
-    Plots the abundance vs velocity of elements provided simulation result using matplotlib.
-    Parameters
-    ----------
-    sim : tardis.simulation.Simulation
-            TARDIS Simulation object produced by running a simulation
-    Returns
-    -------
-    None
-    """
-    plt.figure(figsize=(20,6))
+class AVData:
+    """The data of simulation model used by Abundace Velocity (AV) Plot.
 
-    # Data for plot
-    plotdf = (sim.model.abundance).T
-    velocity = sim.model.velocity
-    elements=plotdf.columns.values
-
-    # Plot line graph for each element
-    for atomic_number in elements:
-        plt.plot( velocity[:-1], plotdf[atomic_number],marker="o", markersize=12, linewidth=4,label=atomic_number2element_symbol(atomic_number))
+    This preprocesses the data required by AVPlotter class for doing
+    calculations and plotting
+    """
+    def __init__(
+        self,
+        AV_df,
+        velocity,
+        elements
+    ):
     
-    # Edit the layout
-    plt.title("Abundance vs Velocity", fontsize=18)
-    plt.xlabel("Velocity ( Inner Shell Boundary in " + str(sim.model.velocity.unit)+")")
-    plt.ylabel("Abundance (Fraction)")
-    plt.legend(loc="best",title="Atomic number", fontsize=10)
-    plt.show()
+        """
+        Initialize the AVData with required properties of simulation model.
 
+        AV_df : pd.DataFrame
+            Data about the atomic abundance present in simulation model
+        velocity : numpy.ndarray
+             An np array that stores inner shell velocities  in cm /s
+        elements : numpy.ndarray
+             It stores values of all elements present in the model 
+        """  
+        self.AV_df = AV_df
+        self.velocity = velocity
+        self.elements = elements
 
-def abundance_velocity_plotly(sim): 
-    """
-    Plots the abundance vs velocity of elements provided simulation result using plotly.
-    Parameters
-    ----------
-    sim : tardis.simulation.Simulation
+    @classmethod
+    def from_simulation(cls, sim):
+        """
+        Create an instance of AVData from a TARDIS simulation object.
+
+        Parameters
+        ----------
+        sim : tardis.simulation.Simulation
             TARDIS Simulation object produced by running a simulation
-    Returns
-    -------
-    None
+
+
+        Returns
+        -------
+        AVData
+        """
+        AV_df = (sim.model.abundance).T
+        velocity = sim.model.velocity[:-1]
+        elements = AV_df.columns.values
+        return cls(
+            AV_df = AV_df,
+            velocity = velocity,
+            elements = elements
+        )
+
+    
+
+class AVPlotter:
     """
-    fig = go.Figure()
+    Plotting interface for Abundance vs Velocity (AV) Plot.
 
-    # Data for plot
-    plotdf = (sim.model.abundance).T
-    velocity = sim.model.velocity
-    elements=plotdf.columns.values
+    It performs necessary calculations to generate AV Plot for a simulation
+    model, and allows to plot it in matplotlib and plotly.
+    """
 
-    # Plot line graph for each element
-    for atomic_number in elements:
-        fig.add_trace(go.Scatter(x=velocity, y=plotdf[atomic_number],
+    def __init__(self, data):
+        """
+        Initialize the AVPlotter with required data of simulation model.
+
+        Parameters
+        ----------
+        data : DataFrame 
+            Abundance and velocity data required for AV plot
+        """
+        self.data = data
+
+    @classmethod
+    def from_simulation(cls, sim):
+        """
+        Create an instance of AVPlotter from a TARDIS simulation object.
+
+        Parameters
+        ----------
+        sim : tardis.simulation.Simulation
+            TARDIS Simulation object produced by running a simulation
+
+        Returns
+        -------
+        AVPlotter
+        """
+        return cls( 
+                data = AVData.from_simulation(sim)   
+        )
+
+    def abundance_velocity_mpl(self):
+        """
+        Plots the abundance vs velocity of elements provided simulation result using matplotlib
+        
+        Returns
+        -------
+        None
+        """
+
+        plt.figure(figsize=(20,6))
+
+        # Plot line graph for each element
+        for atomic_number in self.data.elements:
+            plt.plot( self.data.velocity, self.data.AV_df[atomic_number],marker="o", markersize=12, linewidth=4,label=atomic_number2element_symbol(atomic_number))
+
+        # Edit the layout
+        plt.title("Abundance vs Velocity", fontsize=18)
+        plt.xlabel("Velocity ( Inner Shell Boundary in cm/s)")
+        plt.ylabel("Abundance (Fraction)")
+        plt.legend(loc="best",title="Atomic number", fontsize=10)
+        plt.show()
+
+
+
+    def abundance_velocity_plotly(self): 
+        """
+        Plots the abundance vs velocity of elements provided simulation result using plotly.
+        
+        Returns
+        -------
+        plotly.graph_objs._figure.Figure
+        """
+        fig = go.Figure()
+
+        # Plot line graph for each element
+        for atomic_number in self.data.elements:
+            fig.add_trace(go.Scatter(x=self.data.velocity, y=self.data.AV_df[atomic_number],
                     mode="lines+markers",
                     name=atomic_number2element_symbol(atomic_number)))
 
-    # Edit the layout
-    fig.update_layout(title="Abundance vs Velocity",
-                   xaxis_title="Velocity ( Inner Shell Boundary in " + str(sim.model.velocity.unit)+")",
+        # Edit the layout
+        fig.update_layout(title="Abundance vs Velocity",
+                   xaxis_title="Velocity ( Inner Shell Boundary in cm/s )",
                    yaxis_title="Abundance (Fraction)"
                    )
-    fig.update_xaxes(tickformat="e") 
-    fig.show()
+        fig.update_xaxes(tickformat="e") 
+        fig.show()
+
+        return fig
 
 
