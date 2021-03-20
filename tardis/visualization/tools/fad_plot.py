a91f67d66d34e43fac56d7f6405717821a6154eb
yuyizh <yuyizh1112@gmail.com>
2021-03-20 20:21:54 +0800

    reformat fad_plot with black

--- tardis/visualization/tools/fad_plot.py
+++ tardis/visualization/tools/fad_plot.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 
 from tardis.util.base import atomic_number2element_symbol
 
+
 class FADPlotter:
     """
     Plotting interface for Fractional AbunDance vs velocity for different shells (FAD) Plot.
@@ -16,13 +17,14 @@ class FADPlotter:
     It offers functions to generate FAD Plot for a simulation
     model, and allows to plot it in matplotlib and plotly.
     """
+
     def __init__(self, data):
         """
         Initialize the FADPlotter with required data of simulation model.
 
         Parameters
         ----------
-        data : DataFrame 
+        data : DataFrame
             Abundance and velocity data required for FAD plot
         """
         self.data = data
@@ -42,12 +44,15 @@ class FADPlotter:
         FADPlotter
         """
         data = sim.model.abundance.T
-        data.index = sim.model.velocity[1:]/100000 # inplace the columns with velocity of shell outer boundary
-        data.columns = data.columns.map(atomic_number2element_symbol) # map atomic number to element symbol
+        data.index = (
+            sim.model.velocity[1:] / 100000
+        )  # inplace the columns with velocity of shell outer boundary
+        data.columns = data.columns.map(
+            atomic_number2element_symbol
+        )  # map atomic number to element symbol
 
         return cls(data)
 
-    
     def generate_plot_mpl(self):
         """
         Generate Spectral element DEComposition (SDEC) Plot using matplotlib.
@@ -57,10 +62,14 @@ class FADPlotter:
         matplotlib.axes._subplots.AxesSubplot
         """
 
-        self.data.plot(figsize=(15, 5), title='Abundance as a function of velocity', marker='o')
-        plt.ylabel('Fractional Abundance')
-        plt.xlabel('Velocity of Shell Outer Boundary (km/s)')
-        plt.legend(loc='best', title='Element')
+        self.data.plot(
+            figsize=(15, 5),
+            title="Abundance as a function of velocity",
+            marker="o",
+        )
+        plt.ylabel("Fractional Abundance")
+        plt.xlabel("Velocity of Shell Outer Boundary (km/s)")
+        plt.legend(loc="best", title="Element")
 
         return plt.gca()
 
@@ -72,22 +81,26 @@ class FADPlotter:
         -------
         plotly.graph_objs._figure.Figure
         """
-        title = 'Fractional abundance vs velocity of different shells'
+        title = "Fractional abundance vs velocity of different shells"
         fig = go.Figure()
 
         for col in self.data.columns:
-            fig.add_trace(go.Line(x=self.data.index, y=self.data[col], name=self.data[col].name))
-            
+            fig.add_trace(
+                go.Line(
+                    x=self.data.index,
+                    y=self.data[col],
+                    name=self.data[col].name,
+                )
+            )
+
         fig.update_layout(
             xaxis=dict(
-                title='Velocity of Shell Outer Boundary (km/s)',
+                title="Velocity of Shell Outer Boundary (km/s)",
                 exponentformat="none",
-                ),
-            yaxis=dict(title='Fractional Abundance', exponentformat="e"),
+            ),
+            yaxis=dict(title="Fractional Abundance", exponentformat="e"),
             height=500,
-            title=title
-            )
-        
+            title=title,
+        )
+
         return fig
-
-
