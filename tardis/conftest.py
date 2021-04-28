3039051b55a9869c9deaafec4cc6cceb9b755905
rodot- <jto33@drexel.edu>
2021-04-28 11:39:59 -0400

    Updated tests and environment to exclude compilers and Cython

--- tardis/conftest.py
+++ tardis/conftest.py
@@ -91,7 +91,7 @@ try:
     PYTEST_HEADER_MODULES["Pandas"] = "pandas"
     PYTEST_HEADER_MODULES["Astropy"] = "astropy"
     PYTEST_HEADER_MODULES["Yaml"] = "yaml"
-    PYTEST_HEADER_MODULES["Cython"] = "cython"
+    #PYTEST_HEADER_MODULES["Cython"] = "cython"
     PYTEST_HEADER_MODULES["h5py"] = "h5py"
     PYTEST_HEADER_MODULES["Matplotlib"] = "matplotlib"
     PYTEST_HEADER_MODULES["Ipython"] = "IPython"
