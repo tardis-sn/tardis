f1659d308ef6b520f389784cf631221e823d9f0a
Andrew Fullard <andrewgfullard@gmail.com>
2021-05-03 11:47:26 -0400

    Existing tests fixed

--- tardis/energy_input/tests/test_energy_input_base.py
+++ tardis/energy_input/tests/test_energy_input_base.py
@@ -6,12 +6,12 @@ from tardis.model import Radial1DModel
 from tardis.io.config_reader import Configuration
 
 
-@pytest.mark.xfail(reason="To be implemented")
+@pytest.mark.skip(reason="To be implemented")
 def test_spawn_gamma_ray():
     assert False
 
 
-@pytest.mark.xfail(reason="To be implemented")
+@pytest.mark.skip(reason="To be implemented")
 def test_main_gamma_ray_loop():
 
     num_packets = 1e5
@@ -19,14 +19,12 @@ def test_main_gamma_ray_loop():
 
     np.random.seed(1)
 
-    config = Configuration.from_yaml(
-        "/home/afullard/tardis/tardis/io/tests/data/tardis_configv1_verysimple.yml"
-    )
+    config = Configuration.from_yaml("tardis_configv1_verysimple.yml")
 
     model = Radial1DModel.from_config(config)
 
     ejecta_energy, ejecta_energy_r, escape_energy, radii = main_gamma_ray_loop(
         num_packets,
         model,
-        path="/home/afullard/Downloads/tardisnuclear/decay_radiation.h5",
+        path="decay_radiation.h5",
     )
