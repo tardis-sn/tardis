1a926148f65a368a16406b595bf2ea276bc31f91
DhruvSondhi <dhruvsondhi05@gmail.com>
2021-05-16 20:28:48 +0530

    Change to condtion for mask_escatter in SDEC Plot

--- tardis/visualization/tools/sdec_plot.py
+++ tardis/visualization/tools/sdec_plot.py
@@ -877,7 +877,7 @@ class SDECPlotter:
             self.data[packets_mode].packets_df["last_interaction_type"][
                 self.packet_nu_range_mask
             ]
-            == 1
+            == InteractionType.ESCATTERING
         ) & (
             self.data[packets_mode].packets_df["last_line_interaction_in_id"][
                 self.packet_nu_range_mask
