2d3468ea83f08a70c5c2efd5b417d23f0d4ee2e1
rodot- <jto33@drexel.edu>
2021-06-10 10:14:43 -0400

    Better implimentation of the parallelism fix using numba typed lists to cache the whole vpacket collections (might be memory intensive)

--- tardis/montecarlo/montecarlo_numba/__init__.py
+++ tardis/montecarlo/montecarlo_numba/__init__.py
@@ -1,7 +1,7 @@
 from llvmlite import binding
 
 binding.set_option("tmp", "-non-global-value-max-name-size=2048")
-njit_dict = {"fastmath": True, "error_model": "numpy", "parallel": False, 'nogil':True}
+njit_dict = {"fastmath": True, "error_model": "numpy", "parallel": True, 'nogil':True}
 njit_dict_no_parallel = {"fastmath": True, "error_model": "numpy", "parallel": False, 'nogil':True}
 
 from tardis.montecarlo.montecarlo_numba.r_packet import RPacket
