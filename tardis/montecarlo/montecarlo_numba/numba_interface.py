bdabf896f7930ee9b3a710819f752e15e8345c59
smithis7 <smithis7@msu.edu>
2021-06-22 20:46:22 -0400

    added r and mu to vpacket logging

--- tardis/montecarlo/montecarlo_numba/numba_interface.py
+++ tardis/montecarlo/montecarlo_numba/numba_interface.py
@@ -196,6 +196,8 @@ vpacket_collection_spec = [
     ("v_packet_spawn_end_frequency", float64),
     ("nus", float64[:]),
     ("energies", float64[:]),
+    ("initial_mus", float64[:]),
+    ("initial_rs", float64[:]),
     ("idx", int64),
     ("number_of_vpackets", int64),
     ("length", int64),
@@ -222,6 +224,8 @@ class VPacketCollection(object):
         self.v_packet_spawn_end_frequency = v_packet_spawn_end_frequency
         self.nus = np.empty(temporary_v_packet_bins, dtype=np.float64)
         self.energies = np.empty(temporary_v_packet_bins, dtype=np.float64)
+        self.initial_mus = np.empty(temporary_v_packet_bins, dtype=np.float64)
+        self.initial_rs = np.empty(temporary_v_packet_bins, dtype=np.float64)
         self.number_of_vpackets = number_of_vpackets
         self.last_interaction_in_nu = np.zeros(
             temporary_v_packet_bins, dtype=np.float64
@@ -243,6 +247,8 @@ class VPacketCollection(object):
         self,
         nu,
         energy,
+        initial_mu,
+        initial_r,
         last_interaction_in_nu,
         last_interaction_type,
         last_interaction_in_id,
@@ -252,6 +258,8 @@ class VPacketCollection(object):
             temp_length = self.length * 2 + self.number_of_vpackets
             temp_nus = np.empty(temp_length, dtype=np.float64)
             temp_energies = np.empty(temp_length, dtype=np.float64)
+            temp_initial_mus = np.empty(temp_length, dtype=np.float64)
+            temp_initial_rs = np.empty(temp_length, dtype=np.float64)
             temp_last_interaction_in_nu = np.empty(
                 temp_length, dtype=np.float64
             )
@@ -260,6 +268,8 @@ class VPacketCollection(object):
             temp_last_interaction_out_id = np.empty(temp_length, dtype=np.int64)
             temp_nus[: self.length] = self.nus
             temp_energies[: self.length] = self.energies
+            temp_initial_mus[: self.length] = self.initial_mus
+            temp_initial_rs[: self.length] = self.initial_rs
             temp_last_interaction_in_nu[
                 : self.length
             ] = self.last_interaction_in_nu
@@ -275,6 +285,8 @@ class VPacketCollection(object):
 
             self.nus = temp_nus
             self.energies = temp_energies
+            self.initial_mus = temp_initial_mus
+            self.initial_rs = temp_initial_rs
             self.last_interaction_in_nu = temp_last_interaction_in_nu
             self.last_interaction_type = temp_last_interaction_type
             self.last_interaction_in_id = temp_last_interaction_in_id
@@ -283,6 +295,8 @@ class VPacketCollection(object):
 
         self.nus[self.idx] = nu
         self.energies[self.idx] = energy
+        self.initial_mus[self.idx] = initial_mu
+        self.initial_rs[self.idx] = initial_r
         self.last_interaction_in_nu[self.idx] = last_interaction_in_nu
         self.last_interaction_type[self.idx] = last_interaction_type
         self.last_interaction_in_id[self.idx] = last_interaction_in_id
