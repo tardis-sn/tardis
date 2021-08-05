6a2fcf38bc7faff0feab0ca92912c402f587b7fb
Dhruv Sondhi <dhruvsondhi05@gmail.com>
2021-08-05 11:20:22 +0530

    Changed set_properties func to track, Removed track_r_packet func from r_packet.py

--- tardis/montecarlo/montecarlo_numba/single_packet_loop.py
+++ tardis/montecarlo/montecarlo_numba/single_packet_loop.py
@@ -72,7 +72,7 @@ def single_packet_loop(
     )
 
     if montecarlo_configuration.RPACKET_TRACKING:
-        track_r_packet(r_packet, tracked_rpacket_properties)
+        tracked_rpacket_properties.track(r_packet)
 
     while r_packet.status == PacketStatus.IN_PROCESS:
         distance, interaction_type, delta_shell = trace_packet(
@@ -88,7 +88,7 @@ def single_packet_loop(
             )
 
             if montecarlo_configuration.RPACKET_TRACKING:
-                track_r_packet(r_packet, tracked_rpacket_properties)
+                tracked_rpacket_properties.track(r_packet)
 
         elif interaction_type == InteractionType.LINE:
             r_packet.last_interaction_type = 2
@@ -107,7 +107,7 @@ def single_packet_loop(
             )
 
             if montecarlo_configuration.RPACKET_TRACKING:
-                track_r_packet(r_packet, tracked_rpacket_properties)
+                tracked_rpacket_properties.track(r_packet)
 
         elif interaction_type == InteractionType.ESCATTERING:
             r_packet.last_interaction_type = 1
@@ -122,7 +122,7 @@ def single_packet_loop(
             )
 
             if montecarlo_configuration.RPACKET_TRACKING:
-                track_r_packet(r_packet, tracked_rpacket_properties)
+                tracked_rpacket_properties.track(r_packet)
 
     # check where else initialize line ID happens!
 
