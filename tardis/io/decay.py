c0eddab46b0b9f1ccc0a87a2986dbb3aadf40847
Thom Lemoine <lemointm@whitman.edu>
2021-10-15 15:03:43 -0700

    Added rd decay calculations to decay.py

--- tardis/io/decay.py
+++ tardis/io/decay.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from radioactivedecay import Nuclide, Inventory
+from radioactivedecay.utils import Z_to_elem
 from pyne import nucname, material
 from astropy import units as u
 
@@ -23,7 +25,7 @@ class IsotopeAbundances(pd.DataFrame):
     def _update_material(self):
         self.comp_dicts = [dict() for i in range(len(self.columns))]
         for (atomic_number, mass_number), abundances in self.iterrows():
-            nuclear_symbol = f"{nucname.name(atomic_number)}{mass_number}"
+            nuclear_symbol = f"{Z_to_elem(atomic_number)}{mass_number}"
             for i in range(len(self.columns)):
                 self.comp_dicts[i][nuclear_symbol] = abundances[i]
 
@@ -48,10 +50,33 @@ class IsotopeAbundances(pd.DataFrame):
                 abundances.loc[cls.id_to_tuple(key), i] = value
 
         return cls(abundances)
+    
+    @classmethod
+    def from_inventories(cls, inventories):
+        multi_index_tuples = set([])
+        for inventory in inventories:
+            multi_index_tuples.update(
+                [cls.id_to_tuple(key) for key in inventory.contents.keys()]
+            )
+
+        index = pd.MultiIndex.from_tuples(
+            multi_index_tuples, names=["atomic_number", "mass_number"]
+        )
+
+        abundances = pd.DataFrame(
+            data=0.0, index=index, columns=range(len(inventories))
+        )
+
+        for i, inventory in enumerate(inventories):
+            for nuclide, abundance in inventory.mass_fractions():
+                abundances.loc[cls.id_to_tuple(nuclide), i] = abundance
+
+        return cls(abundances)
 
     @staticmethod
     def id_to_tuple(atomic_id):
-        return nucname.znum(atomic_id), nucname.anum(atomic_id)
+        nuclide = Nuclide(atomic_id)
+        return nuclide.Z, nuclide.A
 
     def to_materials(self):
         """
@@ -66,10 +91,28 @@ class IsotopeAbundances(pd.DataFrame):
 
         comp_dicts = [dict() for i in range(len(self.columns))]
         for (atomic_number, mass_number), abundances in self.iterrows():
-            nuclear_symbol = f"{nucname.name(atomic_number):s}{mass_number:d}"
+            nuclear_symbol = f"{Z_to_elem(atomic_number)}{mass_number}"
             for i in range(len(self.columns)):
                 comp_dicts[i][nuclear_symbol] = abundances[i]
         return [material.Material(comp_dict) for comp_dict in comp_dicts]
+    
+    def to_inventories(self):
+        """
+        Convert DataFrame to a list of inventories interpreting the MultiIndex as
+        atomic_number and mass_number
+
+        Returns
+        -------
+        list
+            list of radioactivedecay Inventories
+        """
+
+        comp_dicts = [dict() for i in range(len(self.columns))]
+        for (atomic_number, mass_number), abundances in self.iterrows():
+            nuclear_symbol = f"{Z_to_elem(atomic_number)}{mass_number}"
+            for i in range(len(self.columns)):
+                comp_dicts[i][nuclear_symbol] = abundances[i]
+        return [Inventory(comp_dict, "Kg") for comp_dict in comp_dicts]
 
     def decay(self, t):
         """
@@ -86,14 +129,14 @@ class IsotopeAbundances(pd.DataFrame):
             Decayed abundances
         """
 
-        materials = self.to_materials()
+        inventories = self.to_inventories()
         t_second = (
             u.Quantity(t, u.day).to(u.s).value - self.time_0.to(u.s).value
         )
-        decayed_materials = [item.decay(t_second) for item in materials]
-        for i in range(len(materials)):
-            materials[i].update(decayed_materials[i])
-        df = IsotopeAbundances.from_materials(materials)
+        decayed_inventories = [item.decay(t_second) for item in inventories]
+        for i in range(len(inventories)):
+            inventories[i].update(decayed_inventories[i])
+        df = IsotopeAbundances.from_inventories(inventories)
         df.sort_index(inplace=True)
         return df
 
