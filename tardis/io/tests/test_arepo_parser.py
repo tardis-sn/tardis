4c167df43fc50c7f71b9f65f342947cbcf6bf758
AlexHls <alex.holas@gmx.de>
2022-01-21 16:33:54 +0100

    Fixed formatting of test [build docs]

--- tardis/io/tests/test_arepo_parser.py
+++ tardis/io/tests/test_arepo_parser.py
@@ -8,29 +8,38 @@ import json
 
 DATA_PATH = os.path.join(tardis.__path__[0], "io", "tests", "data")
 
+
 @pytest.fixture
 def get_line_csvy_model():
-    with open(os.path.join(DATA_PATH, 'arepo_snapshot.json'), 'r') as json_file:
+    with open(os.path.join(DATA_PATH, "arepo_snapshot.json"), "r") as json_file:
         data = json.loads(json.load(json_file))
 
-    pos, vel, rho, nucs, time = data["pos"], data["vel"], data["rho"], data["xnuc"], data["time"]
+    pos, vel, rho, nucs, time = (
+        data["pos"],
+        data["vel"],
+        data["rho"],
+        data["xnuc"],
+        data["time"],
+    )
     pos = np.array(pos)
     vel = np.array(vel)
     rho = np.array(rho)
 
     # The nuclear data should be in a dict where each element has its own entry (with the key being the element name)
     xnuc = {
-        "ni56" : np.array(nucs[0]),
-        "si28" : np.array(nucs[1]),
+        "ni56": np.array(nucs[0]),
+        "si28": np.array(nucs[1]),
     }
 
     profile = arepo.LineProfile(pos, vel, rho, xnuc, time)
 
     profile.create_profile(inner_radius=1e11, outer_radius=2e11)
 
-    testfile = profile.export(20, os.path.join(DATA_PATH, 'arepo_parser_test.csvy'))
+    testfile = profile.export(
+        20, os.path.join(DATA_PATH, "arepo_parser_test.csvy")
+    )
 
-    with open(testfile, 'r') as file:
+    with open(testfile, "r") as file:
         data = file.readlines()
 
     os.remove(testfile)
@@ -40,27 +49,37 @@ def get_line_csvy_model():
 
 @pytest.fixture
 def get_cone_csvy_model():
-    with open(os.path.join(DATA_PATH, 'arepo_snapshot.json'), 'r') as json_file:
+    with open(os.path.join(DATA_PATH, "arepo_snapshot.json"), "r") as json_file:
         data = json.loads(json.load(json_file))
 
-    pos, vel, rho, nucs, time = data["pos"], data["vel"], data["rho"], data["xnuc"], data["time"]
+    pos, vel, rho, nucs, time = (
+        data["pos"],
+        data["vel"],
+        data["rho"],
+        data["xnuc"],
+        data["time"],
+    )
     pos = np.array(pos)
     vel = np.array(vel)
     rho = np.array(rho)
 
     # The nuclear data should be in a dict where each element has its own entry (with the key being the element name)
     xnuc = {
-        "ni56" : np.array(nucs[0]),
-        "si28" : np.array(nucs[1]),
+        "ni56": np.array(nucs[0]),
+        "si28": np.array(nucs[1]),
     }
 
     profile = arepo.ConeProfile(pos, vel, rho, xnuc, time)
 
-    profile.create_profile(opening_angle=40, inner_radius=1e11, outer_radius=2e11)
+    profile.create_profile(
+        opening_angle=40, inner_radius=1e11, outer_radius=2e11
+    )
 
-    testfile = profile.export(20, os.path.join(DATA_PATH, 'arepo_parser_test.csvy'))
+    testfile = profile.export(
+        20, os.path.join(DATA_PATH, "arepo_parser_test.csvy")
+    )
 
-    with open(testfile, 'r') as file:
+    with open(testfile, "r") as file:
         data = file.readlines()
 
     print(testfile)
@@ -71,27 +90,35 @@ def get_cone_csvy_model():
 
 @pytest.fixture
 def get_full_csvy_model():
-    with open(os.path.join(DATA_PATH, 'arepo_snapshot.json'), 'r') as json_file:
+    with open(os.path.join(DATA_PATH, "arepo_snapshot.json"), "r") as json_file:
         data = json.loads(json.load(json_file))
 
-    pos, vel, rho, nucs, time = data["pos"], data["vel"], data["rho"], data["xnuc"], data["time"]
+    pos, vel, rho, nucs, time = (
+        data["pos"],
+        data["vel"],
+        data["rho"],
+        data["xnuc"],
+        data["time"],
+    )
     pos = np.array(pos)
     vel = np.array(vel)
     rho = np.array(rho)
 
     # The nuclear data should be in a dict where each element has its own entry (with the key being the element name)
     xnuc = {
-        "ni56" : np.array(nucs[0]),
-        "si28" : np.array(nucs[1]),
+        "ni56": np.array(nucs[0]),
+        "si28": np.array(nucs[1]),
     }
 
     profile = arepo.FullProfile(pos, vel, rho, xnuc, time)
 
     profile.create_profile(inner_radius=1e11, outer_radius=2e11)
 
-    testfile = profile.export(20, os.path.join(DATA_PATH, 'arepo_parser_test.csvy'))
+    testfile = profile.export(
+        20, os.path.join(DATA_PATH, "arepo_parser_test.csvy")
+    )
 
-    with open(testfile, 'r') as file:
+    with open(testfile, "r") as file:
         data = file.readlines()
 
     os.remove(testfile)
@@ -101,7 +128,9 @@ def get_full_csvy_model():
 
 @pytest.fixture
 def get_line_reference_data():
-    with open(os.path.join(DATA_PATH, 'arepo_line_reference_model.csvy'), 'r') as file:
+    with open(
+        os.path.join(DATA_PATH, "arepo_line_reference_model.csvy"), "r"
+    ) as file:
         data = file.readlines()
 
     return data
@@ -109,7 +138,9 @@ def get_line_reference_data():
 
 @pytest.fixture
 def get_cone_reference_data():
-    with open(os.path.join(DATA_PATH, 'arepo_cone_reference_model.csvy'), 'r') as file:
+    with open(
+        os.path.join(DATA_PATH, "arepo_cone_reference_model.csvy"), "r"
+    ) as file:
         data = file.readlines()
 
     return data
@@ -117,7 +148,9 @@ def get_cone_reference_data():
 
 @pytest.fixture
 def get_full_reference_data():
-    with open(os.path.join(DATA_PATH, 'arepo_full_reference_model.csvy'), 'r') as file:
+    with open(
+        os.path.join(DATA_PATH, "arepo_full_reference_model.csvy"), "r"
+    ) as file:
         data = file.readlines()
 
     return data
@@ -133,5 +166,3 @@ def test_cone_profile(get_cone_csvy_model, get_cone_reference_data):
 
 def test_full_profile(get_full_csvy_model, get_full_reference_data):
     assert get_full_csvy_model == get_full_reference_data
-
-
