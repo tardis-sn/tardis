3ea5721c8433eafe4279c29e2bcdd05f4b74712e
rodot- <jobrien585@gmail.com>
2022-02-21 16:09:31 -0500

    added in a special fixture for the montecarlo_1e5 model tests as to avoid mixing config fixtures.  Set the montecarlo_main_loop to recompile itself on the first iteration ot set proper config options

--- tardis/model/tests/test_base.py
+++ tardis/model/tests/test_base.py
@@ -387,7 +387,6 @@ class TestModelState:
 def to_hdf_buffer(hdf_file_path, simulation_verysimple):
     simulation_verysimple.model.to_hdf(hdf_file_path, overwrite=True)
 
-
 model_scalar_attrs = ["t_inner"]
 
 
--- tardis/montecarlo/montecarlo_numba/base.py
+++ tardis/montecarlo/montecarlo_numba/base.py
@@ -63,7 +63,7 @@ def montecarlo_radial1d(
     packet_seeds = montecarlo_configuration.packet_seeds
 
     number_of_vpackets = montecarlo_configuration.number_of_vpackets
-
+    if iteration == 0: montecarlo_main_loop.recompile() # Make sure we update
     (
         v_packets_energy_hist,
         last_interaction_type,
