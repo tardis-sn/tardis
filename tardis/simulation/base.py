52106be09f961d7191af300f5759684059bec523
Dhruv Sondhi <66117751+DhruvSondhi@users.noreply.github.com>
2022-02-23 21:53:13 +0530

    Changed Formatting for the logger based on Log Level (#1710)
    * Changed Formatting for the logger based on Log Level,
    Removed redundant \n\t formatting from existing messages
    
    * Reverted log message formatting
    
    * Restructured the colored logger for the log message format

--- tardis/simulation/base.py
+++ tardis/simulation/base.py
@@ -365,7 +365,7 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
 
     def iterate(self, no_of_packets, no_of_virtual_packets=0, last_run=False):
         logger.info(
-            f"Starting iteration {(self.iterations_executed + 1):d} of {self.iterations:d}"
+            f"\n\tStarting iteration {(self.iterations_executed + 1):d} of {self.iterations:d}"
         )
         self.runner.run(
             self.model,
@@ -455,8 +455,8 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
             )
 
         logger.info(
-            f"Simulation finished in {self.iterations_executed:d} iterations "
-            f"Simulation took {(time.time() - start_time):.2f} s\n"
+            f"\n\tSimulation finished in {self.iterations_executed:d} iterations "
+            f"\n\tSimulation took {(time.time() - start_time):.2f} s\n"
         )
         self._call_back()
 
@@ -501,7 +501,7 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
         plasma_state_log.columns.name = "Shell No."
 
         if is_notebook():
-            logger.info("Plasma stratification:")
+            logger.info("\n\tPlasma stratification:")
 
             # Displaying the DataFrame only when the logging level is NOTSET, DEBUG or INFO
             if logger.level <= logging.INFO:
@@ -525,16 +525,16 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
             )
             for value in plasma_output.split("\n"):
                 output_df = output_df + "\t{}\n".format(value)
-            logger.info("Plasma stratification:")
+            logger.info("\n\tPlasma stratification:")
             logger.info(f"\n{output_df}")
 
         logger.info(
-            f"Current t_inner = {t_inner:.3f}\n\tExpected t_inner for next iteration = {next_t_inner:.3f}\n"
+            f"\n\tCurrent t_inner = {t_inner:.3f}\n\tExpected t_inner for next iteration = {next_t_inner:.3f}\n"
         )
 
     def log_run_results(self, emitted_luminosity, absorbed_luminosity):
         logger.info(
-            f"Luminosity emitted   = {emitted_luminosity:.3e}\n"
+            f"\n\tLuminosity emitted   = {emitted_luminosity:.3e}\n"
             f"\tLuminosity absorbed  = {absorbed_luminosity:.3e}\n"
             f"\tLuminosity requested = {self.luminosity_requested:.3e}\n"
         )
