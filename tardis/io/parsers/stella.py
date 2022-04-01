e56193411e14391fa59fe735cba4d1d2a4e0b209
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2022-04-01 17:35:34 -0400

    add new stella functionality

--- tardis/io/parsers/stella.py
+++ tardis/io/parsers/stella.py
@@ -4,18 +4,84 @@ from astropy import units as u
 import numpy as np
 
 
-def read_stella_data(filename):
-    with open(filename) as fh:
-        col = fh.readlines()[5]
-    col_names = re.split(r"\s{3,}", col.strip())
-    col_names = [
-        re.sub(r"\s\(.+\)", "", col_name).replace(" ", "_")
-        for col_name in col_names
-    ]
-    data = pd.read_csv(
-        filename, skiprows=7, delim_whitespace=True, names=col_names
+STELLA_COL_MAPPER = {
+    "mass of cell (g)": "mass_cell",
+    "cell center m (g)": "mass_total",
+    "cell center R (cm)": "r_center",
+    "cell center v (cm/s)": "v_center",
+    "avg density": "density",
+    "radiation pressure": "pressure_rad",
+    "avg temperature": "t_gas",
+    "radiation temperature": "t_radiation",
+    "avg opacity": "opacity",
+    "outer edge m (g)": "mass_outer",
+    "outer edge r (cm)": "r_outer",
+}
+
+
+def read_stella(fname):
+    """
+    Read a STELLA model into a pandas dataframe
+    """
+    with open(fname) as fh:
+        for line in fh:
+            if line.strip().startswith("mass"):
+                break
+    raw_columns = re.split("\s{3,}", line.strip())
+
+    stella_model = pd.read_csv(
+        fname, delim_whitespace=True, skiprows=6, names=raw_columns, index_col=0
+    )
+    return stella_model.rename(columns=STELLA_COL_MAPPER)
+
+
+def convert_stella_to_csvy(fname):
+    """
+    Convert STELLA to TARDIS file
+    """
+    stella_meta, stella_model = read_stella(fname)
+    v_interpolator = interpolate.interp1d(
+        stella_model["r_center"], stella_model["v_center"]
+    )
+    #velocity needed for future check against homology
+    velocity = v_interpolator(stella_model["r_outer"].values) * u.cm / u.s
+    homologous_velocity = (
+        stella_model.r_outer.values * u.cm / (stella_meta["time_explosion"])
+    )
+    csvy_meta = {
+        "name": "STELLA transformation",
+        "model_density_time_0": str(stella_meta["time_explosion"]),
+        "model_isotope_time_0": str(stella_meta["time_explosion"]),
+        "description": "parsed stella file",
+        "tardis_model_config_version": "v1.0",
+        "datatype": {"fields": []},
+    }
+    csvy_table = pd.DataFrame()
+    csvy_table["density"] = stella_model.density
+    csvy_meta["datatype"]["fields"].append(
+        {
+            "name": "density",
+            "unit": "g/cm^3",
+            "desc": "Average density from stella",
+        }
     )
 
-    # drop last row of data
-    data = data.iloc[0:-1]
-    return data
+    csvy_table["velocity"] = homologous_velocity
+    csvy_meta["datatype"]["fields"].append(
+        {
+            "name": "velocity",
+            "unit": "cm/s",
+            "desc": "WARNING - THIS IS NOT THE STELLA VELOCITY but a homologous approximation given radius and t_explosion",
+        }
+    )
+
+    abundances = stella_model.iloc[:, 12:-3]
+    for isotope in abundances.columns:
+        csvy_meta["datatype"]["fields"].append(
+            {"name": isotope, "desc": "{0} mass fraction".format(isotope)}
+        )
+
+    csvy_table = csvy_table.join(abundances)
+    with open(f"{fname}_stella2tardis.csvy", "w") as fh:
+        yaml.dump(csvy_meta, fh)
+        csvy_table.to_csv(fh, index=False)
