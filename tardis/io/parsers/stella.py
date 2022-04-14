d8a09df55f1a4bf34463db1ec46520acf25c3265
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2022-04-14 19:59:46 -0400

    add new functionality

--- tardis/io/parsers/stella.py
+++ tardis/io/parsers/stella.py
@@ -2,7 +2,14 @@ import re
 import pandas as pd
 from astropy import units as u
 import numpy as np
+import yaml
+from scipy import interpolate
+import os
 
+
+STELLA_CONFIG_TEMPLATE_FNAME = os.path.join(
+    os.path.dirname(__file__), "tardis_stella_template.yml.j2"
+)
 
 STELLA_COL_MAPPER = {
     "mass of cell (g)": "mass_cell",
@@ -23,8 +30,11 @@ def read_stella(fname):
     """
     Read a STELLA model into a pandas dataframe
     """
+    stella_meta = {}
     with open(fname) as fh:
         for line in fh:
+            if line.strip().startswith("days post max Lbol"):
+                stella_meta["time_explosion"] = float(line[20:].strip()) * u.day
             if line.strip().startswith("mass"):
                 break
     raw_columns = re.split("\s{3,}", line.strip())
@@ -32,10 +42,11 @@ def read_stella(fname):
     stella_model = pd.read_csv(
         fname, delim_whitespace=True, skiprows=6, names=raw_columns, index_col=0
     )
-    return stella_model.rename(columns=STELLA_COL_MAPPER)
+    stella_model.rename(columns=STELLA_COL_MAPPER, inplace=True)
+    return stella_meta, stella_model
 
 
-def convert_stella_to_csvy(fname):
+def convert_stella_to_csvy(fname, out_fname=None):
     """
     Convert STELLA to TARDIS file
     """
@@ -43,7 +54,7 @@ def convert_stella_to_csvy(fname):
     v_interpolator = interpolate.interp1d(
         stella_model["r_center"], stella_model["v_center"]
     )
-    #velocity needed for future check against homology
+    # velocity needed for future check against homology
     velocity = v_interpolator(stella_model["r_outer"].values) * u.cm / u.s
     homologous_velocity = (
         stella_model.r_outer.values * u.cm / (stella_meta["time_explosion"])
@@ -82,6 +93,9 @@ def convert_stella_to_csvy(fname):
         )
 
     csvy_table = csvy_table.join(abundances)
-    with open(f"{fname}_stella2tardis.csvy", "w") as fh:
+    if out_fname is None:
+        out_fname = f"{fname}_stella2tardis.csvy"
+
+    with open(out_fname, "w") as fh:
         yaml.dump(csvy_meta, fh)
         csvy_table.to_csv(fh, index=False)
