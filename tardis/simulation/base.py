7b920087b2db9c2fea1a78dce8f4c1478b73892a
aman1971 <ak584584@gmail.com>
2022-04-18 23:50:05 +0530

    Removing extra changes

--- tardis/simulation/base.py
+++ tardis/simulation/base.py
@@ -193,7 +193,8 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
         self._cb_next_id = 0
 
         mc_config_module.CONTINUUM_PROCESSES_ENABLED = (
-                not self.plasma.continuum_interaction_species.empty)
+            not self.plasma.continuum_interaction_species.empty
+        )
 
     def estimate_t_inner(
         self, input_t_inner, luminosity_requested, t_inner_update_exponent=-0.5
