ae19f26a63b8f13c61ba697cf577468152769639
Ezequiel Pássaro <epassaro15@gmail.com>
2022-04-28 11:39:06 -0300

    Use Azure mirror for reference data in testing pipeline (#1988)
    * Use Azure mirror for reference data in testing pipeline
    
    * Use azure mirror on compare-refdata pipeline
    
    * Add helper script
    
    * Use for loop in bash script
    
    * Minor changes
    
    * Remove files from array

---
+++ .ci-helpers/download_reference_data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+REF_PATH="$GITHUB_WORKSPACE/tardis-refdata"
+REPO_URL="https://dev.azure.com/tardis-sn/TARDIS/_apis/git/repositories/tardis-refdata"
+
+FILES=('atom_data/kurucz_cd23_chianti_H_He.h5'
+       'atom_data/chianti_He.h5'
+       'montecarlo_1e5_compare_data.h5'
+       'packet_unittest.h5'
+       'sdec_ref.h5'
+       'unit_test_data.h5')
+
+mkdir -p $REF_PATH/atom_data
+
+for FILE in "${FILES[@]}"
+do
+    wget -q "$REPO_URL/items?path=$FILE&resolveLfs=true" -O $REF_PATH/$FILE
+done
+
+exit 0
