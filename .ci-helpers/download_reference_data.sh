5cb28710ca65c5e9a0a4fb49f3313c706a93499a
Ezequiel Pássaro <epassaro15@gmail.com>
2022-04-28 11:02:07 -0300

    Use for loop in bash script

--- .ci-helpers/download_reference_data.sh
+++ .ci-helpers/download_reference_data.sh
@@ -6,10 +6,19 @@ REF_PATH="$GITHUB_WORKSPACE/tardis-refdata"
 REPO_URL="https://dev.azure.com/tardis-sn/TARDIS/_apis/git/repositories/tardis-refdata"
 
 mkdir -p $REF_PATH/atom_data
-wget -q "$REPO_URL/items?path=atom_data/kurucz_cd23_chianti_H_He.h5&resolveLfs=true" -O $REF_PATH/atom_data/kurucz_cd23_chianti_H_He.h5
-wget -q "$REPO_URL/items?path=atom_data/chianti_He.h5&resolveLfs=true" -O $REF_PATH/atom_data/chianti_He.h5
-wget -q "$REPO_URL/items?path=unit_test_data.h5&resolveLfs=true" -O $REF_PATH/unit_test_data.h5&resolveLfs=true
-wget -q "$REPO_URL/items?path=packet_unittest.h5&resolveLfs=true" -O $REF_PATH/packet_unittest.h5&resolveLfs=true
-wget -q "$REPO_URL/items?path=montecarlo_1e5_compare_data.h5&resolveLfs=true" -O $REF_PATH/montecarlo_1e5_compare_data.h5
+
+FILES=('atom_data/kurucz_cd23_chianti_H_He.h5'
+       'atom_data/chianti_He.h5'
+       'montecarlo_1e5_compare_data.h5'
+       'montecarlo_one_packet_compare_data.h5'
+       'packet_unittest.h5'
+       'rpacket_tracking.h5'
+       'sdec_ref.h5'
+       'unit_test_data.h5')
+
+for FILE in "${FILES[@]}"
+do
+    wget -q "$REPO_URL/items?path=$FILE&resolveLfs=true" -O $REF_PATH/$FILE
+done
 
 exit 0
