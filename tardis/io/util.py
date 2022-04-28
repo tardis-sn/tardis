1383b938cc06a7ffbf10844ba252a9376e01ca81
Ezequiel Pássaro <epassaro15@gmail.com>
2022-04-28 12:22:33 -0300

    Initial commit

--- tardis/io/util.py
+++ tardis/io/util.py
@@ -16,6 +16,8 @@ from tqdm.auto import tqdm
 from tardis import constants as const
 from astropy import units as u
 
+from astropy.utils.data import download_file
+
 from tardis import __path__ as TARDIS_PATH
 
 logger = logging.getLogger(__name__)
@@ -391,33 +393,7 @@ class PlasmaWriterMixin(HDFWriterMixin):
         )
 
 
-def download_from_url(url, dst):
-    """
-    kindly used from https://gist.github.com/wy193777/0e2a4932e81afc6aa4c8f7a2984f34e2
-    @param: url to download file
-    @param: dst place to put the file
-    """
+def download_from_url(url, dst, src=None):
 
-    file_size = int(requests.head(url).headers["Content-Length"])
-    if os.path.exists(dst):
-        first_byte = os.path.getsize(dst)
-    else:
-        first_byte = 0
-    if first_byte >= file_size:
-        return file_size
-    header = {f"Range": "bytes={first_byte}-{file_size}"}
-    pbar = tqdm(
-        total=file_size,
-        initial=first_byte,
-        unit="B",
-        unit_scale=True,
-        desc=url.split("/")[-1],
-    )
-    req = requests.get(url, headers=header, stream=True)
-    with open(dst, "ab") as f:
-        for chunk in req.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                pbar.update(1024)
-    pbar.close()
-    return file_size
+    cached_file_path = download_file(url, sources=src, pkgname="tardis")
+    
