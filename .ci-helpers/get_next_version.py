6c2f344c6717ed8c99bb5873265d800397935f3c
Ezequiel Pássaro <epassaro15@gmail.com>
2022-04-30 10:53:10 -0300

    Force fixed number of digits in version date

--- .ci-helpers/get_next_version.py
+++ .ci-helpers/get_next_version.py
@@ -6,7 +6,13 @@ from setuptools_scm.version import guess_next_date_ver
 
 version = version_from_scm(".").tag.public
 version = guess_next_date_ver(version)
+
 version = version.rstrip(".0") if version.endswith(".0") else version
+version = version.split(".")
+
+version[1:3] = ["0" + i if len(i) == 1 else i for i in version[1:3]]
+version = ".".join(version)
 
 print(version)
+
 sys.exit(0)
