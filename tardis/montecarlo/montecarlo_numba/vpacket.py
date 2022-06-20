20cb998017d0d9a76a052336d7325fb92f6a7806
nolanbrown01 <105801608+nolanbrown01@users.noreply.github.com>
2022-06-20 16:05:31 -0400

    Initial geometry restructure (#2063)
    * I moved calculate_distances to a new folder, packet_transfer. Dependencies were renamed.
    
    * I moved calculate_distances to a new folder, transport. Dependencies were renamed.
    
    * I added myself to the .mailmap

--- tardis/montecarlo/montecarlo_numba/vpacket.py
+++ tardis/montecarlo/montecarlo_numba/vpacket.py
@@ -17,7 +17,7 @@ from tardis.montecarlo.montecarlo_numba.r_packet_transport import (
     move_packet_across_shell_boundary,
 )
 
-from tardis.montecarlo.montecarlo_numba.calculate_distances import (
+from tardis.transport.calculate_distances import (
     calculate_distance_boundary,
     calculate_distance_line,
 )
