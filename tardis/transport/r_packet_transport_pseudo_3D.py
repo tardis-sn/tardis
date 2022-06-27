eca9e56ccd9af78a2658c6831aba6acbfda2676a
nolanbrown01 <nolanbrown01@gmail.com>
2022-06-27 10:34:21 -0400

    moved functions from gamma_ray_grid to r_packet_transport_pseudo_3D and calculate_distances_pseudo_3D

---
+++ tardis/transport/r_packet_transport_pseudo_3D.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numba import njit
+
+from tardis.montecarlo.montecarlo_numba import njit_dict_no_parallel
+from tardis.energy_input.util import (
+    doppler_gamma
+)
+
+
+@njit(**njit_dict_no_parallel)
+def move_packet_pseudo_3D(packet, distance):
+    """
+    Moves packet a distance along its direction vector
+
+    Parameters
+    ----------
+    packet : GXPacket object
+    distance : float
+
+    Returns
+    -------
+    packet : GXPacket object
+
+    """
+    location_old = packet.location
+    direction = packet.direction
+
+    location_new = location_old + distance * direction
+
+    packet.location = location_new
+
+    doppler_factor = doppler_gamma(
+        packet.direction, packet.location, packet.time_current
+    )
+
+    packet.nu_cmf = packet.nu_rf * doppler_factor
+    packet.energy_cmf = packet.energy_rf * doppler_factor
+
+    return packet
