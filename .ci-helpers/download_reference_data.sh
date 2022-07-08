bd56137fad98a6a362533c925fd1f9eaeba34270
Alexander Holas <alex.holas@gmx.de>
2022-07-08 16:18:00 -0400

    Added arepo data to refdata helper

--- .ci-helpers/download_reference_data.sh
+++ .ci-helpers/download_reference_data.sh
@@ -10,7 +10,9 @@ FILES=('atom_data/kurucz_cd23_chianti_H_He.h5'
        'montecarlo_1e5_compare_data.h5'
        'packet_unittest.h5'
        'sdec_ref.h5'
-       'unit_test_data.h5')
+       'unit_test_data.h5'
+       'arepo_data/arepo_snapshot.hdf5'
+       'arepo_data/arepo_snapshot.json')
 
 mkdir -p $REF_PATH/atom_data
 
