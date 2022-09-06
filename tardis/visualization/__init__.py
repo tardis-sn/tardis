5715d4a975f41e4877c54d50d7a5c0fb349b6af5
jayantbhakar <jayant.bhakar@gmail.com>
2022-09-06 22:20:19 +0530

    added documentation

--- tardis/visualization/__init__.py
+++ tardis/visualization/__init__.py
@@ -9,3 +9,4 @@ from tardis.visualization.widgets.shell_info import (
 from tardis.visualization.widgets.line_info import LineInfoWidget
 from tardis.visualization.widgets.custom_abundance import CustomAbundanceWidget
 from tardis.visualization.tools.sdec_plot import SDECPlotter
+from tardis.visualization.tools.rpacket_visualization import RPacketPlotter
