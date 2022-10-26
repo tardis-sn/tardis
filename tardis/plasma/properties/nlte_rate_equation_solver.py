ebd513a82b1fda20ad00d9b43004333e29285b79
Sona Chitchyan <chitchyan.sona@gmail.com>
2022-10-26 18:20:52 -0400

    fixed a bug in rate_matrix_index, added an example for checking if rate matrix works properly

--- tardis/plasma/properties/nlte_rate_equation_solver.py
+++ tardis/plasma/properties/nlte_rate_equation_solver.py
@@ -9,34 +9,49 @@ __all__ = [
 
 
 class NLTERateEquationSolver(ProcessingPlasmaProperty):
-    outputs = ("ion_number_density_nlte", "n_e")
-    def calculate(self, *args, **kwargs):
-        return super().calculate(*args, **kwargs)
+    outputs = ("ion_number_density_nlte", "electron_densities_nlte")
+    def calculate(self,
+        gamma,
+        alpha_sp,
+        alpha_stim,
+        coll_ion_coeff,
+        coll_recomb_coeff,
+        partition_function,
+        levels,
+        level_boltzmann_factor,
+        phi,
+        rate_matrix_index,
+        ):
+
+        photo_ion_rate, rad_recomb_rate_coeff, coll_ion_coefficient, coll_recomb_coefficient = self.prepare_ion_recomb_rates_nlte_ion(gamma, alpha_sp,     alpha_stim, coll_ion_coeff, coll_recomb_coeff, partition_function, levels, level_boltzmann_factor)
+        
+        #>>>TODO:initial electron density should be included in the initial guess, added in a future PR
+        initial_electron_density = np.zeros(len(phi.columns))
+        #<<<
+        rate_matrix = self.calculate_rate_matrix(phi[0], initial_electron_density[0], rate_matrix_index, photo_ion_rate[0], rad_recomb_rate_coeff[0], coll_ion_coefficient[0], coll_recomb_coefficient[0])
+        return None
 
 
     def calculate_rate_matrix(
         self,
-        phi,
+        phi_shell,
         electron_density,
-        number_density,
         rate_matrix_index,
-        last_row,
+        photo_ion_rate,
         rad_recomb_rate_coeff,
-        photo_ion_rates,
         coll_ion_coefficient,
         coll_recomb_coefficient,
     ):
-        1/0
         rate_matrix = pd.DataFrame(
             0.0, columns=rate_matrix_index, index=rate_matrix_index
         )
         rad_recomb_rates = rad_recomb_rate_coeff * electron_density
         coll_ion_rates = coll_ion_coefficient * electron_density
         coll_recomb_rates= coll_recomb_coefficient * electron_density**2
-        atomic_numbers = number_density.index
+        atomic_numbers = rate_matrix_index.get_level_values(0).unique()[:-1].values #dropping the n_e index
         for atomic_number in atomic_numbers:
             ion_numbers = rate_matrix.loc[atomic_number].index.get_level_values(0)
-            phi_block = phi.loc[atomic_number]
+            phi_block = phi_shell.loc[atomic_number]
             rate_matrix_block = self.lte_rate_matrix_block(phi_block, electron_density)
 
             nlte_ion_numbers = ion_numbers[
@@ -51,14 +66,15 @@ class NLTERateEquationSolver(ProcessingPlasmaProperty):
                     atomic_number,
                     ion_number,
                     rad_recomb_rates.loc[(atomic_number,)],
-                    photo_ion_rates.loc[(atomic_number,)],
+                    photo_ion_rate.loc[(atomic_number,)],
                     coll_ion_rates.loc[(atomic_number,)],
                     coll_recomb_rates.loc[(atomic_number,)],
                 )
             rate_matrix.loc[
                 (atomic_number, slice(None)), (atomic_number)
             ] = rate_matrix_block
-
+        
+        last_row = self.prepare_last_row(atomic_numbers)
         rate_matrix.loc[("n_e", slice(None))] = last_row
         return rate_matrix
 
@@ -87,7 +103,7 @@ class NLTERateEquationSolver(ProcessingPlasmaProperty):
 
 
     def lte_rate_matrix_block(self, phi_block, electron_density):
-        lte_rate_vector_block = np.hstack([-phi_block, 1.0])
+        lte_rate_vector_block = -1.0 * np.hstack([*phi_block.values, -1.0])
         lte_rate_matrix_block = np.diag(lte_rate_vector_block)
         n_e_initial = np.ones(len(phi_block)) * electron_density
         n_e_matrix = np.diag(n_e_initial, 1)
@@ -125,12 +141,11 @@ class NLTERateEquationSolver(ProcessingPlasmaProperty):
         for atomic_number in atomic_numbers:
             last_row.append(np.arange(0.0, atomic_number + 1))
         last_row = np.hstack([*last_row, -1])
+        #TODO needs to be modified for use in nlte_excitation
         return last_row
 
-
+    @staticmethod
     def prepare_ion_recomb_rates_nlte_ion(
-        self,
-        level_population_fraction,
         gamma,
         alpha_sp,
         alpha_stim,
