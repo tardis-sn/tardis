c71c28eb274c8b1c2382223ccbaf1f9393f56ab4
Le Truong <43793681+PommesFrittes@users.noreply.github.com>
2023-02-07 21:55:54 +0100

    Update tardis/io/parsers/__init__.py
    Co-authored-by: Sona Chitchyan <chitchyan.sona@gmail.com>

--- tardis/io/parsers/__init__.py
+++ tardis/io/parsers/__init__.py
@@ -4,7 +4,7 @@ A collection of parsers to read data of different formats.
 Accepted data formats are Arepo snapshots, the Blondin toy-model
 format, CSVY files and STELLA files. These are parsed into 
 either CSV files, YAML files or Pandas DataFrames, which are 
-data formats usable by TARDIS. 
+data formats interpretable by TARDIS. 
 """
 
 from tardis.io.parsers.blondin_toymodel import (
