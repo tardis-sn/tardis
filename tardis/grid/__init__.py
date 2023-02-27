239e04bd87a8c0ef56cf91d7800671056acb1438
Le Truong <43793681+PommesFrittes@users.noreply.github.com>
2023-02-27 21:42:39 +0100

    Add docstrings to subpackages (#2204)
    * add docstring to subpackage grid
    
    * Add docstrings to subpackages
    
    * Change comments to docstrings
    
    * add docstrings to subpackages
    
    * rename gui/__init__.py
    
    * Reformat using black
    
    * add contributor infos to mailmap file
    
    * add extended summary to docstrings
    
    * Add extended summary to plasma/properties
    
    * Update tardis/io/parsers/__init__.py
    
    Co-authored-by: Sona Chitchyan <chitchyan.sona@gmail.com>
    
    ---------
    
    Co-authored-by: kim <kim@tg-lingemann.de>
    Co-authored-by: Sona Chitchyan <chitchyan.sona@gmail.com>

--- tardis/grid/__init__.py
+++ tardis/grid/__init__.py
@@ -1,1 +1,7 @@
+"""
+Storing a grid of TARDIS parameters that 
+facilitates running large numbers of 
+simulations easily.
+"""
+
 from tardis.grid.base import *
---
+++ tardis/gui/__init__.py
@@ -0,0 +1,9 @@
+"""
+Creation of the basic framework for the TARDIS GUI.
+
+The data that should be displayed in the GUI is put into 
+respective data structures (either a tree or a table), such 
+that they can be used in the widgets. These widgets are 
+used to form the building blocks of the basic framework for
+the GUI.
+"""
