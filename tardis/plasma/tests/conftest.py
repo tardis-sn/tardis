ecfd2c5986fb4a5398a394b048759fe84b6556fc
atharva-2001 <aryaa_1@rknec.edu>
2023-09-22 15:18:30 +0530

    Text based snapshot fixture with custom location

--- tardis/plasma/tests/conftest.py
+++ tardis/plasma/tests/conftest.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import pytest
 from syrupy.location import PyTestLocation
+from syrupy.extensions.amber import AmberSnapshotExtension
 
-MODULE_NAME = "plasma"
+SNAPSHOT_LOCATION = "snapshot_data/plasma"
 
 @pytest.fixture
 def snapshot_pd(snapshot, tardis_ref_path, pandas_snapshot_extention):
-    refpath = tardis_ref_path.joinpath(MODULE_NAME)
+    refpath = tardis_ref_path.joinpath(SNAPSHOT_LOCATION)
 
     class PandasSnapshotExtenstionRefdata(pandas_snapshot_extention):
         @classmethod
@@ -17,10 +18,20 @@ def snapshot_pd(snapshot, tardis_ref_path, pandas_snapshot_extention):
 
 @pytest.fixture
 def snapshot_np(snapshot, tardis_ref_path, numpy_snapshot_extension):
-    refpath = tardis_ref_path.joinpath(MODULE_NAME)
+    refpath = tardis_ref_path.joinpath(SNAPSHOT_LOCATION)
     class NumpySnapshotExtenstionRefdata(numpy_snapshot_extension):
             @classmethod
             def dirname(cls, *, test_location: "PyTestLocation") -> str:
                 return str(Path(test_location.filepath).parent.joinpath(refpath))
     return snapshot.use_extension(NumpySnapshotExtenstionRefdata)
+
+@pytest.fixture
+def snapshot(snapshot, tardis_ref_path):
+    refpath = tardis_ref_path.joinpath(SNAPSHOT_LOCATION)
+    # TODO: try this with SingleFileSnapshotExtension
+    class AmberSnapshotExtensionRefdata(AmberSnapshotExtension):
+            @classmethod
+            def dirname(cls, *, test_location: "PyTestLocation") -> str:
+                return str(Path(test_location.filepath).parent.joinpath(refpath))
+    return snapshot.use_extension(AmberSnapshotExtensionRefdata)
 
