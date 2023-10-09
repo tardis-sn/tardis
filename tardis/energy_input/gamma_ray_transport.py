fc42f85d6a462d572ada080e6c6c97f33212c9f4
Knights-Templars <anirbaniamdutta@gmail.com>
2023-10-09 11:11:22 -0400

    Added a function to calculate shell masses

--- tardis/energy_input/gamma_ray_transport.py
+++ tardis/energy_input/gamma_ray_transport.py
@@ -260,6 +260,28 @@ def initialize_packets(
     )
 
 
+def calculate_shell_masses(model):
+
+    """Function to calculate shell masses
+    Parameters
+    ----------
+    model : tardis.Radial1DModel
+        The tardis model to calculate gamma ray propagation through
+    Returns
+    -------
+    numpy.ndarray
+        shell masses in units of g
+
+    """
+    outer_velocities = model.v_outer.to("cm/s").value
+    inner_velocities = model.v_inner.to("cm/s").value
+    ejecta_density = model.density.to("g/cm^3").value
+    ejecta_volume = model.volume.to("cm^3").value
+    shell_masses = ejecta_volume * ejecta_density
+
+    return shell_masses
+
+
 def main_gamma_ray_loop(
     num_decays,
     model,
