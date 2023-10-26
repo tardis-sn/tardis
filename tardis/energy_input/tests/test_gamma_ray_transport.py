ec64ddf48d4e524b01613cba62640b47d0ddc7f7
Knights-Templars <anirbaniamdutta@gmail.com>
2023-10-26 15:50:11 -0400

    Added test for activity

--- tardis/energy_input/tests/test_gamma_ray_transport.py
+++ tardis/energy_input/tests/test_gamma_ray_transport.py
@@ -4,6 +4,7 @@ import numpy.testing as npt
 
 from tardis.model import SimulationState
 from tardis.io.configuration import config_reader
+from tardis.io.util import yaml_load_file, YAMLLoader
 from tardis.energy_input.gamma_ray_transport import (
     calculate_shell_masses,
     create_isotope_dicts,
@@ -14,18 +15,15 @@ from tardis.energy_input.gamma_ray_transport import (
 import astropy.units as u
 import astropy.constants as c
 
-# pytest.fixtures.
-# Return the simulation object.
 
-
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def config():
     return config_reader.Configuration.from_yaml(
-        "/Users/anirbandutta/Projects/gamma_ray_tardis/tardis_configv1_density_exponential_nebular.yml"
+        "tardis/io/configuration/tests/data/tardis_configv1_density_exponential_nebular.yml"
     )
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module")
 def simulation_setup(config):
     config.model.structure.velocity.start = 1.0 * u.km / u.s
     config.model.structure.density.rho_0 = 5.0e2 * u.g / (u.cm**3)
@@ -39,9 +37,9 @@ def test_activity(simulation_setup):
     ni_mass : mass of 56Ni in grams.
     """
     model = simulation_setup
-    t_half = 6.075 * u.d.to(u.s)  # day
-    decay_constant = 0.693 / t_half  # sec^-1
-    time_delta = 80.0 * u.d.to(u.s)  # day
+    t_half = 6.075 * u.d.to(u.s)
+    decay_constant = 0.693 / t_half
+    time_delta = 80.0 * u.d.to(u.s)
     shell_masses = calculate_shell_masses(model)
     raw_isotope_abundance = model.raw_isotope_abundance
     raw_isotope_abundance_mass = raw_isotope_abundance.apply(
@@ -51,18 +49,15 @@ def test_activity(simulation_setup):
     iso_dict = create_isotope_dicts(raw_isotope_abundance, shell_masses)
     inv_dict = create_inventories_dict(iso_dict)
     total_decays = calculate_total_decays(inv_dict, time_delta)
-    simulated_activity = total_decays[0][28, 56]["Ni-56"]
+    simulated = total_decays[0][28, 56]["Ni-56"]
 
-    isotopic_mass = 55.942132 * (u.g)  # g
+    isotopic_mass = 55.942128 * (u.g)
     number_of_moles = ni_mass * (u.g) / isotopic_mass
     number_of_atoms = number_of_moles * c.N_A
-
-    # Use the radioactivity formula
-    # N = N_0 e-(lambda * t)
     N = number_of_atoms.value * np.exp(-decay_constant * time_delta)
+    actual = number_of_atoms.value - N
 
-    actual_activity = decay_constant * N
-    npt.assert_almost_equal(actual_activity, simulated_activity)
+    np.isclose(simulated, actual)
 
 
 @pytest.mark.xfail(reason="To be implemented")
