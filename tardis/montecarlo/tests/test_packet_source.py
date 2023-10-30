723c0942230f94da8a23329f6c19f4a3a57aa6fc
Christian Vogl <cvogl@mpa-garching.mpg.de>
2023-10-30 22:15:09 +0100

    Add initial test for BlackBodySimpleSourceRelativistic

--- tardis/montecarlo/tests/test_packet_source.py
+++ tardis/montecarlo/tests/test_packet_source.py
@@ -3,8 +3,12 @@ import os
 import numpy as np
 import pandas as pd
 import pytest
+from numpy.testing import assert_allclose
 
-from tardis.montecarlo.packet_source import BlackBodySimpleSource
+from tardis.montecarlo.packet_source import (
+    BlackBodySimpleSource,
+    BlackBodySimpleSourceRelativistic,
+)
 from tardis.montecarlo import (
     montecarlo_configuration as montecarlo_configuration,
 )
@@ -43,6 +47,22 @@ class TestPacketSource:
         yield cls.bb
         montecarlo_configuration.LEGACY_MODE_ENABLED = False
 
+    @pytest.fixture(scope="class")
+    def blackbodysimplesourcerelativistic(self, request):
+        """
+        Create BlackBodySimpleSourceRelativistic instance.
+
+        Yields
+        -------
+        tardis.montecarlo.packet_source.BlackBodySimpleSourceRelativistic
+        """
+        montecarlo_configuration.LEGACY_MODE_ENABLED = True
+        bb_rel = BlackBodySimpleSourceRelativistic(
+            base_seed=1963, legacy_second_seed=2508
+        )
+        yield bb_rel
+        montecarlo_configuration.LEGACY_MODE_ENABLED = False
+
     def test_bb_packet_sampling(
         self,
         request,
@@ -75,3 +95,47 @@ class TestPacketSource:
         assert np.all(np.isclose(nus, ref_df["nus"]))
         assert np.all(np.isclose(mus, ref_df["mus"]))
         assert np.all(np.isclose(unif_energies, ref_df["energies"]))
+
+    def test_bb_packet_sampling_relativistic(
+        self,
+        tardis_ref_data,
+        blackbodysimplesourcerelativistic,
+    ):
+        """
+        Parameters
+        ----------
+        tardis_ref_data : pd.HDFStore
+        blackbodysimplesourcerelativistic : tardis.montecarlo.packet_source.BlackBodySimpleSourceRelativistic
+        """
+        blackbodysimplesourcerelativistic.temperature = 10000
+        blackbodysimplesourcerelativistic.beta = 0.25
+
+        nus = blackbodysimplesourcerelativistic.create_packet_nus(100)
+        unif_energies = (
+            blackbodysimplesourcerelativistic.create_packet_energies(100)
+        )
+        blackbodysimplesourcerelativistic._reseed(2508)
+        mus = blackbodysimplesourcerelativistic.create_packet_mus(10)
+
+        gamma = np.sqrt(1 - 0.25**2) ** -1
+        ref_df = tardis_ref_data["/packet_unittest/blackbody"]
+        expected_nus = ref_df["nus"]
+        expected_unif_energies = ref_df["energies"] * 1.6 / gamma
+        expected_mus = np.array(
+            [
+                0.60420546,
+                0.49899691,
+                0.69583288,
+                0.96812652,
+                0.01544154,
+                0.93562304,
+                0.44306545,
+                0.77010037,
+                0.896973,
+                0.67876489,
+            ]
+        )
+
+        assert_allclose(nus, expected_nus)
+        assert_allclose(unif_energies, expected_unif_energies)
+        assert_allclose(mus, expected_mus, rtol=1e-6)
