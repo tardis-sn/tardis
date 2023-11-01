0d28444f4f6d8a8d189855d2c1fbd94d54042b25
Knights-Templars <anirbaniamdutta@gmail.com>
2023-11-01 17:32:41 -0400

    Added pytest paramterize

--- tardis/energy_input/tests/test_gamma_ray_transport.py
+++ tardis/energy_input/tests/test_gamma_ray_transport.py
@@ -2,6 +2,7 @@ import os
 import pytest
 import numpy as np
 import numpy.testing as npt
+from pathlib import Path
 import radioactivedecay as rd
 
 import tardis
@@ -18,38 +19,36 @@ from tardis.energy_input.gamma_ray_transport import (
 import astropy.units as u
 import astropy.constants as c
 
-DATA_PATH = os.path.join(
-    tardis.__path__[0], "io", "configuration", "tests", "data"
-)
-NI_MASS = rd.Nuclide("Ni-56").atomic_mass * (u.u).to(u.g)
-CO_MASS = rd.Nuclide("Co-56").atomic_mass * (u.u).to(u.g)
+# DATA_PATH = os.path.join(
+#    tardis.__path__[0], "io", "configuration", "tests", "data"
+# )
+nuclide = rd.Nuclide("Ni56")
 
 
 @pytest.fixture(scope="module")
-def config():
+def gamma_ray_config(example_configuration_dir: Path):
     """ """
-    return config_reader.Configuration.from_yaml(
-        os.path.join(
-            DATA_PATH, "tardis_configv1_density_exponential_nebular.yml"
-        )
+    yml_path = (
+        example_configuration_dir
+        / "tardis_configv1_density_exponential_nebular.yml"
     )
 
+    return config_reader.Configuration.from_yaml(yml_path)
+
 
 @pytest.fixture(scope="module")
-def simulation_setup(config):
-    config.model.structure.velocity.start = 1.0 * u.km / u.s
-    config.model.structure.density.rho_0 = 5.0e2 * u.g / (u.cm**3)
-    config.supernova.time_explosion = 1.0 * (u.d)
-    model = SimulationState.from_config(config)
+def simulation_setup(gamma_ray_config):
+    gamma_ray_config.model.structure.velocity.start = 1.0 * u.km / u.s
+    gamma_ray_config.model.structure.density.rho_0 = 5.0e2 * u.g / (u.cm**3)
+    gamma_ray_config.supernova.time_explosion = 1.0 * (u.d)
+    model = SimulationState.from_config(gamma_ray_config)
     return model
 
 
-def test_activity(simulation_setup):
-    """
-    ni_mass : mass of 56Ni in grams.
-    """
+@pytest.mark.parametrize("nuclide", [nuclide])
+def test_activity(simulation_setup, nuclide):
     model = simulation_setup
-    t_half = NI_INV.half_lives("s")["Ni-56"]
+    t_half = nuclide.half_life()
     decay_constant = np.log(2) / t_half
     time_delta = 80.0 * u.d.to(u.s)
     shell_masses = calculate_shell_masses(model)
@@ -57,14 +56,14 @@ def test_activity(simulation_setup):
     raw_isotope_abundance_mass = raw_isotope_abundance.apply(
         lambda x: x * shell_masses, axis=1
     )
-    ni_mass = raw_isotope_abundance_mass.loc[28, 56][0]
+    mass = raw_isotope_abundance_mass.loc[nuclide.Z, nuclide.A][0]
     iso_dict = create_isotope_dicts(raw_isotope_abundance, shell_masses)
     inv_dict = create_inventories_dict(iso_dict)
     total_decays = calculate_total_decays(inv_dict, time_delta)
     actual = total_decays[0][28, 56]["Ni-56"]
 
-    isotopic_mass = NI_MASS._get_atomic_mass("Ni-56") * (u.g)
-    number_of_moles = ni_mass * (u.g) / isotopic_mass
+    isotopic_mass = nuclide.atomic_mass * (u.u).to(u.g)
+    number_of_moles = mass * (u.g) / isotopic_mass
     number_of_atoms = number_of_moles * c.N_A
     N1 = number_of_atoms.value * np.exp(-decay_constant * time_delta)
     expected = number_of_atoms.value - N1
