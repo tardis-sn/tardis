48eb360aec17290546b7aeaabcd1d5802fdbba7a
Knights-Templars <anirbaniamdutta@gmail.com>
2023-11-02 12:11:09 -0400

    Ran test for checking activity of parent nuclide with analytical solution.

--- tardis/energy_input/tests/test_gamma_ray_transport.py
+++ tardis/energy_input/tests/test_gamma_ray_transport.py
@@ -27,7 +27,15 @@ NI56_NUCLIDE = rd.Nuclide("Ni56")
 
 @pytest.fixture(scope="module")
 def gamma_ray_config(example_configuration_dir: Path):
-    """ """
+    """
+    Parameters
+    ----------
+    example_configuration_dir: Path to the configuration directory.
+
+    Returns
+    -------
+    Tardis configuration
+    """
     yml_path = (
         example_configuration_dir
         / "tardis_configv1_density_exponential_nebular.yml"
@@ -38,6 +46,15 @@ def gamma_ray_config(example_configuration_dir: Path):
 
 @pytest.fixture(scope="module")
 def simulation_setup(gamma_ray_config):
+    """
+    Parameters
+    ----------
+    gamma_ray_config:
+
+    Returns
+    -------
+
+    """
     gamma_ray_config.model.structure.velocity.start = 1.0 * u.km / u.s
     gamma_ray_config.model.structure.density.rho_0 = 5.0e2 * u.g / (u.cm**3)
     gamma_ray_config.supernova.time_explosion = 1.0 * (u.d)
@@ -47,6 +64,7 @@ def simulation_setup(gamma_ray_config):
 
 def test_calculate_shell_masses(simulation_setup):
     """
+    Function to test calculation of shell masses.
     Parameters
     ----------
     simulation_setup: A simulation setup which returns a model.
@@ -60,14 +78,20 @@ def test_calculate_shell_masses(simulation_setup):
     npt.assert_almost_equal(shell_masses, desired)
 
 
-@pytest.mark.xfail(reason="To be implemented")
-@pytest.mark.parametrize("nuclide_name", ["Ni-56", "Co-56"])
+@pytest.mark.parametrize("nuclide_name", ["Ni-56"])
 def test_activity(simulation_setup, nuclide_name):
+    """
+    Function to test the decay of 56Ni in radioactivedecay with an analytical solution.
+    Parameters
+    ----------
+    simulation_setup: A simulation setup which returns a model.
+    nuclide_name: Name of the nuclide.
+    """
     nuclide = rd.Nuclide(nuclide_name)
     model = simulation_setup
-    t_half = nuclide.half_life()
+    t_half = nuclide.half_life() * u.s
     decay_constant = np.log(2) / t_half
-    time_delta = 80.0 * u.d.to(u.s)
+    time_delta = 1.0 * u.s
     shell_masses = calculate_shell_masses(model)
     raw_isotope_abundance = model.raw_isotope_abundance
     raw_isotope_abundance_mass = raw_isotope_abundance.apply(
@@ -76,18 +100,20 @@ def test_activity(simulation_setup, nuclide_name):
     mass = raw_isotope_abundance_mass.loc[nuclide.Z, nuclide.A][0]
     iso_dict = create_isotope_dicts(raw_isotope_abundance, shell_masses)
     inv_dict = create_inventories_dict(iso_dict)
+
     total_decays = calculate_total_decays(inv_dict, time_delta)
-    actual = total_decays[0][28, 56]["Ni-56"]
+    actual = total_decays[0][nuclide.Z, nuclide.A][nuclide_name]
 
-    isotopic_mass = nuclide.atomic_mass * (u.u).to(u.g)
+    isotopic_mass = nuclide.atomic_mass * (u.g)
     number_of_moles = mass * (u.g) / isotopic_mass
-    number_of_atoms = number_of_moles * c.N_A
-    N1 = number_of_atoms.value * np.exp(-decay_constant * time_delta)
-    expected = number_of_atoms.value - N1
+    number_of_atoms = (number_of_moles * c.N_A).value
+    N1 = number_of_atoms * np.exp(-decay_constant * time_delta)
+    expected = number_of_atoms - N1.value
 
-    npt.assert_almost_equal(actual, expected)
+    npt.assert_allclose(actual, expected, rtol=1e-7)
 
 
+@pytest.mark.xfail(reason="To be implemented")
 def test_activity_chain(simulation_setup):
     model = simulation_setup
     t_half_Ni = NI_INV.half_lives("s")["Ni-56"]
