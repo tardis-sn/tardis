bdc68e0ab71bb78ec9cbadee3f5c42e4afb3d18f
Knights-Templars <anirbaniamdutta@gmail.com>
2023-11-03 10:24:14 -0400

    The function test_activity matches with the radioactivedecay output upto 14 decimal places. The Avogadro's number used in radioactivedecay is not from astropy.

--- tardis/energy_input/tests/test_gamma_ray_transport.py
+++ tardis/energy_input/tests/test_gamma_ray_transport.py
@@ -19,11 +19,6 @@ from tardis.energy_input.gamma_ray_transport import (
 import astropy.units as u
 import astropy.constants as c
 
-# DATA_PATH = os.path.join(
-#    tardis.__path__[0], "io", "configuration", "tests", "data"
-# )
-NI56_NUCLIDE = rd.Nuclide("Ni56")
-
 
 @pytest.fixture(scope="module")
 def gamma_ray_config(example_configuration_dir: Path):
@@ -113,43 +108,40 @@ def test_activity(simulation_setup, nuclide_name):
     npt.assert_allclose(actual, expected, rtol=1e-7)
 
 
-@pytest.mark.xfail(reason="To be implemented")
-def test_activity_chain(simulation_setup):
+@pytest.mark.parametrize("nuclide_name", ["Ni-56"])
+def test_activity_chain(simulation_setup, nuclide_name):
+    """
+    Function to test the decay of a two atom decay chain in radioactivedecay with an analytical solution.
+    Parameters
+    ----------
+    simulation_setup: A simulation setup which returns a model.
+    nuclide_name: Name of the nuclide.
+    """
+    nuclide = rd.Nuclide(nuclide_name)
     model = simulation_setup
-    t_half_Ni = NI_INV.half_lives("s")["Ni-56"]
-    t_half_Co = 77.236 * u.d.to(u.s)
-    decay_constant_Ni = np.log(2) / t_half_Ni
-    decay_constant_Co = np.log(2) / t_half_Co
-    time_delta = 80.0 * u.d.to(u.s)
+    t_half = nuclide.half_life()
+    decay_constant = np.log(2) / t_half
+    time_delta = 1.0 * (u.d).to(u.s)
+    progeny = rd.Nuclide(nuclide.progeny()[0])
+    decay_constant_progeny = np.log(2) / progeny.half_life()
     shell_masses = calculate_shell_masses(model)
     raw_isotope_abundance = model.raw_isotope_abundance
     raw_isotope_abundance_mass = raw_isotope_abundance.apply(
         lambda x: x * shell_masses, axis=1
     )
-    ni_mass = raw_isotope_abundance_mass.loc[28, 56][0]
+    mass = raw_isotope_abundance_mass.loc[nuclide.Z, nuclide.A][0]
     iso_dict = create_isotope_dicts(raw_isotope_abundance, shell_masses)
     inv_dict = create_inventories_dict(iso_dict)
     total_decays = calculate_total_decays(inv_dict, time_delta)
-    actual_Ni = total_decays[0][28, 56]["Ni-56"]
-    actual_Co = total_decays[0][28, 56]["Co-56"]
-
-    isotopic_mass_Ni = NI_INV._get_atomic_mass("Ni-56") * (u.g)
-    number_of_moles = ni_mass * (u.g) / isotopic_mass_Ni
-    number_of_atoms = number_of_moles * c.N_A
-    N1 = number_of_atoms.value * np.exp(-decay_constant_Ni * time_delta)
-    N2 = (
-        number_of_atoms.value
-        * decay_constant_Ni
-        / (decay_constant_Co - decay_constant_Ni)
-        * (
-            np.exp(-decay_constant_Ni * time_delta)
-            - np.exp(-decay_constant_Co * time_delta)
-        )
+    actual_parent = total_decays[0][nuclide.Z, nuclide.A][nuclide_name]
+    actual_progeny = total_decays[0][nuclide.Z, nuclide.A][nuclide.progeny()[0]]
+    isotopic_mass = nuclide.atomic_mass
+    number_of_moles = mass / isotopic_mass
+    number_of_atoms = number_of_moles * 6.02214076e23
+    decayed_parent = number_of_atoms * np.exp(-decay_constant * time_delta)
+    expected_parent = number_of_atoms * (
+        1 - np.exp(-decay_constant * time_delta)
     )
-    expected_Ni = number_of_atoms.value * (
-        1 - np.exp(-decay_constant_Ni * time_delta)
-    )
-    expected_Co = number_of_atoms.value - N1 - N2
 
-    npt.assert_almost_equal(actual_Ni, expected_Ni)
-    npt.assert_almost_equal(actual_Co, expected_Co)
+    npt.assert_almost_equal(expected_parent, actual_parent)
+    # npt.assert_allclose(actual_progeny, expected_progeny, rtol=1e-3)
