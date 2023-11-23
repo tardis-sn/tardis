8e0de5c4b39defed67ec729266458e983033807b
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2023-11-23 08:29:30 -0800

    add radiation_field_state

---
+++ tardis/model/radiation_field_state.py
@@ -0,0 +1,30 @@
+from tardis.montecarlo.montecarlo_numba.numba_interface import OpacityState
+
+
+import numpy as np
+from astropy import units as u
+
+
+class DiluteThermalRadiationFieldState:
+    """_summary_
+
+    Parameters
+    ----------
+    t_radiative : u.Quantity
+        Radiative temperature in each shell
+    dilution_factor : numpy.ndarray
+        Dilution Factors in each shell
+    """
+
+    def __init__(
+        self,
+        t_radiative: u.Quantity,
+        dilution_factor: np.ndarray,
+    ):
+        # ensuring that the radiation_field has both
+        # dilution_factor and t_radiative equal length
+        assert len(t_radiative) == len(dilution_factor)
+        assert np.all(t_radiative > 0 * u.K)
+        assert np.all(dilution_factor > 0)
+        self.t_radiative = t_radiative
+        self.dilution_factor = dilution_factor
