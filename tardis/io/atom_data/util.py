0e8696de212eb7f6252ffa54b6ff019d98b62dd6
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2023-11-27 16:38:32 -0500

    Update import statements in util.py and shell_info.py

--- tardis/io/atom_data/util.py
+++ tardis/io/atom_data/util.py
@@ -4,6 +4,7 @@ import logging
 from tardis.io.configuration.config_internal import get_data_dir
 from tardis.io.atom_data.atom_web_download import (
     get_atomic_repo_config,
+    download_atom_data,
 )
 
 logger = logging.getLogger(__name__)
