6d64b2d7e8e3cdf8f4823a8f41550372305f8067
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2023-11-28 23:21:06 -0500

    Add test for montecarlo main loop with vpacket tracking

--- tardis/montecarlo/montecarlo_numba/tests/test_base.py
+++ tardis/montecarlo/montecarlo_numba/tests/test_base.py
@@ -74,3 +74,64 @@ def test_montecarlo_main_loop(
     npt.assert_allclose(actual_j_estimator, expected_j_estimator, rtol=1e-13)
     npt.assert_allclose(actual_energy.value, expected_energy, rtol=1e-13)
     npt.assert_allclose(actual_nu.value, expected_nu, rtol=1e-13)
+
+
+def test_montecarlo_main_loop_vpacket_log(
+    config_montecarlo_1e5_verysimple,
+    atomic_dataset,
+    tardis_ref_path,
+    tmpdir,
+    set_seed_fixture,
+    random_call_fixture,
+    request,
+):
+    montecarlo_configuration.LEGACY_MODE_ENABLED = True
+    # Setup model config from verysimple
+    atomic_data = deepcopy(atomic_dataset)
+    config_montecarlo_1e5_verysimple.montecarlo.last_no_of_packets = 1e5
+    config_montecarlo_1e5_verysimple.montecarlo.no_of_virtual_packets = 5
+    config_montecarlo_1e5_verysimple.montecarlo.iterations = 1
+    config_montecarlo_1e5_verysimple.plasma.line_interaction_type = "macroatom"
+
+    del config_montecarlo_1e5_verysimple["config_dirname"]
+
+    sim = Simulation.from_config(
+        config_montecarlo_1e5_verysimple,
+        atom_data=atomic_data,
+        virtual_packet_logging=True,
+    )
+    sim.run_convergence()
+    sim.run_final()
+
+    compare_fname = os.path.join(
+        tardis_ref_path, "montecarlo_1e5_compare_data.h5"
+    )
+    if request.config.getoption("--generate-reference"):
+        sim.to_hdf(compare_fname, overwrite=True)
+
+    # Load compare data from refdata
+    expected_nu = pd.read_hdf(
+        compare_fname, key="/simulation/transport/output_nu"
+    ).values
+    expected_energy = pd.read_hdf(
+        compare_fname, key="/simulation/transport/output_energy"
+    ).values
+    expected_nu_bar_estimator = pd.read_hdf(
+        compare_fname, key="/simulation/transport/nu_bar_estimator"
+    ).values
+    expected_j_estimator = pd.read_hdf(
+        compare_fname, key="/simulation/transport/j_estimator"
+    ).values
+
+    actual_energy = sim.transport.output_energy
+    actual_nu = sim.transport.output_nu
+    actual_nu_bar_estimator = sim.transport.nu_bar_estimator
+    actual_j_estimator = sim.transport.j_estimator
+
+    # Compare
+    npt.assert_allclose(
+        actual_nu_bar_estimator, expected_nu_bar_estimator, rtol=1e-13
+    )
+    npt.assert_allclose(actual_j_estimator, expected_j_estimator, rtol=1e-13)
+    npt.assert_allclose(actual_energy.value, expected_energy, rtol=1e-13)
+    npt.assert_allclose(actual_nu.value, expected_nu, rtol=1e-13)
