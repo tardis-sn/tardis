Fix photo ion calculation and radiation field intensity calculation
Co-authored-by: Christian Vogl <cvogl@mpa-garching.mpg.de>

tardis/model/radiation_field_state.py
@@ -3,6 +3,8 @@ from astropy import units as u
 
 from tardis.util.base import intensity_black_body
 
+from typing import Union
+
 
 class DiluteBlackBodyRadiationFieldState:
     """
@@ -30,7 +32,7 @@ class DiluteBlackBodyRadiationFieldState:
         self.t_radiative = t_radiative
         self.dilution_factor = dilution_factor
 
-    def calculate_mean_intensity(self, nu):
+    def calculate_mean_intensity(self, nu: Union[u.Quantity, np.ndarray]):
         """
         Calculate the intensity of the radiation field at a given frequency.
 
@@ -44,4 +46,6 @@ class DiluteBlackBodyRadiationFieldState:
         intensity : u.Quantity
             Intensity of the radiation field at the given frequency
         """
-        return self.dilution_factor * intensity_black_body(nu, self.t_radiative)
+        return self.dilution_factor * intensity_black_body(
+            nu[np.newaxis].T, self.t_radiative
+        )

tardis/plasma/properties/continuum_processes.py
@@ -1047,7 +1047,8 @@ class PhotoIonBoltzmannFactor(ProcessingPlasmaProperty):
 
     outputs = ("boltzmann_factor_photo_ion",)
 
-    def calculate(self, photo_ion_cross_sections, t_electrons):
+    @staticmethod
+    def calculate(photo_ion_cross_sections, t_electrons):
         nu = photo_ion_cross_sections["nu"].values
 
         boltzmann_factor = np.exp(-nu[np.newaxis].T / t_electrons * (H / K_B))