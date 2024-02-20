178d15b8342ba210f28a39cc040ca3bc7840d7f3
Jing Lu <lujingeve158@gmail.com>
2024-02-20 18:26:34 -0500

    prevent float overflow when calculating He I population

--- tardis/plasma/properties/helium_nlte.py
+++ tardis/plasma/properties/helium_nlte.py
@@ -82,7 +82,10 @@ class HeliumNLTE(ProcessingPlasmaProperty):
             * (1.0 / (2 * float(g.loc[2, 1, 0])))
             * (1 / g_electron)
             * (1 / (w**2.0))
-            * np.exp(ionization_data.loc[2, 1] * beta_rad)
+            * np.minimum(
+                np.exp(ionization_data.loc[2, 1] * beta_rad),
+                np.finfo(np.float64).max,
+            )  # Prevents overflow
         )
 
     @staticmethod
