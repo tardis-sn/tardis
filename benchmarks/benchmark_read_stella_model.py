02c39c877c59bcca2151ae767a4ff8dc03c7b854
Asish Kumar <officialasishkumar@gmail.com>
2024-03-13 11:47:34 +0530

    feat: updated benchmark_read_stella_model.py

--- benchmarks/benchmark_read_stella_model.py
+++ benchmarks/benchmark_read_stella_model.py
@@ -1,11 +1,10 @@
 """Basic TARDIS Benchmark."""
 import os
-from tardis.io.model.readers.stella import read_stella_model
 from tardis.io.model import read_stella_model
 
 
-class Benchmarkruntardis:
-    """Class to benchmark the run_tardis function.
+class Benchmark_read_stella_model:
+    """Class to benchmark the read_stella_model function.
     """
     timeout = 200
 
@@ -13,9 +12,8 @@ class Benchmarkruntardis:
         filename = "messa.stella.dat"
         dir_path = os.path.dirname(os.path.realpath(__file__))
         path = os.path.join(dir_path, "data", filename)
-        config = read_stella_model(path)
-        self.config = config
+        self.path = path
 
     def time_run_stella_model(self):
-        stella_model = read_stella_model(self.config)
+        stella_model = read_stella_model(self.path)
         stella_model
