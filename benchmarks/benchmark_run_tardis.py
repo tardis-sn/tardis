edb0e8f465fd7c55cdf8d4e1247e43e4a53f073c
Israel Roldan <israel.alberto.rv@gmail.com>
2024-03-15 16:40:41 -0600

    Added the development environment to make easy the test of the changes.

--- benchmarks/benchmark_run_tardis.py
+++ benchmarks/benchmark_run_tardis.py
@@ -1,13 +1,18 @@
 """Basic TARDIS Benchmark."""
 import os
-from tardis.io.configuration.config_reader import Configuration
-from tardis import run_tardis
 
-class Benchmarkruntardis:
+from tardis import run_tardis
+from tardis.io.configuration.config_reader import Configuration
+
+
+class BenchmarkRunTardis:
     """Class to benchmark the run_tardis function.
     """
     timeout = 200
-    
+
+    def __init__(self):
+        self.config = None
+
     def setup(self):
         filename = "tardis_configv1_benchmark.yml"
         dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -15,6 +20,6 @@ class Benchmarkruntardis:
         config = Configuration.from_yaml(path)
         config.atom_data = "kurucz_cd23_chianti_H_He.h5"
         self.config = config
-    
+
     def time_run_tardis(self):
         sim = run_tardis(self.config, log_level="ERROR", show_progress_bars=False)
