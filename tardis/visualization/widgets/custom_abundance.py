ac2f13d3de34c47b57345cda2993632ce86c8af1
Sumit112192 <sg0391175@gmail.com>
2024-04-09 14:39:29 +0530

    Fixed CustomAbundanceWidget Bug caused by using wrong parameters of simulation_state

--- tardis/visualization/widgets/custom_abundance.py
+++ tardis/visualization/widgets/custom_abundance.py
@@ -281,8 +281,8 @@ class CustomAbundanceWidgetData:
         -------
         CustomAbundanceWidgetData
         """
-        abundance = sim.simulation_state.raw_abundance.copy()
-        isotope_abundance = sim.simulation_state.raw_isotope_abundance.copy()
+        abundance = sim.simulation_state.abundance.copy()
+        isotope_abundance = sim.simulation_state.composition.raw_isotope_abundance.copy()
 
         # integrate element and isotope to one DataFrame
         abundance["mass_number"] = ""
