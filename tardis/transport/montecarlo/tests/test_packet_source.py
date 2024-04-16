9b66647ebdf61f168f2996503865095cc3d6e8c1
Atharva Arya <aryaatharva18@gmail.com>
2024-04-16 13:58:10 +0530

    separate classes for bb and bb_rel

--- tardis/transport/montecarlo/tests/test_packet_source.py
+++ tardis/transport/montecarlo/tests/test_packet_source.py
@@ -16,34 +16,26 @@ from tardis.transport.montecarlo import (
 from tardis.tests.fixtures.regression_data import RegressionData
 
 
-class TestPacketSource:
-    regression_data = None
+@pytest.fixture(scope="function")
+def packet_unit_test_fpath(tardis_ref_path):
+    """
+    Path to `packet_unittest.h5`.
 
-    @pytest.fixture(scope="class")
-    def config(self, request):
-        request.cls.regression_data = RegressionData(request)
-        request.cls.regression_data.fname = "packet_source.h5"
-        return
+    Parameters
+    ----------
+    tardis_ref_path : pd.HDFStore
 
-    @pytest.fixture(scope="class")
-    def packet_unit_test_fpath(self, tardis_ref_path):
-        """
-        Path to `packet_unittest.h5`.
+    Returns
+    -------
+    os.path
+    """
+    return os.path.abspath(
+        os.path.join(tardis_ref_path, "packet_unittest.h5")
+    )
 
-        Parameters
-        ----------
-        tardis_ref_path : pd.HDFStore
-
-        Returns
-        -------
-        os.path
-        """
-        return os.path.abspath(
-            os.path.join(tardis_ref_path, "packet_unittest.h5")
-        )
-
-    @pytest.fixture(scope="class")
-    def blackbodysimplesource(self, request, config):
+class TestBlackBodySimpleSource:
+    @pytest.fixture(scope="function")
+    def blackbodysimplesource(self, request, regression_data):
         """
         Create BlackBodySimpleSource instance.
 
@@ -52,35 +44,23 @@ class TestPacketSource:
         tardis.transport.montecarlo.packet_source.BlackBodySimpleSource
         """
         cls = type(self)
-        cls.bb = BlackBodySimpleSource(
-            base_seed=1963, legacy_mode_enabled=True, legacy_second_seed=2508
+        montecarlo_configuration.LEGACY_MODE_ENABLED = True
+        bb = BlackBodySimpleSource(
+            radius=123,
+            temperature = 10000 * u.K,
+            base_seed=1963, 
+            legacy_second_seed=2508
         )
-        yield cls.bb
+        nus = bb.create_packet_nus(100).value
+        mus = bb.create_packet_mus(100)
+        unif_energies = bb.create_packet_energies(100).value
+        regression_data.sync_hdf_store(bb)
 
-    @pytest.fixture(scope="class")
-    def blackbody_simplesource_relativistic(self, request, config):
-        """
-        Create BlackBodySimpleSourceRelativistic instance.
+        cls.regression_data =  regression_data
+        cls.bb = bb
 
-        Yields
-        -------
-        tardis.transport.montecarlo.packet_source.BlackBodySimpleSourceRelativistic
-        """
-        bb_rel = BlackBodySimpleSourceRelativistic(
-            base_seed=1963, legacy_mode_enabled=True, legacy_second_seed=2508
-        )
-        self.regression_data.sync_hdf_store(bb_rel, update_fname=False)
-        yield bb_rel
-
-    # def test_blackbody_regressions(
-    #     self, blackbodysimplesource
-    # ):
-    #     expected = pd.read_hdf(
-    #         self.regression_data.fpath, key="/black_body_simple_source/scalars"
-    #     )
-    #     np.testing.assert_allclose(
-    #         expected.base_seed, blackbodysimplesource.base_seed
-    #     )
+        yield nus, mus, unif_energies
+        montecarlo_configuration.LEGACY_MODE_ENABLED = False
 
     def test_bb_packet_sampling(
         self,
@@ -104,16 +84,57 @@ class TestPacketSource:
             pytest.skip("Reference data was generated during this run.")
 
         ref_df = tardis_ref_data["/packet_unittest/blackbody"]
-        self.bb.temperature = 10000 * u.K
-        nus = self.bb.create_packet_nus(100).value
-        mus = self.bb.create_packet_mus(100)
-        unif_energies = self.bb.create_packet_energies(100).value
+        nus, mus, unif_energies = blackbodysimplesource
         assert np.all(np.isclose(nus, ref_df["nus"]))
         assert np.all(np.isclose(mus, ref_df["mus"]))
         assert np.all(np.isclose(unif_energies, ref_df["energies"]))
 
+        expected_bb = pd.read_hdf(
+            self.regression_data.fpath, key="/black_body_simple_source/scalars"
+        )
+        assert_allclose(expected_bb.base_seed, self.bb.base_seed)
+        assert_allclose(expected_bb.temperature, self.bb.temperature.value)
+        assert_allclose(expected_bb.radius, self.bb.radius)
+        
+class TestBlackBodySimpleSourceRel:
+    @pytest.fixture(scope="function")
+    def blackbody_simplesource_relativistic(self, request, regression_data):
+        """
+        Create BlackBodySimpleSourceRelativistic instance.
+
+        Yields
+        -------
+        tardis.montecarlo.packet_source.BlackBodySimpleSourceRelativistic
+        """
+        cls = type(self)
+        montecarlo_configuration.LEGACY_MODE_ENABLED = True
+
+        bb_rel = BlackBodySimpleSourceRelativistic(
+            time_explosion=1123187
+        )
+
+        bb_rel.temperature = 10000 * u.K
+        bb_rel.beta = 0.25
+        nus = bb_rel.create_packet_nus(100).value
+        unif_energies = (
+            bb_rel.create_packet_energies(
+                100
+            ).value
+        )
+        bb_rel._reseed(2508)
+        mus = bb_rel.create_packet_mus(10)
+        gamma = np.sqrt(1 - bb_rel.beta**2) ** -1
+
+        regression_data.sync_hdf_store(bb_rel)
+        cls.regression_data = regression_data
+        cls.bb_rel = bb_rel
+
+        yield nus, mus, unif_energies, gamma
+        montecarlo_configuration.LEGACY_MODE_ENABLED = False
+
     def test_bb_packet_sampling_relativistic(
         self,
+        request,
         tardis_ref_data,
         blackbody_simplesource_relativistic,
     ):
@@ -123,20 +144,15 @@ class TestPacketSource:
         tardis_ref_data : pd.HDFStore
         blackbody_simplesource_relativistic : tardis.transport.montecarlo.packet_source.BlackBodySimpleSourceRelativistic
         """
-        blackbody_simplesource_relativistic.temperature = 10000 * u.K
-        blackbody_simplesource_relativistic.beta = 0.25
+        if request.config.getoption("--generate-reference"):
+            ref_bb = pd.read_hdf(packet_unit_test_fpath, key="/blackbody")
+            ref_bb.to_hdf(
+                tardis_ref_data, key="/packet_unittest/blackbody_rel", mode="a"
+            )
+            pytest.skip("Reference data was generated during this run.")
 
-        nus = blackbody_simplesource_relativistic.create_packet_nus(100).value
-        unif_energies = (
-            blackbody_simplesource_relativistic.create_packet_energies(
-                100
-            ).value
-        )
-        blackbody_simplesource_relativistic._reseed(2508)
-        mus = blackbody_simplesource_relativistic.create_packet_mus(10)
-
-        gamma = np.sqrt(1 - blackbody_simplesource_relativistic.beta**2) ** -1
-        ref_df = tardis_ref_data["/packet_unittest/blackbody"]
+        nus, mus, unif_energies, gamma = blackbody_simplesource_relativistic
+        ref_df = tardis_ref_data["/packet_unittest_rel/blackbody"]
         expected_nus = ref_df["nus"]
         expected_unif_energies = ref_df["energies"] * 1.6 / gamma
         expected_mus = np.array(
@@ -153,7 +169,17 @@ class TestPacketSource:
                 0.67876489,
             ]
         )
-
+        expected_bb = pd.read_hdf(
+            self.regression_data.fpath, key="/black_body_simple_source/scalars"
+        )
         assert_allclose(nus, expected_nus)
         assert_allclose(unif_energies, expected_unif_energies)
         assert_allclose(mus, expected_mus, rtol=1e-6)
+
+        assert_allclose(
+            expected_bb.time_explosion, self.bb_rel.time_explosion
+        )
+        assert_allclose(
+            expected_bb.temperature, self.bb_rel.temperature.value
+        )
+
