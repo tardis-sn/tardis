6ee9556358c12a0d23ad07a48d10846374492427
Atharva Arya <aryaatharva18@gmail.com>
2024-04-17 17:35:06 +0530

    Format using black

--- tardis/transport/montecarlo/tests/test_packet_source.py
+++ tardis/transport/montecarlo/tests/test_packet_source.py
@@ -15,6 +15,7 @@ from tardis.transport.montecarlo import (
 )
 from tardis.tests.fixtures.regression_data import RegressionData
 
+
 class TestBlackBodySimpleSource:
     @pytest.fixture(scope="class")
     def blackbodysimplesource(self, request):
@@ -29,13 +30,12 @@ class TestBlackBodySimpleSource:
         montecarlo_configuration.LEGACY_MODE_ENABLED = True
         bb = BlackBodySimpleSource(
             radius=123,
-            temperature = 10000 * u.K,
-            base_seed=1963, 
-            legacy_second_seed=2508
+            temperature=10000 * u.K,
+            base_seed=1963,
+            legacy_second_seed=2508,
         )
         yield bb
         montecarlo_configuration.LEGACY_MODE_ENABLED = False
-
 
     def test_bb_nus(self, regression_data, blackbodysimplesource):
         actual_nus = blackbodysimplesource.create_packet_nus(100).value
@@ -46,18 +46,25 @@ class TestBlackBodySimpleSource:
         actual_mus = blackbodysimplesource.create_packet_mus(100)
         expected_mus = regression_data.sync_ndarray(actual_mus)
         assert_allclose(actual_mus, expected_mus)
-    
+
     def test_bb_energies(self, regression_data, blackbodysimplesource):
-        actual_unif_energies = blackbodysimplesource.create_packet_energies(100).value
-        expected_unif_energies = regression_data.sync_ndarray(actual_unif_energies)
+        actual_unif_energies = blackbodysimplesource.create_packet_energies(
+            100
+        ).value
+        expected_unif_energies = regression_data.sync_ndarray(
+            actual_unif_energies
+        )
         assert_allclose(actual_unif_energies, expected_unif_energies)
-    
+
     def test_bb_attributes(self, regression_data, blackbodysimplesource):
         actual_bb = blackbodysimplesource
-        expected_bb = regression_data.sync_hdf_store(actual_bb)["/black_body_simple_source/scalars"]
+        expected_bb = regression_data.sync_hdf_store(actual_bb)[
+            "/black_body_simple_source/scalars"
+        ]
         assert_allclose(expected_bb.base_seed, actual_bb.base_seed)
         assert_allclose(expected_bb.temperature, actual_bb.temperature.value)
         assert_allclose(expected_bb.radius, actual_bb.radius)
+
 
 class TestBlackBodySimpleSourceRel:
     @pytest.fixture(scope="class")
@@ -79,14 +86,24 @@ class TestBlackBodySimpleSourceRel:
         montecarlo_configuration.LEGACY_MODE_ENABLED = False
 
     def test_bb_nus(self, regression_data, blackbody_simplesource_relativistic):
-        actual_nus = blackbody_simplesource_relativistic.create_packet_nus(100).value
+        actual_nus = blackbody_simplesource_relativistic.create_packet_nus(
+            100
+        ).value
         expected_nus = regression_data.sync_ndarray(actual_nus)
         assert_allclose(actual_nus, expected_nus)
 
-    def test_bb_energies(self, regression_data, blackbody_simplesource_relativistic):
-        actual_unif_energies = blackbody_simplesource_relativistic.create_packet_energies(100).value
+    def test_bb_energies(
+        self, regression_data, blackbody_simplesource_relativistic
+    ):
+        actual_unif_energies = (
+            blackbody_simplesource_relativistic.create_packet_energies(
+                100
+            ).value
+        )
         gamma = np.sqrt(1 - blackbody_simplesource_relativistic.beta**2) ** -1
-        expected_unif_energies = regression_data.sync_ndarray(actual_unif_energies)
+        expected_unif_energies = regression_data.sync_ndarray(
+            actual_unif_energies
+        )
         assert_allclose(actual_unif_energies, expected_unif_energies)
 
     def test_bb_mus(self, regression_data, blackbody_simplesource_relativistic):
@@ -94,11 +111,14 @@ class TestBlackBodySimpleSourceRel:
         actual_mus = blackbody_simplesource_relativistic.create_packet_mus(10)
         expected_mus = regression_data.sync_ndarray(actual_mus)
         assert_allclose(actual_mus, expected_mus)
-    
-    
-    def test_bb_attributes(self, regression_data, blackbody_simplesource_relativistic):
+
+    def test_bb_attributes(
+        self, regression_data, blackbody_simplesource_relativistic
+    ):
         actual_bb = blackbody_simplesource_relativistic
-        expected_bb = regression_data.sync_hdf_store(actual_bb)["/black_body_simple_source/scalars"]
+        expected_bb = regression_data.sync_hdf_store(actual_bb)[
+            "/black_body_simple_source/scalars"
+        ]
         assert_allclose(expected_bb.base_seed, actual_bb.base_seed)
         assert_allclose(expected_bb.temperature, actual_bb.temperature.value)
         assert_allclose(expected_bb.time_explosion, actual_bb.time_explosion)
