3e868c0d252ebfaad730f60b955ef6dc4cdceb4a
Atharva Arya <aryaatharva18@gmail.com>
2024-06-05 15:59:07 +0530

    Fix ruff rule W291
    Trailing whitespace

--- tardis/__init__.py
+++ tardis/__init__.py
@@ -3,7 +3,7 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import * 
+from ._astropy_init import *
 
 # ----------------------------------------------------------------------------
 
