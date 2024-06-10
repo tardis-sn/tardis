2eeb8bbdc58ff38987600696b80e8383df353c45
Sarthak Srivastava <sarthaksri592@gmail.com>
2024-06-10 15:57:14 +0530

    add no_of_shells

--- tardis/transport/montecarlo/base.py
+++ tardis/transport/montecarlo/base.py
@@ -187,9 +187,9 @@ class MonteCarloTransportSolver(HDFWriterMixin):
             total_iterations=total_iterations,
         )
 
-        transport_state._montecarlo_virtual_luminosity.value[:] = (
-            v_packets_energy_hist
-        )
+        transport_state._montecarlo_virtual_luminosity.value[
+            :
+        ] = v_packets_energy_hist
         transport_state.last_interaction_type = last_interaction_tracker.types
         transport_state.last_interaction_in_nu = last_interaction_tracker.in_nus
         transport_state.last_interaction_in_r = last_interaction_tracker.in_rs
