dcec152d14bafe077a192336e846da81e864256e
Andrew Fullard <andrewgfullard@gmail.com>
2024-06-10 11:36:47 -0400

    Removes unused file and imports

--- tardis/io/__init__.py
+++ tardis/io/__init__.py
@@ -1,18 +1,3 @@
 """
-A collection of subpackages and submodules to handle input and output data. 
+A collection of subpackages and submodules to handle input and output data.
 """
-
-from tardis.io.configuration.config_internal import (
-    get_internal_configuration,
-    get_data_dir,
-)
-
-# readin model_data
-
-from tardis.io.model.readers.generic_readers import (
-    read_simple_ascii_abundances,
-)
-from tardis.io.model.readers.generic_readers import (
-    read_simple_ascii_density,
-)
-from tardis.io.model.readers.base import read_density_file
