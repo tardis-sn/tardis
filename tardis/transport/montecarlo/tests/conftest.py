51b0002ffb41a2bb098006d5c67f6bc1a3567a5b
Sumit112192 <sg0391175@gmail.com>
2024-06-10 18:03:13 +0530

    Write more tests using actual data

--- tardis/transport/montecarlo/tests/conftest.py
+++ tardis/transport/montecarlo/tests/conftest.py
@@ -7,6 +7,7 @@ from tardis.transport.montecarlo.packet_collections import (
     VPacketCollection,
 )
 
+from tardis.base import run_tardis
 from tardis.simulation import Simulation
 from tardis.transport.montecarlo import RPacket
 from tardis.transport.montecarlo.estimators.radfield_mc_estimators import (
@@ -138,3 +139,21 @@ def random_call_fixture():
         np.random.random()
 
     return njit(random_call)
+
+
+@pytest.fixture(scope="package")
+def simulation_rpacket_tracking_enabled(config_verysimple, atomic_dataset):
+    config_verysimple.montecarlo.iterations = 3
+    config_verysimple.montecarlo.no_of_packets = 4000
+    config_verysimple.montecarlo.last_no_of_packets = -1
+    config_verysimple.spectrum.virtual.virtual_packet_logging = True
+    config_verysimple.montecarlo.no_of_virtual_packets = 1
+    config_verysimple.montecarlo.tracking.track_rpacket = True
+    config_verysimple.spectrum.num = 2000
+    atomic_data = deepcopy(atomic_dataset)
+    sim = run_tardis(
+        config_verysimple,
+        atom_data=atomic_data,
+        show_convergence_plots=False,
+    )
+    return sim
