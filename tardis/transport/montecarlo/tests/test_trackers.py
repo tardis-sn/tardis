74d45b65c97f3ee69d0dfa05adedb4a92ba89849
Sumit112192 <sg0391175@gmail.com>
2024-06-11 19:09:35 +0530

    Break the test to seperate out fixtures from the acutal test

--- tardis/transport/montecarlo/tests/test_trackers.py
+++ tardis/transport/montecarlo/tests/test_trackers.py
@@ -8,7 +8,9 @@ from tardis.transport.montecarlo.packet_trackers import (
 
 
 @pytest.fixture()
-def last_interactions(simulation_rpacket_tracking_enabled):
+def last_interactions_from_both_implementations(
+    simulation_rpacket_tracking_enabled,
+):
     sim = simulation_rpacket_tracking_enabled
     transport_state = sim.transport.transport_state
     checkedTypes = transport_state.last_interaction_type
@@ -21,6 +23,121 @@ def last_interactions(simulation_rpacket_tracking_enabled):
         toCheckTypes[i] = rpacket_last_interaction_tracker.interaction_type
 
     return (checkedTypes, toCheckTypes)
+
+
+@pytest.fixture()
+def last_interaction_from_rpacket_trackers(simulation_rpacket_tracking_enabled):
+    sim = simulation_rpacket_tracking_enabled
+
+    rpacket_trackers = sim.transport.transport_state.rpacket_tracker
+
+    # ToDo - use no_of_packets from config object.
+    no_of_packets = len(rpacket_trackers)
+
+    index_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
+
+    r_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
+
+    nu_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
+
+    energy_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
+
+    shell_id_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
+
+    type_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
+
+    for i in range(no_of_packets):
+        num_interactions = rpacket_trackers[i].num_interactions
+
+        index_from_rpacket_tracker[i] = rpacket_trackers[i].index
+
+        r_from_rpacket_tracker[i] = rpacket_trackers[i].r[num_interactions - 1]
+
+        nu_from_rpacket_tracker[i] = rpacket_trackers[i].nu[
+            num_interactions - 1
+        ]
+
+        energy_from_rpacket_tracker[i] = rpacket_trackers[i].energy[
+            num_interactions - 1
+        ]
+
+        shell_id_from_rpacket_tracker[i] = rpacket_trackers[i].shell_id[
+            num_interactions - 1
+        ]
+
+        type_from_rpacket_tracker[i] = rpacket_trackers[i].interaction_type[
+            num_interactions - 1
+        ]
+    return (
+        index_from_rpacket_tracker,
+        r_from_rpacket_tracker,
+        energy_from_rpacket_tracker,
+        shell_id_from_rpacket_tracker,
+        type_from_rpacket_tracker,
+    )
+
+
+@pytest.fixture()
+def last_interaction_from_rpacket_last_interaction_trackers(
+    simulation_rpacket_tracking_enabled,
+):
+
+    sim = simulation_rpacket_tracking_enabled
+    rpacket_last_interaction_trackers = (
+        sim.transport.transport_state.rpacket_last_interaction_trackers
+    )
+    # ToDo - use no_of_packets from config object.
+    no_of_packets = len(rpacket_last_interaction_trackers)
+
+    index_from_last_interaction_tracker = np.empty(
+        no_of_packets, dtype=np.int64
+    )
+
+    r_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.float64)
+
+    nu_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.float64)
+
+    energy_from_last_interaction_tracker = np.empty(
+        no_of_packets, dtype=np.float64
+    )
+
+    shell_id_from_last_interaction_tracker = np.empty(
+        no_of_packets, dtype=np.int64
+    )
+
+    type_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.int64)
+
+    for i in range(no_of_packets):
+        index_from_last_interaction_tracker[
+            i
+        ] = rpacket_last_interaction_trackers[i].index
+
+        r_from_last_interaction_tracker[i] = rpacket_last_interaction_trackers[
+            i
+        ].r
+
+        nu_from_last_interaction_tracker[i] = rpacket_last_interaction_trackers[
+            i
+        ].nu
+
+        energy_from_last_interaction_tracker[
+            i
+        ] = rpacket_last_interaction_trackers[i].energy
+
+        shell_id_from_last_interaction_tracker[
+            i
+        ] = rpacket_last_interaction_trackers[i].shell_id
+
+        type_from_last_interaction_tracker[
+            i
+        ] = rpacket_last_interaction_trackers[i].interaction_type
+    return (
+        index_from_last_interaction_tracker,
+        r_from_last_interaction_tracker,
+        energy_from_last_interaction_tracker,
+        shell_id_from_last_interaction_tracker,
+        type_from_last_interaction_tracker,
+    )
 
 
 def test_defaults():
@@ -42,98 +159,25 @@ def test_tracking_manual(static_packet):
     npt.assert_almost_equal(tracker.energy, 0.9)
 
 
-def test_tracking_from_last_interaction_simulation(last_interactions):
-    checkedTypes, toCheckTypes = last_interactions
+def test_tracking_from_last_interaction_simulation(
+    last_interactions_from_both_implementations,
+):
+    checkedTypes, toCheckTypes = last_interactions_from_both_implementations
     npt.assert_array_equal(checkedTypes, toCheckTypes)
 
 
 def test_tracking_from_rpacket_tracker_simulation(
-    simulation_rpacket_tracking_enabled,
+    last_interaction_from_rpacket_trackers,
+    last_interaction_from_rpacket_last_interaction_trackers,
 ):
-    sim = simulation_rpacket_tracking_enabled
-    rpacket_trackers = sim.transport.transport_state.rpacket_tracker
-    rpacket_last_interaction_trackers = (
-        sim.transport.transport_state.rpacket_last_interaction_trackers
-    )
-    no_of_packets = len(rpacket_trackers)
-
-    index_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
-    index_from_last_interaction_tracker = np.empty(
-        no_of_packets, dtype=np.int64
-    )
-
-    r_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
-    r_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.float64)
-
-    nu_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
-    nu_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.float64)
-
-    energy_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.float64)
-    energy_from_last_interaction_tracker = np.empty(
-        no_of_packets, dtype=np.float64
-    )
-
-    shell_id_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
-    shell_id_from_last_interaction_tracker = np.empty(
-        no_of_packets, dtype=np.int64
-    )
-
-    type_from_rpacket_tracker = np.empty(no_of_packets, dtype=np.int64)
-    type_from_last_interaction_tracker = np.empty(no_of_packets, dtype=np.int64)
-
-    for i in range(no_of_packets):
-        interactions = rpacket_trackers[i].num_interactions
-
-        index_from_rpacket_tracker[i] = rpacket_trackers[i].index
-        index_from_last_interaction_tracker[
-            i
-        ] = rpacket_last_interaction_trackers[i].index
-
-        r_from_rpacket_tracker[i] = rpacket_trackers[i].r[interactions - 1]
-        r_from_last_interaction_tracker[i] = rpacket_last_interaction_trackers[
-            i
-        ].r
-
-        nu_from_rpacket_tracker[i] = rpacket_trackers[i].nu[interactions - 1]
-        nu_from_last_interaction_tracker[i] = rpacket_last_interaction_trackers[
-            i
-        ].nu
-
-        energy_from_rpacket_tracker[i] = rpacket_trackers[i].energy[
-            interactions - 1
-        ]
-        energy_from_last_interaction_tracker[
-            i
-        ] = rpacket_last_interaction_trackers[i].energy
-
-        shell_id_from_rpacket_tracker[i] = rpacket_trackers[i].shell_id[
-            interactions - 1
-        ]
-        shell_id_from_last_interaction_tracker[
-            i
-        ] = rpacket_last_interaction_trackers[i].shell_id
-
-        type_from_rpacket_tracker[i] = rpacket_trackers[i].interaction_type[
-            interactions - 1
-        ]
-        type_from_last_interaction_tracker[
-            i
-        ] = rpacket_last_interaction_trackers[i].interaction_type
-
-    npt.assert_array_equal(
-        index_from_rpacket_tracker, index_from_last_interaction_tracker
-    )
-    npt.assert_allclose(r_from_rpacket_tracker, r_from_last_interaction_tracker)
+    # Both the arguments are tuples with the same type stored at the same index position
+    # index -> data stored
+    # 0     -> index of Rpacket
+    # 1     -> radius
+    # 2     -> energy
+    # 3     -> shell_id
+    # 4     -> type
     npt.assert_allclose(
-        nu_from_rpacket_tracker, nu_from_last_interaction_tracker
-    )
-    npt.assert_allclose(
-        energy_from_rpacket_tracker,
-        energy_from_last_interaction_tracker,
-    )
-    npt.assert_array_equal(
-        shell_id_from_rpacket_tracker, shell_id_from_last_interaction_tracker
-    )
-    npt.assert_array_equal(
-        type_from_rpacket_tracker, type_from_last_interaction_tracker
+        last_interaction_from_rpacket_trackers,
+        last_interaction_from_rpacket_last_interaction_trackers,
     )
