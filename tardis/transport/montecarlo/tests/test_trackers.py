c4f2163c9b1b9bdf22f20b81c8326588f1c12205
Sumit112192 <sg0391175@gmail.com>
2024-06-19 10:46:17 +0530

    Remove config flag for RPacketLastInteractionTracker

--- tardis/transport/montecarlo/tests/test_trackers.py
+++ tardis/transport/montecarlo/tests/test_trackers.py
@@ -11,35 +11,12 @@ from tardis.transport.montecarlo.packet_trackers import (
 )
 
 
-@pytest.fixture(scope="module")
-def config_last_interaction(example_configuration_dir):
-    return Configuration.from_yaml(
-        example_configuration_dir / "tardis_configv1_verysimple.yml"
-    )
-
-
-@pytest.fixture(scope="module")
-def simulation_last_interaction_tracking_enabled(
-    config_last_interaction, atomic_dataset
-):
-    config_last_interaction.montecarlo.iterations = 3
-    config_last_interaction.montecarlo.no_of_packets = 4000
-    config_last_interaction.montecarlo.last_no_of_packets = -1
-    config_last_interaction.montecarlo.tracking.track_last_interaction = True
-    atomic_data = deepcopy(atomic_dataset)
-    sim = run_tardis(
-        config_last_interaction,
-        atom_data=atomic_data,
-        show_convergence_plots=False,
-    )
-    return sim
-
-
 @pytest.fixture()
 def interaction_type_last_interaction_class1(
-    simulation_last_interaction_tracking_enabled,
+    nb_simulation_verysimple,
 ):
-    sim = simulation_last_interaction_tracking_enabled
+    """Last interaction types of rpacket from LastInteractionTracker"""
+    sim = nb_simulation_verysimple
     transport_state = sim.transport.transport_state
     interaction_type = transport_state.last_interaction_type
     return interaction_type
@@ -47,9 +24,10 @@ def interaction_type_last_interaction_class1(
 
 @pytest.fixture()
 def interaction_type_last_interaction_class2(
-    simulation_last_interaction_tracking_enabled,
+    nb_simulation_verysimple,
 ):
-    sim = simulation_last_interaction_tracking_enabled
+    """Last interaction types of rpacket from RPacketLastInteractionTracker"""
+    sim = nb_simulation_verysimple
     transport_state = sim.transport.transport_state
     interaction_type = np.empty(
         len(transport_state.rpacket_tracker), dtype=np.int64
@@ -64,18 +42,20 @@ def interaction_type_last_interaction_class2(
 
 @pytest.fixture()
 def nu_from_packet_collection(
-    simulation_last_interaction_tracking_enabled,
+    nb_simulation_verysimple,
 ):
-    sim = simulation_last_interaction_tracking_enabled
+    """Last interaction output nus of rpacket from packet_collection"""
+    sim = nb_simulation_verysimple
     packet_collection = sim.transport.transport_state.packet_collection
     return packet_collection.output_nus
 
 
 @pytest.fixture()
 def nu_from_last_interaction_class(
-    simulation_last_interaction_tracking_enabled,
+    nb_simulation_verysimple,
 ):
-    sim = simulation_last_interaction_tracking_enabled
+    """Last interaction output nus of rpacket from RPacketLastInteractionTracker"""
+    sim = nb_simulation_verysimple
     transport_state = sim.transport.transport_state
     nu = np.empty(len(transport_state.rpacket_tracker), dtype=np.float64)
     for i, last_interaction_tracker in enumerate(
