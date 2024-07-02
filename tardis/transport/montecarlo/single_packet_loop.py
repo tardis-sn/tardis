82e90b4e67c392d4210c0d40f8dbf44d99e5eff5
Sumit112192 <sg0391175@gmail.com>
2024-07-02 14:59:15 +0530

    Implement Dead Branch Pruning Approach

--- tardis/transport/montecarlo/single_packet_loop.py
+++ tardis/transport/montecarlo/single_packet_loop.py
@@ -41,6 +41,7 @@ def single_packet_loop(
     vpacket_collection,
     rpacket_tracker,
     montecarlo_configuration,
+    ENABLE_RPACKET_TRACKING,
 ):
     """
     Parameters
@@ -83,7 +84,7 @@ def single_packet_loop(
         montecarlo_configuration.CONTINUUM_PROCESSES_ENABLED,
     )
 
-    if montecarlo_configuration.ENABLE_RPACKET_TRACKING:
+    if ENABLE_RPACKET_TRACKING is not None:
         rpacket_tracker.track(r_packet)
 
     # this part of the code is temporary and will be better incorporated
@@ -266,7 +267,7 @@ def single_packet_loop(
             pass
         if (
             interaction_type != InteractionType.BOUNDARY
-            and montecarlo_configuration.ENABLE_RPACKET_TRACKING
+            and ENABLE_RPACKET_TRACKING is not None
         ):
             rpacket_tracker.track(r_packet)
 
