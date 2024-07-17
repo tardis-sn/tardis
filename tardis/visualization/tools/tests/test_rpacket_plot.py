1e2b00518f9b273518a3002fbef532214bf29fbd
Sumit112192 <sg0391175@gmail.com>
2024-07-17 15:26:00 +0530

    Remove unused imports and fixtures

--- tardis/visualization/tools/tests/test_rpacket_plot.py
+++ tardis/visualization/tools/tests/test_rpacket_plot.py
@@ -11,41 +11,6 @@ from copy import deepcopy
 from tardis.visualization import RPacketPlotter
 
 
-@pytest.fixture(scope="module")
-def simulation_simple(config_verysimple, atomic_dataset):
-    """
-    Creating a simulation object using a simple configuration.
-
-    Parameters
-    ----------
-    config_verysimple : tardis.io.config_reader.Configuration
-        Configuration object for a very simple simulation.
-    atomic_dataset : str or tardis.atomic.AtomData
-        Atomic data.
-
-    Returns
-    -------
-    sim: tardis.simulation.base.Simulation
-        Simulation object.
-    """
-    # Setup simulation configuration using config_verysimple and
-    # override properties in such a way to make the simulation run faster
-    config_verysimple.montecarlo.iterations = 3
-    config_verysimple.montecarlo.no_of_packets = 50
-    config_verysimple.montecarlo.last_no_of_packets = -1
-    config_verysimple.montecarlo.no_of_virtual_packets = 1
-    # enabling rpacket tracking
-    config_verysimple.montecarlo.tracking.track_rpacket = True
-
-    atomic_data = deepcopy(atomic_dataset)
-    sim = run_tardis(
-        config_verysimple,
-        atom_data=atomic_data,
-        show_convergence_plots=False,
-    )
-    return sim
-
-
 @pytest.mark.rpacket_tracking
 class TestRPacketPlotter:
     """Test the RPacketPlotter class."""
