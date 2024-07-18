7be27566288a4f10da2641161df060d76daf6d3c
Sumit112192 <sg0391175@gmail.com>
2024-07-18 13:44:01 +0530

    Add test for last interaction tracker

---
+++ tardis/transport/montecarlo/tests/test_rpacket_last_interaction_tracker.py
@@ -0,0 +1,140 @@
+import pytest
+import numpy as np
+import numpy.testing as npt
+
+from tardis.transport.montecarlo.r_packet import InteractionType
+from tardis.transport.montecarlo.packet_trackers import (
+    RPacketLastInteractionTracker,
+)
+
+
+@pytest.fixture(scope="module")
+def interaction_type_last_interaction_class_old(
+    nb_simulation_verysimple,
+):
+    """Last interaction types of rpacket from LastInteractionTracker"""
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    interaction_type = transport_state.last_interaction_type
+    return interaction_type
+
+
+@pytest.fixture()
+def shell_id_last_interaction_class_old(
+    nb_simulation_verysimple,
+    interaction_type_last_interaction_class_old,
+):
+    """Last interaction types of rpacket from LastInteractionTracker"""
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    shell_id = transport_state.last_line_interaction_shell_id
+    mask = interaction_type_last_interaction_class_old == InteractionType.LINE
+    return shell_id[mask]
+
+
+@pytest.fixture(scope="module")
+def interaction_type_last_interaction_class_new(
+    nb_simulation_verysimple,
+):
+    """Last interaction types of rpacket from RPacketLastInteractionTracker"""
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    interaction_type = np.empty(
+        len(transport_state.rpacket_tracker), dtype=np.int64
+    )
+    for i, last_interaction_tracker in enumerate(
+        transport_state.rpacket_tracker
+    ):
+        interaction_type[i] = last_interaction_tracker.interaction_type
+
+    return interaction_type
+
+
+@pytest.fixture()
+def shell_id_last_interaction_class_new(
+    nb_simulation_verysimple,
+    interaction_type_last_interaction_class_new,
+):
+    """Last interaction types of rpacket from RPacketLastInteractionTracker"""
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    shell_id = np.empty(len(transport_state.rpacket_tracker), dtype=np.int64)
+    for i, last_interaction_tracker in enumerate(
+        transport_state.rpacket_tracker
+    ):
+        shell_id[i] = last_interaction_tracker.shell_id
+    mask = interaction_type_last_interaction_class_new == InteractionType.LINE
+    return shell_id[mask]
+
+
+@pytest.fixture()
+def nu_from_packet_collection(
+    nb_simulation_verysimple,
+):
+    """Last interaction output nus of rpacket from packet_collection"""
+    packet_collection = (
+        nb_simulation_verysimple.transport.transport_state.packet_collection
+    )
+    return packet_collection.output_nus
+
+
+@pytest.fixture()
+def nu_from_last_interaction_class_new(
+    nb_simulation_verysimple,
+):
+    """Last interaction output nus of rpacket from RPacketLastInteractionTracker"""
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    nu = np.empty(len(transport_state.rpacket_tracker), dtype=np.float64)
+    for i, last_interaction_tracker in enumerate(
+        transport_state.rpacket_tracker
+    ):
+        nu[i] = last_interaction_tracker.nu
+
+    return nu
+
+
+def test_defaults():
+    tracker = RPacketLastInteractionTracker()
+    assert tracker.index == -1
+    assert tracker.shell_id == -1
+    assert tracker.interaction_type == -1
+    npt.assert_almost_equal(tracker.r, -1.0)
+    npt.assert_almost_equal(tracker.nu, 0.0)
+    npt.assert_almost_equal(tracker.energy, 0.0)
+
+
+def test_tracking_manual(static_packet):
+    tracker = RPacketLastInteractionTracker()
+    tracker.track(static_packet)
+    assert tracker.index == 0
+    npt.assert_almost_equal(tracker.r, 7.5e14)
+    npt.assert_almost_equal(tracker.nu, 0.4)
+    npt.assert_almost_equal(tracker.energy, 0.9)
+
+
+def test_config_flags(nb_simulation_verysimple):
+    ENABLE_RPACKET_TRACKING = (
+        nb_simulation_verysimple.transport.montecarlo_configuration.ENABLE_RPACKET_TRACKING
+    )
+    ENABLE_RPACKET_LAST_INTERACTION_TRACKING = (
+        nb_simulation_verysimple.transport.montecarlo_configuration.ENABLE_RPACKET_LAST_INTERACTION_TRACKING
+    )
+
+    assert ENABLE_RPACKET_TRACKING == None
+    assert ENABLE_RPACKET_LAST_INTERACTION_TRACKING == True
+
+
+@pytest.mark.parametrize(
+    "expected,obtained",
+    [
+        (
+            "interaction_type_last_interaction_class_old",
+            "interaction_type_last_interaction_class_new",
+        ),
+        (
+            "shell_id_last_interaction_class_old",
+            "shell_id_last_interaction_class_new",
+        ),
+        ("nu_from_packet_collection", "nu_from_last_interaction_class_new"),
+    ],
+)
+def test_last_interaction_properties(expected, obtained, request):
+    expected = request.getfixturevalue(expected)
+    obtained = request.getfixturevalue(obtained)
+    npt.assert_allclose(expected, obtained)
