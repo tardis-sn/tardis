2766eddd960a69c9c7a12f9d663d17d3c7638d3a
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2024-07-25 11:23:24 -0400

    [Restructure/plasma] Refactor the simulation state initialization (#2664)
    * change numberdensity to input
    
    * fixed number density
    
    * some fixes
    
    * removing density
    
    * remove atomic and isotope mass
    
    * add isotopic_number_density
    
    * add opacities package
    
    * Update imports in property_collections.py, base.py, test_numba_interface.py, transport_montecarlo_numba_interface.py, conftest.py, formal_integral.py, base.py, and macro_atom.py
    
    * Add calculate_transition_probabilities function to util.py in macro_atom package
    
    * Add calculate_transition_probabilities function to util.py in macro_atom package
    
    * Remove unused imports and update plasma properties
    
    * add __init__ to macroatom
    
    * blackify tardis
    
    * blackified
    
    * chore: Update imports and remove unused code
    
    * chore: Add PlanckRadiationField and DilutePlanckRadiationField classes
    
    * chore: Update imports and remove unused code
    
    * removed density
    
    * ruff output
    
    * cleanup and adding object mode
    
    * starting to make radiation_field a thing
    
    * switched over to old tau_sobolev calculation
    
    * renamed function to indicate numba use
    
    * address comments
    
    * added dilute planckian radiation field
    
    * refactor: Convert species lists to proper format in assemble_plasma function
    
    * moved radiation field into plasma. Resulting in some renames
    
    * some fixes
    
    * black montecarlo
    
    * chore: Initialize atom data and simulation state in `initialization.py`
    
    * updating the documentation
    
    * remove parse_input.py
    
    * chore: Refactor radiation field configuration parsing and state creation
    
    Refactor the `parse_radiation_field_configuration.py` module to improve code organization and readability. Update the import statements to reflect the changes in the module structure. Replace the deprecated `DiluteBlackBodyRadiationFieldState` class with the new `DilutePlanckianRadiationField` class from the `tardis.plasma.radiation_field` module. This change ensures consistency and compatibility with the latest codebase.
    
    Also, update the `tardis/simulation/base.py` module to import the `DilutePlanckianRadiationField` class from the `tardis.plasma.radiation_field` module. This change ensures that the correct class is used for creating the radiation field in the simulation.
    
    * revert astropy_helpers
    
    * remove astropy_helpers
    
    * removed test.txt
    
    * blackified code
    
    * cleanup simulation from merges
    
    * fix
    
    * remove unused Input
    
    * added description
    
    * blackiefied codebase
    
    * blackify code
    
    * chore: Refactor atom data parsing and simulation state initialization
    
    * restructure logger
    
    * merge changes
    
    * blackify
    
    * ruffify

---
+++ tardis/io/model/parse_simulation_state.py
@@ -0,0 +1,54 @@
+from tardis.io.model.parse_packet_source_configuration import (
+    initialize_packet_source,
+)
+from tardis.model import SimulationState
+
+
+def parse_simulation_state(
+    config, packet_source, enable_legacy_mode, kwargs, atom_data
+):
+    """
+    Initialize the simulation state.
+
+    Parameters
+    ----------
+    config : object
+        The configuration object for the simulation.
+    packet_source : object
+        The packet source for the simulation.
+    legacy_mode_enabled : bool
+        Flag indicating if legacy mode is enabled.
+    kwargs : dict
+        Additional keyword arguments.
+    atom_data : object
+        The atom data for the simulation.
+
+    Returns
+    -------
+    object
+        The initialized simulation state.
+    """
+    if "model" in kwargs:
+        simulation_state = kwargs["model"]
+    else:
+        if hasattr(config, "csvy_model"):
+            simulation_state = SimulationState.from_csvy(
+                config,
+                atom_data=atom_data,
+                legacy_mode_enabled=enable_legacy_mode,
+            )
+        else:
+            simulation_state = SimulationState.from_config(
+                config,
+                atom_data=atom_data,
+                legacy_mode_enabled=enable_legacy_mode,
+            )
+        if packet_source is not None:
+            simulation_state.packet_source = initialize_packet_source(
+                config,
+                simulation_state.geometry,
+                packet_source,
+                enable_legacy_mode,
+            )
+
+    return simulation_state
