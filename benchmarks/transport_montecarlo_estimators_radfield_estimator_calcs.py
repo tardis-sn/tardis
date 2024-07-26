7abf514173d47cb66b1791cdef48035a3168a789
Asish Kumar <officialasishkumar@gmail.com>
2024-07-26 21:55:09 +0530

    Refactor montecarlo estimators in benchmark code
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- benchmarks/transport_montecarlo_estimators_radfield_estimator_calcs.py
+++ benchmarks/transport_montecarlo_estimators_radfield_estimator_calcs.py
@@ -2,15 +2,12 @@
 Basic TARDIS Benchmark.
 """
 
-import tardis.opacities.opacities as opacities
-import tardis.transport.geometry.calculate_distances as calculate_distances
-import tardis.transport.montecarlo.r_packet_transport as r_packet_transport
-import tardis.transport.montecarlo.utils as utils
+import functools
+
 from benchmarks.benchmark_base import BenchmarkBase
 from tardis.transport.montecarlo.estimators.radfield_estimator_calcs import (
     update_line_estimators,
 )
-from asv_runner.benchmarks.mark import parameterize
 
 
 class BenchmarkMontecarloMontecarloNumbaPacket(BenchmarkBase):
@@ -18,40 +15,12 @@ class BenchmarkMontecarloMontecarloNumbaPacket(BenchmarkBase):
     Class to benchmark the numba packet function.
     """
 
-    @parameterize(
-        {
-            "Parameters": [
-                {
-                    "cur_line_id": 0,
-                    "distance_trace": 1e12,
-                    "time_explosion": 5.2e7,
-                    "enable_full_relativity": True,
-                },
-                {
-                    "cur_line_id": 0,
-                    "distance_trace": 0,
-                    "time_explosion": 5.2e7,
-                    "enable_full_relativity": True,
-                },
-                {
-                    "cur_line_id": 1,
-                    "distance_trace": 1e5,
-                    "time_explosion": 1e10,
-                    "enable_full_relativity": False,
-                },
-            ]
-        }
-    )
-    def time_update_line_estimators(self, parameters):
-        cur_line_id = parameters["cur_line_id"]
-        distance_trace = parameters["distance_trace"]
-        time_explosion = parameters["time_explosion"]
-        enable_full_relativity = parameters["enable_full_relativity"]
+    @functools.cache
+    def setup(self):
+        self.Estimators = self.estimators
+        self.StaticPacket = self.static_packet
+
+    def time_update_line_estimators(self):
         update_line_estimators(
-            self.estimators,
-            self.static_packet,
-            cur_line_id,
-            distance_trace,
-            time_explosion,
-            enable_full_relativity,
+            self.Estimators, self.StaticPacket, 1, 1e12, 1e10, True
         )
