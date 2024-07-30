f13004635487c9e2a06f1bcfd358c75df650d8b4
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2024-07-30 17:26:38 -0400

    feat: Refactor code to improve readability and maintainability

--- tardis/plasma/detailed_balance/rates.py
+++ tardis/plasma/detailed_balance/rates.py
@@ -1,7 +1,15 @@
 class RadiativeRatesSolver:
     def __init__(self, einstein_coefficients):
-        self.einstein_coefficients = einstein_coefficients
+        assert einstein_coefficients.index.names == [
+            "atomic_number",
+            "ion_number",
+            "level_number_lower",
+            "level_number_upper",
+        ]
+        assert {"A_ul", "B_ul", "B_lu", "nu"} - set(
+            einstein_coefficients.columns
+        ) == set()
+        self.einstein_coefficients = einstein_coefficients.sort_index()
 
-    def solve(self, mean_intensities_blue):
-        
-
+    def solve(self, mean_intensities):
+        pass
