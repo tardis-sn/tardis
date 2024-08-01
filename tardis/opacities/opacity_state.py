c82dde5fb55a81dafc67e8b182efb9084a0d58a3
Jack O'Brien <jobrien585@gmail.com>
2024-08-01 17:34:19 -0400

    Opacity state slice (#2758)
    * Added basic sliing operation to opacitystate and set the transport solver to use the bounds of the gemoetry indices
    
    * Reformatted the slicing index
    
    * Added slicing operation with __getitem__
    
    * Added docstring
    
    * removed old slice method
    
    * Ran black
    
    * Added back the 'slice' method because __getitem__ is wacky
    
    * added missing argument
    
    * fixed missing argument
    
    * ran black
    
    * fixed the tests
    
    * ran black
    
    * Updated the slice to use __getitem__ again and modified the opacitystate object defined in the opacities module
    
    * Removed old slice method and print statements
    
    * removed print statement from the test
    
    * added type annotation to OpacityState getitem
    
    * updated numba interface

--- tardis/opacities/opacity_state.py
+++ tardis/opacities/opacity_state.py
@@ -106,6 +106,41 @@ class OpacityState:
         self.photo_ion_activation_idx = photo_ion_activation_idx
         self.k_packet_idx = k_packet_idx
 
+    def __getitem__(self, i: slice):
+        """Get a shell or slice of shells of the attributes of the opacity state
+
+        Args:
+            i (slice): shell slice.  Will fail if slice is int since class only supports array types
+
+        Returns:
+            OpacityState : a shallow copy of the current instance
+        """
+        # NOTE: This currently will not work with continuum processes since it does not slice those arrays
+        return OpacityState(
+            self.electron_density[i],
+            self.t_electrons[i],
+            self.line_list_nu,
+            self.tau_sobolev[:, i],
+            self.transition_probabilities[:, i],
+            self.line2macro_level_upper,
+            self.macro_block_references,
+            self.transition_type,
+            self.destination_level_id,
+            self.transition_line_id,
+            self.bf_threshold_list_nu,
+            self.p_fb_deactivation,
+            self.photo_ion_nu_threshold_mins,
+            self.photo_ion_nu_threshold_maxs,
+            self.photo_ion_block_references,
+            self.chi_bf,
+            self.x_sect,
+            self.phot_nus,
+            self.ff_opacity_factor,
+            self.emissivities,
+            self.photo_ion_activation_idx,
+            self.k_packet_idx,
+        )
+
 
 def opacity_state_initialize(
     plasma,
--- tardis/transport/montecarlo/base.py
+++ tardis/transport/montecarlo/base.py
@@ -23,7 +23,7 @@ from tardis.transport.montecarlo.montecarlo_main_loop import (
 from tardis.transport.montecarlo.montecarlo_transport_state import (
     MonteCarloTransportState,
 )
-from tardis.transport.montecarlo.numba_interface import (
+from tardis.opacities.opacity_state import ( 
     opacity_state_initialize,
 )
 from tardis.transport.montecarlo.packet_trackers import (
@@ -110,9 +110,6 @@ class MonteCarloTransportSolver(HDFWriterMixin):
         packet_collection = self.packet_source.create_packets(
             no_of_packets, seed_offset=iteration
         )
-        estimators = initialize_estimator_statistics(
-            plasma.tau_sobolevs.shape, gamma_shape
-        )
 
         geometry_state = simulation_state.geometry.to_numba()
         opacity_state = opacity_state_initialize(
@@ -120,6 +117,12 @@ class MonteCarloTransportSolver(HDFWriterMixin):
             self.line_interaction_type,
             self.montecarlo_configuration.DISABLE_LINE_SCATTERING,
         )
+        opacity_state = opacity_state[simulation_state.geometry.v_inner_boundary_index: simulation_state.geometry.v_outer_boundary_index]
+        
+        estimators = initialize_estimator_statistics(
+            opacity_state.tau_sobolev.shape, gamma_shape
+        )
+
         transport_state = MonteCarloTransportState(
             packet_collection,
             estimators,
