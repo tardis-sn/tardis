5d07c65917f45143682a94da6ec0ac308893c48b
rodot- <jobrien585@gmail.com>
2024-08-02 12:42:18 -0400

    Added new test for the numba opacity state

---
+++ tardis/opacities/tests/test_opacity_state_numba.py
@@ -0,0 +1,77 @@
+import pytest
+from tardis.opacities.opacity_state import opacity_state_to_numba
+from tardis.opacities.opacity_solver import OpacitySolver
+import numpy.testing as npt
+import numpy as np
+
+
+@pytest.mark.parametrize(
+    "line_interaction_type,sliced",
+    [
+        ("scatter", False),
+        ("macroatom", False),
+        ("macroatom", True),
+        ("downbranch", False),
+        ("downbranch", True),
+    ],
+)
+def test_opacity_state_initialize(
+    nb_simulation_verysimple, line_interaction_type, sliced
+):
+    legacy_plasma = nb_simulation_verysimple.plasma
+
+    opacity_solver = OpacitySolver(
+        line_interaction_type=line_interaction_type,
+        disable_line_scattering=False,
+    )
+    opacity_state = opacity_solver.solve(legacy_plasma)
+    actual = opacity_state_to_numba(opacity_state, line_interaction_type)
+
+    if sliced:
+        index = slice(2, 5)
+        actual = actual[index]
+    else:
+        index = ...
+
+    npt.assert_allclose(
+        actual.electron_density, plasma.electron_densities.values[index]
+    )
+    npt.assert_allclose(actual.line_list_nu, plasma.atomic_data.lines.nu.values)
+    npt.assert_allclose(
+        actual.tau_sobolev, plasma.tau_sobolevs.values[:, index]
+    )
+    if line_interaction_type == "scatter":
+        empty = np.zeros(1, dtype=np.int64)
+        npt.assert_allclose(
+            actual.transition_probabilities, np.zeros((1, 1), dtype=np.float64)
+        )
+        npt.assert_allclose(actual.line2macro_level_upper, empty)
+        npt.assert_allclose(actual.macro_block_references, empty)
+        npt.assert_allclose(actual.transition_type, empty)
+        npt.assert_allclose(actual.destination_level_id, empty)
+        npt.assert_allclose(actual.transition_line_id, empty)
+    else:
+        npt.assert_allclose(
+            actual.transition_probabilities,
+            plasma.transition_probabilities.values[:, index],
+        )
+        npt.assert_allclose(
+            actual.line2macro_level_upper,
+            plasma.atomic_data.lines_upper2macro_reference_idx,
+        )
+        npt.assert_allclose(
+            actual.macro_block_references,
+            plasma.atomic_data.macro_atom_references["block_references"].values,
+        )
+        npt.assert_allclose(
+            actual.transition_type,
+            plasma.atomic_data.macro_atom_data["transition_type"].values,
+        )
+        npt.assert_allclose(
+            actual.destination_level_id,
+            plasma.atomic_data.macro_atom_data["destination_level_idx"].values,
+        )
+        npt.assert_allclose(
+            actual.transition_line_id,
+            plasma.atomic_data.macro_atom_data["lines_idx"].values,
+        )
