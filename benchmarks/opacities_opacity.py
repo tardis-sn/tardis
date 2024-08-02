0349e7a90c90bfb9d9dbc5a106adfcbfda9a6f39
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2024-08-03 01:18:15 +0530

    Cache benchmark functions (#2749)
    * added setup function
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * refactor geometry calculate distances
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * Refactor montecarlo estimators in benchmark code
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * add cache and setup functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * add cache and setup functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * ran ruff linter
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * Thou hath vanquished the redundant variables and functions
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * fix bugs
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * change benchmark yml
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * repeat in class
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * rounds in benchmarks
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * fix
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * fix error in formal integral
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * more cache
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * sort by ratio
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    * cache in setup function
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>
    
    ---------
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- benchmarks/opacities_opacity.py
+++ benchmarks/opacities_opacity.py
@@ -2,8 +2,6 @@
 Basic TARDIS Benchmark.
 """
 
-from asv_runner.benchmarks.mark import parameterize
-
 import tardis.opacities.opacities as calculate_opacity
 from benchmarks.benchmark_base import BenchmarkBase
 from tardis.opacities.opacities import compton_opacity_calculation
@@ -14,95 +12,25 @@ class BenchmarkMontecarloMontecarloNumbaOpacities(BenchmarkBase):
     Class to benchmark the numba opacities function.
     """
 
-    @parameterize(
-        {
-            "Electron number density": [
-                1.0e11,
-                1e15,
-                1e5,
-            ],
-            "Energy": [
-                511.0,
-                255.5,
-                511.0e7,
-            ],
-        }
-    )
-    def time_compton_opacity_calculation(self, electron_number_density, energy):
-        compton_opacity_calculation(energy, electron_number_density)
-
-    @parameterize(
-        {
-            "Parameters": [
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 511.0,
-                    "Iron_group_fraction": 0.5,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 5110000000.0,
-                    "Iron_group_fraction": 0.0,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 0.0,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 0.5,
-                },
-                {
-                    "Ejecta_density": 100000.0,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 1.0,
-                }
-            ]
-        }
-    )
-    def time_photoabsorption_opacity_calculation(self, parameters):
-        calculate_opacity.photoabsorption_opacity_calculation(
-            parameters["Energy"],
-            parameters["Ejecta_density"],
-            parameters["Iron_group_fraction"],
+    def time_compton_opacity_calculation(self):
+        energy = 511.0
+        electron_number_density = 1e15
+        calculate_opacity.compton_opacity_calculation(
+            energy, electron_number_density
         )
 
-    @parameterize(
-        {
-            "Parameters": [
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 511.0,
-                    "Iron_group_fraction": 0.5,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 5110000000.0,
-                    "Iron_group_fraction": 0.0,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 0.0,
-                },
-                {
-                    "Ejecta_density": 0.01,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 0.5,
-                },
-                {
-                    "Ejecta_density": 100000.0,
-                    "Energy": 255.5,
-                    "Iron_group_fraction": 1.0,
-                }
-            ]
-        }
-    )
-    def time_pair_creation_opacity_calculation(self, parameters):
+    def time_photoabsorption_opacity_calculation(self):
+        energy = 255.5
+        ejecta_density = 100000.0
+        iron_group_fraction = 0.5
+        calculate_opacity.photoabsorption_opacity_calculation(
+            energy, ejecta_density, iron_group_fraction
+        )
+
+    def time_pair_creation_opacity_calculation(self):
+        energy = 255.9
+        ejecta_density = 100000.009
+        iron_group_fraction = 0.5
         calculate_opacity.pair_creation_opacity_calculation(
-            parameters["Energy"],
-            parameters["Ejecta_density"],
-            parameters["Iron_group_fraction"],
+            energy, ejecta_density, iron_group_fraction
         )
--- benchmarks/transport_montecarlo_single_packet_loop.py
+++ benchmarks/transport_montecarlo_single_packet_loop.py
@@ -2,9 +2,10 @@
 Basic TARDIS Benchmark.
 """
 
+from numba.np.ufunc.parallel import get_num_threads, get_thread_id
+
 from benchmarks.benchmark_base import BenchmarkBase
 from tardis.transport.montecarlo import single_packet_loop
-from numba.np.ufunc.parallel import get_num_threads, get_thread_id
 
 
 class BenchmarkTransportMontecarloSinglePacketLoop(BenchmarkBase):
@@ -12,14 +13,18 @@ class BenchmarkTransportMontecarloSinglePacketLoop(BenchmarkBase):
     Class to benchmark the single packet loop function.
     """
 
+    repeat = 2
+
     def time_single_packet_loop(self):
         single_packet_loop.single_packet_loop(
             self.packet,
             self.verysimple_numba_radial_1d_geometry,
             self.verysimple_time_explosion,
             self.verysimple_opacity_state,
-            self.transport_state.radfield_mc_estimators.create_estimator_list(get_num_threads())[get_thread_id()],
+            self.transport_state.radfield_mc_estimators.create_estimator_list(
+                get_num_threads()
+            )[get_thread_id()],
             self.verysimple_3vpacket_collection,
             self.rpacket_tracker,
-            self.montecarlo_configuration
+            self.montecarlo_configuration,
         )
