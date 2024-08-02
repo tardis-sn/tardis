3131a6b8fc03b3d95edb9f30bdbff17941a9b95b
rodot- <jobrien585@gmail.com>
2024-08-02 12:07:53 -0400

    added conftest to the opacities module

---
+++ tardis/opacities/tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from copy import deepcopy
+from tardis.simulation import Simulation
+
+@pytest.fixture(scope="package")
+def nb_simulation_verysimple(config_verysimple, atomic_dataset):
+    atomic_data = deepcopy(atomic_dataset)
+    sim = Simulation.from_config(config_verysimple, atom_data=atomic_data)
+    sim.iterate(10)
+    return sim
