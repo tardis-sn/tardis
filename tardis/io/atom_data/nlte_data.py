c85ab6b107589a8bacc0370ab40d0b7181fbf3ad
Andrew Fullard <andrewgfullard@gmail.com>
2024-08-06 14:11:50 -0400

    Black format

--- tardis/io/atom_data/nlte_data.py
+++ tardis/io/atom_data/nlte_data.py
@@ -32,12 +32,12 @@ class NLTEData:
                 & (self.lines.ion_number == species[1])
             )
             self.lines_idx[species] = lines_idx
-            self.lines_level_number_lower[species] = (
-                self.lines.level_number_lower.values[lines_idx].astype(int)
-            )
-            self.lines_level_number_upper[species] = (
-                self.lines.level_number_upper.values[lines_idx].astype(int)
-            )
+            self.lines_level_number_lower[
+                species
+            ] = self.lines.level_number_lower.values[lines_idx].astype(int)
+            self.lines_level_number_upper[
+                species
+            ] = self.lines.level_number_upper.values[lines_idx].astype(int)
 
             self.A_uls[species] = self.lines.A_ul.values[lines_idx]
             self.B_uls[species] = self.lines.B_ul.values[lines_idx]
@@ -72,9 +72,9 @@ class NLTEData:
                 line,
             ) in collision_group.get_group(species).iterrows():
                 # line.columns : delta_e, g_ratio, temperatures ...
-                C_ul_matrix[level_number_lower, level_number_upper, :] = (
-                    line.values[2:]
-                )
+                C_ul_matrix[
+                    level_number_lower, level_number_upper, :
+                ] = line.values[2:]
                 delta_E_matrix[level_number_lower, level_number_upper] = line[
                     "delta_e"
                 ]
