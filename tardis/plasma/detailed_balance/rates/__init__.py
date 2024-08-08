4a4aa47d1d9226d6f8bbeeca936f61ed3ef2f47e
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2024-08-08 16:29:58 -0400

    further work o nthe collisional rates

--- tardis/plasma/detailed_balance/rates/__init__.py
+++ tardis/plasma/detailed_balance/rates/__init__.py
@@ -1,7 +1,11 @@
 from tardis.plasma.detailed_balance.rates.radiative_rates import (
     RadiativeRatesSolver,
 )
-from tardis.plasma.detailed_balance.rates.collisional_strengths import (
+from tardis.plasma.detailed_balance.rates.collision_strengths import (
     UpsilonRegemorterSolver,
     UpsilonCMFGENSolver,
+)
+
+from tardis.plasma.detailed_balance.rates.collisional_rates import (
+    ThermalCollisionalRateSolver,
 )
