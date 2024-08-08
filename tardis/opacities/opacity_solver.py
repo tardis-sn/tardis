82a70d4db937cd8f4c01e1cfe94997a3b570db0d
rodot- <jobrien585@gmail.com>
2024-08-08 09:04:11 -0400

    Removed switch to auto-build continuum macrostate using legacy mode

--- tardis/opacities/opacity_solver.py
+++ tardis/opacities/opacity_solver.py
@@ -77,7 +77,7 @@ class OpacitySolver(object):
                 legacy_plasma.stimulated_emission_factor,
             )
 
-        if self.line_interaction_type != "scatter" or not montecarlo_globals.CONTINUUM_PROCESSES_ENABLED:
+        if self.line_interaction_type != "scatter": # Need to pass though the continuum species
             macroatom_state = self.macro_atom_solver.solve(
                 legacy_plasma,
                 atomic_data,
