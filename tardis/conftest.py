9fa3464341515edafb27cf16b54ec2fd0df77eb4
Atharva Arya <aryaatharva18@gmail.com>
2024-08-23 10:21:24 +0530

    Remove refdata reference from .py files, tests pass

--- tardis/conftest.py
+++ tardis/conftest.py
@@ -92,9 +92,6 @@ def pytest_configure(config):
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--tardis-refdata", default=None, help="Path to Tardis Reference Folder"
-    )
-    parser.addoption(
         "--tardis-regression-data",
         default=None,
         help="Path to the TARDIS regression data directory",
@@ -144,15 +141,13 @@ def generate_reference(request):
     else:
         return option
 
-
 @pytest.fixture(scope="session")
-def tardis_ref_path(request):
-    tardis_ref_path = request.config.getoption("--tardis-refdata")
-    if tardis_ref_path is None:
-        pytest.skip("--tardis-refdata was not specified")
+def tardis_regression_path(request):
+    tardis_regression_path = request.config.getoption("--tardis-regression-data")
+    if tardis_regression_path is None:
+        pytest.skip("--tardis-regression-data was not specified")
     else:
-        return Path(os.path.expandvars(os.path.expanduser(tardis_ref_path)))
-
+        return Path(os.path.expandvars(os.path.expanduser(tardis_regression_path)))
 
 @pytest.fixture(scope="session")
 def tardis_snapshot_path(request):
