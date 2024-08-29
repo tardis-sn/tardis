cb78fc0242d453f21981ccb49f9fc6c68a384f68
Sumit112192 <sg0391175@gmail.com>
2024-08-29 17:02:56 +0530

    Write tests to check radius

--- tardis/transport/montecarlo/tests/test_rpacket_last_interaction_tracker.py
+++ tardis/transport/montecarlo/tests/test_rpacket_last_interaction_tracker.py
@@ -24,12 +24,26 @@ def shell_id_in_use(
     interaction_type_in_use,
 ):
     """
-    shell_id when last interaction is line from LastInteractionTracker class
+    `shell_id` when last interaction is line from LastInteractionTracker class
     """
     transport_state = nb_simulation_verysimple.transport.transport_state
     shell_id = transport_state.last_line_interaction_shell_id
     mask = interaction_type_in_use == InteractionType.LINE
     return shell_id[mask]
+
+
+@pytest.fixture()
+def r_in_use(
+    nb_simulation_verysimple,
+    interaction_type_in_use,
+):
+    """
+    `r` when last interaction is line from LastInteractionTracker class
+    """
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    r = transport_state.last_interaction_in_r
+    mask = interaction_type_in_use == InteractionType.LINE
+    return r[mask]
 
 
 @pytest.fixture(scope="module")
@@ -67,6 +81,24 @@ def shell_id_to_check(
         shell_id[i] = last_interaction_tracker.shell_id
     mask = interaction_type_to_check == InteractionType.LINE
     return shell_id[mask]
+
+
+@pytest.fixture()
+def r_to_check(
+    nb_simulation_verysimple,
+    interaction_type_to_check,
+):
+    """
+    `r` when last interaction is line from RPacketLastInteractionTracker class
+    """
+    transport_state = nb_simulation_verysimple.transport.transport_state
+    r = np.empty(len(transport_state.rpacket_tracker), dtype=np.int64)
+    for i, last_interaction_tracker in enumerate(
+        transport_state.rpacket_tracker
+    ):
+        r[i] = last_interaction_tracker.r
+    mask = interaction_type_to_check == InteractionType.LINE
+    return r[mask]
 
 
 @pytest.fixture()
@@ -126,6 +158,10 @@ def test_tracking_manual(static_packet):
         (
             "shell_id_in_use",
             "shell_id_to_check",
+        ),
+        (
+            "r_in_use",
+            "r_to_check",
         ),
         ("nu_packet_collection", "nu_to_check"),
     ],
