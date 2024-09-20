0a2ffbb0e7853c646864a2b967e2451076b0b23e
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-09-20 18:56:40 +0530

    Refactor assignment of Helium population updated  values

--- tardis/plasma/properties/level_population.py
+++ tardis/plasma/properties/level_population.py
@@ -98,5 +98,5 @@ class LevelNumberDensityHeNLTE(LevelNumberDensity):
             partition_function,
         )
         if helium_population_updated is not None:
-            level_number_density.loc[2].update(helium_population_updated)
+            level_number_density.loc[2, helium_population_updated.columns] = helium_population_updated.values
         return level_number_density
