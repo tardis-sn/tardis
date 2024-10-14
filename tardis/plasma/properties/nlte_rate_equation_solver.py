e7d152cadc188b8d85fbe3588c3d8eff1257f2ca
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-10-14 13:09:25 +0530

    remove explicit float conversion

--- tardis/plasma/properties/nlte_rate_equation_solver.py
+++ tardis/plasma/properties/nlte_rate_equation_solver.py
@@ -933,7 +933,7 @@ def ion_matrix(ion_coefficients, atomic_number, ion_number):
     offdiag = np.zeros(atomic_number)
     index = ion_coefficients.index
     for i in index:
-        offdiag[i] = float(ion_coefficients.loc[i].values[0])
+        offdiag[i] = ion_coefficients.loc[i].values[0]
     diag = np.hstack([-offdiag, np.zeros(1)])
     return (np.diag(diag) + np.diag(offdiag, k=-1))[ion_number, :]
 
@@ -957,7 +957,7 @@ def recomb_matrix(recomb_coefficients, atomic_number, ion_number):
     offdiag = np.zeros(atomic_number)
     index = recomb_coefficients.index
     for i in index:
-        offdiag[i] = float(recomb_coefficients.loc[i].values[0])
+        offdiag[i] = recomb_coefficients.loc[i].values[0]
     diag = np.hstack([np.zeros(1), -offdiag])
     return (np.diag(diag) + np.diag(offdiag, k=1))[ion_number, :]
 
