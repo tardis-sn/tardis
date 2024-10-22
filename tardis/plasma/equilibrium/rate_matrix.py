8afca4470454d1e63d67c206c844c36b0348e1d8
Andrew Fullard <andrewgfullard@gmail.com>
2024-10-22 11:28:24 -0400

    Rates matrix solver

--- tardis/plasma/equilibrium/rate_matrix.py
+++ tardis/plasma/equilibrium/rate_matrix.py
@@ -1,21 +1,65 @@
+import numpy as np
 import pandas as pd
+from scipy.sparse import coo_matrix
 
 
 class RateMatrix:
-    def __init__(self, rates: list):
-        self.rates = rates
+    def __init__(
+        self,
+        rate_solvers: list,
+        levels: pd.DataFrame,
+        electron_number_densities,
+    ):
+        self.rate_solvers = rate_solvers
+        self.levels = levels
+        self.electron_number_densities = electron_number_densities
 
     def solve(self):
-        rate_matrix = pd.DataFrame()
-        for rate in self.rates:
-            rate_df = rate.solve()
-            rate_matrix += rate_df
+        rates_df_list = [
+            solver.solve(input) for solver, input in self.rate_solvers
+        ]
+        # Extract all indexes
+        all_indexes = set()
+        for df in rates_df_list:
+            all_indexes.update(df.index)
 
-        # get unique species (atomic, ion numbers) and group by them
-        # fill NAN values with 0
-        """ rate_matrices
-        for species_id, rates in rates.groupby(level=(‘atomic_number’, ‘ion_number’)):
-            rate_matrices.loc[species_id] = make_rate_matrix_from_rates """
-        # make first n = 1 to normalize (see plasma docs and L237 partition_functions.py)
+        # Create a union of all indexes
+        all_indexes = sorted(all_indexes)
 
-        return rate_matrix
+        # Reindex each dataframe to ensure consistent indices
+        rates_df_list = [
+            df.reindex(all_indexes, fill_value=0) for df in rates_df_list
+        ]
+
+        rates_df_list[1] *= self.electron_number_densities
+        rates_df = sum(rates_df_list)
+
+        grouped_rates_df = rates_df.groupby(
+            level=("atomic_number", "ion_number")
+        )
+
+        rate_matrices = pd.DataFrame(
+            index=grouped_rates_df.groups.keys(), columns=rates_df.columns
+        )
+
+        for species_id, rates in grouped_rates_df:
+            number_of_levels = self.levels.energy.loc[species_id].count()
+            for shell in range(len(rates.columns)):
+                matrix = coo_matrix(
+                    (
+                        rates[shell],
+                        (
+                            rates.index.get_level_values(
+                                "level_number_destination"
+                            ),
+                            rates.index.get_level_values("level_number_source"),
+                        ),
+                    ),
+                    shape=(number_of_levels, number_of_levels),
+                )
+                matrix_array = matrix.toarray()
+                np.fill_diagonal(matrix_array, -np.sum(matrix_array, axis=0))
+                matrix_array[0, :] = 1
+                rate_matrices.loc[species_id, shell] = matrix_array
+
+        return rate_matrices
