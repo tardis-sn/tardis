541c369b8e84d503d4aad75c76650396d440c072
Andrew Fullard <andrewgfullard@gmail.com>
2024-11-05 10:59:16 -0500

    Better name for rate matrix variable

--- tardis/plasma/equilibrium/level_populations.py
+++ tardis/plasma/equilibrium/level_populations.py
@@ -30,9 +30,13 @@ class LevelPopulationSolver:
         np.ndarray
             The normalized, per-level population.
         """
-        x = np.zeros(rates_matrix.shape[0])  # give better name
-        x[0] = 1.0
-        normalized_level_population = np.linalg.solve(rates_matrix[:, :], x)
+        normalized_ion_population = np.zeros(
+            rates_matrix.shape[0]
+        )  # give better name
+        normalized_ion_population[0] = 1.0
+        normalized_level_population = np.linalg.solve(
+            rates_matrix[:, :], normalized_ion_population
+        )
         return normalized_level_population
 
     def solve(self):
