cc0b125d9b4d5a92cf7a7a0072ffe259f8028262
Andrew <andrewgfullard@gmail.com>
2024-12-03 15:43:14 -0500

    Rates matrix solver (#2865)
    * Rename detailed_balance to equilibrium
    
    * Add ion number source and destination as index columns
    
    * Rates matrix solver
    
    * Normalized level population solver (not working)
    
    * Correct shape of assignment
    
    * Correctly updating the dataframe (still chained assignment)
    
    * Adds docstrings
    
    * Fixes level population indexing and add TODO for the rate matrix solver
    
    * Refactor of the rate matrix solver to adapt to different inputs
    
    Also include electron distribution class to hold relevant info.
    
    * Add consistent
    
    * Adds support for no approximation of collisional rates
    
    * Fix transformation of rate matrix
    
    * Make electron_distribution a dataclass
    
    * Better name for rate matrix variable
    
    * Adds simple tests for level pop solver
    
    * Correct index in col strength test
    
    * Resolve test indexing issue
    
    * Specify CGS for the electron distribution
    
    * Rename electron_distribution to electron_energy_distribution
    
    * Adds basic regression data test to the level population solver
    
    * Adds proper rate matrix and level population solver tests
    
    * Rename electron energy dist. input
    
    * Adds example notebooks and comment on tests
    
    * Remove outdated notebook
    
    * Address comment
    
    * Pre-run tardis solver workflow

--- tardis/io/atom_data/base.py
+++ tardis/io/atom_data/base.py
@@ -171,7 +171,6 @@ class AtomData:
     # Either all or none of the related dataframes must be given
     related_groups = [
         ("macro_atom_data_all", "macro_atom_references_all"),
-        ("collision_data", "collision_data_temperatures"),
     ]
 
     @classmethod
@@ -203,35 +202,32 @@ class AtomData:
                     store["metadata"].loc[("format", "version")].value
                 )
                 carsus_version = tuple(map(int, carsus_version_str.split(".")))
-                if carsus_version == (1, 0):
-                    # Checks for various collisional data from Carsus files
-                    if "collisions_data" in store:
-                        try:
+                # Checks for various collisional data from Carsus files
+                if "collisions_data" in store:
+                    try:
+                        if carsus_version == (1, 0):
                             dataframes["collision_data_temperatures"] = store[
                                 "collisions_metadata"
                             ].temperatures
-                            if "cmfgen" in store["collisions_metadata"].dataset:
-                                dataframes["yg_data"] = store["collisions_data"]
-                                dataframes["collision_data"] = "dummy value"
-                            elif (
-                                "chianti"
-                                in store["collisions_metadata"].dataset
-                            ):
-                                dataframes["collision_data"] = store[
-                                    "collisions_data"
-                                ]
-                            else:
-                                raise KeyError(
-                                    "Atomic Data Collisions Not a Valid Chanti or CMFGEN Carsus Data File"
-                                )
-                        except KeyError as e:
-                            logger.warning(
-                                "Atomic Data is not a Valid Carsus Atomic Data File"
+                        if "cmfgen" in store["collisions_metadata"].dataset:
+                            dataframes["yg_data"] = store["collisions_data"]
+                            dataframes["collision_data"] = "dummy value"
+                        elif "chianti" in store["collisions_metadata"].dataset:
+                            dataframes["collision_data"] = store[
+                                "collisions_data"
+                            ]
+                        else:
+                            raise KeyError(
+                                "Atomic Data Collisions Not a Valid Chanti or CMFGEN Carsus Data File"
                             )
-                            raise
-                    dataframes["levels"] = store["levels_data"]
-                    dataframes["lines"] = store["lines_data"]
-                else:
+                    except KeyError as e:
+                        logger.warning(
+                            "Atomic Data is not a Valid Carsus Atomic Data File"
+                        )
+                        raise
+                dataframes["levels"] = store["levels_data"]
+                dataframes["lines"] = store["lines_data"]
+                if carsus_version != (1, 0) and carsus_version != (2, 0):
                     raise ValueError(
                         f"Current carsus version, {carsus_version}, is not supported."
                     )
@@ -528,9 +524,9 @@ class AtomData:
         )
 
         level_idxs2continuum_idx = photo_ion_levels_idx.copy()
-        level_idxs2continuum_idx[
-            "continuum_idx"
-        ] = self.level2continuum_edge_idx
+        level_idxs2continuum_idx["continuum_idx"] = (
+            self.level2continuum_edge_idx
+        )
         self.level_idxs2continuum_idx = level_idxs2continuum_idx.set_index(
             ["source_level_idx", "destination_level_idx"]
         )
@@ -564,31 +560,33 @@ class AtomData:
                 self.macro_atom_references = self.macro_atom_references.loc[
                     self.macro_atom_references["count_down"] > 0
                 ]
-                self.macro_atom_references.loc[
-                    :, "count_total"
-                ] = self.macro_atom_references["count_down"]
-                self.macro_atom_references.loc[
-                    :, "block_references"
-                ] = np.hstack(
-                    (
-                        0,
-                        np.cumsum(
-                            self.macro_atom_references["count_down"].values[:-1]
-                        ),
+                self.macro_atom_references.loc[:, "count_total"] = (
+                    self.macro_atom_references["count_down"]
+                )
+                self.macro_atom_references.loc[:, "block_references"] = (
+                    np.hstack(
+                        (
+                            0,
+                            np.cumsum(
+                                self.macro_atom_references["count_down"].values[
+                                    :-1
+                                ]
+                            ),
+                        )
                     )
                 )
 
             elif line_interaction_type == "macroatom":
-                self.macro_atom_references.loc[
-                    :, "block_references"
-                ] = np.hstack(
-                    (
-                        0,
-                        np.cumsum(
-                            self.macro_atom_references["count_total"].values[
-                                :-1
-                            ]
-                        ),
+                self.macro_atom_references.loc[:, "block_references"] = (
+                    np.hstack(
+                        (
+                            0,
+                            np.cumsum(
+                                self.macro_atom_references[
+                                    "count_total"
+                                ].values[:-1]
+                            ),
+                        )
                     )
                 )
 
---
+++ tardis/plasma/equilibrium/level_populations.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+
+class LevelPopulationSolver:
+    def __init__(self, rates_matrices: pd.DataFrame, levels: pd.DataFrame):
+        """Solve the normalized level population values from the rate matrices.
+
+        Parameters
+        ----------
+        rates_matrices : pd.DataFrame
+            DataFrame of rate matrices indexed by atomic number and ion number,
+            with each column being a cell.
+        levels : pd.DataFrame
+            DataFrame of energy levels.
+        """
+        self.rates_matrices = rates_matrices
+        self.levels = levels
+
+    def __calculate_level_population(self, rates_matrix: np.ndarray):
+        """Helper function to calculate the normalized, per-level boltzmann factor.
+
+        Parameters
+        ----------
+        rates_matrix : np.ndarray
+            The rate matrix for a given species and cell.
+
+        Returns
+        -------
+        np.ndarray
+            The normalized, per-level population.
+        """
+        normalized_ion_population = np.zeros(rates_matrix.shape[0])
+        normalized_ion_population[0] = 1.0
+        normalized_level_population = np.linalg.solve(
+            rates_matrix[:, :], normalized_ion_population
+        )
+        return normalized_level_population
+
+    def solve(self):
+        """Solves the normalized level population values from the rate matrices.
+
+        Returns
+        -------
+        pd.DataFrame
+            Normalized level population values indexed by atomic number, ion
+            number and level number. Columns are cells.
+        """
+        normalized_level_populations = pd.DataFrame(
+            index=self.levels.index,
+            columns=self.rates_matrices.columns,
+            dtype=np.float64,
+        )
+
+        # try converting the set of vectors into a single 2D array and then applying index
+        for species_id in self.rates_matrices.index:
+            # TODO: resolve the chained assignment here. Maybe an intermediate df
+            # is needed
+
+            solved_matrices = self.rates_matrices.loc[species_id].apply(
+                lambda rates_matrix: self.__calculate_level_population(
+                    rates_matrix
+                )
+            )
+            normalized_level_populations.loc[species_id, :].update(
+                np.vstack(solved_matrices.values).T
+            )
+
+        return normalized_level_populations
