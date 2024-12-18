ce16856ea443a67e0961c8978081104996f44bb3
Andrew Fullard <andrewgfullard@gmail.com>
2024-12-18 16:15:27 -0500

    Adds arguments and notes to the rate solver

--- tardis/plasma/equilibrium/rates/photoionization_rates.py
+++ tardis/plasma/equilibrium/rates/photoionization_rates.py
@@ -6,33 +6,50 @@ from tardis.plasma.equilibrium.rates.photoionization_strengths import (
 
 
 class PhotoionizationRateSolver:
-    def __init__():
-        pass
+    def __init__(
+        self, photoionization_cross_sections, level2continuum_edge_idx
+    ):
+        self.photoionization_cross_sections = photoionization_cross_sections
+        self.level2continuum_edge_idx = level2continuum_edge_idx
 
-    def solve(n_i, n_k, n_e, saha_factor, type="analytic"):
+    def solve(
+        self, electron_temperature, n_i, n_k, n_e, saha_factor, type="analytic"
+    ):
         if type == "analytic":
+            # TODO: try merging the analytic and estimator based approaches
+            # look at the Lucy 2003 equations and our MC estimator classes
             photoionization_rate_coeff_solver = (
-                AnalyticPhotoionizationCoeffSolver()
+                AnalyticPhotoionizationCoeffSolver(
+                    self.photoionization_cross_sections
+                )
             )
         elif type == "estimated":
             photoionization_rate_coeff_solver = (
-                EstimatedPhotoionizationCoeffSolver()
+                EstimatedPhotoionizationCoeffSolver(
+                    self.level2continuum_edge_idx
+                )
             )
         else:
             raise ValueError(f"Type {type} not supported")
 
         spontaneous_recombination_rate_coeff_solver = (
-            SpontaneousRecombinationCoeffSolver()
+            SpontaneousRecombinationCoeffSolver(
+                self.photoionization_cross_sections
+            )
         )
 
         photoionization_rate_coeff, stimulated_recombination_rate_coeff = (
+            # TODO: bifurcation of classes here is a problem
             photoionization_rate_coeff_solver.solve()
         )
 
         spontaneous_recombination_rate_coeff = (
-            spontaneous_recombination_rate_coeff_solver.solve()
+            spontaneous_recombination_rate_coeff_solver.solve(
+                electron_temperature
+            )
         )
 
+        # TODO: decide if these n_i, n_k, n_e numbers should be here (probably not)
         photoionization_rate = (
             photoionization_rate_coeff * n_i
             - saha_factor * stimulated_recombination_rate_coeff * n_k * n_e
