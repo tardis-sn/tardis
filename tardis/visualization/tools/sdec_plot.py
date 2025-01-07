560274a52f20f66c38b524cc17ba7a8890387c20
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-01-07 15:33:13 +0530

    Function to  create a dictionary containing virtual and real packet data

--- tardis/visualization/tools/sdec_plot.py
+++ tardis/visualization/tools/sdec_plot.py
@@ -23,6 +23,7 @@ from tardis.util.base import (
 from tardis.visualization import plot_util as pu
 from tardis.visualization.tools.simulation_packet_data import (
     SimulationPacketData,
+    create_packet_data_dict,
 )
 
 logger = logging.getLogger(__name__)
