0f50ffde96459a5e00b41c380e37bc41646cc55d
Swayam Shah <swayamshah66@gmail.com>
2025-01-10 12:53:36 +0530

    opacity_state_to_numba -> to_numba and also a method of OpacityState

--- tardis/transport/montecarlo/base.py
+++ tardis/transport/montecarlo/base.py
@@ -7,8 +7,9 @@ import tardis.transport.montecarlo.configuration.constants as constants
 from tardis import constants as const
 from tardis.io.logger import montecarlo_tracking as mc_tracker
 from tardis.io.util import HDFWriterMixin
+from tardis.opacities.continuum.continuum_state import ContinuumState
 from tardis.opacities.opacity_state import (
-    opacity_state_to_numba,
+    OpacityState,
 )
 from tardis.transport.montecarlo.configuration.base import (
     MonteCarloConfiguration,
@@ -114,9 +115,20 @@ class MonteCarloTransportSolver(HDFWriterMixin):
         )
 
         geometry_state = simulation_state.geometry.to_numba()
-
-        opacity_state_numba = opacity_state_to_numba(
-            opacity_state, macro_atom_state, self.line_interaction_type
+        if hasattr(plasma, "photo_ion_cross_sections"):
+            continuum_state = ContinuumState.from_legacy_plasma(plasma)
+        else:
+            continuum_state = None
+        self.opacity_state_numba = OpacityState(
+            electron_density=plasma.electron_densities,
+            t_electrons=plasma.t_electrons,
+            line_list_nu=plasma.atomic_data.lines.nu,
+            tau_sobolev=None,
+            beta_sobolev=plasma.beta_sobolev,
+            continuum_state=continuum_state,
+        ).to_numba(
+            macro_atom_state,
+            self.line_interaction_type,
         )
         opacity_state_numba = opacity_state_numba[
             simulation_state.geometry.v_inner_boundary_index : simulation_state.geometry.v_outer_boundary_index
