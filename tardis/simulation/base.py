921b52f5791a991edfcd668b50acc1a32208396f
roee yaron <roeeyaron@h-MacBook-Pro-sl-roee.local>
2025-02-20 22:13:56 +0200

    Fix init bug for: line_interaction_type=scatter

--- tardis/simulation/base.py
+++ tardis/simulation/base.py
@@ -444,6 +444,10 @@ class Simulation(PlasmaStateStorerMixin, HDFWriterMixin):
         logger.info(
             f"\n\tStarting iteration {(self.iterations_executed + 1):d} of {self.iterations:d}"
         )
+    
+        if self.macro_atom is None:
+            self.plasma.beta_sobolev = None
+            macro_atom_state = None
 
         opacity_state = self.opacity.legacy_solve(self.plasma)
         if self.macro_atom is not None:
