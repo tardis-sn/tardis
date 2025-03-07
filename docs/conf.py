3c749a3609b538cb92d387439050591e8222fca0
Atharva Arya <aryaatharva18@gmail.com>
2025-03-07 16:19:07 +0530

    html-manager

--- docs/conf.py
+++ docs/conf.py
@@ -319,6 +319,7 @@ from ipywidgets.embed import DEFAULT_EMBED_REQUIREJS_URL
 
 html_js_files = [
     DEFAULT_EMBED_REQUIREJS_URL,
+    "https://cdn.jsdelivr.net/npm/@jupyter-widgets/html-manager@1.0.13/dist/embed-amd.min.js"
 ]
 
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
