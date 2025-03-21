84aa907abc0dbd5e6ec5b000f217f609ba3b385e
Atharva Arya <aryaatharva18@gmail.com>
2025-03-21 15:27:28 +0530

    remove deprecated astropy testrunner

--- tardis/__init__.py
+++ tardis/__init__.py
@@ -3,9 +3,6 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
-
-# ----------------------------------------------------------------------------
 
 __all__ = []
 
