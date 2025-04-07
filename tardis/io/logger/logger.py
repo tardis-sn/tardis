5152aa7c4cbb92da6e9a4cba23331f5416aa3d0e
Atharva Arya <aryaatharva18@gmail.com>
2025-04-07 17:43:22 +0530

    add dataframes

--- tardis/io/logger/logger.py
+++ tardis/io/logger/logger.py
@@ -93,16 +93,13 @@ class AsyncEmitLogHandler(logging.Handler):
             stream_handler.emit(record)
             return
 
-        # if isinstance(record.msg, pd.DataFrame):
-            # html_output = record.msg.to_html(
-            #     border=0.1,
-            # )
-            # pass
-
-        # else:
-        log_entry = self.format(record)
-        clean_log_entry = self._remove_ansi_escape_sequences(log_entry)
-        html_output = self._format_html_output(clean_log_entry, record)
+        if isinstance(record.msg, pd.DataFrame):
+            df_str = record.msg.to_string(index=True)
+            html_output = f'<pre style="white-space: pre; margin: 0;">{df_str}</pre>'
+        else:
+            log_entry = self.format(record)
+            clean_log_entry = self._remove_ansi_escape_sequences(log_entry)
+            html_output = self._format_html_output(clean_log_entry, record)
         self._emit_to_widget(record.levelno, html_output)
 
     @staticmethod
