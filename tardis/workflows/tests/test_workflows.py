1a05a36760e197587f98693a40d27c75e186391d
Erin Visser <erin.visser1@gmail.com>
2025-04-09 10:41:31 -0400

    created shell functions to fix later

--- tardis/workflows/tests/test_workflows.py
+++ tardis/workflows/tests/test_workflows.py
@@ -1,12 +1,18 @@
-import pandas as pd
+from pathlib import Path
+
 import pytest
 
 from tardis.io.configuration.config_reader import Configuration
-from tardis.simulation import Simulation
+# from tardis.simulation import Simulation
 from tardis.workflows.v_inner_solver import InnerVelocitySolverWorkflow
 from tardis.workflows.simple_tardis_workflow import SimpleTARDISWorkflow
 from tardis.workflows.standard_tardis_workflow import StandardTARDISWorkflow
 
+@pytest.fixture(scope="module")
+def config(example_configuration_dir):
+    return Configuration.from_yaml(
+        example_configuration_dir / "tardis_configv1_verysimple.yml"
+    )
 
 @pytest.fixture(scope="function")
 def v_inner_config(example_configuration_dir):
@@ -21,8 +27,20 @@ def run_v_inner(v_inner_config, atomic_data_fname):
     return sim
 
 def test_v_inner_solver_workflow(run_v_inner):
-    actual = getattr(run_v_inner, 'completed_iterations')
+    actual = getattr(run_v_inner, "completed_iterations")
     if hasattr(actual, "value"):
         actual = actual.value
     expected = 10 
     assert actual == expected
+
+def test_simple_tardis_workflow(config):
+    sim = SimpleTARDISWorkflow(config)
+    sim.run()
+    assert hasattr(sim, "completed_iterations")
+    assert sim.completed_iterations == 10 # placeholder value
+
+def test_standard_tardis_workflow(config):
+    sim = StandardTARDISWorkflow(config)
+    sim.run()
+    assert hasattr(sim, "completed_iterations")
+    assert sim.completed_iterations == 10 # placeholder value
