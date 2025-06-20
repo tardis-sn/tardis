7be6366f62fa86f68dc36c03d582dd0722433034
Jing Lu <48139543+DeerWhale@users.noreply.github.com>
2025-06-20 16:42:02 -0400

    Add assertion check in v_inner_solver workflow (#3092)
    * add assertion error if the initial v_inner_boundary is not the first shell in the v_inner_solver csvy mode
    
    * Update tardis/workflows/simple_tardis_workflow.py
    
    Co-authored-by: Andrew <andrewgfullard@gmail.com>
    
    * change check to be np isclose
    
    * Revert "change check to be np isclose"
    
    This reverts commit 90cdaa16575c365deb5f8602bbd430de00c7c880.
    
    * change to np isclose and add the unit convertion just in case
    
    * update the assertion warning message
    
    * Josh said to add the issue number as well for reference perference purpose
    
    ---------
    
    Co-authored-by: Wolfgang Kerzendorf <wkerzendorf@gmail.com>
    Co-authored-by: Andrew <andrewgfullard@gmail.com>

--- tardis/workflows/simple_tardis_workflow.py
+++ tardis/workflows/simple_tardis_workflow.py
@@ -49,6 +49,13 @@ class SimpleTARDISWorkflow(WorkflowLogging):
             self.simulation_state = SimulationState.from_csvy(
                 configuration, atom_data=atom_data
             )
+            assert np.isclose(
+                self.simulation_state.v_inner_boundary.to(u.km / u.s).value,
+                self.simulation_state.geometry.v_inner[0].to(u.km / u.s).value,
+            ), (
+                "If using csvy density input in the workflow, the initial v_inner_boundary must start at the first shell, see issue #3129."
+            )
+
         else:
             self.simulation_state = SimulationState.from_config(
                 configuration,
