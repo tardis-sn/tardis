26c6b701e0b77f4b9fda4cb4db9da4d9c86475bc
Haille <hmlperkins@hotmail.com>
2025-07-11 15:14:39 -0500

    fix call to make_source_function

--- benchmarks/spectrum_formal_integral.py
+++ benchmarks/spectrum_formal_integral.py
@@ -7,6 +7,7 @@ import functools
 from numba import config
 
 from benchmarks.benchmark_base import BenchmarkBase
+from tardis.spectrum.formal_integral.base import make_source_function
 from tardis.spectrum.formal_integral.formal_integral import FormalIntegrator
 from tardis.spectrum.formal_integral.formal_integral_numba import intensity_black_body
 
@@ -27,7 +28,7 @@ class BenchmarkTransportMontecarloFormalIntegral(BenchmarkBase):
             self.sim.simulation_state, self.sim.plasma, self.sim.transport
         )
 
-    # Bencmark for intensity black body function
+    # Benchmark for intensity black body function
     def time_intensity_black_body(self):
         nu = 1e14
         temperature = 1e4
@@ -38,11 +39,11 @@ class BenchmarkTransportMontecarloFormalIntegral(BenchmarkBase):
         self.FormalIntegrator.calculate_spectrum(
             self.sim.spectrum_solver.spectrum_frequency_grid
         )
-        self.FormalIntegrator.make_source_function(
+        make_source_function(
             self.FormalIntegrator.simulation_state, 
-            self.FormalIntegrator.opacity_state, 
+            self.FormalIntegrator.opacity_state,
+            self.FormalIntegrator.transport,  
             self.FormalIntegrator.plasma, 
-            self.FormalIntegrator.transport, 
             self.FormalIntegrator.interpolate_shells
         )
         self.FormalIntegrator.generate_numba_objects()
