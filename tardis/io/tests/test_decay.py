69346fe7cdbcce02332003b2eff5a080360366eb
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2025-07-29 22:07:05 -0400

    Refactor test functions to include type hints and update decay method calls

--- tardis/io/tests/test_decay.py
+++ tardis/io/tests/test_decay.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import pytest
+from astropy import units as u
 from numpy.testing import assert_almost_equal
 
 from tardis.model.matter.decay import IsotopicMassFraction
 
 
 @pytest.fixture
-def simple_abundance_model():
+def simple_abundance_model() -> IsotopicMassFraction:
     index = pd.MultiIndex.from_tuples(
         [(28, 56)], names=["atomic_number", "mass_number"]
     )
     return IsotopicMassFraction([[1.0, 1.0]], index=index)
 
 
-def test_simple_decay(simple_abundance_model):
-    decayed_abundance = simple_abundance_model.decay(100)
+def test_simple_decay(simple_abundance_model: IsotopicMassFraction) -> None:
+    decayed_abundance = simple_abundance_model.calculate_decayed_mass_fractions(
+        100 * u.day
+    )
     assert_almost_equal(decayed_abundance.loc[26, 56][0], 0.55754786)
     assert_almost_equal(decayed_abundance.loc[26, 56][1], 0.55754786)
     assert_almost_equal(decayed_abundance.loc[27, 56][0], 0.44235126)
@@ -24,14 +27,16 @@ def test_simple_decay(simple_abundance_model):
 
 
 @pytest.fixture
-def raw_abundance_simple():
+def raw_abundance_simple() -> pd.DataFrame:
     abundances = pd.DataFrame([[0.2, 0.2], [0.1, 0.1]], index=[28, 30])
     abundances.index = abundances.index.rename("atomic_number")
     return abundances
 
 
-def test_abundance_merge(simple_abundance_model, raw_abundance_simple):
-    decayed_df = simple_abundance_model.decay(100)
+def test_abundance_merge(
+    simple_abundance_model: IsotopicMassFraction, raw_abundance_simple: pd.DataFrame
+) -> None:
+    decayed_df = simple_abundance_model.calculate_decayed_mass_fractions(100 * u.day)
     isotope_df = decayed_df.as_atoms()
     combined_df = decayed_df.merge(raw_abundance_simple, normalize=False)
 
