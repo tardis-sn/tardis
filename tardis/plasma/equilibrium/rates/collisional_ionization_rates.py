a732a371b711c06da5467414889ef067e276e5db
Andrew <andrewgfullard@gmail.com>
2025-08-18 13:25:32 -0400

    Ionization calculations (#3070)
    * Implementation of the rates matrix and level pop solvers into plasma
    
    * Demonstration notebook
    
    * Vague ideas
    
    * Functional ion population solver
    
    - Currently produces weird results
    
    * Add corrected coefficient solver
    
    * Correct multiplication to compute strengths
    
    * Simplify and fix various parts of the rate construction
    
    * Update rate matrix construction
    
    * Add recombination rates to matrix
    
    * Handle charge conservation and matrix solving
    
    * Add collisional rates to the matrix
    
    * Correct recombination rate calculation
    
    * Iterative ion population solver
    
    * Formatting
    
    * Collisional rate index fix, ion solver iteration value
    
    * Add demo notebook
    
    * Basic collisional ionization strength tests
    
    Uses mock data and needs a regression data test
    
    * Improve estimator-based rate calculation
    
    * Very basic tests for collisional ionization rate class
    
    * Hydrogen continuum property
    
    Also output electron fractions and remove unneeded assert
    
    * Output electron densities from the continuum solver
    
    * Some basic (failing) tests
    
    * Hydrogen plasma additions
    
    * Remove unnecessary lambda
    
    * Batch of test fixes
    
    * Further test fixes
    
    * Fix the rest of the tests
    
    * Fix tests and improve/correct docstrings
    
    * Update notebook
    
    * Factor out reindexing utility
    
    * Response to comments
    
    * Replace DiluteBlackBodyContinuumPropertiesSolver with AnalyticPhotoionizationCoeffSolver
    
    * Correct property ordering
    
    * Clarify population solution cuts
    
    * Remove unnecessary call
    
    * Respond to comments
    
    Clean up notebook
    Nicer loops
    Explanatory comments
    
    * Major corrections to rate matrix construction and population solver
    
    * Improvements to rate calculations
    
    - Compute the level population fraction using partition function and boltzmann factor
    - Multiply spontaneous recombination rate by the level population ratio
    - Simplify balance array function
    
    * Comparison notebook
    
    * Add correct comparison notebook
    
    * Fix calculation of level population ratio, handle Lyman continuum for recombination
    
    * Remove incorrect comparison notebook
    
    * Update tests to include boltzmann factor
    
    * Move solver tolerance to kwarg
    
    * Store comparison plots
    
    * Add some notes about iteration dependent quantities
    
    * Finalize ion population tests
    
    * Address Jack's comments
    
    * Cleanup, renames, comments with equations
    
    * Fix excess unit conversion
    
    * Use fancy new sync dataframe
    
    * Revert "Use fancy new sync dataframe"
    
    This reverts commit 4cef991bfca8d5db0bbb1a48d0b07a2eae596bdc.
    
    * Lock in docs notebooks
    
    * Use the real fancy new sync dataframe
    
    * Run tests
    
    * Fix test failure in old NLTE solver

--- tardis/plasma/equilibrium/rates/collisional_ionization_rates.py
+++ tardis/plasma/equilibrium/rates/collisional_ionization_rates.py
@@ -1,23 +1,41 @@
 from tardis.plasma.equilibrium.rates.collisional_ionization_strengths import (
     CollisionalIonizationSeaton,
 )
+from tardis.plasma.equilibrium.rates.util import (
+    reindex_ionization_rate_dataframe,
+)
 
 
 class CollisionalIonizationRateSolver:
     """Solver for collisional ionization and recombination rates."""
 
     def __init__(self, photoionization_cross_sections):
+        """Initialize the collisional ionization rate solver.
+
+        Parameters
+        ----------
+        photoionization_cross_sections : pd.DataFrame
+            Photoionization cross sections.
+        """
         self.photoionization_cross_sections = photoionization_cross_sections
 
-    def solve(self, electron_temperature, saha_factor, approximation="seaton"):
+    def solve(
+        self,
+        electron_distribution,
+        level_to_ion_population_factor,
+        partition_function,
+        level_boltzmann_factor,
+        approximation="seaton",
+    ):
         """Solve the collisional ionization and recombination rates.
 
         Parameters
         ----------
-        electron_temperature : u.Quantity
-            Electron temperatures per cell
-        saha_factor : pandas.DataFrame, dtype float
-            The Saha factor for each cell. Indexed by atom number, ion number, level number.
+        electron_distribution : ThermalElectronEnergyDistribution
+            Electron distribution per cell
+        level_to_ion_population_factor : pandas.DataFrame, dtype float
+            The level to ion population factor for each cell, Lucy 2003 Eq 14.
+            Indexed by atom number, ion number, level number.
         approximation : str, optional
             The rate approximation to use, by default "seaton"
 
@@ -40,11 +58,32 @@ class CollisionalIonizationRateSolver:
         else:
             raise ValueError(f"approximation {approximation} not supported")
 
-        collision_ionization_rates = strength_solver.solve(electron_temperature)
+        collision_ionization_rates = strength_solver.solve(
+            electron_distribution.temperature
+        )
 
         # Inverse of the ionization rate for equilibrium
         collision_recombination_rates = collision_ionization_rates.multiply(
-            saha_factor.loc[collision_ionization_rates.index]
+            level_to_ion_population_factor
         )
+
+        # TODO: Update for non-Hydrogenic species
+        level_population_fraction = level_boltzmann_factor / partition_function
+
+        # used to scale the photoionization rate because we keep the level population
+        # fixed while we calculated the ion number density
+        collision_ionization_rates = (
+            reindex_ionization_rate_dataframe(
+                collision_ionization_rates * level_population_fraction,
+                recombination=False,
+            )
+            * electron_distribution.number_density
+        )
+
+        collision_recombination_rates = (
+            reindex_ionization_rate_dataframe(
+                collision_recombination_rates, recombination=True
+            )
+        ) * electron_distribution.number_density**2
 
         return collision_ionization_rates, collision_recombination_rates
