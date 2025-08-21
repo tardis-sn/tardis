b044b0f152c51253481496b1467423325cb6234b
Josh Shields <shields.joshua.v@gmail.com>
2025-08-21 11:36:17 -0400

    plotters broken but otherwise works

--- tardis/opacities/tests/test_opacity_state_numba.py
+++ tardis/opacities/tests/test_opacity_state_numba.py
@@ -1,7 +1,9 @@
 import pytest
 from tardis.opacities.opacity_state import OpacityState
 from tardis.opacities.opacity_solver import OpacitySolver
-from tardis.opacities.macro_atom.macroatom_solver import LegacyMacroAtomSolver
+from tardis.opacities.macro_atom.macroatom_solver import (
+    BoundBoundMacroAtomSolver,
+)
 import numpy.testing as npt
 import numpy as np
 
@@ -27,12 +29,12 @@ def test_opacity_state_to_numba(
     )
     opacity_state = opacity_solver.legacy_solve(legacy_plasma)
     if line_interaction_type in ("downbranch", "macroatom"):
-        macro_atom_state = LegacyMacroAtomSolver().solve(
+        macro_atom_state = BoundBoundMacroAtomSolver(
+            legacy_plasma.atomic_data.levels, legacy_plasma.atomic_data.lines
+        ).solve(
             legacy_plasma.j_blues,
-            legacy_plasma.atomic_data,
-            opacity_state.tau_sobolev,
+            opacity_state.beta_sobolev,
             legacy_plasma.stimulated_emission_factor,
-            beta_sobolev=opacity_state.beta_sobolev,
         )
     else:
         macro_atom_state = None
@@ -63,32 +65,32 @@ def test_opacity_state_to_numba(
         npt.assert_allclose(actual.transition_type, empty)
         npt.assert_allclose(actual.destination_level_id, empty)
         npt.assert_allclose(actual.transition_line_id, empty)
-    else:
-        npt.assert_allclose(
-            actual.transition_probabilities,
-            legacy_plasma.transition_probabilities.values[:, index],
-        )
-        npt.assert_allclose(
-            actual.line2macro_level_upper,
-            legacy_plasma.atomic_data.lines_upper2macro_reference_idx,
-        )
-        npt.assert_allclose(
-            actual.macro_block_references,
-            legacy_plasma.atomic_data.macro_atom_references[
-                "block_references"
-            ].values,
-        )
-        npt.assert_allclose(
-            actual.transition_type,
-            legacy_plasma.atomic_data.macro_atom_data["transition_type"].values,
-        )
-        npt.assert_allclose(
-            actual.destination_level_id,
-            legacy_plasma.atomic_data.macro_atom_data[
-                "destination_level_idx"
-            ].values,
-        )
-        npt.assert_allclose(
-            actual.transition_line_id,
-            legacy_plasma.atomic_data.macro_atom_data["lines_idx"].values,
-        )
+    # else: #We don't expect the new ones to match the legacy ones
+    #     npt.assert_allclose(
+    #         actual.transition_probabilities,
+    #         legacy_plasma.transition_probabilities.values[:, index],
+    #     )
+    #     npt.assert_allclose(
+    #         actual.line2macro_level_upper,
+    #         legacy_plasma.atomic_data.lines_upper2macro_reference_idx,
+    #     )
+    #     npt.assert_allclose(
+    #         actual.macro_block_references,
+    #         legacy_plasma.atomic_data.macro_atom_references[
+    #             "block_references"
+    #         ].values,
+    #     )
+    #     npt.assert_allclose(
+    #         actual.transition_type,
+    #         legacy_plasma.atomic_data.macro_atom_data["transition_type"].values,
+    #     )
+    #     npt.assert_allclose(
+    #         actual.destination_level_id,
+    #         legacy_plasma.atomic_data.macro_atom_data[
+    #             "destination_level_idx"
+    #         ].values,
+    #     )
+    #     npt.assert_allclose(
+    #         actual.transition_line_id,
+    #         legacy_plasma.atomic_data.macro_atom_data["lines_idx"].values,
+    #     )
