b00e56ad851e5cf51dabbc69453ca82306e107f8
Haille Perkins <hmlperkins@hotmail.com>
2025-08-28 17:18:52 -0400

    made setup_formal_integral func and tests pass

--- tardis/spectrum/formal_integral/base.py
+++ tardis/spectrum/formal_integral/base.py
@@ -193,3 +193,270 @@ def interpolate_integrator_quantities(
     Jred_lu = Jred_lu.clip(0.0)
     e_dot_u = e_dot_u.clip(0.0)
     return att_S_ul, Jred_lu, Jblue_lu, e_dot_u
+
+
+import numpy as np
+from numba import njit, prange
+
+from tardis.spectrum.formal_integral.base import (
+    C_INV,
+    BoundsError,
+    intensity_black_body,
+    calculate_p_values,
+)
+from tardis.transport.montecarlo import njit_dict, njit_dict_no_parallel
+from tardis.transport.montecarlo.configuration.constants import SIGMA_THOMSON
+
+@njit(**njit_dict_no_parallel)
+def calculate_z(r, p, inv_t):
+    """Calculate distance to p line
+
+    Calculate half of the length of the p-line inside a shell
+    of radius r in terms of unit length (c * t_exp).
+    If shell and p-line do not intersect, return 0.
+
+    Inputs:
+        :r: (double) radius of the shell
+        :p: (double) distance of the p-line to the center of the supernova
+        :inv_t: (double) inverse time_explosion is needed to norm to unit-length
+    """
+    if r > p:
+        return np.sqrt(r * r - p * p) * C_INV * inv_t
+    else:
+        return 0
+
+
+@njit(**njit_dict_no_parallel)
+def populate_z(geometry, time_explosion, p, oz, oshell_id):
+    """Calculate p line intersections
+
+    This function calculates the intersection points of the p-line with
+    each shell
+
+    Inputs:
+        :p: (double) distance of the integration line to the center
+        :oz: (array of doubles) will be set with z values. the array is truncated
+                    by the value `1`.
+        :oshell_id: (int64) will be set with the corresponding shell_ids
+    """
+    # abbreviations
+    r = geometry.r_outer
+    N = len(geometry.r_inner)  # check
+    inv_t = 1 / time_explosion
+    z = 0
+    offset = N
+
+    if p <= geometry.r_inner[0]:
+        # intersect the photosphere
+        for i in range(N):
+            oz[i] = 1 - calculate_z(r[i], p, inv_t)
+            oshell_id[i] = i
+        return N
+    else:
+        # no intersection with photosphere
+        # that means we intersect each shell twice
+        for i in range(N):
+            z = calculate_z(r[i], p, inv_t)
+            if z == 0:
+                continue
+            if offset == N:
+                offset = i
+            # calculate the index in the resulting array
+            i_low = N - i - 1  # the far intersection with the shell
+            i_up = N + i - 2 * offset  # the nearer intersection with the shell
+
+            # setting the arrays; check return them?
+            oz[i_low] = 1 + z
+            oshell_id[i_low] = i
+            oz[i_up] = 1 - z
+            oshell_id[i_up] = i
+        return 2 * (N - offset)
+
+
+@njit(**njit_dict_no_parallel)
+def reverse_binary_search(x, x_insert, imin, imax):
+    """Look for a place to insert a value in an inversely sorted float array.
+
+    Inputs:
+        :x: (array) an inversely (largest to lowest) sorted float array
+        :x_insert: (value) a value to insert
+        :imin: (int) lower bound
+        :imax: (int) upper bound
+
+    Outputs:
+        index of the next boundary to the left
+    """
+    # ret_val = TARDIS_ERROR_OK # check
+    if (x_insert > x[imin]) or (x_insert < x[imax]):
+        raise BoundsError  # check
+    return len(x) - 1 - np.searchsorted(x[::-1], x_insert, side="right")
+
+
+@njit(**njit_dict_no_parallel)
+def line_search(nu, nu_insert, number_of_lines):
+    """
+    Insert a value in to an array of line frequencies
+
+    Inputs:
+        :nu: (array) line frequencies
+        :nu_insert: (int) value of nu key
+        :number_of_lines: (int) number of lines in the line list
+
+    Outputs:
+        index of the next line ot the red.
+                If the key value is redder
+                 than the reddest line returns number_of_lines.
+    """
+    # TODO: fix the TARDIS_ERROR_OK
+    # tardis_error_t ret_val = TARDIS_ERROR_OK # check
+    imin = 0
+    imax = number_of_lines - 1
+    if nu_insert > nu[imin]:
+        result = imin
+    elif nu_insert < nu[imax]:
+        result = imax + 1
+    else:
+        result = reverse_binary_search(nu, nu_insert, imin, imax)
+        result = result + 1
+    return result
+
+
+@njit(**njit_dict_no_parallel)
+def trapezoid_integration(array, h):
+    """in the future, let's just replace
+    this with the numpy trapz
+    since it is numba compatable
+    """
+    return np.trapz(array, dx=h)
+
+
+# numba jit
+calculate_p_values = njit(calculate_p_values, **njit_dict_no_parallel)
+intensity_black_body = njit(intensity_black_body, **njit_dict_no_parallel)
+
+
+# initialize I_nu_p with the blackbody intensities (as necessary)
+@njit(**njit_dict)
+def init_Inup_numba(inu, ps, iT, Rph, N, size_shell, geometry, time_explosion):
+    inu_size = len(inu)
+
+    I_nu_p = np.zeros((inu_size, N), dtype=np.float64)
+    zs = np.zeros((N, 2 * size_shell), dtype=np.float64)
+    shell_ids = np.zeros((N, 2 * size_shell), dtype=np.int64)
+    size_zs = np.zeros(N, dtype=np.int64)
+
+    # loop over nu and p
+    for nu_idx in prange(inu_size):
+        I_nu = I_nu_p[nu_idx]
+        nu = inu[nu_idx]
+        for p_idx in range(1, N):
+            p = ps[p_idx]
+
+            # get zs
+            size_z = populate_z(
+                geometry, time_explosion, p, zs[p_idx], shell_ids[p_idx]
+            )
+            size_zs[p_idx] = size_z
+
+            # if inside the photosphere, set to black body intensity
+            # otherwise zero
+            if p <= Rph:
+                I_nu[p_idx] = intensity_black_body(nu * zs[p_idx][0], iT)
+            else:
+                I_nu[p_idx] = 0
+
+    return I_nu_p, zs, shell_ids, size_zs
+
+@njit(**njit_dict)
+def setup_formal_integral_inputs(
+        inu,
+        iT,
+        N, 
+        geometry, 
+        time_explosion,
+        line_list_nu,
+        tau_sobolev, 
+        electron_densities
+    ):
+    """
+    Prepare all of the arrays and values needed for the loops inside the formal integral
+
+    Needed parameters at the end:
+        nu, ps, Inup
+        att_S_ul, Jred_lu, Jblue_lu, exp_tau
+        escat_op
+        line_list
+        shell_ids, size_lines,
+        direction (?) can also be precomputed
+    """
+
+    inu_size = len(inu)
+    size_line, size_shell = tau_sobolev.shape
+    exp_tau = np.exp(-tau_sobolev.T.ravel())
+    R_max = geometry.r_outer[size_shell - 1]
+    Rph = geometry.r_inner[0]
+
+    ps = np.zeros(N, dtype=np.float64)
+    ps[::] = calculate_p_values(R_max, N)
+
+    I_nu_p = np.zeros((inu_size, N), dtype=np.float64)
+    zs = np.zeros((N, 2 * size_shell), dtype=np.float64)
+    shell_ids = np.zeros((N, 2 * size_shell), dtype=np.int64)
+    size_zs = np.zeros(N, dtype=np.int64)
+
+    # nu start and nu_start idx not needed other than to compute internal values
+    lines_idx = np.zeros((inu_size, N), dtype=np.int64)
+    lines_idx_offset = np.zeros((inu_size, N), dtype=np.int64)
+    
+    zstarts = np.zeros(N, dtype=np.float64)
+
+    nu_ends = np.zeros((inu_size, N, 2 * size_shell-1), dtype=np.float64)
+    nu_ends_idxs = np.zeros((inu_size, N, 2 * size_shell-1), dtype=np.int64)
+
+    escat_ops = np.zeros((inu_size, N, 2 * size_shell), dtype=np.float64)
+    directions = np.zeros((inu_size, N, 2 * size_shell), dtype=np.int64)
+
+    # loop over nu and p
+    for nu_idx in prange(inu_size):
+        I_nu = I_nu_p[nu_idx]
+        nu = inu[nu_idx]
+        for p_idx in range(1, N):
+            p = ps[p_idx]
+
+            # get zs
+            size_z = populate_z(
+                geometry, time_explosion, p, zs[p_idx], shell_ids[p_idx]
+            )
+            size_zs[p_idx] = size_z
+            z = zs[p_idx]
+            shell_id = shell_ids[p_idx]
+
+            # if inside the photosphere, set to black body intensity
+            # otherwise zero
+            if p <= Rph:
+                I_nu[p_idx] = intensity_black_body(nu * z[0], iT)
+            else:
+                I_nu[p_idx] = 0
+
+            nu_start = nu * z[0]
+
+            idx_nu_start = line_search(line_list_nu, nu_start, size_line)
+            lines_idx[nu_idx, p_idx] = int(idx_nu_start)
+            lines_idx_offset[nu_idx, p_idx] = int(idx_nu_start + (shell_id[0] * size_line))
+            
+            zstarts[p_idx] = time_explosion / C_INV * (1.0 - z[0]) 
+
+            nu_ends[nu_idx, p_idx] = nu * z[1:]
+            nu_ends_idxs[nu_idx, p_idx] = size_line - np.searchsorted(
+                line_list_nu[::-1], nu_ends[nu_idx, p_idx], side="right"
+            )
+            
+            for i in range(size_z - 1):
+                escat_ops[nu_idx, p_idx, i] = (
+                    electron_densities[int(shell_ids[p_idx][i])] * SIGMA_THOMSON
+                )
+                # compute nu_end_idx - line_idx
+                directions[nu_idx, p_idx, i] = int((shell_ids[p_idx][i + 1] - shell_ids[p_idx][i]) * size_line)
+
+
+    return I_nu_p, ps, size_zs, lines_idx, lines_idx_offset, nu_ends, nu_ends_idxs, zstarts, escat_ops, directions, exp_tau
--- tardis/spectrum/formal_integral/formal_integral_numba.py
+++ tardis/spectrum/formal_integral/formal_integral_numba.py
@@ -4,140 +4,11 @@ from numba import njit, prange
 from tardis.spectrum.formal_integral.base import (
     C_INV,
     BoundsError,
-    intensity_black_body,
-    calculate_p_values,
+    trapezoid_integration,
+    setup_formal_integral_inputs
 )
 from tardis.transport.montecarlo import njit_dict, njit_dict_no_parallel
 from tardis.transport.montecarlo.configuration.constants import SIGMA_THOMSON
-
-
-@njit(**njit_dict_no_parallel)
-def calculate_z(r, p, inv_t):
-    """Calculate distance to p line
-
-    Calculate half of the length of the p-line inside a shell
-    of radius r in terms of unit length (c * t_exp).
-    If shell and p-line do not intersect, return 0.
-
-    Inputs:
-        :r: (double) radius of the shell
-        :p: (double) distance of the p-line to the center of the supernova
-        :inv_t: (double) inverse time_explosion is needed to norm to unit-length
-    """
-    if r > p:
-        return np.sqrt(r * r - p * p) * C_INV * inv_t
-    else:
-        return 0
-
-
-@njit(**njit_dict_no_parallel)
-def populate_z(geometry, time_explosion, p, oz, oshell_id):
-    """Calculate p line intersections
-
-    This function calculates the intersection points of the p-line with
-    each shell
-
-    Inputs:
-        :p: (double) distance of the integration line to the center
-        :oz: (array of doubles) will be set with z values. the array is truncated
-                    by the value `1`.
-        :oshell_id: (int64) will be set with the corresponding shell_ids
-    """
-    # abbreviations
-    r = geometry.r_outer
-    N = len(geometry.r_inner)  # check
-    inv_t = 1 / time_explosion
-    z = 0
-    offset = N
-
-    if p <= geometry.r_inner[0]:
-        # intersect the photosphere
-        for i in range(N):
-            oz[i] = 1 - calculate_z(r[i], p, inv_t)
-            oshell_id[i] = i
-        return N
-    else:
-        # no intersection with photosphere
-        # that means we intersect each shell twice
-        for i in range(N):
-            z = calculate_z(r[i], p, inv_t)
-            if z == 0:
-                continue
-            if offset == N:
-                offset = i
-            # calculate the index in the resulting array
-            i_low = N - i - 1  # the far intersection with the shell
-            i_up = N + i - 2 * offset  # the nearer intersection with the shell
-
-            # setting the arrays; check return them?
-            oz[i_low] = 1 + z
-            oshell_id[i_low] = i
-            oz[i_up] = 1 - z
-            oshell_id[i_up] = i
-        return 2 * (N - offset)
-
-
-@njit(**njit_dict_no_parallel)
-def reverse_binary_search(x, x_insert, imin, imax):
-    """Look for a place to insert a value in an inversely sorted float array.
-
-    Inputs:
-        :x: (array) an inversely (largest to lowest) sorted float array
-        :x_insert: (value) a value to insert
-        :imin: (int) lower bound
-        :imax: (int) upper bound
-
-    Outputs:
-        index of the next boundary to the left
-    """
-    # ret_val = TARDIS_ERROR_OK # check
-    if (x_insert > x[imin]) or (x_insert < x[imax]):
-        raise BoundsError  # check
-    return len(x) - 1 - np.searchsorted(x[::-1], x_insert, side="right")
-
-
-@njit(**njit_dict_no_parallel)
-def line_search(nu, nu_insert, number_of_lines):
-    """
-    Insert a value in to an array of line frequencies
-
-    Inputs:
-        :nu: (array) line frequencies
-        :nu_insert: (int) value of nu key
-        :number_of_lines: (int) number of lines in the line list
-
-    Outputs:
-        index of the next line ot the red.
-                If the key value is redder
-                 than the reddest line returns number_of_lines.
-    """
-    # TODO: fix the TARDIS_ERROR_OK
-    # tardis_error_t ret_val = TARDIS_ERROR_OK # check
-    imin = 0
-    imax = number_of_lines - 1
-    if nu_insert > nu[imin]:
-        result = imin
-    elif nu_insert < nu[imax]:
-        result = imax + 1
-    else:
-        result = reverse_binary_search(nu, nu_insert, imin, imax)
-        result = result + 1
-    return result
-
-
-@njit(**njit_dict_no_parallel)
-def trapezoid_integration(array, h):
-    """in the future, let's just replace
-    this with the numpy trapz
-    since it is numba compatable
-    """
-    return np.trapz(array, dx=h)
-
-
-# numba jit
-calculate_p_values = njit(calculate_p_values, **njit_dict_no_parallel)
-intensity_black_body = njit(intensity_black_body, **njit_dict_no_parallel)
-
 
 @njit(**njit_dict_no_parallel)
 def increment_escat_contrib(
@@ -151,6 +22,7 @@ def increment_escat_contrib(
     Jred_lu,
     I_nu_p,
 ):
+
     if first == 1:
         # first contribution to integration
         # NOTE: this treatment of I_nu_b (given
@@ -168,41 +40,7 @@ def increment_escat_contrib(
 
     return escat_contrib, first, pJred_lu
 
-
-# initialize I_nu_p with the blackbody intensities (as necessary)
 @njit(**njit_dict)
-def init_Inup_numba(inu, ps, iT, Rph, N, size_shell, geometry, time_explosion):
-    inu_size = len(inu)
-
-    I_nu_p = np.zeros((inu_size, N), dtype=np.float64)
-    zs = np.zeros((N, 2 * size_shell), dtype=np.float64)
-    shell_ids = np.zeros((N, 2 * size_shell), dtype=np.int64)
-    size_zs = np.zeros(N, dtype=np.int64)
-
-    # loop over nu and p
-    for nu_idx in prange(inu_size):
-        I_nu = I_nu_p[nu_idx]
-        nu = inu[nu_idx]
-        for p_idx in range(1, N):
-            p = ps[p_idx]
-
-            # get zs
-            size_z = populate_z(
-                geometry, time_explosion, p, zs[p_idx], shell_ids[p_idx]
-            )
-            size_zs[p_idx] = size_z
-
-            # if inside the photosphere, set to black body intensity
-            # otherwise zero
-            if p <= Rph:
-                I_nu[p_idx] = intensity_black_body(nu * zs[p_idx][0], iT)
-            else:
-                I_nu[p_idx] = 0
-
-    return I_nu_p, zs, shell_ids, size_zs
-
-
-@njit(**njit_dict_no_parallel)
 def numba_formal_integral(
     geometry,
     time_explosion,
@@ -229,18 +67,23 @@ def numba_formal_integral(
     # Initialize the output which is shared among threads
     L = np.zeros(inu_size, dtype=np.float64)
     # global read-only values
-    size_line, size_shell = tau_sobolev.shape
-    size_tau = size_line * size_shell
-    R_ph = geometry.r_inner[0]  # make sure these are cgs
-    R_max = geometry.r_outer[size_shell - 1]
-    pp = np.zeros(N, dtype=np.float64)  # check
-    exp_tau = np.zeros(size_tau, dtype=np.float64)
-    exp_tau = np.exp(-tau_sobolev.T.ravel())  # maybe make this 2D?
-    pp[::] = calculate_p_values(R_max, N)
+    # size_line, size_shell = tau_sobolev.shape
+    # size_tau = size_line * size_shell
+    # R_ph = geometry.r_inner[0]  # make sure these are cgs
+    # R_max = geometry.r_outer[size_shell - 1]
+    # pp = np.zeros(N, dtype=np.float64)  # check
+    # exp_tau = np.zeros(size_tau, dtype=np.float64)
+    # exp_tau = np.exp(-tau_sobolev.T.ravel())  # maybe make this 2D?
+    # pp[::] = calculate_p_values(R_max, N)
     line_list_nu = plasma.line_list_nu
-    I_nu_p, zs, shell_ids, size_zs = init_Inup_numba(
-        inu, pp, iT, R_ph, N, size_shell, geometry, time_explosion
-    )
+    
+    # I_nu_p, zs, shell_ids, size_zs = init_Inup_numba(
+    #     inu, pp, iT, R_ph, N, size_shell, geometry, time_explosion
+    # )
+    R_max = geometry.r_outer[-1]
+    (I_nu_p, ps, size_zs, lines_idx, lines_idx_offset, nu_ends, nu_ends_idxs, zstarts, escat_ops, directions, exp_tau ) = setup_formal_integral_inputs(
+            inu, iT, N, geometry, time_explosion, line_list_nu, tau_sobolev, electron_density)
+
 
     # done with instantiation
     # now loop over wavelength in spectrum
@@ -251,78 +94,92 @@ def numba_formal_integral(
         # now loop over discrete values along line
         for p_idx in range(1, N):
             escat_contrib = 0
-            p = pp[p_idx]
-            z = zs[p_idx]
-            shell_id = shell_ids[p_idx]
+            p = ps[p_idx]
+            # z = zs[p_idx]
+            # shell_id = shell_ids[p_idx]
             size_z = size_zs[p_idx]
 
             # find first contributing lines
-            nu_start = nu * z[0]
-            idx_nu_start = line_search(plasma.line_list_nu, nu_start, size_line)
-            offset = shell_id[0] * size_line
+            # nu_start = nu * z[0]
+            # idx_nu_start = line_search(plasma.line_list_nu, nu_start, size_line)
+            # offset = shell_id[0] * size_line
             # start tracking accumulated e-scattering optical depth
-            zstart = time_explosion / C_INV * (1.0 - z[0])
+            # zstart = time_explosion / C_INV * (1.0 - z[0])
+            zstart = zstarts[p_idx]
 
             # Initialize "pointers"
-            pline = int(idx_nu_start)
-            pline_offset = int(offset + idx_nu_start)
-            pJred_lu = int(offset + idx_nu_start)
+            # pline = int(idx_nu_start)
+            # pline_offset = int(offset + idx_nu_start)
+            # pJred_lu = int(offset + idx_nu_start)
+            line_idx = lines_idx[nu_idx, p_idx]
+            line_idx_offset = lines_idx_offset[nu_idx, p_idx]
+            line_Jred_lu_idx = lines_idx_offset[nu_idx, p_idx]
+
 
             # flag for first contribution to integration on current p-ray
             first = 1
-            nu_ends = nu * z[1:]
-            nu_ends_idxs = size_line - np.searchsorted(
-                line_list_nu[::-1], nu_ends, side="right"
-            )
+            # nu_ends = nu * z[1:]
+            # nu_ends = nu_ends
+            # nu_ends_idxs = size_line - np.searchsorted(
+            #     line_list_nu[::-1], nu_ends, side="right"
+            # )
+            # nu_ends_idxs = nu_ends_idxs[nu_idx, p_idx]
+
             # loop over all interactions
             for i in range(size_z - 1):
-                escat_op = electron_density[int(shell_id[i])] * SIGMA_THOMSON
-                nu_end = nu_ends[i]
-                nu_end_idx = nu_ends_idxs[i]
-                for _ in range(max(nu_end_idx - pline, 0)):
+                # escat_op = electron_density[int(shell_id[i])] * SIGMA_THOMSON
+                escat_op = escat_ops[nu_idx, p_idx, i]
+                nu_end = nu_ends[nu_idx, p_idx, i]
+                nu_end_idx = nu_ends_idxs[nu_idx, p_idx, i]
+                for _ in range(max(nu_end_idx - line_idx, 0)):
                     # calculate e-scattering optical depth to next resonance point
                     zend = (
                         time_explosion
                         / C_INV
-                        * (1.0 - line_list_nu[pline] / nu)
+                        * (1.0 - line_list_nu[line_idx] / nu)
                     )  # check
 
-                    escat_contrib, first, pJred_lu = increment_escat_contrib(
+                    try:
+                        Jblue_lu[line_idx_offset]
+                    except:
+                        print("line_idx_offset", line_idx_offset)
+
+                    escat_contrib, first, line_Jred_lu_idx = increment_escat_contrib(
                         escat_contrib,
                         first,
-                        pJred_lu,
+                        line_Jred_lu_idx,
                         zend,
                         zstart,
                         escat_op,
-                        Jblue_lu[pline_offset],
-                        Jred_lu[pJred_lu],
+                        Jblue_lu[line_idx_offset],
+                        Jred_lu[line_Jred_lu_idx],
                         I_nu[p_idx],
                     )
 
                     I_nu[p_idx] += escat_contrib
                     # // Lucy 1999, Eq 26
-                    I_nu[p_idx] *= exp_tau[pline_offset]
-                    I_nu[p_idx] += att_S_ul[pline_offset]
+                    I_nu[p_idx] *= exp_tau[line_idx_offset]
+                    I_nu[p_idx] += att_S_ul[line_idx_offset]
 
                     # // reset e-scattering opacity
                     escat_contrib = 0
                     zstart = zend
 
-                    pline += 1
-                    pline_offset += 1
+                    line_idx += 1
+                    line_idx_offset += 1
 
                 # calculate e-scattering optical depth to grid cell boundary
-                Jkkp = 0.5 * (Jred_lu[pJred_lu] + Jblue_lu[pline_offset])
+                Jkkp = 0.5 * (Jred_lu[line_Jred_lu_idx] + Jblue_lu[line_idx_offset])
                 zend = time_explosion / C_INV * (1.0 - nu_end / nu)  # check
                 escat_contrib += (
                     (zend - zstart) * escat_op * (Jkkp - I_nu[p_idx])
-                )
+                ) # check if cant use increment function here
                 zstart = zend
 
                 # advance pointers
-                direction = int((shell_id[i + 1] - shell_id[i]) * size_line)
-                pline_offset += direction
-                pJred_lu += direction
+                direction = directions[nu_idx, p_idx, i]
+                line_idx_offset += direction
+                line_Jred_lu_idx += direction
             I_nu[p_idx] *= p
         L[nu_idx] = 8 * np.pi * np.pi * trapezoid_integration(I_nu, R_max / N)
 
