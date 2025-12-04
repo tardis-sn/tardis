473c2db1c4a0f4e63998f59d77a438a258ef5c13
connor-mcclellan <b.connor.mcc@gmail.com>
2025-12-04 11:01:25 -0500

    remove carsus imports, add argparse command line arguments for files

--- tardis/scripts/convert_atomic_data.py
+++ tardis/scripts/convert_atomic_data.py
@@ -2,7 +2,9 @@
 Quick-and-dirty script for converting an older atomic data format to one compatible with modern TARDIS.
 """
 
+import argparse
 import hashlib
+import pickle
 import platform
 import uuid
 from datetime import datetime
@@ -12,10 +14,53 @@ import numpy as np
 import pandas as pd
 import pytz
 
-from carsus.util import (
-    hash_pandas_object,
-    serialize_pandas_object,
-)
+
+def serialize_pandas_object(pd_object):
+    """
+    Serialize Pandas objects with Pickle.
+
+    Parameters
+    ----------
+    pd_object : pandas.Series or pandas.DataFrame
+        Pandas object to be serialized with Pickle.
+
+    Returns
+    -------
+    Pickle serialized Python object.
+    """
+    return pickle.dumps(pd_object)
+
+
+def hash_pandas_object(pd_object, algorithm="md5"):
+    """
+    Hash Pandas objects.
+
+    Parameters
+    ----------
+    pd_object : pandas.Series or pandas.DataFrame
+        Pandas object to be hashed.
+    algorithm : str, optional
+        Algorithm available in `hashlib`, by default "md5"
+
+    Returns
+    -------
+    str
+        Hash values.
+
+    Raises
+    ------
+    ValueError
+        If `algorithm` is not available in `hashlib`.
+    """
+    algorithm = algorithm.lower()
+
+    if hasattr(hashlib, algorithm):
+        hash_func = getattr(hashlib, algorithm)
+
+    else:
+        raise ValueError('algorithm not supported')
+
+    return hash_func(serialize_pandas_object(pd_object)).hexdigest()
 
 
 def multiindex_port(olddata, templatedata, templatekey, oldkey=None):
@@ -63,143 +108,147 @@ def simple_port(olddata, templatedata, templatekey, oldkey=None):
 
     return newdata
 
+## Files used to convert Christian's atomic data:
+# oldatomdata = "merged_mod_20SNG_forbidden_yg_fix_H30_cmfgen_yg.h5"
+# pi_filename = "photoionization_data_H30_He.h5"
+# template = "/home/connor/tardis-regression-data/atom_data/nlte_atom_data/TestNLTE_He_Ti.h5"
 
-# Load data in old format - use h5py for now, not pandas
-filename = "merged_mod_20SNG_forbidden_yg_fix_H30_cmfgen_yg.h5"
-old = h5py.File(filename)
+if __name__ == "__main__":
 
-# Photoionization data stored in separate file in Christian's version
-pi_filename = "photoionization_data_H30_He.h5"
-pi_data = pd.HDFStore(pi_filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("oldatomdata", type=str)
+    parser.add_argument("oldPIdata", type=str)
+    parser.add_argument("template", type=str)
+    parser.add_argument("newatomdata", type=str)
+    args = parser.parse_args()
 
-# Reference atomic data file for the format we want to convert to
-template = pd.HDFStore(
-    "/home/connor/tardis-regression-data/atom_data/nlte_atom_data/TestNLTE_He_Ti.h5"
-)
+    # Load data in old format - use h5py for now, not pandas
+    old = h5py.File(args.oldatomdata)
 
-# Open up a new pandas HDFStore to port the old data into
-basename, ext = filename.split(".")
-new_filename = f"{basename}_CONVERTED.{ext}"
-new = pd.HDFStore(new_filename)
+    # Photoionization data stored in separate file in Christian's version
+    pi_data = pd.HDFStore(args.oldPIdata)
 
+    # Reference atomic data file for the format we want to convert to
+    template = pd.HDFStore(args.template)
 
-### ATOM DATA
-new_atom_data = multiindex_port(
-    old, template, "atom_data", oldkey="basic_atom_data"
-)
-new["atom_data"] = new_atom_data
+    # Open up a new pandas HDFStore to port the old data into
+    new = pd.HDFStore(args.newatomdata)
 
+    ### ATOM DATA
+    new_atom_data = multiindex_port(
+        old, template, "atom_data", oldkey="basic_atom_data"
+    )
+    new["atom_data"] = new_atom_data
 
-### COLLISIONS DATA
-# Note: this can actually just be done by re-loading the filename with pd.HDFStore
-# The main dataset is already formatted correctly but with temperatures in the
-# columns instead of in the metadata. Leaving as-is for now since I already wrote
-# all this nonsense below.
+    ### COLLISIONS DATA
+    # Note: this can actually just be done by re-loading the filename with pd.HDFStore
+    # The main dataset is already formatted correctly but with temperatures in the
+    # columns instead of in the metadata. Leaving as-is for now since I already wrote
+    # all this nonsense below.
 
-# Collision strengths and temperature bins are stored in block 0
-collisions_data = np.array(old["Y/g"]["block0_values"][:])
-collisions_temps = np.array(
-    old["Y/g"]["block0_items"][:]
-)  # these go in the metadata in the new format
+    # Collision strengths and temperature bins are stored in block 0
+    collisions_data = np.array(old["Y/g"]["block0_values"][:])
+    collisions_temps = np.array(
+        old["Y/g"]["block0_items"][:]
+    )  # these go in the metadata in the new format
 
-# The multi-index is stored in block 1
-collisions_index_names = np.array(old["Y/g"]["block1_items"][:].astype(str))
-collisions_index_values = np.array(old["Y/g"]["block1_values"][:])
-collisions_multiindex = pd.MultiIndex.from_arrays(
-    collisions_index_values.T, names=collisions_index_names
-)
-collisions_df = pd.DataFrame(
-    collisions_data,
-    index=collisions_multiindex,
-    columns=template["collisions_data"].columns,
-)
+    # The multi-index is stored in block 1
+    collisions_index_names = np.array(old["Y/g"]["block1_items"][:].astype(str))
+    collisions_index_values = np.array(old["Y/g"]["block1_values"][:])
+    collisions_multiindex = pd.MultiIndex.from_arrays(
+        collisions_index_values.T, names=collisions_index_names
+    )
+    collisions_df = pd.DataFrame(
+        collisions_data,
+        index=collisions_multiindex,
+        columns=template["collisions_data"].columns,
+    )
 
-# Write the new pandas dataframe for collisions_data to the HDFStore
-new["collisions_data"] = collisions_df
+    # Write the new pandas dataframe for collisions_data to the HDFStore
+    new["collisions_data"] = collisions_df
 
+    ### COLLISIONS METADATA
+    new_metadata = template["collisions_metadata"].copy()
+    new_metadata["temperatures"] = collisions_temps.astype(np.int64)
+    new["collisions_metadata"] = new_metadata
 
-### COLLISIONS METADATA
-new_metadata = template["collisions_metadata"].copy()
-new_metadata["temperatures"] = collisions_temps.astype(np.int64)
-new["collisions_metadata"] = new_metadata
+    ### IONIZATION DATA
+    new["ionization_data"] = multiindex_port(old, template, "ionization_data")
 
-### IONIZATION DATA
-new["ionization_data"] = multiindex_port(old, template, "ionization_data")
+    ### LEVELS DATA
+    new["levels_data"] = multiindex_port(old, template, "levels_data")
 
-### LEVELS DATA
-new["levels_data"] = multiindex_port(old, template, "levels_data")
+    ### LINES DATA
+    new["lines_data"] = multiindex_port(old, template, "lines_data")
 
-### LINES DATA
-new["lines_data"] = multiindex_port(old, template, "lines_data")
+    ### LINES METADATA
+    new["lines_metadata"] = template["lines_metadata"].copy()
 
-### LINES METADATA
-new["lines_metadata"] = template["lines_metadata"].copy()
+    ### MACRO ATOM DATA
+    new["macro_atom_data"] = simple_port(old, template, "macro_atom_data")
 
-### MACRO ATOM DATA
-new["macro_atom_data"] = simple_port(old, template, "macro_atom_data")
+    ### MACRO ATOM REFERENCES
+    new["macro_atom_references"] = multiindex_port(
+        old, template, "macro_atom_references"
+    )
 
-### MACRO ATOM REFERENCES
-new["macro_atom_references"] = multiindex_port(
-    old, template, "macro_atom_references"
-)
+    ### PHOTOIONIZATION DATA
+    ### Note this comes from a DIFFERENT file!
+    new["photoionization_data"] = pi_data["photoionization_data"].copy()
 
-### PHOTOIONIZATION DATA
-### Note this comes from a DIFFERENT file!
-new["photoionization_data"] = pi_data["photoionization_data"].copy()
+    ### ZETA DATA
+    zeta_index = pd.MultiIndex.from_arrays(
+        old["zeta_data"][:, :2].T.astype(np.int64),
+        names=template["zeta_data"].index.names,
+    )
+    zeta_temps = pd.Index(
+        old["zeta_data"].attrs["t_rad"].astype(np.float64), name="temp"
+    )
+    new_zeta_data = pd.DataFrame(
+        old["zeta_data"][:, 2:], index=zeta_index, columns=zeta_temps
+    )
+    new["zeta_data"] = new_zeta_data
 
-### ZETA DATA
-zeta_index = pd.MultiIndex.from_arrays(
-    old["zeta_data"][:, :2].T.astype(np.int64),
-    names=template["zeta_data"].index.names,
-)
-zeta_temps = pd.Index(
-    old["zeta_data"].attrs["t_rad"].astype(np.float64), name="temp"
-)
-new_zeta_data = pd.DataFrame(
-    old["zeta_data"][:, 2:], index=zeta_index, columns=zeta_temps
-)
-new["zeta_data"] = new_zeta_data
+    ### METADATA
+    # Copied over from Andrew's notebook demonstrating how to do this
+    meta = []
+    meta.append(("format", "version", "2.0"))
 
-### METADATA
-# Copied over from Andrew's notebook demonstrating how to do this
-meta = []
-meta.append(("format", "version", "2.0"))
+    total_checksum = hashlib.md5()
+    for key in new.keys():
+        # update the total checksum to sign the file
+        total_checksum.update(serialize_pandas_object(new[key]))
 
-total_checksum = hashlib.md5()
-for key in new.keys():
-    # update the total checksum to sign the file
-    total_checksum.update(serialize_pandas_object(new[key]))
+        # save individual DataFrame/Series checksum
+        checksum = hash_pandas_object(new[key])
+        meta.append(("md5sum", key.lstrip("/"), checksum))
 
-    # save individual DataFrame/Series checksum
-    checksum = hash_pandas_object(new[key])
-    meta.append(("md5sum", key.lstrip("/"), checksum))
+    # relevant package versions
+    meta.append(("software", "python", platform.python_version()))
+    imports = [
+        "carsus",
+        "astropy",
+        "numpy",
+        "pandas",
+        "tables",
+        "ChiantiPy",
+    ]
+    for package in imports:
+        meta.append(("software", package, __import__(package).__version__))
+    meta_df = pd.DataFrame.from_records(
+        meta, columns=["field", "key", "value"], index=["field", "key"]
+    )
+    uuid1 = uuid.uuid1().hex
+    new.root._v_attrs["MD5"] = total_checksum.hexdigest()
+    new.root._v_attrs["UUID1"] = uuid1
+    new.root._v_attrs["FORMAT_VERSION"] = "2.0"
+    tz = pytz.timezone("UTC")
+    date = datetime.now(tz).isoformat()
+    new.root._v_attrs["DATE"] = date
+    new.put("/metadata", meta_df)
 
-# relevant package versions
-meta.append(("software", "python", platform.python_version()))
-imports = [
-    "carsus",
-    "astropy",
-    "numpy",
-    "pandas",
-    "tables",
-    "ChiantiPy",
-]
-for package in imports:
-    meta.append(("software", package, __import__(package).__version__))
-meta_df = pd.DataFrame.from_records(
-    meta, columns=["field", "key", "value"], index=["field", "key"]
-)
-uuid1 = uuid.uuid1().hex
-new.root._v_attrs["MD5"] = total_checksum.hexdigest()
-new.root._v_attrs["UUID1"] = uuid1
-new.root._v_attrs["FORMAT_VERSION"] = "2.0"
-tz = pytz.timezone("UTC")
-date = datetime.now(tz).isoformat()
-new.root._v_attrs["DATE"] = date
-new.put("/metadata", meta_df)
-
-# Close / write out files
-old.close()
-template.close()
-pi_data.close()
-new.close()
+    # Close / write out files
+    old.close()
+    template.close()
+    pi_data.close()
+    new.close()
