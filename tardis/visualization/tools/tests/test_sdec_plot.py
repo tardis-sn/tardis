fix inconsistency in tolerance thresholds for SDEC tests - previously failing on machine precision

tardis/visualization/tools/tests/test_sdec_plot.py
@@ -276,7 +276,7 @@ class TestSDECPlotter:
             # save artists which correspond to element contributions
             if isinstance(data, PolyCollection):
                 for index2, path in enumerate(data.get_paths()):
-                    np.testing.assert_almost_equal(
+                    np.testing.assert_allclose(
                         path.vertices,
                         expected.get(
                             "plot_data_hdf/"
@@ -286,6 +286,8 @@ class TestSDECPlotter:
                             + "ind_"
                             + str(index2)
                         ),
+                        atol=0,
+                        rtol=RELATIVE_TOLERANCE_SDEC
                     )
         expected.close()
 