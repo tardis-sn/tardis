8e0d69b99430c4386a3815d2889f292d77ab8954
Andrew Fullard <andrewgfullard@gmail.com>
2026-01-12 16:51:35 -0500

    Add tests for post-MC run values

--- tardis/workflows/tests/test_iip_workflow.py
+++ tardis/workflows/tests/test_iip_workflow.py
@@ -12,6 +12,11 @@ from tardis.io.configuration.config_reader import Configuration
 
 
 @pytest.fixture
+def iip_regression_path(tardis_regression_path):
+    return tardis_regression_path / "tardis" / "workflows" / "tests"
+
+
+@pytest.fixture
 def iip_atom_data(tardis_regression_path):
     # identical atomic data to that used by C Vogl
     atom_data = AtomData.from_hdf(
@@ -40,17 +45,16 @@ def ctardis_compare_config():
     config = Configuration.from_yaml(
         "tardis/workflows/tests/data/ctardis_compare.yml"
     )
+    config.plasma.nlte.species = [
+        (1, 0)
+    ]  # Hack to force config necessary for Christian's plasma
     return config
 
 
 @pytest.fixture
-def elemental_number_density(tardis_regression_path):
+def elemental_number_density(iip_regression_path):
     elemental_number_density = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_elemental_number_density.h5",
+        iip_regression_path / "ctardis_elemental_number_density.h5",
         key="data",
     )
     elemental_number_density.columns = elemental_number_density.columns.astype(
@@ -82,15 +86,12 @@ def iip_plasma(iip_atom_data, elemental_number_density, ctardis_compare_config):
     return plasma
 
 
-def test_iip_plasma_initialization(
-    iip_plasma, ctardis_compare_config, tardis_regression_path
+@pytest.fixture
+def iip_plasma_nlte_init(
+    iip_regression_path, iip_plasma, ctardis_compare_config
 ):
     j_blues_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_j_blues_ctardis_init_nlte.h5",
+        iip_regression_path / "ctardis_j_blues_ctardis_init_nlte.h5",
         key="data",
     )
 
@@ -133,51 +134,34 @@ def test_iip_plasma_initialization(
         n_e_convergence_threshold=0.05,
         **{},
     )
+    return iip_plasma
 
+
+def test_iip_plasma_initialization(iip_plasma_nlte_init, iip_regression_path):
     tau_sobolevs_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_tau_sobolevs_init_nlte.h5",
+        iip_regression_path / "ctardis_tau_sobolevs_init_nlte.h5",
         key="data",
     )
 
     beta_sobolevs_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_beta_sobolevs_init_nlte.h5",
+        iip_regression_path / "ctardis_beta_sobolevs_init_nlte.h5",
         key="data",
     )
     ion_number_density_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_ion_density_init_nlte.h5",
+        iip_regression_path / "ctardis_ion_density_init_nlte.h5",
         key="data",
     )
     level_number_density_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_level_number_density_init_nlte.h5",
+        iip_regression_path / "ctardis_level_number_density_init_nlte.h5",
         key="data",
     )
     transition_probabilities_ctardis = pd.read_hdf(
-        tardis_regression_path
-        / "tardis"
-        / "workflows"
-        / "tests"
-        / "ctardis_transition_probabilities_init_nlte.h5",
+        iip_regression_path / "ctardis_transition_probabilities_init_nlte.h5",
         key="data",
     )
 
     pd.testing.assert_frame_equal(
-        iip_plasma.transition_probabilities,
+        iip_plasma_nlte_init.transition_probabilities,
         transition_probabilities_ctardis,
         rtol=1e-7,
         atol=0,
@@ -185,7 +169,7 @@ def test_iip_plasma_initialization(
     )
 
     pd.testing.assert_frame_equal(
-        iip_plasma.ion_number_density,
+        iip_plasma_nlte_init.ion_number_density,
         ion_number_density_ctardis,
         rtol=1e-6,
         atol=0,
@@ -194,23 +178,228 @@ def test_iip_plasma_initialization(
 
     # Sobolev values are stored differently between codes, so comparing raw data instead
     np.testing.assert_allclose(
-        iip_plasma.tau_sobolevs.values,
+        iip_plasma_nlte_init.tau_sobolevs.values,
         tau_sobolevs_ctardis.values,
         rtol=1e-6,
         atol=0,
     )
 
     np.testing.assert_allclose(
-        iip_plasma.beta_sobolev.values,
+        iip_plasma_nlte_init.beta_sobolev.values,
         beta_sobolevs_ctardis.values,
         rtol=1e-6,
         atol=0,
     )
 
     pd.testing.assert_frame_equal(
-        iip_plasma.level_number_density,
+        iip_plasma_nlte_init.level_number_density,
         level_number_density_ctardis,
         rtol=1e-7,
+        atol=0,
+        check_dtype=False,
+    )
+
+
+def test_iip_plasma_after_mc(
+    iip_regression_path, iip_plasma_nlte_init, ctardis_compare_config
+):
+    j_blues_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_j_blues_ctardis_after_mc.h5",
+        key="data",
+    )
+
+    radiation_temp = np.array(
+        [
+            9992.27229695,
+            9992.59224105,
+            9983.78000964,
+            9980.58614386,
+            9979.83477025,
+            9968.05132981,
+            9957.88724805,
+            9949.36369847,
+            9946.8743961,
+            9937.71425418,
+            9934.85610192,
+            9928.23880919,
+            9926.40535242,
+            9916.93223133,
+            9912.22246589,
+            9911.051763,
+            9910.26097021,
+            9901.72775668,
+            9895.9432972,
+            9891.58754489,
+            9886.70685954,
+            9880.93185734,
+            9876.00858684,
+            9872.59842944,
+        ]
+    )
+
+    dilution_factor = np.array(
+        [
+            0.3571996,
+            0.31756545,
+            0.27019532,
+            0.36604569,
+            0.33787167,
+            0.31579601,
+            0.29590609,
+            0.27936991,
+            0.2634541,
+            0.24940025,
+            0.23579985,
+            0.22373621,
+            0.21241799,
+            0.20254584,
+            0.19309261,
+            0.18394483,
+            0.1755579,
+            0.16798016,
+            0.16076174,
+            0.15381029,
+            0.14730572,
+            0.14119434,
+            0.13532174,
+            0.13124624,
+        ]
+    )
+
+    photo_ion_estimator = pd.read_hdf(
+        iip_regression_path / "ctardis_photo_ion_estimator_after_mc.h5",
+        key="data",
+    )
+
+    stim_recomb_estimator = pd.read_hdf(
+        iip_regression_path / "ctardis_stim_recomb_estimator_after_mc.h5",
+        key="data",
+    )
+
+    bf_heating_estimator = pd.read_hdf(
+        iip_regression_path / "ctardis_bf_heating_estimator.h5",
+        key="data",
+    )
+
+    stim_recomb_cooling_estimator = pd.read_hdf(
+        iip_regression_path / "ctardis_stim_recomb_cooling_estimator.h5",
+        key="data",
+    )
+
+    coll_deexc_heating_estimator = pd.read_hdf(
+        iip_regression_path / "ctardis_coll_deexc_heating_estimator.h5",
+        key="data",
+    )
+
+    ff_heating_estimator = [
+        4.89135279e-24,
+        4.37696370e-24,
+        3.75869301e-24,
+        4.97847160e-24,
+        4.52158002e-24,
+        4.21024499e-24,
+        3.94991540e-24,
+        3.72915649e-24,
+        3.58902110e-24,
+        3.40170224e-24,
+        3.20848519e-24,
+        3.03540032e-24,
+        2.87314722e-24,
+        2.74328938e-24,
+        2.61063140e-24,
+        2.50640248e-24,
+        2.38164559e-24,
+        2.26967531e-24,
+        2.24509826e-24,
+        2.12378192e-24,
+        2.02063266e-24,
+        1.92509873e-24,
+        1.83070678e-24,
+        1.77346374e-24,
+    ]
+
+    continuum_estimators = {}
+
+    continuum_estimators["photo_ion_estimator"] = photo_ion_estimator
+    continuum_estimators["stim_recomb_estimator"] = stim_recomb_estimator
+    continuum_estimators["bf_heating_estimator"] = bf_heating_estimator
+    continuum_estimators["stim_recomb_cooling_estimator"] = (
+        stim_recomb_cooling_estimator
+    )
+    continuum_estimators["coll_deexc_heating_estimator"] = (
+        coll_deexc_heating_estimator
+    )
+    continuum_estimators["ff_heating_estimator"] = ff_heating_estimator
+
+    iip_plasma_nlte_init.update_radiationfield(
+        radiation_temp,
+        dilution_factor,
+        j_blues_ctardis,
+        ctardis_compare_config.plasma.nlte,
+        initialize_nlte=False,
+        n_e_convergence_threshold=0.05,
+        **continuum_estimators,
+    )
+
+    tau_sobolevs_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_tau_sobolevs_after_mc.h5",
+        key="data",
+    )
+
+    beta_sobolevs_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_beta_sobolevs_after_mc.h5",
+        key="data",
+    )
+    ion_number_density_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_ion_density_after_mc.h5",
+        key="data",
+    )
+    level_number_density_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_level_number_density_after_mc.h5",
+        key="data",
+    )
+    transition_probabilities_ctardis = pd.read_hdf(
+        iip_regression_path / "ctardis_transition_probabilities_after_mc.h5",
+        key="data",
+    )
+
+    # tolerances are much worse than after init
+
+    pd.testing.assert_frame_equal(
+        iip_plasma_nlte_init.transition_probabilities,
+        transition_probabilities_ctardis,
+        rtol=1e-5,
+        atol=0,
+        check_dtype=False,
+    )
+
+    pd.testing.assert_frame_equal(
+        iip_plasma_nlte_init.ion_number_density,
+        ion_number_density_ctardis,
+        rtol=1e-5,
+        atol=0,
+        check_dtype=False,
+    )
+
+    # Sobolev values are stored differently between codes, so comparing raw data instead
+    np.testing.assert_allclose(
+        iip_plasma_nlte_init.tau_sobolevs.values,
+        tau_sobolevs_ctardis.values,
+        rtol=1e-5,
+        atol=0,
+    )
+
+    np.testing.assert_allclose(
+        iip_plasma_nlte_init.beta_sobolev.values,
+        beta_sobolevs_ctardis.values,
+        rtol=1e-5,
+        atol=0,
+    )
+
+    pd.testing.assert_frame_equal(
+        iip_plasma_nlte_init.level_number_density,
+        level_number_density_ctardis,
+        rtol=1e-5,
         atol=0,
         check_dtype=False,
     )
