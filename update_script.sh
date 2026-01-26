24bf183bd0d72e711d1c102c0d897b9080c3ae94
BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>
2026-01-26 14:52:21 +0530

    Switch to conda-lock render --kind env
    Signed-off-by: BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>

--- update_script.sh
+++ update_script.sh
@@ -1,15 +1,10 @@
-# This script shows how to update env.yml from conda-lock.yml
+# Generate env.yml from conda-lock.yml for ASV benchmarks
 # Run this whenever conda-lock.yml is updated
 
 set -e
 
 echo "Generating env.yml from conda-lock.yml"
-conda-lock render --kind explicit
+conda-lock render --kind env --filename-template "env" -p linux-64
 
-mv conda-linux-64.lock env.yml
-
-echo " env.yml updated with full package URLs and hashes"
-
-rm -f conda-osx-64.lock conda-osx-arm64.lock conda-linux-64.lock.yml conda-osx-64.lock.yml conda-osx-arm64.lock.yml
-
+echo "env.yml generated successfully"
 wc -l env.yml
