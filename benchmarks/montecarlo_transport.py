ac4e12bf97f126517e45990ed598bc47738f12c4
Deshan <deshang74@gmail.com>
2026-03-07 11:05:38 +0545

    mailmap and orcid entries

--- benchmarks/montecarlo_transport.py
+++ benchmarks/montecarlo_transport.py
@@ -1,9 +1,6 @@
 """
 Basic TARDIS Benchmark.
 """
-
-import functools
-
 from asv_runner.benchmarks.mark import parameterize
 
 from benchmarks.benchmark_base import BenchmarkBase
@@ -18,21 +15,19 @@ from tardis.transport.montecarlo.packets.radiative_packet import RPacket
 @parameterize({"Interaction type": ["scatter", "macroatom"]})
 class BenchmarkMontecarloTransport(BenchmarkBase):
     """
-    Class to benchmark the Monte Carlo transport module.
+    Class to benchmark core Monte Carlo transport distance calculations.
     """
 
     repeat = 2
 
-    @functools.cache
     def setup(self, interaction_type):
-        self.sim = self.nb_simulation_verysimple
-        self.transport = self.sim.transport
-        self.geometry = self.transport.transport_state.geometry_state
-        self.opacity_state = self.sim.opacity_state
-        self.macro_atom_state = self.sim.macro_atom_state
-        self.time_explosion = self.transport.transport_state.time_explosion
+        sim = self.nb_simulation_verysimple
+        geometry = sim.transport.transport_state.geometry_state
+        self.r_inner = geometry.r_inner[0]
+        self.r_outer = geometry.r_outer[0]
+        self.time_explosion = sim.transport.transport_state.time_explosion
         self.r_packet = RPacket(
-            r=self.geometry.r_inner[0],
+            r=self.r_inner,
             mu=0.5,
             nu=1e15,
             energy=1.0,
@@ -40,23 +35,12 @@ class BenchmarkMontecarloTransport(BenchmarkBase):
             index=0,
         )
 
-    def time_initialize_transport_state(self, interaction_type):
-        self.transport.initialize_transport_state(
-            self.sim.simulation_state,
-            self.opacity_state,
-            self.macro_atom_state,
-            self.sim.plasma,
-            self.sim.no_of_packets,
-            self.sim.no_of_virtual_packets,
-            iteration=0,
-        )
-
     def time_calculate_distance_boundary(self, interaction_type):
         calculate_distance_boundary(
-            self.geometry.r_inner[0],
+            self.r_inner,
             0.5,
-            self.geometry.r_inner[0],
-            self.geometry.r_outer[0],
+            self.r_inner,
+            self.r_outer,
         )
 
     def time_calculate_distance_line(self, interaction_type):
