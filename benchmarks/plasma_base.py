f05968eebddabed6013b6163a8fc5b8022256dd9
Deshan <deshang74@gmail.com>
2026-03-17 20:55:44 +0545

    Add docstrings to benchmark methods

--- benchmarks/plasma_base.py
+++ benchmarks/plasma_base.py
@@ -12,18 +12,23 @@ class BenchmarkBasePlasma(BenchmarkBase):
     repeat = 2
 
     def setup(self):
+        """Set up the simulation and plasma for benchmarking."""
         self.sim = self.nb_simulation_verysimple
         self.plasma = self.sim.plasma
         self.j_blues = self.plasma.j_blues
 
     def time_build_graph(self):
+        """Benchmark the time to build the plasma graph."""
         self.plasma._build_graph()
 
     def time_update_plasma(self):
+        """Benchmark the time to update the plasma state via j_blues propagation."""
         self.plasma.update(j_blues=self.j_blues)
 
     def time_resolve_update_list(self):
+        """Benchmark the time to resolve the update list for single variable."""
         self.plasma._resolve_update_list(["j_blues"])
 
     def time_get_value(self):
+        """Benchmark the time to get the value of a single plasma property value."""
         self.plasma.get_value("t_rad")
