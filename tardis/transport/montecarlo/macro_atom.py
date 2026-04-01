38523658b86ffc4d44b8152c8c3e95ceaea6c812
Josh Shields <shields.joshua.v@gmail.com>
2026-04-01 12:06:25 -0400

    clarify transitions enum

--- tardis/transport/montecarlo/macro_atom.py
+++ tardis/transport/montecarlo/macro_atom.py
@@ -17,28 +17,31 @@ class MacroAtomError(ValueError):
 
 
 class MacroAtomTransitionType(IntEnum):
-    PHOTOIONIZATION_INTERNAL = 3
-    PHOTOIONIZATION_TO_K_PACKET = 16
-    PHOTO_RECOMB_INTERNAL = 2
-    PHOTO_RECOMB_EMISSION = -9
+    # Bound bound transition block
     INTERNAL_UP = 1
     INTERNAL_DOWN = 0
     BB_EMISSION = -1
+
     BF_EMISSION = -2  # This is recombination emission, aka k-packet to r-packet
     FF_EMISSION = -3
     ADIABATIC_COOLING = -4
     BF_COOLING = -5  # TODO: Maybe merge this with BF_EMISSION - Yes this is taken care of by BF_EMISSION
     TWO_PHOTON = -6
+    # Photoionization block
+    PHOTOIONIZATION_INTERNAL = 2
+    PHOTOIONIZATION_TO_K_PACKET = 10  # Double check that this it to K
+    PHOTO_RECOMB_INTERNAL = 3
+    PHOTO_RECOMB_EMISSION = -9  # BF emission?
     # Collisions block
-    COLL_DOWN_TO_K_PACKET = 9
-    COLL_UP_INTERNAL = 10
-    COLL_DOWN_INTERNAL = 11
-    COLL_EXC_COOL_TO_MACRO = 12
-    COLL_ION_COOL_TO_MACRO = 13
-    COLL_ION_INTERNAL = 14
-    COLL_ION_EMISSION = -7
-    COLL_RECOMB_INTERNAL = 15
-    COLL_RECOMB_EMISSION = -8
+    COLL_DOWN_TO_K_PACKET = 11
+    COLL_UP_INTERNAL = 4
+    COLL_DOWN_INTERNAL = 5
+    COLL_EXC_COOL_TO_MACRO = 20  # From K
+    COLL_ION_COOL_TO_MACRO = 21  # From K
+    COLL_ION_INTERNAL = 6
+    COLL_ION_EMISSION = 12  # K packet creation?
+    COLL_RECOMB_INTERNAL = 7
+    COLL_RECOMB_EMISSION = -8  # BF emission?
 
 
 @njit(**njit_dict_no_parallel)
