d4dfc297522e6ee30b56259652542d90670dd8ec
connor-mcclellan <b.connor.mcc@gmail.com>
2026-04-27 16:39:08 -0400

    fix test for non homologous grid quartic root solver

--- tardis/transport/montecarlo/tests/test_nonhomologous.py
+++ tardis/transport/montecarlo/tests/test_nonhomologous.py
@@ -1,35 +1,23 @@
 import pytest
 from numpy.testing import assert_almost_equal
 
-import tardis.transport.montecarlo.nonhomologous_grid as nonhomologous_grid
+from tardis.transport.montecarlo.nonhomologous_grid import depressed_quartic
 
 
 @pytest.mark.parametrize(
-    ["a", "b", "c", "d", "e", "threshold", "expected_roots"],
+    ["A", "B", "C", "D", "E", "expected_roots"],
     [
-        (
-            0.0,
-            0.0,
-            0.0,
-            2.0,
-            -1.0,
-            0.0,
-            {"result": [0.5]},
-        ),
-        (
-            1.0,
-            2.0,
-            0.0,
-            2.0,
-            0.0,
-            0.0,
-            {"result": []},
-        ),
-        (1.0, -14.0, 71.0, -154.0, 120.0, 2.5, {"result": [3, 4, 5]}),
+        # x^4 - 14x^3 + 71x^2 - 154x + 120 = 0
+        # roots 2, 3, 4, 5
+        (1.0, -14.0, 71.0, -154.0, 120.0, [5.0, 4.0, 3.0, 2.0]),
+        # x^4 - x^3 = 0
+        # x^3(x + 1) = 0; only real root other than 0 is 1.0
+        (1.0, -1.0, 0.0, 0.0, 0.0, [1.0, 0.0, 0.0, 0.0]),
+        # x^4 - 10x^3 + 35x^2 - 50x + 24 = 0
+        # roots 1, 2, 3, 4
+        (1.0, -10.0, 35.0, -50.0, 24.0, [4.0, 3.0, 2.0, 1.0]),
     ],
 )
-def test_quartic_roots(a, b, c, d, e, threshold, expected_roots):
-    obtained_roots = nonhomologous_grid.quartic_roots(a, b, c, d, e, threshold)
-    obtained_roots.sort()
-
-    assert_almost_equal(obtained_roots, expected_roots["result"])
+def test_depressed_quartic(A, B, C, D, E, expected_roots):
+    roots = depressed_quartic(A, B, C, D, E)
+    assert_almost_equal(roots, expected_roots)
