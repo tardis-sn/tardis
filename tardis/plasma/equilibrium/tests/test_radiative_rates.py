33a4297e1aaf177592ad8ba5153febe0debbf142
rishmitar <rishmita.rao12@gmail.com>
2026-06-02 14:30:52 -0500

    delete unused fixture for equilibrium tests and change slicing of df to be easier to read.

--- tardis/plasma/equilibrium/tests/test_radiative_rates.py
+++ tardis/plasma/equilibrium/tests/test_radiative_rates.py
@@ -10,13 +10,6 @@ from tardis.plasma.radiation_field.planck_rad_field import (
 )
 
 
-@pytest.fixture(scope="class")
-def einstein_coefficients_df():
-    atom_data = AtomData.from_hdf('kurucz_cd23_chianti_H_He_latest.h5')
-    lines_df = atom_data.lines
-    radiative_transitions = lines_df.loc[(1,0, slice(None), slice(None)), :]
-    return radiative_transitions
-
 @pytest.fixture(
     scope="function",
     params=["invalid_index", "invalid_column", "invalid_lower_higher"]
@@ -38,14 +31,16 @@ def mock_radiation_field():
     temperature = [10000, 20000]* u.K
     return PlanckianRadiationField(temperature=temperature)
 
-def test_radiative_rate_solver_init(einstein_coefficients_df,regression_data):
+def test_radiative_rate_solver_init(new_chianti_atomic_dataset_si,regression_data):
+    einstein_coefficients_df = new_chianti_atomic_dataset_si.xs((1,0),drop_level=False)
     solver = RadiativeRatesSolver(einstein_coefficients_df)
     actual_einstein_coeffs = solver.einstein_coefficients
     expected_einstein_coeffs = regression_data.sync_dataframe(
         actual_einstein_coeffs, key="einstein_coeffs")
     pdt.assert_frame_equal(actual_einstein_coeffs,expected_einstein_coeffs)
 
-def test_radiative_rate_solver_solve(einstein_coefficients_df, mock_radiation_field, regression_data):
+def test_radiative_rate_solver_solve(new_chianti_atomic_dataset_si, mock_radiation_field, regression_data):
+    einstein_coefficients_df = new_chianti_atomic_dataset_si.xs((1,0),drop_level=False)
     solver = RadiativeRatesSolver(einstein_coefficients_df)
     actual_radiative_rates = solver.solve(mock_radiation_field)
     expected_radiative_rates = regression_data.sync_dataframe(
