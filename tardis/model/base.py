5bde59e98417a6150e88aa0e3c350ffe654fb033
connor-mcclellan <b.connor.mcc@gmail.com>
2026-08-12 13:28:07 -0400

    Add radius-plus-velocity CSVY geometry support

--- tardis/model/base.py
+++ tardis/model/base.py
@@ -344,6 +344,7 @@ class SimulationState(HDFWriterMixin):
             "density",
             "t_rad",
             "dilution_factor",
+            "radius",
         }
 
         if Path(config.csvy_model).is_absolute():
